69734d2e9c22d923860e3c0348da25440fd66e72
José Simões <jose.simoes@eclo.solutions>
2017-01-31 14:25:32 +0000

    Add Monitor_Reboot command
    - this is for ChibiOS reference implementation (nanoBooter only)
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/CLR/WireProtocol/WireProtocol_MonitorCommands.h
+++ src/CLR/WireProtocol/WireProtocol_MonitorCommands.h
@@ -11,6 +11,19 @@
 #include "WireProtocol_Message.h"
 
 //////////////////////////////////////////
+// enums
+
+// structure for Monitor Reboot options
+// backwards compatible with .NETMF
+typedef enum Monitor_Reboot_Options
+{
+    Monitor_Reboot_c_NormalReboot    = 0,
+    Monitor_Reboot_c_EnterBootloader = 1,
+    Monitor_Reboot_c_ClrRebootOnly   = 2,
+    Monitor_Reboot_c_ClrStopDebugger = 4
+}Monitor_Reboot_Options;
+
+//////////////////////////////////////////
 // typedefs
 
 // structure for Monitor Ping Reply
@@ -37,6 +50,11 @@ typedef struct CLR_DBG_Commands_Monitor_WriteMemory
 
 }CLR_DBG_Commands_Monitor_WriteMemory;
 
+typedef struct Monitor_Reboot_Command
+{
+    uint32_t m_flags;
+
+}Monitor_Reboot_Command;
 
 //////////////////////////////////////////
 // function declarations (commands)
@@ -44,5 +62,6 @@ typedef struct CLR_DBG_Commands_Monitor_WriteMemory
 bool Monitor_Ping(WP_Message* message);
 bool Monitor_OemInfo(WP_Message* message);
 bool Monitor_WriteMemory(WP_Message* message);
+bool Monitor_Reboot(WP_Message* message);
 
 #endif //_WIREPROTOCOL_COMMANDS_H_
