8206429321c2b8895158be23ff3b02d2d407e9fa
Peter Wessel <piwi1263@gmail.com>
2017-02-16 02:39:27 +0100

    New target board NUCLEO144-F746ZG for ChibiOS (V2)
    Support for the F7 board in the overlay concept was added, here are the
    additional (target) files for the above mentioned board. This is initial
    work so there will be corrections to make.
    
    - initiated a new target board and added files accordingly
    - additional files made to make use of the overlay concept
    - changed cmake files to match board specifics
    - added all copyright headers
    - added empty end lines to make Git diff happy
    
    Target has been build, compiled and deployed successfully to the board.
    A larson scanner should be shown once deployed.
    
    Signed-off-by: Peter Wessel piwi1263@gmail.com

---
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoBooter/main.c
@@ -0,0 +1,103 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <ch.h>
+#include <hal.h>
+#include <cmsis_os.h>
+
+#include <usbcfg.h>
+#include <WireProtocol_ReceiverThread.h>
+#include <LaunchCLR.h>
+
+void BlinkerThread(void const * argument)
+{
+  (void)argument;
+
+  // loop until thread receives a request to terminate
+  while (!chThdShouldTerminateX()) {
+
+      palSetPad(GPIOB, GPIOB_LED1);
+      osDelay(250);
+
+      palClearPad(GPIOB, GPIOB_LED1);
+      palSetPad(GPIOB, GPIOB_LED2);
+      osDelay(250);
+
+      palClearPad(GPIOB, GPIOB_LED2);
+      palSetPad(GPIOB, GPIOB_LED3);
+      osDelay(250);
+
+      palClearPad(GPIOB, GPIOB_LED3);
+      palSetPad(GPIOB, GPIOB_LED2);
+      osDelay(250);
+
+      palClearPad(GPIOB, GPIOB_LED2);
+
+  }
+  
+  // nothing to deinitialize or cleanup, so it's safe to return
+  return 0;
+}
+osThreadDef(BlinkerThread, osPriorityNormal, 128);
+
+// need to declare the Receiver thread here
+osThreadDef(ReceiverThread, osPriorityNormal, 1024);
+
+//  Application entry point.
+int main(void) {
+
+  osThreadId blinkerThreadId;
+  osThreadId receiverThreadId;
+
+  // HAL initialization, this also initializes the configured device drivers
+  // and performs the board-specific initializations.
+  halInit();
+
+  // The kernel is initialized but not started yet, this means that
+  // main() is executing with absolute priority but interrupts are already enabled.
+  osKernelInitialize();
+
+  //  Initializes a serial-over-USB CDC driver.
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg);
+
+  // Activates the USB driver and then the USB bus pull-up on D+.
+  // Note, a delay is inserted in order to not have to disconnect the cable after a reset.
+  usbDisconnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg.usbp, &usbcfg);
+  usbConnectBus(serusbcfg.usbp);
+
+  // Creates the blinker thread, it does not start immediately.
+  blinkerThreadId = osThreadCreate(osThread(BlinkerThread), NULL);
+
+  // create the receiver thread
+  receiverThreadId = osThreadCreate(osThread(ReceiverThread), NULL);
+
+  // start kernel, after this the main() thread has priority osPriorityNormal by default
+  osKernelStart();
+
+  //  Normal main() thread
+  while (true) {
+
+    // check for button pressed
+    if (palReadPad(GPIOC, GPIOC_BUTTON))
+    {
+      // Start the shutdown sequence
+
+      // terminate threads
+      osThreadTerminate(receiverThreadId);
+      osThreadTerminate(blinkerThreadId);
+      
+      // stop the serial-over-USB CDC driver
+      sduStop(&SDU1);
+      
+      // launch nanoCLR
+      LaunchCLR(0x08008000);
+    }
+    
+    osDelay(500);
+  }
+}
