fe07b084b2c903532485c17c05df27b6932bb046
José Simões <josesimoes@users.noreply.github.com>
2017-03-09 10:54:03 +0000

    Add code to check for valid CLR image and launch it (#155)
    - add simple and expedited check if a CLR image is flashed
    - if that check passes nanoBooter launches the CLR
    
    Fixes #116
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/common/LaunchCLR.c
+++ targets/CMSIS-OS/ChibiOS/common/LaunchCLR.c
@@ -33,3 +33,41 @@ void LaunchCLR(uint32_t address)
     JumpToNanoCLR();
 }
 
+bool CheckValidCLRImage(uint32_t address)
+{
+    // load nanoCLR vector table
+    const vectors_t* nanoCLRVectorTable = (vectors_t*) address;
+
+    // 1st check: the flash content pointed by the address can't be all 0's neither all F's
+    // meaning that the Flash is neither 'all burnt' or erased
+    
+    // the stack pointer is at the 1st position of vectors_t
+    if(nanoCLRVectorTable->init_stack == 0xFFFFFFFF ||
+       nanoCLRVectorTable->init_stack == 0x00000000)
+    {
+        // check failed, there is no valid CLR image
+        //return false;
+    }
+
+    // volatile uint16_t* temp1 = (uint16_t*)nanoCLRVectorTable->reset_handler;
+    // temp1 = ((uint8_t*)temp1) - 0x1;
+    // volatile uint16_t temp = (uint16_t)(*temp1);
+
+    
+    // 2nd check: the content pointed by the reset vector has to be 0xB672 
+    // this is the opcode for 'CPSID I' which is the very 1st assembly instruction of a ChibiOS nanoCLR image
+    // the casts bellow are there because the opcode is a 16 bit value and we need to subtract 1 from the reset vector address pointing to it
+    uint16_t* opCodeAddress = (uint16_t*)nanoCLRVectorTable->reset_handler;
+    opCodeAddress = (uint8_t*)opCodeAddress - 0x1;
+
+    if((uint16_t)*opCodeAddress == 0xB672)
+    {
+        // check, there seems to be a valid CLR image
+        return true;
+    }
+    else
+    {
+        // got here, assume that there isn't a valid CLR imaged flashed
+        return false;
+    }
+}
