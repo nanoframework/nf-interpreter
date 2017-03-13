82f29b1c7790f7d0d1b5f2ef353329fe2e6545e3
Christophe Gerbier <christophe@mikrobusnet.org>
2017-03-13 11:03:09 +0100

    Quail board support (#168)
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

---
+++ targets/CMSIS-OS/ChibiOS/MBN_QUAIL/common/usbcfg.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// Portions Copyright (c) 2006..2015 Giovanni Di Sirio.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _USBCFG_H_
+#define _USBCFG_H_
+
+extern const USBConfig usbcfg;
+extern SerialUSBConfig serusbcfg;
+extern SerialUSBDriver SDU1;
+
+#endif  /* _USBCFG_H_ */
+
+/** @} */
+
