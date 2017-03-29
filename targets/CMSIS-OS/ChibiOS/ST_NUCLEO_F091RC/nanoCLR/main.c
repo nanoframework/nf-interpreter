923d4409425d27d739b42e01e5475cfe553c8b39
José Simões <josesimoes@users.noreply.github.com>
2017-03-29 09:54:22 +0100

    Fix #259 (#261)
    - turns out that the code configuring and starting USB was commented out
    - add missing code to launch the CLRStartup
    - add missing comment in F091
    - clean-up code
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_NUCLEO_F091RC/nanoCLR/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO_F091RC/nanoCLR/main.c
@@ -45,6 +45,7 @@ osThreadDef(BlinkerThread, osPriorityNormal, 128);
 // need to declare the Receiver thread here
 osThreadDef(ReceiverThread, osPriorityNormal, 1024);
 
+// declare CLRStartup thread here
 osThreadDef(CLRStartupThread, osPriorityNormal, 1024);
 
 //  Application entry point.
