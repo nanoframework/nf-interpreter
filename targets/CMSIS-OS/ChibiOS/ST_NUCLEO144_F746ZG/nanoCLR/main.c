bf010c8885ac4e919494ab87e83f7f76e84be3a8
José Simões <josesimoes@users.noreply.github.com>
2017-03-29 10:18:12 +0100

    Add missing CLRStartup thread to reference boards (#262)
    * Add missing CLRStartup thread to reference boards
    
    - also enable USB config and start on some of them
    - code clean up
    - enable flash driver for ST_NUCLEO144_F746ZG
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/main.c
@@ -8,7 +8,7 @@
 #include <cmsis_os.h>
 
 #include "usbcfg.h"
-#include <nanoCLR_Application.h>
+#include <CLR_Startup_Thread.h>
 #include <WireProtocol_ReceiverThread.h>
 
 void BlinkerThread(void const * argument)
@@ -51,6 +51,9 @@ osThreadDef(BlinkerThread, osPriorityNormal, 128);
 // need to declare the Receiver thread here
 osThreadDef(ReceiverThread, osPriorityNormal, 1024);
 
+// declare CLRStartup thread here
+osThreadDef(CLRStartupThread, osPriorityNormal, 1024);
+
 //  Application entry point.
 int main(void) {
 
@@ -62,35 +65,28 @@ int main(void) {
   // main() is executing with absolute priority but interrupts are already enabled.
   osKernelInitialize();
 
-  // //  Initializes a serial-over-USB CDC driver.
-  // sduObjectInit(&SDU1);
-  // sduStart(&SDU1, &serusbcfg);
+  //  Initializes a serial-over-USB CDC driver.
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg);
 
-  // // Activates the USB driver and then the USB bus pull-up on D+.
-  // // Note, a delay is inserted in order to not have to disconnect the cable after a reset
-  // usbDisconnectBus(serusbcfg.usbp);
-  // chThdSleepMilliseconds(1500);
-  // usbStart(serusbcfg.usbp, &usbcfg);
-  // usbConnectBus(serusbcfg.usbp);
+  // Activates the USB driver and then the USB bus pull-up on D+.
+  // Note, a delay is inserted in order to not have to disconnect the cable after a reset
+  usbDisconnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg.usbp, &usbcfg);
+  usbConnectBus(serusbcfg.usbp);
 
   // Creates the blinker thread, it does not start immediately.
   osThreadCreate(osThread(BlinkerThread), NULL);
 
   // create the receiver thread
   osThreadCreate(osThread(ReceiverThread), NULL);
+  
+  // create the CLR Startup thread
+  osThreadCreate(osThread(CLRStartupThread), NULL);
 
   // start kernel, after this the main() thread has priority osPriorityNormal by default
   osKernelStart();
-
-  CLR_SETTINGS clrSettings;
-
-  memset(&clrSettings, 0, sizeof(CLR_SETTINGS));
-
-  clrSettings.MaxContextSwitches         = 50;
-  clrSettings.WaitForDebugger            = false;
-  clrSettings.EnterDebuggerLoopAfterExit = true;
-
-  ClrStartup(clrSettings);
 
   while (true) {
     osDelay(1000);
