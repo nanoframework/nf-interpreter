16f1be8ae5740c6b6332c658ac9b6174df164eb3
José Simões <josesimoes@users.noreply.github.com>
2017-03-29 09:28:13 +0100

    Correct button check (#258)
    - the user button in this board has a pull-up resistor
    - fix #257
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_STM32F4_DISCOVERY/nanoBooter/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F4_DISCOVERY/nanoBooter/main.c
@@ -57,7 +57,7 @@ int main(void) {
   // the board to remain in nanoBooter and not launching nanoCLR
 
   // if the USER button (blue one) is pressed, skip the check for a valid CLR image and remain in booter
-  if (palReadPad(GPIOA, GPIOA_BUTTON))
+  if (!palReadPad(GPIOA, GPIOA_BUTTON))
   {
     // check for valid CLR image 
     if(CheckValidCLRImage((uint32_t)&__nanoImage_end__))
