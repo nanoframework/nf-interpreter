6c57a6643666cf20cadcd883fbbff58c70fb3e3a
José Simões <jose.simoes@eclo.solutions>
2017-04-24 13:13:11 +0100

    Add stdafx.h include
    - required for file reuse in MetaDataProcessor VS solution
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/CLR/Core/StringTable.cpp
+++ src/CLR/Core/StringTable.cpp
@@ -3,6 +3,9 @@
 // Portions Copyright (c) Microsoft Corporation.  All rights reserved.
 // See LICENSE file in the project root for full license information.
 //
+
+#include "stdafx.h"
+
 #include "Core.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
--- src/CLR/Core/TypeSystem.cpp
+++ src/CLR/Core/TypeSystem.cpp
@@ -3,6 +3,8 @@
 // Portions Copyright (c) Microsoft Corporation.  All rights reserved.
 // See LICENSE file in the project root for full license information.
 //
+#include "stdafx.h"
+
 #include "Core.h"
 #include "corhdr_private.h"
 
