40c0fcef5e06e01ab8d74c3631a0d57f6e719692
Christophe Gerbier <christophe@mikrobusnet.org>
2017-05-13 22:44:22 +0200

    Windows.Devices.Gpio POC
    This is work in progress for evaluation only.
    Goal here is to check if we are going the right way of doing things regarding file locations, methods and so on. Code is really incomplete and many methods do not contain any safety checks.
    
    This PR also needs changes in nf-class-libraries projects.
    
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

---
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio.Provider/windows_devices_gpio_provider_native_Windows_Devices_Gpio_Provider_DefaultGpioControllerProvider.h
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+
+#ifndef _WINDOWS_DEVICES_GPIO_PROVIDER_NATIVE_WINDOWS_DEVICES_GPIO_PROVIDER_DEFAULTGPIOCONTROLLERPROVIDER_H_
+#define _WINDOWS_DEVICES_GPIO_PROVIDER_NATIVE_WINDOWS_DEVICES_GPIO_PROVIDER_DEFAULTGPIOCONTROLLERPROVIDER_H_
+
+namespace Windows
+{
+    namespace Devices
+    {
+        namespace Gpio
+        {
+            namespace Provider
+            {
+                struct DefaultGpioControllerProvider
+                {
+                    // Helper Functions to access fields of managed object
+                    // Declaration of stubs. These functions are implemented by Interop code developers
+                    static signed int get_PinCount( CLR_RT_HeapBlock* pMngObj, HRESULT &hr );
+                };
+            }
+        }
+    }
+}
+#endif  //_WINDOWS_DEVICES_GPIO_PROVIDER_NATIVE_WINDOWS_DEVICES_GPIO_PROVIDER_DEFAULTGPIOCONTROLLERPROVIDER_H_
