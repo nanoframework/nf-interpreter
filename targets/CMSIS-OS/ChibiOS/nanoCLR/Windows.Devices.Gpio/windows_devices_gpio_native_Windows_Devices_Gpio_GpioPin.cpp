0e523a649ea74070d5bb6f05945c78a7072137e7
Christophe Gerbier <christophe@mikrobusnet.org>
2017-05-28 20:55:03 +0200

    Fix issue #337 (#338)
    Reworked code for GPIO declaration.
    
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin.cpp
@@ -19,7 +19,35 @@
 
 using namespace Windows::Devices::Gpio;
 
-stm32_gpio_t* gpioPort[] = { GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, GPIOG, GPIOH, GPIOI, GPIOJ };
+stm32_gpio_t* gpioPort[] = { GPIOA, GPIOB
+#if STM32_HAS_GPIOC
+, GPIOC
+#endif
+#if STM32_HAS_GPIOD
+, GPIOD
+#endif
+#if STM32_HAS_GPIOE
+, GPIOE
+#endif
+#if STM32_HAS_GPIOF
+, GPIOF
+#endif
+#if STM32_HAS_GPIOG
+, GPIOG
+#endif
+#if STM32_HAS_GPIOH
+, GPIOH
+#endif
+#if STM32_HAS_GPIOI
+, GPIOI
+#endif
+#if STM32_HAS_GPIOJ
+, GPIOJ
+#endif
+#if STM32_HAS_GPIOK
+, GPIOK
+#endif
+ };
 
 #define GPIO_PORT(pin) (gpioPort[(pin/16) - 1])
 
