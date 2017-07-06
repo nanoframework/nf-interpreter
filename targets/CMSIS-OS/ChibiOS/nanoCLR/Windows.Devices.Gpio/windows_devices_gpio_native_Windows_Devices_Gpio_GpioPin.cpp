665a6202e4d09b46248b33ef59ae86039f8a70b3
Christophe Gerbier <christophe@mikrobusnet.org>
2017-07-06 21:17:54 +0200

    Devices.Gpio fixes
    Fix some bugs in the first implementation of Winwods.Devices.Gpio
    
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin.cpp
@@ -49,7 +49,7 @@ stm32_gpio_t* gpioPort[] = { GPIOA, GPIOB
 #endif
  };
 
-#define GPIO_PORT(pin) (gpioPort[(pin/16) - 1])
+#define GPIO_PORT(pin) (gpioPort[pin/16])
 
 bool GpioPin::NativeIsDriveModeSupported( CLR_RT_HeapBlock* pMngObj, unsigned char param0, HRESULT &hr )
 {
@@ -63,7 +63,7 @@ signed int GpioPin::NativeRead( CLR_RT_HeapBlock* pMngObj, signed int param0, HR
 
 void GpioPin::NativeWrite( CLR_RT_HeapBlock* pMngObj, signed int param0, unsigned char param1, HRESULT &hr )
 {
-	if (param1 == 0) palClearPad(GPIO_PORT(param0), param0 % 16);
+    if (param1 == 0) palClearPad(GPIO_PORT(param0), param0 % 16);
     else palSetPad(GPIO_PORT(param0), param0 % 16);
 }
 
@@ -71,17 +71,17 @@ void GpioPin::NativeSetDriveMode( CLR_RT_HeapBlock* pMngObj, signed int param0, 
 {
 	switch (param1)
     {
-        case 0 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_ALTERNATE(PAL_MODE_INPUT));
+        case 0 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_INPUT);
                     break;
-        case 1 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_ALTERNATE(PAL_MODE_INPUT_PULLDOWN));
+        case 1 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_INPUT_PULLDOWN);
                     break;
-        case 2 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_ALTERNATE(PAL_MODE_INPUT_PULLUP));
+        case 2 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_INPUT_PULLUP);
                     break;
-        case 3 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_ALTERNATE(PAL_MODE_OUTPUT_PUSHPULL));
+        case 3 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_OUTPUT_PUSHPULL);
                     break;
-        case 4 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_ALTERNATE(PAL_MODE_OUTPUT_OPENDRAIN ));
+        case 4 :    palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_OUTPUT_OPENDRAIN);
                     break;
-        default :   palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_ALTERNATE(PAL_MODE_INPUT));
+        default :   palSetPadMode(GPIO_PORT(param0), param0 % 16, PAL_MODE_INPUT_PULLDOWN);
                     break;
     }
 }
