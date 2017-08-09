c1e38905c8a91dce9b1fcebba298ff9a07b4a684
José Simões <josesimoes@users.noreply.github.com>
2017-08-09 19:30:58 +0100

    Work on Devices.Gpio (#418)
    - update declarations to match latest managed version
    - rework code to make use of internal fields
    - replace copyright notices
    - add support for events (WIP)
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin.h
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin.h
@@ -1,14 +1,8 @@
-//-----------------------------------------------------------------------------
 //
-//                   ** WARNING! ** 
-//    This file was generated automatically by a tool.
-//    Re-running the tool will overwrite this file.
-//    You should copy this file to a custom location
-//    before adding any customization in the copy to
-//    prevent loss of your changes when the tool is
-//    re-run.
+// Copyright (c) 2017 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
 //
-//-----------------------------------------------------------------------------
 
 
 #ifndef _WINDOWS_DEVICES_GPIO_NATIVE_WINDOWS_DEVICES_GPIO_GPIOPIN_H_
@@ -23,20 +17,25 @@ namespace Windows
             struct GpioPin
             {
                 // Helper Functions to access fields of managed object
+                static UNSUPPORTED_TYPE& Get__syncLock( CLR_RT_HeapBlock* pMngObj )    { return Interop_Marshal_GetField_UNSUPPORTED_TYPE( pMngObj, Library_windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___syncLock ); }
+
                 static signed int& Get__pinNumber( CLR_RT_HeapBlock* pMngObj )    { return Interop_Marshal_GetField_INT32( pMngObj, Library_windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___pinNumber ); }
 
                 static signed int& Get__driveMode( CLR_RT_HeapBlock* pMngObj )    { return Interop_Marshal_GetField_INT32( pMngObj, Library_windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___driveMode ); }
 
+                static UNSUPPORTED_TYPE& Get__callbacks( CLR_RT_HeapBlock* pMngObj )    { return Interop_Marshal_GetField_UNSUPPORTED_TYPE( pMngObj, Library_windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___callbacks ); }
+
                 static bool& Get__disposedValue( CLR_RT_HeapBlock* pMngObj )    { return Interop_Marshal_GetField_bool( pMngObj, Library_windows_devices_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___disposedValue ); }
 
                 // Declaration of stubs. These functions are implemented by Interop code developers
-                static bool NativeIsDriveModeSupported( CLR_RT_HeapBlock* pMngObj, unsigned char param0, HRESULT &hr );
-                static signed int NativeRead( CLR_RT_HeapBlock* pMngObj, signed int param0, HRESULT &hr );
-                static void NativeWrite( CLR_RT_HeapBlock* pMngObj, signed int param0, unsigned char param1, HRESULT &hr );
-                static void NativeSetDriveMode( CLR_RT_HeapBlock* pMngObj, signed int param0, unsigned char param1, HRESULT &hr );
                 static signed int get_DebounceTimeout( CLR_RT_HeapBlock* pMngObj, HRESULT &hr );
                 static void set_DebounceTimeout( CLR_RT_HeapBlock* pMngObj, signed int param0, HRESULT &hr );
+                static signed int Read( CLR_RT_HeapBlock* pMngObj, HRESULT &hr );
+                static void Write( CLR_RT_HeapBlock* pMngObj, signed int param0, HRESULT &hr );
                 static void DisposeNative( CLR_RT_HeapBlock* pMngObj, HRESULT &hr );
+                static bool NativeIsDriveModeSupported( CLR_RT_HeapBlock* pMngObj, signed int param0, HRESULT &hr );
+                static void NativeSetDriveMode( CLR_RT_HeapBlock* pMngObj, signed int param0, HRESULT &hr );
+                static bool NativeInit( CLR_RT_HeapBlock* pMngObj, signed int param0, HRESULT &hr );
             };
         }
     }
