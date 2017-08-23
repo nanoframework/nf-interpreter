cc37bb8f33773c23c01ad4451160d28969954d1d
Christophe Gerbier <christophe@mikrobusnet.org>
2017-08-23 15:21:04 +0200

    Various fixes
    Related to José's comment on the PR.
    
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

--- targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/halconf.h
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/halconf.h
@@ -66,9 +66,10 @@
 /**
  * @brief   Enables the I2C subsystem.
  */
-#if !defined(HAL_USE_I2C) || defined(__DOXYGEN__)
-#define HAL_USE_I2C                 FALSE
-#endif
+// this option is set at target_board.h (from config file)
+//#if !defined(HAL_USE_I2C) || defined(__DOXYGEN__)
+//#define HAL_USE_I2C                 TRUE
+//#endif
 
 /**
  * @brief   Enables the I2S subsystem.
