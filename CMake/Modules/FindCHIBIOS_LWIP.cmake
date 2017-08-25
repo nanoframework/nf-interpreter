f7b09d5d3ec76469054f0c0948130ce9c7a62d9b
José Simões <jose.simoes@eclo.solutions>
2017-08-25 10:05:10 +0100

    Tweaks related with ChibiOS LwIP
    - improve document formating, add missing empty line at end, add missing copyright header
    - add missing inclusion of include dirs for LwIP on targets CMake.txt
    - rearrange order of options in cmake.variants template
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- CMake/Modules/FindCHIBIOS_LWIP.cmake
+++ CMake/Modules/FindCHIBIOS_LWIP.cmake
@@ -1,8 +1,12 @@
+#
+# Copyright (c) 2017 The nanoFramework project contributors
+# See LICENSE file in the project root for full license information.
+#
 
 execute_process(
     COMMAND ${CMAKE_COMMAND} -E tar xvf ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip-1.4.1_patched.7z
     WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/
-    )
+)
 
 
 # List of the required lwIp include files.
@@ -13,68 +17,69 @@ list(APPEND CHIBIOS_LWIP_INCLUDE_DIRS ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/l
 
 
 set(LWIP_SRCS
-# bindings
-lwipthread.c
-sys_arch.c
+    # bindings
+    lwipthread.c
+    sys_arch.c
 
-# netif
-etharp.c
+    # netif
+    etharp.c
 
-# core
-dhcp.c
-dns.c
-init.c
-mem.c
-memp.c
-netif.c
-pbuf.c
-raw.c
-stats.c
-sys.c
-tcp.c
-tcp_in.c
-tcp_out.c
-udp.c
+    # core
+    dhcp.c
+    dns.c
+    init.c
+    mem.c
+    memp.c
+    netif.c
+    pbuf.c
+    raw.c
+    stats.c
+    sys.c
+    tcp.c
+    tcp_in.c
+    tcp_out.c
+    udp.c
 
-# ipv4
-autoip.c
-icmp.c
-igmp.c
-inet.c
-inet_chksum.c
-ip.c
-ip_addr.c
-ip_frag.c
-def.c
-timers.c
+    # ipv4
+    autoip.c
+    icmp.c
+    igmp.c
+    inet.c
+    inet_chksum.c
+    ip.c
+    ip_addr.c
+    ip_frag.c
+    def.c
+    timers.c
 
-# api
-api_lib.c
-api_msg.c
-err.c
-netbuf.c
-netdb.c
-netifapi.c
-sockets.c
-tcpip.c
+    # api
+    api_lib.c
+    api_msg.c
+    err.c
+    netbuf.c
+    netdb.c
+    netifapi.c
+    sockets.c
+    tcpip.c
 )
 
 
 foreach(SRC_FILE ${LWIP_SRCS})
-set(LWIP_SRC_FILE SRC_FILE -NOTFOUND)
-find_file(LWIP_SRC_FILE ${SRC_FILE}
-    PATHS 
-        ${PROJECT_BINARY_DIR}/ChibiOS_Source/os/various/lwip_bindings
-        ${PROJECT_BINARY_DIR}/ChibiOS_Source/os/various/lwip_bindings/arch
-        ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/netif
-        ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/core
-        ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/core/ipv4
-        ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/api
+    set(LWIP_SRC_FILE SRC_FILE -NOTFOUND)
+    find_file(LWIP_SRC_FILE ${SRC_FILE}
+        PATHS 
 
-    CMAKE_FIND_ROOT_PATH_BOTH
-)
-# message("${SRC_FILE} >> ${LWIP_SRC_FILE}") # debug helper
-list(APPEND CHIBIOS_LWIP_SOURCES ${LWIP_SRC_FILE})
+            ${PROJECT_BINARY_DIR}/ChibiOS_Source/os/various/lwip_bindings
+            ${PROJECT_BINARY_DIR}/ChibiOS_Source/os/various/lwip_bindings/arch
+            ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/netif
+            ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/core
+            ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/core/ipv4
+            ${PROJECT_BINARY_DIR}/ChibiOS_Source/ext/lwip/src/api
+
+        CMAKE_FIND_ROOT_PATH_BOTH
+    )
+    # message("${SRC_FILE} >> ${LWIP_SRC_FILE}") # debug helper
+    list(APPEND CHIBIOS_LWIP_SOURCES ${LWIP_SRC_FILE})
 endforeach()
 
 
