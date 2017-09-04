e21091ded2b91169739079f5be24c740936d567e
José Simões <josesimoes@users.noreply.github.com>
2017-09-04 10:26:29 +0100

    Remove setting of PB3 for SWO (#482)
    - not required anymore as this is now handled in SwoInit()
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F746ZG/nanoCLR/main.c
@@ -57,13 +57,7 @@ int main(void) {
 
   // init SWO as soon as possible to make it available to output ASAP
   #if (SWO_OUTPUT == TRUE)
-
-  // need to set SWO pin (PB3) to alternate mode (0 == the status after RESET) 
-  // because it's being to a different function in board config
-  palSetPadMode(GPIOB, 0x03, PAL_MODE_ALTERNATE(0) );
-
   SwoInit();
-  
   #endif
 
   // The kernel is initialized but not started yet, this means that
