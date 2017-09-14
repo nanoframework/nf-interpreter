20369be81726dff70548fe48c0ba706a303b302d
José Simões <josesimoes@users.noreply.github.com>
2017-09-14 15:04:34 +0100

    Update CHIBIOS_STM32F0xx_GCC_options.cmake
    - remove wrong duplicate specs for linker

--- CMake/Modules/CHIBIOS_STM32F0xx_GCC_options.cmake
+++ CMake/Modules/CHIBIOS_STM32F0xx_GCC_options.cmake
@@ -11,6 +11,6 @@ set(CMAKE_C_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m0  -Wall -std=c11 -w -ffun
 set(CMAKE_CXX_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m0 -Wall -std=c++11 -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -mabi=aapcs -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ftree-vectorize -fcheck-new -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -specs=nano.specs" CACHE INTERNAL "cxx compiler flags")
 set(CMAKE_ASM_FLAGS "-c -mthumb -mcpu=cortex-m0 -g -Wa,--no-warn -x assembler-with-cpp " CACHE INTERNAL "asm compiler flags")
 
-set(CMAKE_EXE_LINKER_FLAGS " -Wl,-gc-sections -Wl,--no-wchar-size-warning -mthumb -mcpu=cortex-m0 -mabi=aapcs" CACHE INTERNAL "executable linker flags")
-set(CMAKE_MODULE_LINKER_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs -specs=nano.specs " CACHE INTERNAL "module linker flags")
-set(CMAKE_SHARED_LINKER_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs -specs=nano.specs " CACHE INTERNAL "shared linker flags")
+set(CMAKE_EXE_LINKER_FLAGS " -Wl,-gc-sections -Wl,--no-wchar-size-warning -mthumb -mcpu=cortex-m0 -mabi=aapcs " CACHE INTERNAL "executable linker flags")
+set(CMAKE_MODULE_LINKER_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs " CACHE INTERNAL "module linker flags")
+set(CMAKE_SHARED_LINKER_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs " CACHE INTERNAL "shared linker flags")
