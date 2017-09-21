25df6376b32cca0c5224ab5d34f6f8b54de547cf
Peter Wessel <piwi1263@gmail.com>
2017-09-21 18:39:45 +0200

    Add NETDUINO3_WIFI as reference board (#514)

---
+++ targets/CMSIS-OS/ChibiOS/NETDUINO3_WIFI/nanoCLR/main.c
@@ -0,0 +1,72 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+// Using Devantech LCD03 display in I2C mode @ address 0xC8
+
+#include <ch.h>
+#include <hal.h>
+#include <cmsis_os.h>
+
+#include "usbcfg.h"
+#include <swo.h>
+#include <CLR_Startup_Thread.h>
+#include <WireProtocol_ReceiverThread.h>
+#include <nanoCLR_Application.h>
+#include <nanoPAL_BlockStorage.h>
+
+// need this definition here because it depends on the specifics of the target (how many INT lines exist on that series/device)
+#if (HAL_USE_EXT == TRUE)
+EXTConfig extInterruptsConfiguration = { .channels = { {EXT_CH_MODE_DISABLED, NULL} }};
+#endif
+
+// need to declare the Receiver thread here
+osThreadDef(ReceiverThread, osPriorityNormal, 2048, "ReceiverThread");
+// declare CLRStartup thread here 
+osThreadDef(CLRStartupThread, osPriorityNormal, 2048, "CLRStartupThread"); 
+
+//  Application entry point.
+int main(void) {
+
+  // HAL initialization, this also initializes the configured device drivers
+  // and performs the board-specific initializations.
+  halInit();
+  
+  // init SWO as soon as possible to make it available to output ASAP
+  #if (SWO_OUTPUT == TRUE)  
+  SwoInit();
+  #endif
+
+  // The kernel is initialized but not started yet, this means that
+  // main() is executing with absolute priority but interrupts are already enabled.
+  osKernelInitialize();
+
+  //  Initializes a serial-over-USB CDC driver.
+  sduObjectInit(&SDU1);
+  sduStart(&SDU1, &serusbcfg);
+
+  // Activates the USB driver and then the USB bus pull-up on D+.
+  // Note, a delay is inserted in order to not have to disconnect the cable after a reset
+  usbDisconnectBus(serusbcfg.usbp);
+  chThdSleepMilliseconds(1500);
+  usbStart(serusbcfg.usbp, &usbcfg);
+  usbConnectBus(serusbcfg.usbp);
+
+  // create the receiver thread
+  osThreadCreate(osThread(ReceiverThread), NULL);
+  // create the CLR Startup thread 
+  osThreadCreate(osThread(CLRStartupThread), NULL); 
+
+  // EXT driver needs to be started from main   
+  #if (HAL_USE_EXT == TRUE)
+  extStart(&EXTD1, &extInterruptsConfiguration);
+  #endif
+
+  // start kernel, after this main() will behave like a thread with priority osPriorityNormal
+  osKernelStart();
+  
+  while (true) { 
+    osDelay(100);
+  }
+}
