cef117215d5c5bb4053ddbeab86a054fb7580fc9
José Simões <jose.simoes@eclo.solutions>
2017-09-22 16:24:37 +0100

    Work on CMake scripts
    - replace deprecated compiler detection with updated method
    - update ChibiOS toolchain CMake accordingly
    - refactor several CMakes scripts to use funtions instead of repeated code
    - clean-up CMakes in result of that refactoring
    - optimize target CMakes with the above
    - improve compiler and linker flags to produce better and smaller images
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- CMake/Modules/CHIBIOS_STM32F0xx_GCC_options.cmake
+++ CMake/Modules/CHIBIOS_STM32F0xx_GCC_options.cmake
@@ -7,10 +7,42 @@
 # WHEN ADDING A NEW SERIES add the appropriate GCC options bellow
 #################################################################
 
-set(CMAKE_C_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m0 -Wall -std=c11 -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -mabi=aapcs -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ftree-vectorize -specs=nano.specs" CACHE INTERNAL "c compiler flags")
-set(CMAKE_CXX_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m0 -Wall -std=c++11 -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -mabi=aapcs -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ftree-vectorize -fcheck-new -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -specs=nano.specs" CACHE INTERNAL "cxx compiler flags")
-set(CMAKE_ASM_FLAGS "-c -mthumb -mcpu=cortex-m0 -g -Wa,--no-warn -x assembler-with-cpp " CACHE INTERNAL "asm compiler flags")
+# need to specify this for assembler
+set(CMAKE_ASM_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
 
-set(CMAKE_EXE_LINKER_FLAGS " -Wl,-gc-sections -Wl,--no-wchar-size-warning -mthumb -mcpu=cortex-m0 -mabi=aapcs " CACHE INTERNAL "executable linker flags")
-set(CMAKE_MODULE_LINKER_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs " CACHE INTERNAL "module linker flags")
-set(CMAKE_SHARED_LINKER_FLAGS " -mthumb -mcpu=cortex-m0 -mabi=aapcs " CACHE INTERNAL "shared linker flags")
+# need to specify linker flags here
+set(CMAKE_EXE_LINKER_FLAGS " -Wl,--gc-sections -Wl,--no-wchar-size-warning -mthumb -mcpu=cortex-m0 -mabi=aapcs -nostartfiles " CACHE INTERNAL "executable linker flags")
+
+
+function(NF_SET_COMPILER_OPTIONS TARGET)
+
+    # include any extra options comming from any extra args?
+    target_compile_options(${TARGET} PUBLIC  ${ARGN} -mthumb -mcpu=cortex-m0 -mabi=aapcs -nostdlib -Wall -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fno-builtin -fno-common -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ffast-math -ftree-vectorize -fcheck-new -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics)
+            
+endfunction()
+
+
+function(NF_SET_LINKER_OPTIONS TARGET PRINTF_FLOAT_SUPPORT)
+
+    # request to include support for float printf form newlib nano
+    if(PRINTF_FLOAT_SUPPORT)
+        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " --specs=nano.specs -u _printf_float")
+    else()
+        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " --specs=nano.specs ")
+    endif()
+
+    # set extra linker flags
+    set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " ${ARGN}")
+
+    # set optimization linker flags for RELEASE and MinSizeRel
+    if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " -flto -fno-strict-aliasing ")
+    endif()
+
+    # set optimization flags
+    nf_set_optimization_options(${TARGET})
+    
+    # set link map
+    nf_set_link_map(${TARGET})
+
+endfunction()
