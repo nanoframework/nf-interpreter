1fe0a4ee1d7f494bcaf5e797b5dcb2663b1c614f
José Simões <josesimoes@users.noreply.github.com>
2017-09-22 17:02:40 +0100

    Major rework on CMake scripts (#517)

--- CMake/Modules/CHIBIOS_STM32F7xx_GCC_options.cmake
+++ CMake/Modules/CHIBIOS_STM32F7xx_GCC_options.cmake
@@ -14,10 +14,42 @@ endif()
 # WHEN ADDING A NEW SERIES add the appropriate GCC options bellow
 #################################################################
 
-set(CMAKE_C_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -Wall -std=c11 -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -mabi=aapcs -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ffast-math -ftree-vectorize -specs=nano.specs" CACHE INTERNAL "c compiler flags")
-set(CMAKE_CXX_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -Wall -std=c++11 -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -mabi=aapcs -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ffast-math -ftree-vectorize -fcheck-new -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -specs=nano.specs" CACHE INTERNAL "cxx compiler flags")
-set(CMAKE_ASM_FLAGS "-mthumb -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
+# need to specify this for assembler
+set(CMAKE_ASM_FLAGS " -mthumb -mcpu=cortex-m7 -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
 
-set(CMAKE_EXE_LINKER_FLAGS " -Wl,--gc-sections -Wl,--no-wchar-size-warning -mthumb -Wl,--no-wchar-size-warning -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -mabi=aapcs" CACHE INTERNAL "executable linker flags")
-set(CMAKE_MODULE_LINKER_FLAGS " -mthumb -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -mabi=aapcs" CACHE INTERNAL "module linker flags")
-set(CMAKE_SHARED_LINKER_FLAGS " -mthumb -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -mabi=aapcs" CACHE INTERNAL "shared linker flags")
+# need to specify linker flags here
+set(CMAKE_EXE_LINKER_FLAGS " -Wl,--gc-sections -Wl,--no-wchar-size-warning -mthumb -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -mabi=aapcs -nostartfiles " CACHE INTERNAL "executable linker flags")
+
+
+function(NF_SET_COMPILER_OPTIONS TARGET)
+
+    # include any extra options comming from any extra args?
+    target_compile_options(${TARGET} PUBLIC  ${ARGN} -mthumb -mcpu=cortex-m7 -mfpu=fpv5-sp-d16 ${FLOAT_ABI_OPTION} -mabi=aapcs -nostdlib -Wall -w -ffunction-sections -fshort-wchar -falign-functions=16 -fdata-sections -fno-builtin -fno-common -fsingle-precision-constant -fomit-frame-pointer -mlong-calls -fdollars-in-identifiers -fno-exceptions -fno-unroll-loops -mstructure-size-boundary=8 -ffast-math -ftree-vectorize -fcheck-new -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics)
+    
+endfunction()
+
+
+function(NF_SET_LINKER_OPTIONS TARGET PRINTF_FLOAT_SUPPORT)
+
+    # request to include support for float printf form newlib nano
+    if(PRINTF_FLOAT_SUPPORT)
+        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " --specs=nano.specs -u _printf_float")
+    else()
+        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " --specs=nano.specs ")
+    endif()
+
+    # set extra linker flags
+    set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " ${ARGN}")
+
+    # set optimization linker flags for RELEASE and MinSizeRel
+    if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " -flto -fno-strict-aliasing ")
+    endif()
+       
+    # set optimization flags
+    nf_set_optimization_options(${TARGET})
+    
+    # set link map
+    nf_set_link_map(${TARGET})
+
+endfunction()
--- CMake/toolchain.ChibiOS.GCC.cmake
+++ CMake/toolchain.ChibiOS.GCC.cmake
@@ -1,16 +1,27 @@
+#
+# Copyright (c) 2017 The nanoFramework project contributors
+# See LICENSE file in the project root for full license information.
+#
+
 INCLUDE(CMakeForceCompiler)
 # if( DEFINED CMAKE_CROSSCOMPILING )
 #     # subsequent toolchain loading is not really needed
 #     return()
 # endif()
 
-# check for toolchain path
-if(NOT TOOLCHAIN_PREFIX)
-     message(STATUS "\n-- ########################################\nNo TOOLCHAIN_PREFIX specified, need one!\nCall CMake with -DTOOLCHAIN_PREFIX=\"<path_to_your_gcc_toolchain>\"\n specifing the path to your GCC toolchain (ex: E:/GNU_Tools_ARM_Embedded/5_4_2016q3)")
-     message(STATUS "\nNOTE: mind the forward slash in the path, without trailing slash.)")
-     message(STATUS "########################################\n\n")
-     message(FATAL_ERROR "No TOOLCHAIN_PREFIX specified")
-endif()
+
+###################################################
+# set C and C++ flags for compiler detection tests
+###################################################
+# IMPORTANT: these flags need to be removed after 
+# the compiler detection otherwise they'll get 
+# added to all compiler build calls and 
+# will colide with the use of nano.specs
+###################################################
+set(CMAKE_C_FLAGS_INIT "-specs=nosys.specs")
+set(CMAKE_CXX_FLAGS_INIT "-specs=nosys.specs")
+###################################################
+
 
 # set toolchain directories
 set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
@@ -31,46 +42,37 @@ endmacro()
 # setup C compiler
 if(NOT CMAKE_C_COMPILER)
     SET_COMPILER_VAR(C_COMPILER gcc)
-   if(CMAKE_C_COMPILER)
-      CMAKE_FORCE_C_COMPILER(${CMAKE_C_COMPILER} GNU)
-   endif()
 endif()
-SET_COMPILER_VAR(C_COMPILER gcc)
 
 # setup C++ compiler
 if(NOT CMAKE_CXX_COMPILER)
     SET_COMPILER_VAR(CXX_COMPILER g++)
-   if(CMAKE_CXX_COMPILER)
-      CMAKE_FORCE_CXX_COMPILER(${CMAKE_CXX_COMPILER} GNU)
-   endif()
 endif()
 
 # setup Assembler compiler
 SET_COMPILER_VAR(ASM-ATT_COMPILER as)
- 
-# other toolchain configurations  
+
+# other toolchain configurations
 set(CMAKE_OBJCOPY ${TOOLCHAIN_BIN_DIR}/arm-none-eabi-objcopy CACHE INTERNAL "objcopy tool")
 set(CMAKE_OBJDUMP ${TOOLCHAIN_BIN_DIR}/arm-none-eabi-objdump CACHE INTERNAL "objdump tool")
 set(CMAKE_SIZE ${TOOLCHAIN_BIN_DIR}/arm-none-eabi-size CACHE INTERNAL "size tool")
 set(CMAKE_DEBUGER ${TOOLCHAIN_BIN_DIR}/arm-none-eabi-gdb CACHE INTERNAL "debuger")
-
-# set debug flags
-set(CMAKE_C_FLAGS_DEBUG " -Og -femit-class-debug-always -g3 -ggdb" CACHE INTERNAL "c compiler flags debug")
-set(CMAKE_CXX_FLAGS_DEBUG " -Og -femit-class-debug-always -g3 -ggdb" CACHE INTERNAL "cxx compiler flags debug")
-set(CMAKE_ASM_FLAGS_DEBUG " -g3 -ggdb" CACHE INTERNAL "asm compiler flags debug")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "" CACHE INTERNAL "linker flags debug")
-
-# set release flags
-set(CMAKE_C_FLAGS_RELEASE " -Os -flto" CACHE INTERNAL "c compiler flags release")
-set(CMAKE_CXX_FLAGS_RELEASE " -Os -flto" CACHE INTERNAL "cxx compiler flags release")
-set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "asm compiler flags release")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-flto" CACHE INTERNAL "linker flags release")
 
 # root paths to search on the filesystem for cross-compiling
 set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PREFIX}/arm-none-eabi ${EXTRA_FIND_PATH})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# set required C and C++ standard for ALL targets
+set(CMAKE_C_STANDARD 11 CACHE INTERNAL "C standard for all targets")
+set(CMAKE_CXX_STANDARD 11 CACHE INTERNAL "C++ standard for all targets")
+
+# set all C and C++ extensions to be OFF on ALL targets
+# this forces the use of -std=c11 and -std=c++11 instead of -std=gnu11 and -std=gnu++11
+set(CMAKE_C_EXTENSIONS OFF CACHE INTERNAL "C compiler extensions OFF")
+set(CMAKE_CXX_EXTENSIONS OFF CACHE INTERNAL "C++ compiler extensions OFF")
+
 
 function(CHIBIOS_ADD_HEX_BIN_DUMP_TARGETS TARGET)
     if(EXECUTABLE_OUTPUT_PATH)
@@ -86,6 +88,7 @@ function(CHIBIOS_ADD_HEX_BIN_DUMP_TARGETS TARGET)
     add_custom_target(${TARGET}.dump DEPENDS ${TARGET} COMMAND ${CMAKE_OBJDUMP} -d -EL -S ${FILENAME} ${FILENAME}.dump)
 endfunction()
 
+
 function(CHIBIOS_PRINT_SIZE_OF_TARGETS TARGET)
     if(EXECUTABLE_OUTPUT_PATH)
       set(FILENAME "${EXECUTABLE_OUTPUT_PATH}/${TARGET}")
@@ -93,4 +96,29 @@ function(CHIBIOS_PRINT_SIZE_OF_TARGETS TARGET)
       set(FILENAME "${TARGET}")
     endif()
     add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_SIZE} ${FILENAME})
+endfunction()
+
+
+function(NF_SET_OPTIMIZATION_OPTIONS TARGET) 
+
+    target_compile_options(${TARGET} PRIVATE
+        $<$<CONFIG:Debug>:-Og -femit-class-debug-always -g3 -ggdb>
+        $<$<CONFIG:Release>:-Os -flto -fno-strict-aliasing>
+        $<$<CONFIG:MinSizeRel>:-Os -flto -fno-strict-aliasing>
+        $<$<CONFIG:RelWithDebInfo>:-Os -femit-class-debug-always -g3 -ggdb>
+    )
+
+endfunction()
+
+
+function(NF_SET_LINK_MAP TARGET) 
+
+
+    # need to remove the .elf suffix from target name
+    string(FIND ${TARGET} "." TARGET_EXTENSION_DOT_INDEX)
+    string(SUBSTRING ${TARGET} 0 ${TARGET_EXTENSION_DOT_INDEX} TARGET_SHORT)
+    
+    # add linker flags to generate map file
+    set_property(TARGET ${TARGET_SHORT}.elf APPEND_STRING PROPERTY LINK_FLAGS " -Wl,-Map=${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.map,--library-path=${PROJECT_SOURCE_DIR}/targets/CMSIS-OS/ChibiOS/common")
+
 endfunction()
