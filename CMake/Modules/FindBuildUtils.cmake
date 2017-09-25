45edbadb2a78243c969811738f931cebd3992dfc
José Simões <jose.simoes@eclo.solutions>
2017-09-25 11:41:52 +0100

    Improvements in CMake scripts
    - refactor several scripts making the targets simpler
    - moved repeated functions in series sources CMakes to the toolchain CMake
    - renamed several function to keep consistency with general naming
    - update reference targets  CMake's accordingly
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- CMake/Modules/FindBuildUtils.cmake
+++ CMake/Modules/FindBuildUtils.cmake
@@ -20,62 +20,67 @@ function(NF_GENERATE_DFU_PACKAGE FILE1 ADDRESS1 FILE2 ADDRESS2 OUTPUTFILENAME)
 
 endfunction()
 
-function(NF_GENERATE_BUILD_OUTPUT_FILES)
+function(NF_GENERATE_BUILD_OUTPUT_FILES TARGET)
 
-    if(CMAKE_BUILD_TYPE EQUAL "Release")
+    # need to remove the .elf suffix from target name
+    string(FIND ${TARGET} "." TARGET_EXTENSION_DOT_INDEX)
+    string(SUBSTRING ${TARGET} 0 ${TARGET_EXTENSION_DOT_INDEX} TARGET_SHORT)
 
-        add_custom_command(TARGET ${NANOBOOTER_PROJECT_NAME}.elf POST_BUILD
+    set(TARGET_HEX_FILE ${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.hex)
+    set(TARGET_S19_FILE ${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.s19)
+    set(TARGET_BIN_FILE ${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.bin)
+    set(TARGET_DUMP_FILE ${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.lst)
+
+    if(CMAKE_BUILD_TYPE EQUAL "Release" OR CMAKE_BUILD_TYPE EQUAL "MinSizeRel")
+
+        add_custom_command(TARGET ${TARGET_SHORT}.elf POST_BUILD
                 # copy target image to other formats
-                COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${NANOBOOTER_HEX_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Osrec $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${NANOBOOTER_S19_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${NANOBOOTER_BIN_FILE}
+                COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${TARGET_SHORT}.elf> ${TARGET_HEX_FILE}
+                COMMAND ${CMAKE_OBJCOPY} -Osrec $<TARGET_FILE:${TARGET_SHORT}.elf> ${TARGET_S19_FILE}
+                COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${TARGET_SHORT}.elf> ${TARGET_BIN_FILE}
 
                 # copy target file to build folder (this is only usefull for debugging in VS Code because of path in launch.json)
-                COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${PROJECT_SOURCE_DIR}/build/${NANOBOOTER_PROJECT_NAME.elf}
+                COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${TARGET_SHORT}.elf> ${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.elf
 
                 COMMENT "Generate nanoBooter HEX and BIN files for deployment")
 
-        add_custom_command(TARGET ${NANOCLR_PROJECT_NAME}.elf POST_BUILD
-                # copy target image to other formats
-                COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${NANOCLR_HEX_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Osrec $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${NANOCLR_S19_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${NANOCLR_BIN_FILE}
-
-                # copy target file to build folder (this is only usefull for debugging in VS Code because of path in launch.json)
-                COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${PROJECT_SOURCE_DIR}/build/${NANOCLR_PROJECT_NAME}.elf
-
-                COMMENT "Generate nanoCLR HEX and BIN files for deployment")
-
     else()
 
-        add_custom_command(TARGET ${NANOBOOTER_PROJECT_NAME}.elf POST_BUILD
+        add_custom_command(TARGET ${TARGET_SHORT}.elf POST_BUILD
                 # copy target image to other formats
-                COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${NANOBOOTER_HEX_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Osrec $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${NANOBOOTER_S19_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${NANOBOOTER_BIN_FILE}
+                COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${TARGET_SHORT}.elf> ${TARGET_HEX_FILE}
+                COMMAND ${CMAKE_OBJCOPY} -Osrec $<TARGET_FILE:${TARGET_SHORT}.elf> ${TARGET_S19_FILE}
+                COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${TARGET_SHORT}.elf> ${TARGET_BIN_FILE}
 
                 # copy target file to build folder (this is only usefull for debugging in VS Code because of path in launch.json)
-                COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> ${PROJECT_SOURCE_DIR}/build/${NANOBOOTER_PROJECT_NAME}.elf
+                COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${TARGET_SHORT}.elf> ${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.elf
 
-                # dump target image as source code listing
-                COMMAND ${CMAKE_OBJDUMP} -d -EL -S $<TARGET_FILE:${NANOBOOTER_PROJECT_NAME}.elf> > ${NANOBOOTER_DUMP_FILE}
+                # dump target image as source code listing 
+                # ONLY when DEBUG info is available, this is on 'Debug' and 'RelWithDebInfo'
+                COMMAND ${CMAKE_OBJDUMP} -d -EL -S $<TARGET_FILE:${TARGET_SHORT}.elf> > ${TARGET_DUMP_FILE}
 
                 COMMENT "Generate nanoBooter HEX and BIN files for deployment, LST file for debug")
 
-        add_custom_command(TARGET ${NANOCLR_PROJECT_NAME}.elf POST_BUILD
-                # copy target image to other formats
-                COMMAND ${CMAKE_OBJCOPY} -Oihex $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${NANOCLR_HEX_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Osrec $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${NANOCLR_S19_FILE}
-                COMMAND ${CMAKE_OBJCOPY} -Obinary $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${NANOCLR_BIN_FILE}
-
-                # copy target file to build folder (this is only usefull for debugging in VS Code because of path in launch.json)
-                COMMAND ${CMAKE_OBJCOPY} $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> ${PROJECT_SOURCE_DIR}/build/${NANOCLR_PROJECT_NAME}.elf
-
-                # dump target image as source code listing
-                COMMAND ${CMAKE_OBJDUMP} -d -EL -S $<TARGET_FILE:${NANOCLR_PROJECT_NAME}.elf> > ${NANOCLR_DUMP_FILE}
-            
-                COMMENT "Generate nanoCLR HEX and BIN files for deployment, LST file for debug")
-
     endif()
+
+    nf_add_hex_bin_dump_targets(${TARGET})
+        
+    # add this to print the size of the output targets
+    nf_print_size_of_targets(${TARGET})
+
+endfunction()
+
+
+#######################################################################################################################################
+# this function sets the linker options AND a specific linker file (full path and name, including extension)
+function(NF_SET_LINKER_OPTIONS_AND_FILE TARGET LINKER_FILE_NAME)
+
+    get_target_property(TARGET_LD_FLAGS ${TARGET} LINK_FLAGS)
+    if(TARGET_LD_FLAGS)
+        set(TARGET_LD_FLAGS "-T${LINKER_FILE_NAME} ${TARGET_LD_FLAGS}")
+    else()
+        set(TARGET_LD_FLAGS "-T${LINKER_FILE_NAME}")
+    endif()
+    set_target_properties(${TARGET} PROPERTIES LINK_FLAGS ${TARGET_LD_FLAGS})
 
 endfunction()
