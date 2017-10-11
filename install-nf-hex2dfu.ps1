2c4c3e89dbf715ff0610411a483a8b1c7a023801
José Simões <jose.simoes@eclo.solutions>
2017-10-11 18:05:22 +0100

    Improvements in AppVeyor
    - on install of ARM GNU GCC the documentation and samples folder are deleted to save cache
    - remove unused cache entry
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- install-nf-hex2dfu.ps1
+++ install-nf-hex2dfu.ps1
@@ -5,10 +5,14 @@ $hex2dfuPathExists = Test-Path $env:HEX2DFU_PATH -ErrorAction SilentlyContinue
 
 If($hex2dfuPathExists -eq $False)
 {
+    md $($env:HEX2DFU_PATH) 
+
     Write-Host "Downloading hex2dfu..."
 
     $url = "https://github.com/nanoframework/nf-tools/releases/download/v1.0.0/hex2dfu.exe"
 
+    $output = "$env:HEX2DFU_PATH\hex2dfu.exe"
+
     # download executable with utility
-    (New-Object Net.WebClient).DownloadFile($url, $env:HEX2DFU_PATH)
+    (New-Object Net.WebClient).DownloadFile($url, $output)
 }
