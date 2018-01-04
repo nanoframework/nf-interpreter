b19728625e56caa96175b62992151776b20d09ee
José Simões <jose.simoes@eclo.solutions>
2018-01-04 13:15:02 +0000

    Add native support for Runtime.Native.RTC
    - Add code files for stubs and implementation for ChibiOS
    - Update CMakes accordingly
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/CLR/Runtime.Native/nf_rt_native.cpp
+++ src/CLR/Runtime.Native/nf_rt_native.cpp
@@ -22,12 +22,15 @@ static const CLR_RT_MethodHandler method_lookup[] =
     NULL,
     NULL,
     Library_nf_rt_native_nanoFramework_Runtime_Native_ExecutionConstraint::Install___STATIC__VOID__I4__I4,
+    NULL,
+    NULL,
+    Library_nf_rt_native_nanoFramework_Runtime_Native_RTC::Native_RTC_SetSystemTime___STATIC__BOOLEAN__I4__U1__U1__U1__U1__U1__U1,
+    Library_nf_rt_native_nanoFramework_Runtime_Native_RTC::Native_RTC_SetAlarm___STATIC__BOOLEAN__I4__U1__U1__U1__U1__U1__U1,
 };
 
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_nanoFramework_Runtime_Native =
 {
     "nanoFramework.Runtime.Native", 
-    0x10DD04A1,
+    0xABCB605C,
     method_lookup
 };
-
