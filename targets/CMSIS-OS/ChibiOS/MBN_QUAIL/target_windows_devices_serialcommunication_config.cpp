46cd0839ba8ed1f4f43ab7b0a9a5f66dd3a41e23
Christophe Gerbier <christophe@mikrobusnet.org>
2018-01-13 13:36:40 +0100

    Update target_windows_devices_serialcommunication_config.cpp
    Add UART configuration for the four sockets.

--- targets/CMSIS-OS/ChibiOS/MBN_QUAIL/target_windows_devices_serialcommunication_config.cpp
+++ targets/CMSIS-OS/ChibiOS/MBN_QUAIL/target_windows_devices_serialcommunication_config.cpp
@@ -5,6 +5,39 @@
 
 #include "win_dev_serial_native.h"
 
+///////////
+// UART1 //
+///////////
+
+// pin configuration for UART1
+// port: GPIOC
+// TX pin: is GPIOC_6
+// RX pin: is GPIOC_7
+// GPIO alternate pin function is 8 (see "Table 12. STM32F427xx and STM32F429xx alternate function mapping" in STM32F427xx and STM32F429xx datasheet)
+UART_CONFIG_PINS(1, GPIOA, 9, 10, 7)
+
+///////////
+// UART2 //
+///////////
+
+// pin configuration for UART1
+// port: GPIOC
+// TX pin: is GPIOC_6
+// RX pin: is GPIOC_7
+// GPIO alternate pin function is 8 (see "Table 12. STM32F427xx and STM32F429xx alternate function mapping" in STM32F427xx and STM32F429xx datasheet)
+UART_CONFIG_PINS(2, GPIOD, 5, 6, 7)
+
+///////////
+// UART3 //
+///////////
+
+// pin configuration for UART3
+// port: GPIOC
+// TX pin: is GPIOC_6
+// RX pin: is GPIOC_7
+// GPIO alternate pin function is 8 (see "Table 12. STM32F427xx and STM32F429xx alternate function mapping" in STM32F427xx and STM32F429xx datasheet)
+UART_CONFIG_PINS(3, GPIOD, 8, 9, 7)
+
 
 ///////////
 // UART6 //
@@ -19,8 +52,14 @@ UART_CONFIG_PINS(6, GPIOC, 6, 7, 8)
 
 // buffers size
 // tx buffer size: 256 bytes
+#define UART1_TX_SIZE  256
+#define UART2_TX_SIZE  256
+#define UART3_TX_SIZE  256
 #define UART6_TX_SIZE  256
 // rx buffer size: 256 bytes
+#define UART1_RX_SIZE  256
+#define UART2_RX_SIZE  256
+#define UART3_RX_SIZE  256
 #define UART6_RX_SIZE  256
 
 // buffers
@@ -29,15 +68,45 @@ UART_CONFIG_PINS(6, GPIOC, 6, 7, 8)
 #if defined(__GNUC__)
 __attribute__((aligned (32)))
 #endif
+uint8_t Uart1_TxBuffer[UART1_TX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart2_TxBuffer[UART2_TX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart3_TxBuffer[UART3_TX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
 uint8_t Uart6_TxBuffer[UART6_TX_SIZE];
 
 #if defined(__GNUC__)
 __attribute__((aligned (32)))
 #endif
+uint8_t Uart1_RxBuffer[UART1_RX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart2_RxBuffer[UART2_RX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart3_RxBuffer[UART3_RX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
 uint8_t Uart6_RxBuffer[UART6_RX_SIZE];
 
-// initialization for UART6
+// initialization for UARTx
+UART_INIT(1, UART1_TX_SIZE, UART1_RX_SIZE)
+UART_INIT(2, UART2_TX_SIZE, UART2_RX_SIZE)
+UART_INIT(3, UART3_TX_SIZE, UART3_RX_SIZE)
 UART_INIT(6, UART6_TX_SIZE, UART6_RX_SIZE)
 
-// un-initialization for UART6
+// un-initialization for UARTx
+UART_UNINIT(1)
+UART_UNINIT(2)
+UART_UNINIT(3)
 UART_UNINIT(6)
