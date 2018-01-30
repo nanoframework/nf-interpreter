78fefedc2a1320b2b679a0c2b3d4725dbb4a9217
José Simões <jose.simoes@eclo.solutions>
2018-01-30 10:51:15 +0000

    Improve build
    - Replace AppVeyor build matrix with PS collection
    - Other minor corrections in AppVeyor yaml
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ get-stm32-targets-to-build.ps1
@@ -0,0 +1,112 @@
+#build matrx with target names and build options
+$BuildMatrix = ("MBN_QUAIL", "-DTARGET_SERIES=STM32F4xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=OFF -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=ON -DAPI_Windows.Devices.Spi=ON -DAPI_Windows.Devices.I2c=ON", 'True'),
+("ST_STM32F4_DISCOVERY", "-DTARGET_SERIES=STM32F4xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=ON -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=ON -DAPI_Windows.Devices.Spi=ON -DAPI_Windows.Devices.I2c=ON", 'False'),
+("ST_STM32F429I_DISCOVERY", "-DTARGET_SERIES=STM32F4xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=ON -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=ON -DAPI_Windows.Devices.Spi=ON -DAPI_Windows.Devices.I2c=ON", 'False'),
+("ST_NUCLEO64_F091RC", "-DTARGET_SERIES=STM32F0xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=OFF -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=OFF", 'False'),
+("ST_NUCLEO144_F746ZG", "-DTARGET_SERIES=STM32F7xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=ON -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=ON -DAPI_Windows.Devices.Spi=ON -DAPI_Windows.Devices.I2c=ON", 'False'),
+("ST_STM32F769I_DISCOVERY", "-DTARGET_SERIES=STM32F7xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=ON -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=ON -DAPI_Windows.Devices.Spi=ON -DAPI_Windows.Devices.I2c=ON", 'False'),
+("NETDUINO3_WIFI", "-DTARGET_SERIES=STM32F4xx -DRTOS=CHIBIOS -DUSE_FPU=TRUE -DNF_FEATURE_DEBUGGER=TRUE -DSWO_OUTPUT=OFF -DNF_FEATURE_RTC=ON -DAPI_Windows.Devices.Gpio=ON -DAPI_Windows.Devices.Spi=ON -DAPI_Windows.Devices.I2c=ON", 'True')
+
+
+# get commit message
+$commitMessage = "$env:APPVEYOR_REPO_COMMIT_MESSAGE" + " " + "$env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED"
+
+# is there a target name in the commit message?
+$targetCandidate = [regex]::Matches("$commitMessage",'[#]+\w+[#]').Value
+
+if($targetCandidate -is [array])
+{
+    $global:BUILD_MATRIX = @(,@()) 
+
+    ForEach($candidate in $targetCandidate)
+    {
+        # remove the leading and trailinig '#'
+        $thisCandidate = $candidate -replace "#", ""
+
+        # find if there is a target with this name
+        ForEach($item in $BuildMatrix)
+        {
+            if(!$item[0].CompareTo($thisCandidate))
+            {
+                if($global:BUILD_MATRIX)
+                {
+                    $global:BUILD_MATRIX += , $item
+                }
+                else
+                {
+                    $global:BUILD_MATRIX = , $item
+                }
+
+                break;
+            }
+        }
+    }
+}
+else
+{
+    if($targetCandidate)
+    {
+        # special case for #ALL_STM32# (build all targets)
+        if(!$targetCandidate.CompareTo('#ALL_STM32#'))
+        {
+            # target to build is the board name
+            $env:BOARD_NAME = "STM32"
+
+            $global:BUILD_MATRIX = $BuildMatrix
+        }
+        else
+        {
+            # remove the leading and trailinig '#'
+            $targetCandidate = $targetCandidate -replace "#", ""
+
+            # find if there is a target with this name
+            ForEach($item in $BuildMatrix)
+            {
+                if(!$item[0].CompareTo($targetCandidate))
+                {
+                    $env:BOARD_NAME = $item[0]
+                    $env:BUILD_OPTIONS = $item[1]
+                    $env:NEEDS_DFU = $item[2]
+            
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        # default is to build all STM32 targets
+        $env:BOARD_NAME = 'STM32'
+
+        $global:BUILD_MATRIX = $BuildMatrix
+    }
+}
+
+if($env:BOARD_NAME)
+{
+    if(!$env:BOARD_NAME.CompareTo('STM32'))
+    {
+        Write-Host "Build all STM32 targets"
+    }
+    else 
+    {
+        Write-Host "Target board is " $env:BOARD_NAME
+    }
+}
+else 
+{
+    if($global:BUILD_MATRIX)
+    {
+        Write-Host "Build targets:"
+
+        ForEach($item in $global:BUILD_MATRIX)
+        {
+            $env:BOARD_NAME += $item[0]+'|'
+            Write-Host $item[0]
+        }        
+    }
+    else
+    {
+        Write-Host "No STM32 target to build"
+    }
+}
