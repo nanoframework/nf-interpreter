1f4a4d02c56e17669135ffb0c494bf2c10d72f02
Robin Jones <robin@networkfusion.co.uk>
2018-02-15 16:27:37 +0000

    update appveyor to be inline with develop branch (removed custom URL)

--- install-arm-gcc-toolchain.ps1
+++ install-arm-gcc-toolchain.ps1
@@ -8,11 +8,9 @@ If($GnuGccPathExists -eq $False)
     Install-Module 7Zip4PowerShell -Force -Verbose
 
     Write-Host "Downloading ARM GNU GCC toolchain..."
-    #todo: add file and re-enable
-    #$url = "https://bintray.com/nfbot/internal-build-tools/download_file?file_path=gcc-arm-none-eabi-6-2017-q2-update-win32.7z"
-    $url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-win32.zip?revision=df1b65d3-7c8d-4e82-b114-e0b6ad7c2e6d?product=GNU%20Arm%20Embedded%20Toolchain,ZIP,,Windows,7-2017-q4-major"
-    #$output = "$PSScriptRoot\gcc-arm.7z"
-    $output = "$PSScriptRoot\gcc-arm.zip"
+    
+    $url = "https://bintray.com/nfbot/internal-build-tools/download_file?file_path=gcc-arm-none-eabi-7-2017-q4-major-win32.7z"
+    $output = "$PSScriptRoot\gcc-arm.7z"
     
     # download 7zip with toolchain
     (New-Object Net.WebClient).DownloadFile($url, $output)
