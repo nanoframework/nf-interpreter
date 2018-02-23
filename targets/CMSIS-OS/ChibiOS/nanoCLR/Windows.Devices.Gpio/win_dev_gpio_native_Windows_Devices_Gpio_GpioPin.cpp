03a9e1c5a5aca3336f700cfef70672d776836e76
josesimoes <jose.simoes@eclo.solutions>
2018-02-23 12:21:43 +0000

    Update GPIO code to use new PAL callbacks
    - Update code to use ChibiOS IoLine which encodes the port+pad making the code simple and more readable
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/win_dev_gpio_native_Windows_Devices_Gpio_GpioPin.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/win_dev_gpio_native_Windows_Devices_Gpio_GpioPin.cpp
@@ -43,16 +43,20 @@ enum GpioPinValue
 ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
-// this structure is part of the target because it can have different sizes according to the device
-//extern EXTConfig extInterruptsConfiguration;
-
-// this array keeps track of the Gpio pins that are assigned to each channel
-CLR_RT_HeapBlock* channelPinMapping[] = { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
-
 // this timer is used to handle the debounce time
 static virtual_timer_t debounceTimer;
 
 volatile uint16_t lastPadValue;
+
+// this is an utility function to get a ChibiOS PAL IoLine from our "encoded" pin number
+ioline_t GetIoLine(int16_t pinNumber)
+{
+    stm32_gpio_t* port  = GPIO_PORT(pinNumber);
+    int16_t pad = pinNumber % 16;
+
+    return PAL_LINE(port, pad);
+
+}
 
 static void debounceTimer_Callback( void* arg )
 {
@@ -73,13 +77,11 @@ static void debounceTimer_Callback( void* arg )
         return;
     }
     
-    // get pin number and take the port and pad references from that one
+    // get pin number
     int16_t pinNumber = pThis[ Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___pinNumber ].NumericByRefConst().s4;
-    stm32_gpio_t* port  = GPIO_PORT(pinNumber);
-    int16_t pad = pinNumber % 16;
 
-    // read pad
-    uint16_t currentValue = palReadPad(port, pad);
+    // read line
+    uint16_t currentValue = palReadLine(GetIoLine(pinNumber));
 
     if(lastPadValue == currentValue)
     {
@@ -89,16 +91,15 @@ static void debounceTimer_Callback( void* arg )
     }
 }
 
-static void ExtInterruptHandler(EXTDriver *extp, expchannel_t channel)
+static void GpioEventCallback(void *arg)
 {
-    (void)extp;
-    (void)channel;
+    CLR_RT_HeapBlock*  pThis = (CLR_RT_HeapBlock*)arg;
 
     NATIVE_INTERRUPT_START
 
     chSysLockFromISR();
 
-    CLR_RT_HeapBlock*  pThis = channelPinMapping[channel];
+    //CLR_RT_HeapBlock*  pThis = channelPinMapping[channel];
 
     if(pThis == NULL)
     {
@@ -120,8 +121,9 @@ static void ExtInterruptHandler(EXTDriver *extp, expchannel_t channel)
     
     // get pin number and take the port and pad references from that one
     int16_t pinNumber = pThis[ Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___pinNumber ].NumericByRefConst().s4;
-    stm32_gpio_t* port  = GPIO_PORT(pinNumber);
-    int16_t pad = pinNumber % 16;
+
+    // get IoLine from pin number
+    ioline_t ioLine = GetIoLine(pinNumber);
 
     // check if there is a debounce time set
     int64_t debounceTimeoutMilsec = (CLR_INT64_TEMP_CAST) pThis[ Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::FIELD___debounceTimeout ].NumericByRefConst().s8 / TIME_CONVERSION__TO_MILLISECONDS;
@@ -141,7 +143,7 @@ static void ExtInterruptHandler(EXTDriver *extp, expchannel_t channel)
         }
 
         // read pad
-        lastPadValue = palReadPad(port, pad);
+        lastPadValue = palReadLine(ioLine);
 
         // setup timer
         chVTSetI(&debounceTimer, TIME_MS2I(debounceTimeoutMilsec), debounceTimer_Callback, pThis);
@@ -149,12 +151,13 @@ static void ExtInterruptHandler(EXTDriver *extp, expchannel_t channel)
     else
     {
         // post a managed event with the current pin reading
-        PostManagedEvent( EVENT_GPIO, 0, pinNumber, palReadPad(port, pad) );
+        PostManagedEvent( EVENT_GPIO, 0, pinNumber, palReadLine(ioLine) );
     }
 
     chSysUnlockFromISR();
 
     NATIVE_INTERRUPT_END
+    
 }
 
 HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::Read___WindowsDevicesGpioGpioPinValue( CLR_RT_StackFrame& stack )
@@ -169,9 +172,7 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::Read___Windows
             NANOCLR_SET_AND_LEAVE(CLR_E_OBJECT_DISPOSED);
         }
 
-        int16_t pinNumber = pThis[ FIELD___pinNumber ].NumericByRefConst().s4;
-
-        stack.SetResult_I4( palReadPad(GPIO_PORT(pinNumber), pinNumber % 16) );
+        stack.SetResult_I4( GetIoLine(pThis[ FIELD___pinNumber ].NumericByRefConst().s4) );
     }
     NANOCLR_NOCLEANUP();
 }
@@ -182,25 +183,15 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::DisposeNative_
     {
         CLR_RT_HeapBlock* pThis = stack.This();  FAULT_ON_NULL(pThis);
 
-        // get pin number and take the port and pad references from that one
-        int16_t pinNumber = pThis[ FIELD___pinNumber ].NumericByRefConst().s4;
-        stm32_gpio_t* port  = GPIO_PORT(pinNumber);
-        int16_t pad = pinNumber % 16;
+        // get IoLine from pin number
+        ioline_t ioLine = GetIoLine(pThis[ FIELD___pinNumber ].NumericByRefConst().s4);
 
         // disable the EXT interrupt channel
         // it's OK to do always this, no matter if it's enabled or not
-        chSysLock();
-        extChannelDisable(&EXTD1, pad);
-        chSysUnlock();
-
-        // don't need to do anything else about the callbacks because the pin is 
-        // removed from the managed event listener in the managed Dispose() method 
-
-        // clear this channel in channel pin mapping
-        channelPinMapping[pad] = NULL;
+        palDisableLineEvent(ioLine);
 
         // set pin to input to save power
-        palSetPadMode(port, pad, PAL_MODE_INPUT);
+        palSetLineMode(ioLine, PAL_MODE_INPUT);
     }
     NANOCLR_NOCLEANUP();
 }
@@ -240,11 +231,9 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::NativeSetDrive
             NANOCLR_SET_AND_LEAVE(CLR_E_OBJECT_DISPOSED);
         }
 
-        // get pin number and take the port and pad references from that one
-        int16_t pinNumber = pThis[ FIELD___pinNumber ].NumericByRefConst().s4;
-        stm32_gpio_t* port  = GPIO_PORT(pinNumber);
-        int16_t pad = pinNumber % 16;
-
+        // get IoLine from pin number
+        ioline_t ioLine = GetIoLine(pThis[ FIELD___pinNumber ].NumericByRefConst().s4);
+        
         // it's better cast this this to the appropriate enum
         GpioPinDriveMode driveMode = (GpioPinDriveMode)stack.Arg1().NumericByRef().s4;
 
@@ -264,50 +253,29 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::NativeSetDrive
         // flag to determine if there are any callbacks registered in managed code
         bool callbacksRegistered = (pThis[ FIELD___callbacks ].Dereference() != NULL);
 
-        // initial check on whether there are callbacks registered AND the drive mode is input
-        // the EXT driver can only handle 16 interrupts and each channel can serve only one port
-        // ex: channel 0 can be triggered by pad 0 on port A, B, C.... but EXCLUSIVELY by ONLY ONE of those
-        // see MCU programming manual for details (External interrupt/event controller (EXTI))
-        if(extInterruptsConfiguration.channels[pad].mode != 0 && callbacksRegistered && driveModeIsInput)
-        {
-            // channel for this pad is already taken
-            // TODO: probably better to have a dedicated exception here, this one is too generic...
-            NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
-        }
-
-        // always disable the EXT interrupt channel before setting the function on a pad otherwise the interrupts won't fire
-        chSysLock();
-        extChannelDisable(&EXTD1, pad);
-        chSysUnlock();
-
-        // protect from GC 
-
-        // clear this channel in channel pin mapping
-        channelPinMapping[pad] = NULL;
-
         switch (driveMode)
         {
             case GpioPinDriveMode_Input:
-                palSetPadMode(port, pad, PAL_MODE_INPUT);
+                palSetLineMode(ioLine, PAL_MODE_INPUT);
                 setupInterrupt = true;
                 break;
 
             case GpioPinDriveMode_InputPullDown:
-                palSetPadMode(port, pad, PAL_MODE_INPUT_PULLDOWN);
+                palSetLineMode(ioLine, PAL_MODE_INPUT_PULLDOWN);
                 setupInterrupt = true;
                 break;
 
             case GpioPinDriveMode_InputPullUp:
-                palSetPadMode(port, pad, PAL_MODE_INPUT_PULLUP);
+                palSetLineMode(ioLine, PAL_MODE_INPUT_PULLUP);
                 setupInterrupt = true;
                 break;
 
             case GpioPinDriveMode_Output:
-                palSetPadMode(port, pad, PAL_MODE_OUTPUT_PUSHPULL);
+                palSetLineMode(ioLine, PAL_MODE_OUTPUT_PUSHPULL);
                 break;
 
             case GpioPinDriveMode_OutputOpenDrain:
-                palSetPadMode(port, pad, PAL_MODE_OUTPUT_OPENDRAIN);
+                palSetLineMode(ioLine, PAL_MODE_OUTPUT_OPENDRAIN);
                 break;
 
             default:
@@ -319,29 +287,25 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::NativeSetDrive
         // if drive mode is output, read the pad to update the managed field _lastOutputValue
         if(!driveModeIsInput)
         {
-            pThis[ FIELD___lastOutputValue ].NumericByRef().s4 = palReadPad(port, pad);
+            pThis[ FIELD___lastOutputValue ].NumericByRef().s4 = palReadLine(ioLine);
         }
 
         if(callbacksRegistered && setupInterrupt)
         {
             // there are callbacks registered and...
             // the drive mode is input so need to setup the interrupt
-            chSysLock();
 
             // save pin
-            channelPinMapping[pad] = pThis;
+            //channelPinMapping[pad] = pThis;
 
-            int16_t extChannelCode = ((pinNumber/16) << 8);
-            const EXTChannelConfig configToSetupChannel = {EXT_CH_MODE_BOTH_EDGES | extChannelCode, ExtInterruptHandler};
-            extSetChannelModeI(&EXTD1, pad, &configToSetupChannel);                
-
-            chSysUnlock();
+            palEnableLineEvent(ioLine, PAL_EVENT_MODE_BOTH_EDGES);
+            palSetLineCallback(ioLine, GpioEventCallback, pThis);
 
             // protect this from GC so that the callback is where it's supposed to
             CLR_RT_ProtectFromGC         gc( *pThis );
             
             // read pad and store current value to check on debounce callback
-            lastPadValue = palReadPad(port, pad);
+            lastPadValue = palReadLine(ioLine);
         }
     }
     NANOCLR_NOCLEANUP();
@@ -383,7 +347,9 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::WriteNative___
             NANOCLR_SET_AND_LEAVE(CLR_E_OBJECT_DISPOSED);
         }
 
-        int16_t pinNumber = pThis[ FIELD___pinNumber ].NumericByRefConst().s4;
+        // get IoLine from pin number
+        ioline_t ioLine = GetIoLine(pThis[ FIELD___pinNumber ].NumericByRefConst().s4);
+
         GpioPinDriveMode driveMode = (GpioPinDriveMode)pThis[ FIELD___driveMode ].NumericByRefConst().s4;
 
         GpioPinValue state = (GpioPinValue)stack.Arg1().NumericByRef().s4;
@@ -395,14 +361,7 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::WriteNative___
             (driveMode == GpioPinDriveMode_OutputOpenSource) ||
             (driveMode == GpioPinDriveMode_OutputOpenSourcePullDown))
         {
-            if (state == GpioPinValue_Low)
-            {
-                palClearPad(GPIO_PORT(pinNumber), pinNumber % 16);
-            }
-            else 
-            {
-                palSetPad(GPIO_PORT(pinNumber), pinNumber % 16);
-            }
+            palWriteLine(ioLine, state);
         }
         else
         {
@@ -424,15 +383,13 @@ HRESULT Library_win_dev_gpio_native_Windows_Devices_Gpio_GpioPin::NativeSetAlter
             NANOCLR_SET_AND_LEAVE(CLR_E_OBJECT_DISPOSED);
         }
 
-        // get pin number and take the port and pad references from that one
-        int16_t pinNumber = pThis[ FIELD___pinNumber ].NumericByRefConst().s4;
-        stm32_gpio_t* port  = GPIO_PORT(pinNumber);
-        int16_t pad = pinNumber % 16;
+        // get IoLine from pin number
+        ioline_t ioLine = GetIoLine(pThis[ FIELD___pinNumber ].NumericByRefConst().s4);
 
         // get alternate function argument
 		int32_t alternateFunction = stack.Arg1().NumericByRef().s4;
 
-        palSetPadMode(port, pad, PAL_MODE_ALTERNATE(alternateFunction));
+        palSetLineMode(ioLine, PAL_MODE_ALTERNATE(alternateFunction));
 
     }
     NANOCLR_NOCLEANUP();
