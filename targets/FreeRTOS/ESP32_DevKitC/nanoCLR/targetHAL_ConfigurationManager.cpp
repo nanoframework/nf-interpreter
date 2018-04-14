8c86aa7f1148c562f222b46308842f6f4ed4fe5b
adriansoundy <adriansoundy@gmail.com>
2018-04-15 00:17:11 +1200

    Fix to Esp32 configurationManager so it compiles

--- targets/FreeRTOS/ESP32_DevKitC/nanoCLR/targetHAL_ConfigurationManager.cpp
+++ targets/FreeRTOS/ESP32_DevKitC/nanoCLR/targetHAL_ConfigurationManager.cpp
@@ -6,12 +6,11 @@
 #include <nanoHAL.h>
 #include <nanoHAL_v2.h>
 #include <nanoWeak.h>
-<<<<<<< HEAD
 #include "Esp32_os.h"
-=======
-#include <Esp32_os.h>
-#include <nvs.h>
->>>>>>> 32fab16bd6f245811e65468a29d021e12cc730e7
+
+// NVS parameters for Interface config
+#define NVS_NAMESPACE	    "nanoF"
+
 
 // initialization of configuration manager
 // provided as weak so it can be replaced at target level, if required because of the target implementing the storage with a mechanism other then saving to flash
@@ -33,30 +32,40 @@ void ConfigurationManager_EnumerateConfigurationBlocks()
 bool ConfigurationManager_GetConfigurationBlock(void* configurationBlock, DeviceConfigurationOption configuration, uint32_t configurationIndex)
 {
 	bool result = FALSE;
+    char configName[10];
+
 	nvs_handle out_handle;
-	size_t     blobSize;
+	size_t     blobSize = 0;
 
     // validate if the requested block exists
     if(configuration == DeviceConfigurationOption_Network)
     {
         // set blob size
         blobSize = sizeof(Configuration_NetworkInterface);
+        configName[0] = 'N';
     }
     else if(configuration == DeviceConfigurationOption_Wireless80211Network)
     {
         // set blob size
         blobSize = sizeof(Configuration_Wireless80211NetworkInterface);
+        configName[0] = 'W';
     }
 
-	// Open NVS storage using NanoFramework namespace
-	esp_err_t ret = nvs_open( NVS_NAMESPACE, NVS_READONLY, &out_handle);
-	if ( ret == ESP_OK )
-	{
-	   // copy the config block content to the pointer in the argument
-		ret = nvs_get_blob(out_handle, NVS_NETWORK_CONFIG1, (void *)configurationBlock, &blobSize);
-		if (ret == ESP_OK) result = TRUE;
-		nvs_close(out_handle);
-	}
+    // Anything to get
+    if  (blobSize != 0 ) 
+    {
+        // Open NVS storage using NanoFramework namespace
+        esp_err_t ret = nvs_open( NVS_NAMESPACE, NVS_READONLY, &out_handle);
+        if ( ret == ESP_OK )
+        {
+            itoa(configurationIndex, configName+1, 10);
+
+        // copy the config block content to the pointer in the argument
+            ret = nvs_get_blob(out_handle, configName, (void *)configurationBlock, &blobSize);
+            if (ret == ESP_OK) result = TRUE;
+            nvs_close(out_handle);
+        }
+    }
 
     return result;
 }
@@ -67,48 +76,55 @@ bool ConfigurationManager_StoreConfigurationBlock(void* configurationBlock, Devi
 	bool result = FALSE;
 	nvs_handle out_handle;
     bool requiresEnumeration = FALSE;
-	size_t     blobSize;
+	size_t     blobSize = 0;
+    char configName[10];
 
     if(configuration == DeviceConfigurationOption_Network)
     {
         // set blob size
         blobSize = sizeof(Configuration_NetworkInterface);
+        configName[0] = 'N';
     }
     else if(configuration == DeviceConfigurationOption_Wireless80211Network)
     {
         // set blob size
         blobSize = sizeof(Configuration_Wireless80211NetworkInterface);
+        configName[0] = 'W';
     }
     else if(configuration == DeviceConfigurationOption_All)
     {
         // particular situation where we are receiving the full configuration block
 
+        configName[0] = 'D';
+
         // always enumerate the blocks again after storing it
         // requiresEnumeration = TRUE;
     }
+    
+    // Anything to save
+    if  (blobSize != 0 ) 
+    {
+        // copy the config block content to the pointer in the argument
+        esp_err_t ret = nvs_open( NVS_NAMESPACE, NVS_READWRITE, &out_handle);
+        if ( ret == ESP_OK )
+        {
+            itoa(configurationIndex, configName+1, 10);
 
-    // copy the config block content to the pointer in the argument
-	esp_err_t ret = nvs_open( NVS_NAMESPACE, NVS_READWRITE, &out_handle);
-	if ( ret == ESP_OK )
-	{
-		ret = nvs_set_blob(out_handle, NVS_NETWORK_CONFIG1, (void *)configurationBlock, blobSize);
-		if ( ret == ESP_OK ) 
-		{
-			ret = nvs_commit(out_handle);
-			if (ret == ESP_OK) result = TRUE;
-		}
-		nvs_close(out_handle);
-	}
-
+            ret = nvs_set_blob(out_handle, configName, (void *)configurationBlock, blobSize);
+            if ( ret == ESP_OK ) 
+            {
+                ret = nvs_commit(out_handle);
+                if (ret == ESP_OK) result = TRUE;
+            }
+            nvs_close(out_handle);
+        }
+    }
+    
     if(requiresEnumeration)
     {
         // perform enumeration of configuration blocks
         ConfigurationManager_EnumerateConfigurationBlocks();
     }
 
-<<<<<<< HEAD
-    return success;
-=======
     return result;
->>>>>>> 32fab16bd6f245811e65468a29d021e12cc730e7
 }
