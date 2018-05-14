bdd08b98e6b2b48ba907c750f2efc9aea3f2bf1c
josesimoes <jose.simoes@eclo.solutions>
2018-05-14 16:54:49 +0100

    Improvements in STM32 I2C class lib code
    - Remove static write and read buffers for I2C
    - Add function to compute estimated time to perform I2C transaction
    - Rework I2C NativeTransmit to use CLR events (or not) depending on transaction time
    - Remove I2C init and uninit calls (not required anymore)
    - Remove target configurations for I2C (not required anymore)
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/MBN_QUAIL/target_windows_devices_i2c_config.cpp
+++ targets/CMSIS-OS/ChibiOS/MBN_QUAIL/target_windows_devices_i2c_config.cpp
@@ -5,30 +5,6 @@
 
 #include "win_dev_i2c_native.h"
 
-//////////
-// I2C1 //
-//////////
-
-// buffers size
-// tx buffer size (in bytes)
-#define I2C1_WR_SIZE  100
-// rx buffer size (in bytes)
-#define I2C1_RD_SIZE  100
-
-// buffers declaration
-// buffers that are R/W by DMA are recommended to be aligned with 32 bytes cache page size boundary
-// because of issues with cache coherency and DMA (this is particularly important with Cortex-M7 because of cache)
-#if defined(__GNUC__)
-__attribute__((aligned (32)))
-#endif
-uint8_t I2C1_WriteBuffer[I2C1_WR_SIZE];
-#if defined(__GNUC__)
-__attribute__((aligned (32)))
-#endif
-uint8_t I2C1_ReadBuffer[I2C1_RD_SIZE];
-
-// initialization for I2C1
-I2C_INIT(1, I2C1_WR_SIZE, I2C1_RD_SIZE)
-
-// un-initialization for I2C1
-I2C_UNINIT(1)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
+///////////////////////////////////////////////////////////////////////////////////////////////////
