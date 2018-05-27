dda6a8565aa48bb878d50f6b4a40f818f1be526d
josesimoes <jose.simoes@eclo.solutions>
2018-05-27 01:44:17 +0100

    Revert last batch of improvements on STM32 I2C code
    - Rever I2C code @ 99ad98a5102ea7e5b472e41a5292c7f63466aefa

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native.h
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native.h
@@ -1,7 +1,15 @@
+//-----------------------------------------------------------------------------
 //
-// Copyright (c) 2017 The nanoFramework project contributors
-// See LICENSE file in the project root for full license information.
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
 //
+//-----------------------------------------------------------------------------
+
 
 #ifndef _WIN_DEV_I2C_NATIVE_H_
 #define _WIN_DEV_I2C_NATIVE_H_
@@ -10,6 +18,13 @@
 #include <nanoCLR_Runtime.h>
 #include <nanoCLR_Checks.h>
 #include <hal.h>
+
+struct nfI2CConfig
+{
+    I2CConfig Configuration;
+    I2CDriver * Driver;
+    uint16_t SlaveAddress;
+};
 
 struct Library_win_dev_i2c_native_Windows_Devices_I2c_I2cConnectionSettings
 {
@@ -24,8 +39,7 @@ struct Library_win_dev_i2c_native_Windows_Devices_I2c_I2cConnectionSettings
 
 struct Library_win_dev_i2c_native_Windows_Devices_I2c_I2cController
 {
-    static const int FIELD_STATIC__s_instance = 0;
-    static const int FIELD_STATIC__s_deviceCollection = 1;
+    static const int FIELD_STATIC__DeviceCollection = 0;
 
 
     //--//
@@ -37,60 +51,28 @@ struct Library_win_dev_i2c_native_Windows_Devices_I2c_I2cDevice
     static const int FIELD___syncLock = 1;
     static const int FIELD___deviceId = 2;
     static const int FIELD___connectionSettings = 3;
-    static const int FIELD___disposed = 4;
+    static const int FIELD___i2cBus = 4;
+    static const int FIELD___disposedValue = 5;
 
     NANOCLR_NATIVE_DECLARE(NativeInit___VOID);
     NANOCLR_NATIVE_DECLARE(DisposeNative___VOID);
-    NANOCLR_NATIVE_DECLARE(NativeTransmit___WindowsDevicesI2cI2cTransferResult__SZARRAY_U1__SZARRAY_U1);
+    NANOCLR_NATIVE_DECLARE(NativeTransmit___I4__SZARRAY_U1__U4__SZARRAY_U1__U4);
     NANOCLR_NATIVE_DECLARE(GetDeviceSelector___STATIC__STRING);
 
     //--//
-    static void GetI2cConfig(CLR_RT_HeapBlock* managedConfig, I2CConfig* llConfig);
-    static bool IsLongRunningOperation(int writeSize, int readSize, float byteTime, int& estimatedDurationMiliseconds);
+    static nfI2CConfig GetConfig(int bus, CLR_RT_HeapBlock* config);
 };
 
 struct Library_win_dev_i2c_native_Windows_Devices_I2c_I2cTransferResult
 {
-    static const int FIELD___bytesTransferred = 1;
-    static const int FIELD___status = 2;
+    static const int FIELD__BytesTransferred = 1;
+    static const int FIELD__Status = 2;
 
 
     //--//
 
 };
 
-extern const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_I2c;
-
-// struct representing the I2C 
-struct NF_PAL_I2C
-{
-    I2CDriver*  Driver;
-    I2CConfig   Configuration;
-    thread_t*   WorkingThread;
-    i2caddr_t   Address;
-    float ByteTime;
-
-    uint8_t* WriteBuffer;
-    uint8_t  WriteSize;
-
-    uint8_t* ReadBuffer;
-    uint8_t  ReadSize;
-};
-
-///////////////////////////////////////////
-// declaration of the the I2C PAL strucs //
-///////////////////////////////////////////
-#if STM32_I2C_USE_I2C1
-    extern NF_PAL_I2C I2C1_PAL;
-#endif
-#if STM32_I2C_USE_I2C2
-    extern NF_PAL_I2C I2C2_PAL;
-#endif
-#if STM32_I2C_USE_I2C3
-    extern NF_PAL_I2C I2C3_PAL;
-#endif
-#if STM32_I2C_USE_I2C4
-    extern NF_PAL_I2C I2C4_PAL;
-#endif
+    extern const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_I2c;
 
 #endif  //_WIN_DEV_I2C_NATIVE_H_
