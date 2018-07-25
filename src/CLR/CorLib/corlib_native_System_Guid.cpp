58f6cf5f017c458fc134f33459773c2d227df190
Adrian Soundy <adriansoundy@gmail.com>
2018-07-26 01:13:00 +1200

    Add native GUID implementaion (#804)
    - Also added target random implementation for ESP32

--- src/CLR/CorLib/corlib_native_System_Guid.cpp
+++ src/CLR/CorLib/corlib_native_System_Guid.cpp
@@ -5,12 +5,29 @@
 //
 #include "CorLib.h"
 
+//
+//  Generate a new GUID
+//
+//  Based on the version 4 GUID (random)
+//  http://guid.one/guid/make
+
 
 HRESULT Library_corlib_native_System_Guid::GenerateNewGuid___STATIC__SZARRAY_U1( CLR_RT_StackFrame& stack )
 {
     NANOCLR_HEADER();
 
-    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+    CLR_RT_Random       rand;
+    CLR_UINT8*          buf;
+    CLR_RT_HeapBlock&   top = stack.PushValueAndClear();
+ 
+    // Create a array of 16 bytes on top of stack to return
+    NANOCLR_CHECK_HRESULT(CLR_RT_HeapBlock_Array::CreateInstance( top, 16, g_CLR_RT_WellKnownTypes.m_UInt8 ));
+    buf = top.DereferenceArray()->GetFirstElement();
 
+    rand.NextBytes(buf, 16);           // fill with random numbers
+
+    buf[7] =  (buf[7] & 0x0f) | 0x40;  // Set verion
+    buf[9] =  (buf[7] & 0x3f) | 0x80;  // Set variant
+    
     NANOCLR_NOCLEANUP();
 }
