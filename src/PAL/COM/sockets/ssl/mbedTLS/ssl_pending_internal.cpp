d67f3d9b37a43a0232e0861e66de96db7e98b075
José Simões <jose.simoes@eclo.solutions>
2018-07-30 20:13:24 +0100

    Add stub implementation of  ssl_pending_internal for mbed TLS
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/PAL/COM/sockets/ssl/mbedTLS/ssl_pending_internal.cpp
+++ src/PAL/COM/sockets/ssl/mbedTLS/ssl_pending_internal.cpp
@@ -2,3 +2,25 @@
 // Copyright (c) 2018 The nanoFramework project contributors
 // See LICENSE file in the project root for full license information.
 //
+
+#include <ssl.h>
+#include <nanoPAL.h>
+#include "mbedtls.h"
+
+int ssl_pending_internal( int sd )
+{
+    (void)sd;
+    int avail = 0;
+
+    //mbedTLS_NFContext* context= (mbedTLS_NFContext*)SOCKET_DRIVER.GetSocketSslData(sd);
+    //mbedtls_ssl_context *ssl = context->ssl;
+
+    // if(ssl == NULL || ssl == (void*)SSL_SOCKET_ATTEMPTED_CONNECT)
+    // {
+    //     return SOCK_SOCKET_ERROR;
+    // }
+    
+    //avail = SSL_pending(ssl);  /* send SSL/TLS close_notify */
+
+    return avail;
+}
