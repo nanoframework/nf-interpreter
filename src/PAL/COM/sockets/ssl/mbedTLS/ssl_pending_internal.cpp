b637d2dd83c787ee98babb725aa2a17f9286b9d8
José Simões <jose.simoes@eclo.solutions>
2018-07-31 11:04:49 +0100

    Implement mbed TLS ssl_pending_internal
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/PAL/COM/sockets/ssl/mbedTLS/ssl_pending_internal.cpp
+++ src/PAL/COM/sockets/ssl/mbedTLS/ssl_pending_internal.cpp
@@ -9,18 +9,13 @@
 
 int ssl_pending_internal( int sd )
 {
-    (void)sd;
-    int avail = 0;
+    mbedTLS_NFContext* context= (mbedTLS_NFContext*)SOCKET_DRIVER.GetSocketSslData(sd);
+    mbedtls_ssl_context *ssl = context->ssl;
 
-    //mbedTLS_NFContext* context= (mbedTLS_NFContext*)SOCKET_DRIVER.GetSocketSslData(sd);
-    //mbedtls_ssl_context *ssl = context->ssl;
-
-    // if(ssl == NULL || ssl == (void*)SSL_SOCKET_ATTEMPTED_CONNECT)
-    // {
-    //     return SOCK_SOCKET_ERROR;
-    // }
+    if(ssl == NULL)
+    {
+        return SOCK_SOCKET_ERROR;
+    }
     
-    //avail = SSL_pending(ssl);  /* send SSL/TLS close_notify */
-
-    return avail;
+    return mbedtls_ssl_check_pending(ssl);
 }
