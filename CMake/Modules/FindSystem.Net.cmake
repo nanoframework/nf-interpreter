866c1eeff9dd6ec3254fc60acd63df3ecf047909
José Simões <jose.simoes@eclo.solutions>
2018-08-02 10:19:57 +0100

    Move System.Net source code to new folder DeviceInterfaces
    - Update CMake accordingly
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- CMake/Modules/FindSystem.Net.cmake
+++ CMake/Modules/FindSystem.Net.cmake
@@ -3,15 +3,9 @@
 # See LICENSE file in the project root for full license information.
 #
 
-# native code directory
-set(BASE_PATH_FOR_THIS_MODULE  "${BASE_PATH_FOR_CLASS_LIBRARIES_MODULES}/System.Net")
-
 # set include directories
-list(APPEND System.Net_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src/CLR/Core")
-list(APPEND System.Net_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src/CLR/Include")
 list(APPEND System.Net_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src/HAL/Include")
-list(APPEND System.Net_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src/PAL/Include")
-list(APPEND System.Net_INCLUDE_DIRS "${BASE_PATH_FOR_THIS_MODULE}")
+list(APPEND System.Net_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src/DeviceInterfaces/System.Net")
 
 # source files
 set(System.Net_SRCS
@@ -37,14 +31,13 @@ foreach(SRC_FILE ${System.Net_SRCS})
     find_file(System.Net_SRC_FILE ${SRC_FILE}
         PATHS
 
-            "${PROJECT_SOURCE_DIR}/src/HAL/System.Net"
+            "${PROJECT_SOURCE_DIR}/src/DeviceInterfaces/System.Net"
 
         CMAKE_FIND_ROOT_PATH_BOTH
     )
     # message("${SRC_FILE} >> ${System.Net_SRC_FILE}") # debug helper
     list(APPEND System.Net_SOURCES ${System.Net_SRC_FILE})
 endforeach()
-
 
 include(FindPackageHandleStandardArgs)
 
