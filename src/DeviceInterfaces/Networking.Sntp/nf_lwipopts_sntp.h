412ecaae9b5b84e4ee2eebe0032a0f19b5d7a4b4
José Simões <jose.simoes@eclo.solutions>
2018-08-03 17:25:41 +0100

    Add weak implementation of ctime to keep compiler happy
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/DeviceInterfaces/Networking.Sntp/nf_lwipopts_sntp.h
+++ src/DeviceInterfaces/Networking.Sntp/nf_lwipopts_sntp.h
@@ -8,6 +8,9 @@
 #ifndef __NF_LWIPOPTS_SNTP_H__
 #define __NF_LWIPOPTS_SNTP_H__
 
+#include <time.h>
+#include <nanoWeak.h>
+
 // SNTP servers can be IP or full address and are resolved at 
 #define SNTP_SERVER_DNS         1
 
@@ -17,12 +20,13 @@
 // set update delay (in milliseconds)
 #define SNTP_UPDATE_DELAY       3600000
 
-// requiring time precision on the milliseconds
-#define SNTP_SET_SYSTEM_TIME_US
-
 // retry timeout (15 minutes)
 #define SNTP_RETRY_TIMEOUT      900000
 
+// call to set the system time with seconds and milliseconds
 #define SNTP_SET_SYSTEM_TIME_US(sec, us)    
+
+// need to declare this here to keep compiler happy 
+__nfweak char* ctime (const time_t * timer);
 
 #endif // __NF_LWIPOPTS_SNTP_H__
