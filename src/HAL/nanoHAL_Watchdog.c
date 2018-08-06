cb765861d8d3bcfdede408d141d0b94784dc9e61
Vishnu Sharma <26829246+sharmavishnu@users.noreply.github.com>
2018-08-06 16:33:25 -0700

    Updated watchdog API default implementation
    Updated watchdog API default implementations after changes to the header file

--- src/HAL/nanoHAL_Watchdog.c
+++ src/HAL/nanoHAL_Watchdog.c
@@ -8,37 +8,16 @@
 
 /**
 * Method to initialize the watchdog. Watchdog must be initialized 
-* only one (when the system starts, generally in main after system and HAL initialization).
+* only once (when the system starts, generally in main after system and HAL initialization).
 */
 __nfweak void Watchdog_Init(){}
 
-/**
- * Start the watchdog. 
- * @return  true when watchdog started successfully else false
- */ 
-__nfweak bool Watchdog_Start(){return false;}
 
 /**
- * Stop the watchdog
- */  
-__nfweak void Watchdog_Stop(){}
-/**
- * Reset the watchdog. If not done without the timout period,
- * the watchdog will trigger and MCU will reset
+ * Reset the watchdog. If not done within the timout period,
+ * the watchdog will trigger and MCU will reset. Specific implementation
+ * must provide required thread-safety (e.g. whether called from thread or an 
+ * ISR, reentrant/non-reentrant)
  */ 
 __nfweak void Watchdog_Reset(){}
-/**
- * Check if watchdog is running
- * @return bool
- */ 
-__nfweak bool Watchdog_IsRunning() {return false;}
-/**
- * Set the timeout required for the watchdog trigger.
- * This must be called before the watchdog is started
- * @param   timeoutMillis   The timeout in milliseconds
- */ 
-__nfweak void Watchdog_SetTimeout(uint16_t timeoutMillis){};
-/**
- * Set the maximum timeout for the platform
- */ 
-__nfweak void Watchdog_SetMaxTimeout(){}; 
+
