c8d956d6286da7c89b6325e3bcb588fa1c4b9fad
Robin Jones <robin@networkfusion.co.uk>
2018-08-27 21:26:44 +0100

    demo code (will need reverting) TODO: throws on f_mount for an unknown reason!

--- targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/nanoCLR/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/nanoCLR/main.c
@@ -16,10 +16,142 @@
 #include <nanoHAL_v2.h>
 #include <targetPAL.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <ff.h>
+#include "..\ffconf.h"
+
 // need to declare the Receiver thread here
 osThreadDef(ReceiverThread, osPriorityHigh, 2048, "ReceiverThread");
 // declare CLRStartup thread here 
 osThreadDef(CLRStartupThread, osPriorityNormal, 4096, "CLRStartupThread"); 
+
+
+
+  // *************** DEMO CODE FOR FATFS *******************
+
+/*===========================================================================*/
+/* Card insertion monitor.                                                   */
+/*===========================================================================*/
+
+#define POLLING_INTERVAL                10
+#define POLLING_DELAY                   10
+
+/**
+ * @brief   Card monitor timer.
+ */
+static virtual_timer_t tmr;
+
+/**
+ * @brief   Debounce counter.
+ */
+static unsigned cnt;
+
+/**
+ * @brief   Card event sources.
+ */
+static event_source_t inserted_event, removed_event;
+
+/**
+ * @brief   Insertion monitor timer callback function.
+ *
+ * @param[in] p         pointer to the @p BaseBlockDevice object
+ *
+ * @notapi
+ */
+static void tmrfunc(void *p) {
+  BaseBlockDevice *bbdp = p;
+
+  chSysLockFromISR();
+  if (cnt > 0) {
+    if (blkIsInserted(bbdp)) {
+      if (--cnt == 0) {
+        chEvtBroadcastI(&inserted_event);
+      }
+    }
+    else
+      cnt = POLLING_INTERVAL;
+  }
+  else {
+    if (!blkIsInserted(bbdp)) {
+      cnt = POLLING_INTERVAL;
+      chEvtBroadcastI(&removed_event);
+    }
+  }
+  chVTSetI(&tmr, TIME_MS2I(POLLING_DELAY), tmrfunc, bbdp);
+  chSysUnlockFromISR();
+}
+
+/**
+ * @brief   Polling monitor start.
+ *
+ * @param[in] p         pointer to an object implementing @p BaseBlockDevice
+ *
+ * @notapi
+ */
+static void tmr_init(void *p) {
+
+  chEvtObjectInit(&inserted_event);
+  chEvtObjectInit(&removed_event);
+  chSysLock();
+  cnt = POLLING_INTERVAL;
+  chVTSetI(&tmr, TIME_MS2I(POLLING_DELAY), tmrfunc, p);
+  chSysUnlock();
+}
+
+/*===========================================================================*/
+/* FatFs related.                                                            */
+/*===========================================================================*/
+
+/**
+ * @brief FS object.
+ */
+static FATFS SDC_FS;
+
+/* FS mounted and ready.*/
+static bool fs_ready = FALSE;
+
+///* Generic large buffer.*/
+//static uint8_t fbuff[1024];
+
+/*
+ * Card insertion event.
+ */
+static void InsertHandler(eventid_t id) {
+  FRESULT err;
+
+  (void)id;
+  /*
+   * On insertion SDC initialization and FS mount.
+   */
+  if (sdcConnect(&SDCD1))
+    return;
+
+  err = f_mount(&SDC_FS, "/", 1);
+   if (err != FR_OK) {
+     sdcDisconnect(&SDCD1);
+     return;
+   }
+  fs_ready = TRUE;
+}
+
+/*
+ * Card removal event.
+ */
+static void RemoveHandler(eventid_t id) {
+
+  (void)id;
+    sdcDisconnect(&SDCD1);
+  fs_ready = FALSE;
+}
+
+  
+
+  // *******************************************************
+
+
+
+
 
 //  Application entry point.
 int main(void) {
@@ -72,7 +204,33 @@ int main(void) {
   // start kernel, after this main() will behave like a thread with priority osPriorityNormal
   osKernelStart();
 
+
+  // *************** DEMO CODE FOR FATFS *******************
+static const evhandler_t evhndl[] = {
+    InsertHandler,
+    RemoveHandler
+  };
+  event_listener_t el0, el1;
+  /*
+   * Activates the  SDC driver 1 using default configuration.
+   */
+
+  sdcStart(&SDCD1, NULL);
+
+  /*
+   * Activates the card insertion monitor.
+   */
+  tmr_init(&SDCD1);
+
+chEvtRegister(&inserted_event, &el0, 0);
+chEvtRegister(&removed_event, &el1, 1);
+
+
+
+  // *******************************************************
+
   while (true) { 
     osDelay(100);
+    chEvtDispatch(evhndl, chEvtWaitOneTimeout(ALL_EVENTS, TIME_MS2I(500)));
   }
 }
