e39705b0b2abe4b0e22fbb754b2c5aea99d375a0
José Simões <josesimoes@users.noreply.github.com>
2018-08-27 15:42:59 +0100

    Add back GenericPort_Write to ESP32 (#829)

---
+++ targets/FreeRTOS/ESP32_DevKitC/common/GenericPort.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <esp32_os.h>
+
+#include "nanoCLR_Types.h"
+
+uint32_t GenericPort_Write( int portNum, const char* data, size_t size )
+{
+    (void)portNum;
+
+    char* p = (char*)data;
+    int counter = 0;
+
+    // send characters directly to the trace port
+    while(*p != '\0' || counter < (int)size)
+    {
+        ets_printf( "%c", *p++); 
+        counter++;
+    }
+    return counter;
+}
