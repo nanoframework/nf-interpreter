905535b1f3430c22525b6491a12f626b1e01cba7
Adrian Soundy <adriansoundy@gmail.com>
2018-08-31 21:59:45 +1200

    update wifi version (#834)

--- targets/FreeRTOS/ESP32_DevKitC/nanoCLR/Windows.Devices.Wifi/win_dev_wifi_native.cpp
+++ targets/FreeRTOS/ESP32_DevKitC/nanoCLR/Windows.Devices.Wifi/win_dev_wifi_native.cpp
@@ -59,5 +59,5 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_Wifi =
     "Windows.Devices.Wifi", 
     0x5AC050C9,
     method_lookup,
-    { 1, 0, 0, 1 }
+    { 1, 0, 0, 0 }
 };
