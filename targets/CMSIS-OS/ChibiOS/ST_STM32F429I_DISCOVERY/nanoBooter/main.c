1d02005b3d30e2a0a3e9ecc1ade15617d426bab9
José Simões <jose.simoes@eclo.solutions>
2018-09-17 11:21:06 +0100

    Fix enumeration of config blocks when device has no config block
    - Add comment on booter main for targets that DO NOT have config block to stress that the address being used it's because there is no config block (in case someone needs to change that)
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_STM32F429I_DISCOVERY/nanoBooter/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F429I_DISCOVERY/nanoBooter/main.c
@@ -42,6 +42,7 @@ int main(void) {
   if (!palReadPad(GPIOA, GPIOA_BUTTON))
   {
     // check for valid CLR image 
+    // this target DOES NOT have configuration block, so we need to use the __nanoImage_end__ address here
     if(CheckValidCLRImage((uint32_t)&__nanoImage_end__))
     {
       // there seems to be a valid CLR image
