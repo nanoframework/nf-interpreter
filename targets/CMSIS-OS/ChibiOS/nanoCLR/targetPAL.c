bdba390c251a008d36e523bc0f6aa13bc8a53c96
José Simões <jose.simoes@eclo.solutions>
2018-09-19 11:54:11 +0100

    Fix STM32 series defines throughout the code
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/nanoCLR/targetPAL.c
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/targetPAL.c
@@ -5,7 +5,7 @@
 
 #include <hal.h>
 
-#if defined(STM32L0xx_MCUCONF) || defined(STM32F0xx_MCUCONF) || defined(STM32F4xx_MCUCONF) || defined(STM32F7xx_MCUCONF) || defined(STM32H7xx_MCUCONF)
+#if defined(STM32L0XX) || defined(STM32F0XX) || defined(STM32F4XX) || defined(STM32F7XX) || defined(STM32H7XX)
 
 stm32_gpio_t* gpioPort[] = { GPIOA, GPIOB
 #if STM32_HAS_GPIOC
