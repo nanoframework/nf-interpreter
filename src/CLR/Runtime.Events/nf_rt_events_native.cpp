30f932e8bc1f585275ae6e4db72cf43dc90ae4cd
José Simões <jose.simoes@eclo.solutions>
2018-10-01 09:31:18 +0100

    commitMessage

--- src/CLR/Runtime.Events/nf_rt_events_native.cpp
+++ src/CLR/Runtime.Events/nf_rt_events_native.cpp
@@ -54,5 +54,5 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_nanoFramework_Runtime_Event
     "nanoFramework.Runtime.Events", 
     0x9ACFB207,
     method_lookup,
-    { 1, 0, 0, 0 }
+    { 1, 0, 0, 200 }
 };
