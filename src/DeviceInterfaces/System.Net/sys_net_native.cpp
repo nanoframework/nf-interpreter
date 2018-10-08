0992f105470c4e7857500238d472775b625e3442
nfbot <nfbot@users.noreply.github.com>
2018-10-08 09:04:59 +0100

    normalize line endings (#899)

--- src/DeviceInterfaces/System.Net/sys_net_native.cpp
+++ src/DeviceInterfaces/System.Net/sys_net_native.cpp
@@ -1,4 +1,4 @@
-//
+﻿//
 // Copyright (c) 2018 The nanoFramework project contributors
 // Portions Copyright (c) Microsoft Corporation.  All rights reserved.
 // See LICENSE file in the project root for full license information.
