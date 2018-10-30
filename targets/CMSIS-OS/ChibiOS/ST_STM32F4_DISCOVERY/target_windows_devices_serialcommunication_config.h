df0504021bd23bac969d7aed2e92a417e5d74bc8
José Simões <josesimoes@users.noreply.github.com>
2018-10-30 18:52:48 +0000

    Rework inclusion of UART declarations in STM32 (#951)

---
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F4_DISCOVERY/target_windows_devices_serialcommunication_config.h
@@ -0,0 +1,31 @@
+//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+///////////
+// UART2 //
+///////////
+
+// enable USART2
+#define NF_SERIAL_COMM_STM32_UART_USE_USART2    TRUE
+
+// buffers size
+// tx buffer size: 256 bytes
+#define UART2_TX_SIZE  256
+// rx buffer size: 256 bytes
+#define UART2_RX_SIZE  256
+
+
+///////////
+// UART3 //
+///////////
+
+// enable USART3
+#define NF_SERIAL_COMM_STM32_UART_USE_USART3    TRUE
+
+// buffers size
+// tx buffer size: 256 bytes
+#define UART3_TX_SIZE  256
+// rx buffer size: 256 bytes
+#define UART3_RX_SIZE  256
