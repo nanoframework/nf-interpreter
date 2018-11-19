6d36048b2cfecf8cafe6fe692752f7020cc38697
nfbot <nfbot@users.noreply.github.com>
2018-11-19 22:02:21 +0000

    Update Windows.Devices.Spi version to 1.0.2-preview-011
    [version update]

--- targets/FreeRTOS/ESP32_DevKitC/nanoCLR/Windows.Devices.Spi/win_dev_spi_native.cpp
+++ targets/FreeRTOS/ESP32_DevKitC/nanoCLR/Windows.Devices.Spi/win_dev_spi_native.cpp
@@ -69,5 +69,5 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_Spi =
     "Windows.Devices.Spi", 
     0x83B879F7,
     method_lookup,
-    { 1, 0, 2, 7 }
+    { 1, 0, 2, 11 }
 };
