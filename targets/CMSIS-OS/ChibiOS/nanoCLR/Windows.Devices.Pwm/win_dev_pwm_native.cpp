0d552360ea7bbcf6d861951c0af0943368b8c898
José Simões <josesimoes@users.noreply.github.com>
2018-11-21 19:33:52 +0000

    Update Windows.Devices.Pwm version

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Pwm/win_dev_pwm_native.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Pwm/win_dev_pwm_native.cpp
@@ -58,5 +58,5 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_Pwm =
     "Windows.Devices.Pwm", 
     0xBBD6237A,
     method_lookup,
-    { 1, 0, 0, 0 }
+    { 1, 0, 2, 8 }
 };
