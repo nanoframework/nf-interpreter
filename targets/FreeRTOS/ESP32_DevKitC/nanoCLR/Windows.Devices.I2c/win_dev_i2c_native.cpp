bfc8aea50ee3f83ea5dcb2a3d67b61c0cd32de59
nfbot <nfbot@users.noreply.github.com>
2018-11-23 18:50:26 +0000

    Update Windows.Devices.I2c version to 1.0.2-preview-020 (#1015)

--- targets/FreeRTOS/ESP32_DevKitC/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native.cpp
+++ targets/FreeRTOS/ESP32_DevKitC/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native.cpp
@@ -62,7 +62,7 @@ static const CLR_RT_MethodHandler method_lookup[] =
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_I2c =
 {
     "Windows.Devices.I2c", 
-    0x7F0ABA1C,
+    0x0C4EA1B2,
     method_lookup,
     { 1, 0, 2, 20 }
 };
