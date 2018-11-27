2941b844665a90f45759eb6268396b0413293009
nfbot <nfbot@users.noreply.github.com>
2018-11-27 14:35:46 +0000

    Update Windows.Devices.Spi version to 1.0.2-preview-015 ***NO_CI***
    [version update]

--- targets/os/win32/nanoCLR/Windows.Devices.Spi/win_dev_spi_native.cpp
+++ targets/os/win32/nanoCLR/Windows.Devices.Spi/win_dev_spi_native.cpp
@@ -68,7 +68,7 @@ static const CLR_RT_MethodHandler method_lookup[] =
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_Spi =
 {
     "Windows.Devices.Spi", 
-    0x83B879F7,
+    0x3DE083A4,
     method_lookup,
-    { 1, 0, 2, 11 }
+    { 1, 0, 2, 15 }
 };
