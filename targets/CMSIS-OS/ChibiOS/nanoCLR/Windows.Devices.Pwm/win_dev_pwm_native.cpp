6975880785a13ba7ed15c1074d1e3def847bf5b3
nfbot <nfbot@users.noreply.github.com>
2018-11-27 11:57:27 +0000

    Update Windows.Devices.Pwm version to 1.0.2-preview-013 (#1029)

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Pwm/win_dev_pwm_native.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Pwm/win_dev_pwm_native.cpp
@@ -62,5 +62,5 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_Pwm =
     "Windows.Devices.Pwm", 
     0x84B1FBF8,
     method_lookup,
-    { 1, 0, 2, 12 }
+    { 1, 0, 2, 13 }
 };
