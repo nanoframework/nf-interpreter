d5baf76331e27239030693433799a85ea765a096
Suriya Kuernkuob <geminids14@hotmail.com>
2018-12-21 10:35:09 +0700

    NETDUINO3_WIFI: nanoFramework.Devices.Can Implemented
    nanoFramework.Devices.Can v.1.0.1

---
+++ targets/CMSIS-OS/ChibiOS/NETDUINO3_WIFI/target_nf_devices_can_config.h
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+
+//////////
+// CAN1 //
+//////////
+
+// enable CAN1
+#define NF_CAN_STM32_CAN_USE_CAN1    TRUE
+
+// buffers size
+#define CAN1_RX_BUFFER_SIZE  16
+
+//////////
+// CAN2 //
+//////////
+
+// enable CAN2
+#define NF_CAN_STM32_CAN_USE_CAN2    TRUE
+
+// buffers size
+#define CAN2_RX_BUFFER_SIZE  16
