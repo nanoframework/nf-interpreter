8a5f82d23eef7357560547d05b5b7c6a707311e1
José Simões <josesimoes@users.noreply.github.com>
2018-12-21 12:09:00 +0000

    Work on power down sequence (#1121)

--- src/HAL/Include/nanoHAL_Power.h
+++ src/HAL/Include/nanoHAL_Power.h
@@ -6,6 +6,25 @@
 #ifndef _NANOHAL_POWER_H_
 #define _NANOHAL_POWER_H_ 1
 
-#include <targetHAL_Power.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum PowerLevel
+{
+    PowerLevel__Active,
+    PowerLevel__SelectiveOff,
+    PowerLevel__Sleep,
+    PowerLevel__DeepSleep,
+    PowerLevel__Off
+}PowerLevel_type;
+
+bool CPU_IsSoftRebootSupported();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //_NANOHAL_POWER_H_
