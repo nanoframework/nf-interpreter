b993a4e7fb884256a5ea87da3ed95cd38c9f65de
José Simões <jose.simoes@eclo.solutions>
2019-01-02 16:51:31 +0000

    Update STM32.Hardware
    - Add new methods to Hardware.Stm32.Power class.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/nanoCLR/nanoFramework.Hardware.Stm32/nf_hardware_stm32_native.h
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/nanoFramework.Hardware.Stm32/nf_hardware_stm32_native.h
@@ -10,6 +10,16 @@
 #include <nanoCLR_Runtime.h>
 #include <hal.h>
 
+struct Library_nf_hardware_stm32_native_nanoFramework_Hardware_Stm32_Power
+{
+    NANOCLR_NATIVE_DECLARE(DisableWakeupPin___STATIC__VOID__nanoFrameworkHardwareStm32PowerWakeupPin);
+    NANOCLR_NATIVE_DECLARE(EnableWakeupPin___STATIC__VOID__nanoFrameworkHardwareStm32PowerWakeupPin);
+    NANOCLR_NATIVE_DECLARE(EnterStandbyMode___STATIC__VOID);
+
+    //--//
+
+};
+
 struct Library_nf_hardware_stm32_native_nanoFramework_Hardware_Stm32_BackupMemory
 {
     static const int FIELD_STATIC___size = 0;
@@ -23,14 +33,6 @@ struct Library_nf_hardware_stm32_native_nanoFramework_Hardware_Stm32_BackupMemor
     // need to address the fact that the majority of the series have 32 bits wide registers and F1 has 16bits wide.
     // the backup size is calculated with the number of available backup registers and their size (in bytes)
     static const int BACKUP_SIZE = RTC_BKP_NUMBER * sizeof(RTC_BKP0R_Msk);
-};
-
-struct Library_nf_hardware_stm32_native_nanoFramework_Hardware_Stm32_Power
-{
-    NANOCLR_NATIVE_DECLARE(EnterStandbyMode___STATIC__VOID);
-
-    //--//
-
 };
 
 struct Library_nf_hardware_stm32_native_nanoFramework_Hardware_Stm32_RTC
