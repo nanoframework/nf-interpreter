644c8c8cb48dc5df11ed99d229e2cc0e5421f444
José Simões <jose.simoes@eclo.solutions>
2019-01-02 12:29:42 +0000

    Fix I2C config.
    - Add missing target configurations.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F439ZI/target_windows_devices_i2c_config.cpp
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO144_F439ZI/target_windows_devices_i2c_config.cpp
@@ -5,6 +5,26 @@
 
 #include "win_dev_i2c_native.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+// I2C1 //
+//////////
+
+// pin configuration for I2C1
+// port for SCL pin is: GPIOB
+// port for SDA pin is: GPIOB
+// SCL pin: is GPIOB_8
+// SDA pin: is GPIOB_9
+// GPIO alternate pin function is 4 (see alternate function mapping table in device datasheet)
+I2C_CONFIG_PINS(1, GPIOB, GPIOB, 8, 9, 4)
+
+//////////
+// I2C2 //
+//////////
+
+// pin configuration for I2C1
+// port for SCL pin is: GPIOF
+// port for SDA pin is: GPIOF
+// SCL pin: is 1
+// SDA pin: is 0
+// GPIO alternate pin function is 4 (see alternate function mapping table in device datasheet)
+I2C_CONFIG_PINS(2, GPIOF, GPIOF, 1, 0, 4)
