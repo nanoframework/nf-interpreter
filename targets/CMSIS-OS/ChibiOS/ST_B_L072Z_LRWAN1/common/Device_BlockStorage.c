560618d3957f822cdba562e21a683185f8b27483
José Simões <jose.simoes@eclo.solutions>
2019-01-02 12:42:06 +0000

    Increase CLR deployment area
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/common/Device_BlockStorage.c
+++ targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/common/Device_BlockStorage.c
@@ -10,8 +10,8 @@
 const BlockRange BlockRange1[] = 
 {
     { BlockRange_BLOCKTYPE_BOOTSTRAP ,   0    , 67   },        // 0x08000000 nanoBooter          
-    { BlockRange_BLOCKTYPE_CODE      ,   68   , 1110 },        // 0x08002200 nanoCLR
-    { BlockRange_BLOCKTYPE_DEPLOYMENT,   1111 , 1535 }         // 0x08022B80 deployment  
+    { BlockRange_BLOCKTYPE_CODE      ,   68   , 1111 },        // 0x08002200 nanoCLR
+    { BlockRange_BLOCKTYPE_DEPLOYMENT,   1112 , 1535 }         // 0x08022C00 deployment  
 };
 
 const BlockRegionInfo BlockRegions[] = 
