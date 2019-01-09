296de31b681b30da3c6280092e5f5409a62e01e9
José Simões <jose.simoes@eclo.solutions>
2019-01-09 23:59:23 +0000

    Enable timers for PWM
    - Had to change ST timer to TIM5 too as TIM2 is required for PB3 with TIM2_CH2.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/MXCHIP_AZ3166/nanoCLR/mcuconf.h
+++ targets/CMSIS-OS/ChibiOS/MXCHIP_AZ3166/nanoCLR/mcuconf.h
@@ -177,8 +177,8 @@
  */
 #define STM32_PWM_USE_ADVANCED              FALSE
 #define STM32_PWM_USE_TIM1                  FALSE
-#define STM32_PWM_USE_TIM2                  FALSE
-#define STM32_PWM_USE_TIM3                  FALSE
+#define STM32_PWM_USE_TIM2                  TRUE
+#define STM32_PWM_USE_TIM3                  TRUE
 #define STM32_PWM_USE_TIM4                  FALSE
 #define STM32_PWM_USE_TIM5                  FALSE
 #define STM32_PWM_USE_TIM9                  FALSE
@@ -235,7 +235,7 @@
  * ST driver system settings.
  */
 #define STM32_ST_IRQ_PRIORITY               8
-#define STM32_ST_USE_TIMER                  2
+#define STM32_ST_USE_TIMER                  5
 
 /*
  * UART driver system settings.
