55466f77980b7f93a91a3964e98db90b4e930d6f
nfbot <nanoframework@outlook.com>
2019-02-04 16:40:40 +0000

    Update nanoFramework.Windows.Devices.Adc version to 1.1.3 ***NO_CI***

--- targets/FreeRTOS_ESP32/ESP32_WROOM_32/nanoCLR/Windows.Devices.Adc/win_dev_adc_native.cpp
+++ targets/FreeRTOS_ESP32/ESP32_WROOM_32/nanoCLR/Windows.Devices.Adc/win_dev_adc_native.cpp
@@ -61,5 +61,5 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_Adc =
     "Windows.Devices.Adc", 
     0xE5D11571,
     method_lookup,
-    { 1, 1, 3, 0 }
+    { 1, 1, 3, 1 }
 };
