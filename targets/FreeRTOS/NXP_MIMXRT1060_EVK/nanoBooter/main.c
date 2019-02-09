b0d64a45c7d0669210bdcfdfa48cb9659759c324
Mateusz Klatecki <mateusz.klatecki@gc5.pl>
2019-02-09 23:25:47 +0100

    Configure HEAP in nanoBooter in the same way as in nanoCLR

--- targets/FreeRTOS/NXP_MIMXRT1060_EVK/nanoBooter/main.c
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/nanoBooter/main.c
@@ -14,6 +14,10 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+//configure heap memory
+__attribute__((section(".noinit.$SRAM_OC.ucHeap")))
+uint8_t ucHeap[configTOTAL_HEAP_SIZE];
+
 #define LED_GPIO GPIO1
 #define LED_GPIO_PIN (9U)
 
