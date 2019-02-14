deeb19298a0eee8bf9427aa6fbae0dd99cd7983c
Mateusz Klatecki <47118568+MateuszKlatecki@users.noreply.github.com>
2019-02-14 13:05:07 +0100

    🚧 Initial implementation of nanoCLR in iMXRT10xxx 🚧 (#1208)

---
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/nanoCLR/Memory.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+#include <nanoHAL.h>
+
+// Define an area for the CLR managed heap
+#if !defined(MANAGED_HEAP_SIZE)
+#define MANAGED_HEAP_SIZE  (64*1024)
+#endif
+
+uint32_t managedHeap[MANAGED_HEAP_SIZE/ sizeof(uint32_t)];
+
+void HeapLocation(unsigned char*& baseAddress, unsigned int& sizeInBytes)
+{
+    NATIVE_PROFILE_PAL_HEAP();
+
+    baseAddress = (unsigned char*)&managedHeap;
+    sizeInBytes = sizeof(managedHeap);
+}
