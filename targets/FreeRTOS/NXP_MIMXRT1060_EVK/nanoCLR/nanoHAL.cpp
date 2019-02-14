64292ee483f5266ffe7e6d673bab2e0422b52362
Mateusz Klatecki <47118568+MateuszKlatecki@users.noreply.github.com>
2019-02-14 13:05:07 +0100

    🚧 Initial implementation of nanoCLR in iMXRT10xxx 🚧 (#1208)

---
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/nanoCLR/nanoHAL.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <nanoHAL.h>
+
+void HAL_EnterBooterMode()
+{
+}
+
+bool g_fDoNotUninitializeDebuggerPort = false;
