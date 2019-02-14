fa77b87babf555c6975534db26a9440bc3bd9a1e
Mateusz Klatecki <47118568+MateuszKlatecki@users.noreply.github.com>
2019-02-14 13:05:07 +0100

    🚧 Initial implementation of nanoCLR in iMXRT10xxx 🚧 (#1208)

---
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/nanoCLR/targetPAL_Time.h
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _TARGET_PAL_TIME_H_
+#define _TARGET_PAL_TIME_H_ 1
+
+
+#endif //_TARGET_PAL_TIME_H_
