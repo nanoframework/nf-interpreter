08076a526ad8a7a597a923d68d7e16ca1fd053ff
piwi1263 <piwi1263@gmail.com>
2019-02-18 20:01:20 +0100

    Enabled uart for WIFI
    - update serial config to support for UART5 which is for the ESP8266 WIFI add-on board
    
    Signed-off-by: piwi1263 <piwi1263@gmail.com>

--- targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/target_windows_devices_serialcommunication_config.h
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/target_windows_devices_serialcommunication_config.h
@@ -4,6 +4,19 @@
 //
 
 ///////////
+// UART5 //
+///////////
+
+// enable UART5
+#define NF_SERIAL_COMM_STM32_UART_USE_UART5     TRUE
+
+// buffers size
+// tx buffer size: 256 bytes
+#define UART5_TX_SIZE  256
+// rx buffer size: 256 bytes
+#define UART5_RX_SIZE  256
+
+///////////
 // UART6 //
 ///////////
 
