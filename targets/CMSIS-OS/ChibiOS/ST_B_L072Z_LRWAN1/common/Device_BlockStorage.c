689ab82e2cd8ab02e68a381841e4ae283d763807
José Simões <jose.simoes@eclo.solutions>
2019-02-19 15:47:28 +0000

    Update BlockStorage config to new struct
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/common/Device_BlockStorage.c
+++ targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/common/Device_BlockStorage.c
@@ -17,6 +17,7 @@ const BlockRange BlockRange1[] =
 const BlockRegionInfo BlockRegions[] = 
 {
     {
+        (0),                                // no attributes for this region
         0x08000000,                         // start address for block region
         1536,                               // total number of blocks in this region
         0x80,                               // total number of bytes per block
@@ -28,6 +29,7 @@ const BlockRegionInfo BlockRegions[] =
 const DeviceBlockInfo Device_BlockInfo =
 {
     (MediaAttribute_SupportsXIP),
+    2,                                      // UINT32 BytesPerSector
     ARRAYSIZE_CONST_EXPR(BlockRegions),     // UINT32 NumRegions;
     (BlockRegionInfo*)BlockRegions,         // const BlockRegionInfo* pRegions;
 };
