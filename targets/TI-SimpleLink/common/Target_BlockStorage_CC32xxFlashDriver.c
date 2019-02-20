41c3d05bfbed331360d506c4dda7412771e994da
José Simões <jose.simoes@eclo.solutions>
2019-02-20 19:02:43 +0000

    Add Flash driver
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/TI-SimpleLink/common/Target_BlockStorage_CC32xxFlashDriver.c
+++ targets/TI-SimpleLink/common/Target_BlockStorage_CC32xxFlashDriver.c
@@ -5,6 +5,13 @@
 
 #include <Target_BlockStorage_CC32xxFlashDriver.h>
 
+// includes from SimpleLink
+#include "inc/hw_types.h"
+#include <flash.h>
+
+// local defines
+#define FLASH_ERASED_WORD        0x0FFFFFFFFU
+
 bool CC32xxFlashDriver_InitializeDevice(void* context)
 {
 	(void)context;
@@ -39,20 +46,46 @@ bool CC32xxFlashDriver_Read(void* context, ByteAddress startAddress, unsigned in
 bool CC32xxFlashDriver_Write(void* context, ByteAddress startAddress, unsigned int numBytes, unsigned char* buffer, bool readModifyWrite)
 {
 	(void)context;
+	(void)readModifyWrite;
+
+	if(FlashProgram((unsigned long*)buffer, (unsigned long) startAddress, (unsigned long) numBytes) == 0)
+	{
+		return true;
+	}
 	
-	return true;
+	return false;
 }
-   
+
 bool CC32xxFlashDriver_IsBlockErased(void* context, ByteAddress blockAddress, unsigned int length)
 {
 	(void)context;
 
-	return true;
+    unsigned long* cursor = (unsigned long *)blockAddress;
+    unsigned long* endAddress = (unsigned long *)(blockAddress + length);
+
+    // an erased flash address has to read FLASH_ERASED_WORD
+    // OK to check by word (32 bits) because the erase is performed by 'block' whose size is word multiple
+    while(cursor < endAddress)
+    {
+        if(*cursor++ != FLASH_ERASED_WORD)
+        {
+            // found an address with something other than FLASH_ERASED_WORD!!
+            return false;
+        }
+    }
+    
+    // reached here so the block must be erased
+    return true;
 }
 
 bool CC32xxFlashDriver_EraseBlock(void* context, ByteAddress address)
 {
 	(void)context;
 
-	return true;   
+	if(FlashErase((unsigned long) address) == 0)
+	{
+		return true;
+	}
+
+	return false;
 }
