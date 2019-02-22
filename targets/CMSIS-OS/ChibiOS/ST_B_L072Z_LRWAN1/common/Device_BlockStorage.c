5947940a052fee23fbcf9c43355d6b2be189b0aa
José Simões <jose.simoes@eclo.solutions>
2019-02-22 11:46:13 +0000

    Increase CLR region
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/common/Device_BlockStorage.c
+++ targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/common/Device_BlockStorage.c
@@ -10,8 +10,8 @@
 const BlockRange BlockRange1[] = 
 {
     { BlockRange_BLOCKTYPE_BOOTSTRAP ,   0    , 67   },        // 0x08000000 nanoBooter          
-    { BlockRange_BLOCKTYPE_CODE      ,   68   , 1112 },        // 0x08002200 nanoCLR
-    { BlockRange_BLOCKTYPE_DEPLOYMENT,   1113 , 1535 }         // 0x08022C80 deployment  
+    { BlockRange_BLOCKTYPE_CODE      ,   68   , 1123 },        // 0x08002200 nanoCLR
+    { BlockRange_BLOCKTYPE_DEPLOYMENT,   1124 , 1535 }         // 0x08023200 deployment  
 };
 
 const BlockRegionInfo BlockRegions[] = 
