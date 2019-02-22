b579b08f6bed92cf97d4a78e2c84a609325c2801
José Simões <jose.simoes@eclo.solutions>
2019-02-22 19:36:13 +0000

    Enable all GPIO pins on the board
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/TI-SimpleLink/TI_CC3220SF_LAUNCHXL/nanoCLR/ti_drivers_net_wifi_Config.c
+++ targets/TI-SimpleLink/TI_CC3220SF_LAUNCHXL/nanoCLR/ti_drivers_net_wifi_Config.c
@@ -1,20 +1,23 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Texas Instruments Incorporated.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
 
-/* TI-DRIVERS Header files */
+// TI-DRIVERS Header files
 #include <ti/drivers/net/wifi/simplelink.h>
 
-/*
- *  =============================== SimpleLink Wifi ===============================
- */
 
-/*
- *  ======== SimpleLinkWifi_config ========
- */
+/////////////////////////////////////////////////
+// Default Wi-Fi configuration for this target //
+/////////////////////////////////////////////////
+
 const SlWifiCC32XXConfig_t SimpleLinkWifiCC32XX_config = {
 
         .Mode = ROLE_STA,
 		.Ipv4Mode = SL_NETCFG_IPV4_STA_ADDR_MODE,
 		.ConnectionPolicy = SL_WLAN_CONNECTION_POLICY(1,0,0,1),
-        .PMPolicy = SL_WLAN_NORMAL_POLICY,
+        .PMPolicy = SL_WLAN_ALWAYS_ON_POLICY,//SL_WLAN_NORMAL_POLICY,
 		.MaxSleepTimeMS = 0,
 		.ScanPolicy = SL_WLAN_SCAN_POLICY(0,0),
 		.ScanIntervalInSeconds = 0,
