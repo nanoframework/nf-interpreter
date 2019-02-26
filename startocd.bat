6aaf2525bdef77b4e0ab3b2e631e16c65fe7654c
David <DavidAVarley@gmail.com>
2019-02-26 16:59:41 +1100

    Add alternate debug server settings by passing flag symbol to batch file

--- startocd.bat
+++ startocd.bat
@@ -1,5 +1,42 @@
 REM Starts OpenOCD - designed to be called from GDB, itself started from Visual Studio launch.vs.json
 REM If already running it will fail and exit, leaving the existing one to handle GDB requests
-REM Should be easy enough to expand to take a startup parameter, so we can use one file for multiple targets/configurations
-REM DAV 25FEB19
-start C:/Esp32_Tools/openocd-esp32/bin/openocd.exe -s C:/Esp32_Tools/openocd-esp32/share/openocd/scripts -f interface/jlink.cfg -f target/esp32.cfg -c "adapter_khz 3000"
+REM Template allows for multiple target interfaces and boards - add new ones as required
+REM DAV 26FEB19
+setlocal
+set cmd=C:/Esp32_Tools/openocd-esp32/bin/openocd.exe -s C:/Esp32_Tools/openocd-esp32/share/openocd/scripts
+
+if %1.==. goto ESP32_JLINK
+call :%1
+if %errorlevel% equ 1 goto :lblerror
+goto end
+
+REM Start OpenOCD
+:start
+start %cmd% %iface%
+goto :eof
+
+:end
+endlocal
+exit
+
+:lblerror
+mshta javascript:alert("Error: Unknown label %1");close();
+goto end
+
+REM Add additional interface/board combinations here, and add the label to the launch.vs.json file
+
+:ESP32_JLINK
+set iface=-f interface/jlink.cfg -f target/esp32.cfg -c "adapter_khz 3000"
+goto start
+
+:ESP32_OLIMEX
+set iface=-f interface/ftdi/olimex-arm-usb-ocd-h.cfg -f target/esp32.cfg -c "adapter_khz 3000"
+goto start
+
+:STM32_STLINK
+set iface=-f interface/stlink-v2-1.cfg -f board/stm32f7discovery.cfg
+goto start
+
+
+
+
