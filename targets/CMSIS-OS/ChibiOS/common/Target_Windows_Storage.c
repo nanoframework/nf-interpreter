d3ce44603ef0db930cf45ec86645d8ee46aa0e0c
José Simões <jose.simoes@eclo.solutions>
2019-02-27 16:40:49 +0000

    Work on Windows.Storage
    - Update CMake regarding storage options.
    - Add validations and sanity checks for storage and hardware options.
    - Add options and processing for USB MSD.
    - Rework Windos.Storage class lib code to stub USB MSD.
    - Add HAL_USBH_USE_MSD to target platform.
    - Update Azure Pipelines options for SD Card capable targets.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/common/Target_Windows_Storage.c
+++ targets/CMSIS-OS/ChibiOS/common/Target_Windows_Storage.c
@@ -3,147 +3,197 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include <Target_Windows_Storage.h>
+#include <hal.h>
 #include <target_windows_storage_config.h>
+#include <Target_Windows_Storage.h>
+
+// FS mounted and ready
+static bool fileSystemReady = false;
+
+///////////////////////////////////////////
+// code specific to USB MSD
+
+#if HAL_USE_SDC
 
 static FATFS SDC_FS;
 static SDCConfig SDC_CFG;
 
 // Card monitor timer
-static virtual_timer_t cardMonitorTimer;
-
+static virtual_timer_t sdCardMonitorTimer;
 // Debounce counter
-static unsigned debounceCounter;
-
-// FS mounted and ready
-static bool fileSystemReady = FALSE;
-
+static unsigned sdDebounceCounter;
 // Card event sources
-static event_source_t insertedEvent, removedEvent;
+static event_source_t sdInsertedEvent, sdRemovedEvent;
 
-/**
- * @brief   Insertion monitor timer callback function.
- *
- * @param[in] p         pointer to the @p BaseBlockDevice object
- *
- * @notapi
- */
-static void InsertionMonitorCallback(void *p) 
+// Insertion monitor timer callback function.
+static void SdcardInsertionMonitorCallback(void *p) 
 {
-  BaseBlockDevice *bbdp = p;
+    BaseBlockDevice *bbdp = p;
 
-  chSysLockFromISR();
+    chSysLockFromISR();
 
-  if (debounceCounter > 0) 
-  {
-    if (blkIsInserted(bbdp))
+    if (sdDebounceCounter > 0) 
     {
-      if (--debounceCounter == 0)
-      {
-        chEvtBroadcastI(&insertedEvent);
-      }
+        if (blkIsInserted(bbdp))
+        {
+            if (--sdDebounceCounter == 0)
+            {
+                chEvtBroadcastI(&sdInsertedEvent);
+            }
+        }
+        else
+        {
+            sdDebounceCounter = SDCARD_POLLING_INTERVAL;
+        }
     }
-    else
+    else 
     {
-      debounceCounter = POLLING_INTERVAL;
+        if (!blkIsInserted(bbdp))
+        {
+            sdDebounceCounter = SDCARD_POLLING_INTERVAL;
+            chEvtBroadcastI(&sdRemovedEvent);
+        }
     }
-  }
-  else 
-  {
-    if (!blkIsInserted(bbdp))
-    {
-      debounceCounter = POLLING_INTERVAL;
-      chEvtBroadcastI(&removedEvent);
-    }
-  }
 
-  chVTSetI(&cardMonitorTimer, TIME_MS2I(POLLING_DELAY), InsertionMonitorCallback, bbdp);
-  chSysUnlockFromISR();
+    chVTSetI(&sdCardMonitorTimer, TIME_MS2I(SDCARD_POLLING_DELAY), SdcardInsertionMonitorCallback, bbdp);
+    chSysUnlockFromISR();
 }
 
 // Card monitor start
 static void StartCardMonitor(void *p)
 {
-  chEvtObjectInit(&insertedEvent);
-  chEvtObjectInit(&removedEvent);
+    chEvtObjectInit(&sdInsertedEvent);
+    chEvtObjectInit(&sdRemovedEvent);
 
-  chSysLock();
+    chSysLock();
 
-  debounceCounter = POLLING_INTERVAL;
-  chVTSetI(&cardMonitorTimer, TIME_MS2I(POLLING_DELAY), InsertionMonitorCallback, p);
+    sdDebounceCounter = SDCARD_POLLING_INTERVAL;
+    chVTSetI(&sdCardMonitorTimer, TIME_MS2I(SDCARD_POLLING_DELAY), SdcardInsertionMonitorCallback, p);
 
-  chSysUnlock();
+    chSysUnlock();
 }
 
 // Card insertion event handler
-static void InsertHandler(eventid_t id)
+static void SdcardInsertHandler(eventid_t id)
 {
-  FRESULT err;
+    FRESULT err;
 
-  (void)id;
+    (void)id;
 
-  /*
-   * On insertion SDC initialization and FS  mount.
-   */
-  if (sdcConnect(&SD_CARD_DRIVER))
-  {
-    return;
-  }
+    // On insertion SDC initialization and FS  mount
+    if (sdcConnect(&SD_CARD_DRIVER))
+    {
+        return;
+    }
 
-  err = f_mount(&SDC_FS, "D:", 1);
+    err = f_mount(&SDC_FS, SDCARD_DRIVE_LETTER, 1);
 
-  if (err != FR_OK)
-  {
-    osDelay(1000);
-
-    sdcDisconnect(&SD_CARD_DRIVER);
-    return;
-  }
-  else
-  {
-    fileSystemReady = TRUE;
-  }
-
-  if (fileSystemReady)
-  {
-    //****** Test - Create a file!
-    FIL fileObj;
-    err = f_open(&fileObj, "TestMessage.txt", FA_CREATE_ALWAYS);
-    f_close(&fileObj);
-      
     if (err != FR_OK)
     {
-      osDelay(1000);
+        osDelay(1000);
+
+        sdcDisconnect(&SD_CARD_DRIVER);
+        return;
     }
     else
     {
-
+        fileSystemReady = TRUE;
     }
-    //******* End Test
-  }
 }
 
 // Card removal event handler
-static void RemoveHandler(eventid_t id)
+static void SdcardRemoveHandler(eventid_t id)
 {
-  (void)id;
-  
-  sdcDisconnect(&SD_CARD_DRIVER);
+    (void)id;
 
-  fileSystemReady = FALSE;
+    sdcDisconnect(&SD_CARD_DRIVER);
+
+    fileSystemReady = FALSE;
 }
 
-const evhandler_t sdcardEventHandler[] = { InsertHandler, RemoveHandler };
-event_listener_t eventListener0, eventListener1;
+const evhandler_t sdcardEventHandler[] = { SdcardInsertHandler, SdcardRemoveHandler };
+event_listener_t sdEventListener0, sdEventListener1;
+
+#endif
+
+///////////////////////////////////////////
+
+///////////////////////////////////////////
+// code specific to USB MSD
+#if HAL_USBH_USE_MSD
+
+// Card event sources
+static event_source_t usbInsertedEvent;
+
+// Insertion monitor timer callback function.
+static void UsbMsdInsertionMonitorCallback(void *p) 
+{
+    BaseBlockDevice *bbdp = p;
+
+    chSysLockFromISR();
+
+    if (blkGetDriverState(bbdp) == BLK_ACTIVE)
+    {
+        // BLK: Active, connect
+        usbhmsdLUNConnect(bbdp);
+    }
+
+    if (blkGetDriverState(bbdp) != BLK_READY)
+    {
+        // BLK: Ready
+        chEvtBroadcastI(&usbInsertedEvent);
+    }
+
+    chVTSetI(&usbCardMonitorTimer, TIME_MS2I(USB_MSD_POLLING_DELAY), UsbMsdInsertionMonitorCallback, bbdp);
+    chSysUnlockFromISR();
+}
+
+// USB Mass storage device monitor
+static void StartUsbMsdMonitor(void *p)
+{
+    chEvtObjectInit(&usbInsertedEvent);
+
+    chSysLock();
+
+    usbDebounceCounter = USB_MSD_POLLING_INTERVAL;
+    chVTSetI(&usbCardMonitorTimer, TIME_MS2I(USB_MSD_POLLING_DELAY), UsbMsdInsertionMonitorCallback, p);
+
+    chSysUnlock();
+}
+
+// USB MSD insertion event handler
+static void UsbMsdInsertHandler(eventid_t id)
+{
+    (void)id;
+
+    // TODO
+}
+
+const evhandler_t usbEventHandler[] = { UsbMsdInsertHandler };
+event_listener_t usbEventListener0;
+
+#endif
 
 void Target_FileSystemInit()
 {
-  // activates the  SDC driver 2 using default configuration
-  sdcStart(&SD_CARD_DRIVER, &SDC_CFG);
+  #if HAL_USE_SDC
+    // activates the SDC driver using default configuration
+    sdcStart(&SD_CARD_DRIVER, &SDC_CFG);
 
-  // activates the card insertion monitor
-  StartCardMonitor(&SD_CARD_DRIVER);
+    // activates the card insertion monitor
+    StartCardMonitor(&SD_CARD_DRIVER);
 
-  chEvtRegister(&insertedEvent, &eventListener0, 0);
-  chEvtRegister(&removedEvent, &eventListener1, 1);
+    chEvtRegister(&sdInsertedEvent, &sdEventListener0, 0);
+    chEvtRegister(&sdRemovedEvent, &sdEventListener1, 1);
+  #endif
+
+  #if HAL_USBH_USE_MSD
+
+    // activates the USB insertion monitor
+    StartUsbMsdMonitor(&MSBLKD[0]);
+
+    chEvtRegister(&usbInsertedEvent, &usbEventListener0, 0);
+
+  #endif
+
 }
