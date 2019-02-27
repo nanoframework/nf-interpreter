09b41b75ec9e1b2cd134f0254890cec1f72770f9
José Simões <jose.simoes@eclo.solutions>
2019-02-27 15:34:42 +0000

    Fix FAT FS definitions and configs
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F469_NF/target_windows_storage_config.h
@@ -0,0 +1,7 @@
+//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+// the following macro maps the SD card driver
+#define SD_CARD_DRIVER      SDCD1
