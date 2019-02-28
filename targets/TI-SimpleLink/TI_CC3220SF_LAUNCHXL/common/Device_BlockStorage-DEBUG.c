cc3fd2056795098c95a74063c2684f665d5c2e1e
sjmneves <sergio.neves@eclo.solutions>
2019-02-28 09:49:57 +0000

    Increased CLR region
    - needed to increase for UART code

--- targets/TI-SimpleLink/TI_CC3220SF_LAUNCHXL/common/Device_BlockStorage-DEBUG.c
+++ targets/TI-SimpleLink/TI_CC3220SF_LAUNCHXL/common/Device_BlockStorage-DEBUG.c
@@ -11,8 +11,8 @@ const BlockRange BlockRange1[] =
 {
     // the 1st block is reserved for the flash header 
     // so we don't take it into account for the map
-    { BlockRange_BLOCKTYPE_CODE          ,   0  , 105 },           // 0x01000800 nanoCLR
-    { BlockRange_BLOCKTYPE_DEPLOYMENT    ,   106, 510 },           // 0x01035000 deployment
+    { BlockRange_BLOCKTYPE_CODE          ,   0  , 107 },           // 0x01000800 nanoCLR
+    { BlockRange_BLOCKTYPE_DEPLOYMENT    ,   108, 510 },           // 0x01036000 deployment
 };
 
 const BlockRegionInfo BlockRegions[] = 
