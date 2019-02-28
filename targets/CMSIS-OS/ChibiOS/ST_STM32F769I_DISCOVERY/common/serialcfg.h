1ed0b0238be1d964a53a7a6d98302e0904b79ff0
José Simões <jose.simoes@eclo.solutions>
2019-02-28 01:44:12 +0000

    ⚠️ Rework STM32F769I target to use serial debugger instead of serial over USB ⚠️ (#1225)

---
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/common/serialcfg.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef SERIALCFG_H
+#define SERIALCFG_H
+
+// define which serial driver the Wire Protocol will be using 
+#define SERIAL_DRIVER           SD1
+
+#endif  /* SERIALCFG_H */
+
+/** @} */
+
