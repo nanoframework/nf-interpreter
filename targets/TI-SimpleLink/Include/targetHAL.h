c303555541df26c249d57c4736458274f413e08d
José Simões <jose.simoes@eclo.solutions>
2019-03-14 17:45:33 +0000

    Add define for activity heart beat
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/TI-SimpleLink/Include/targetHAL.h
+++ targets/TI-SimpleLink/Include/targetHAL.h
@@ -7,6 +7,8 @@
 #define _TARGET_HAL_H_
 
 #include <target_board.h>
+#include <Board.h>
+#include <gpio.h>
 
 #define GLOBAL_LOCK(x)              portENTER_CRITICAL();
 #define GLOBAL_UNLOCK(x)            portEXIT_CRITICAL();
@@ -46,6 +48,20 @@
 #endif  // !defined(BUILD_RTM)
 
 #define NANOCLR_STOP() HARD_BREAKPOINT()
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// DEBUGGER HELPER                                                                                 //
+// The line bellow is meant to be used as helper on checking that the execution engine is running. //
+// This can be inferred by checking if Events_WaitForEvents loop is running.                       //
+// The implementation should is to be provided by each target at target_common.h.in                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#if defined(BUILD_RTM)
+    #define EVENTS_HEART_BEAT
+#else
+    #ifndef EVENTS_HEART_BEAT
+    #define EVENTS_HEART_BEAT __asm__ __volatile__ ("nop")
+    #endif // EVENTS_HEART_BEAT
+#endif
 
 extern int HeapBegin;
 extern int HeapEnd;
