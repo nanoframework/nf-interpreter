0887631e80fcae3a42fe558fcddbdc1eeb9766f2
José Simões <jose.simoes@eclo.solutions>
2019-03-15 16:41:35 +0000

    Add platform implementation of PLATFORM_DELAY
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/FreeRTOS/NXP_MIMXRT1060_EVK/include/targetHAL.h
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/include/targetHAL.h
@@ -13,6 +13,9 @@
 #define GLOBAL_UNLOCK(x);           portEXIT_CRITICAL();
 #define ASSERT_IRQ_MUST_BE_OFF()    // TODO need to determine if this needs implementation
 
+// platform dependent delay
+#define PLATFORM_DELAY(milliSecs)   vTaskDelay(milliSecs);
+
 // Definitions for Sockets/Network
 #define GLOBAL_LOCK_SOCKETS(x)       
 
