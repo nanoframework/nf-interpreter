b661d764a3be5f6941066eceed26f6c208c13405
José Simões <jose.simoes@eclo.solutions>
2019-03-15 15:24:18 +0000

    Update Debugger.cpp

--- src/CLR/Debugger/Debugger.cpp
+++ src/CLR/Debugger/Debugger.cpp
@@ -1018,7 +1018,7 @@ bool CLR_DBG_Debugger::Debugging_Execution_ChangeConditions( WP_Message* msg)
 
     // updating the debugging execution conditions requires sometime to propagate
     // make sure we allow enough time for that to happen
-    PLATFORM_DELAY(100);
+    OS_DELAY(100);
 
     if((msg->m_header.m_flags & WP_Flags_c_NonCritical) == 0)
     {
