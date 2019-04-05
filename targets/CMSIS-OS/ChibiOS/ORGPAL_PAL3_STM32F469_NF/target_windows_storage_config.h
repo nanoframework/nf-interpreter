1085798a7533287eda590120ecf063c23d3777c0
José Simões <jose.simoes@eclo.solutions>
2019-04-05 12:10:01 +0100

    Turn OFF SD Card on both boards

--- targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F469_NF/target_windows_storage_config.h
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F469_NF/target_windows_storage_config.h
@@ -3,11 +3,11 @@
 // See LICENSE file in the project root for full license information.
 //
 
-// the following macro maps the SD card driver
-#define SD_CARD_DRIVER      SDCD1
+// // the following macro maps the SD card driver
+// #define SD_CARD_DRIVER      SDCD1
 
-// maps the GPIO line connected to SD Card detect switch
-#define SDCARD_LINE_DETECT  LINE_SD_CARD_DETECT
+// // maps the GPIO line connected to SD Card detect switch
+// #define SDCARD_LINE_DETECT  LINE_SD_CARD_DETECT
 
 
 // the following macro maps the USB mass storage device driver
