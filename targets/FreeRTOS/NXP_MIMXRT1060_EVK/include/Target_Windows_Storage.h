71d23ebf735976f16edf95833b450db73f363b02
Mateusz Klatecki <47118568+MateuszKlatecki@users.noreply.github.com>
2019-04-05 12:27:36 +0200

    Fixed BlockStorage, added Reset function and GPIOs support (#1279)

---
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/include/Target_Windows_Storage.h
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _TARGET_WINDOWS_STORAGE_H_
+#define _TARGET_WINDOWS_STORAGE_H_ 1
+
+#endif  //_TARGET_WINDOWS_STORAGE_H_
