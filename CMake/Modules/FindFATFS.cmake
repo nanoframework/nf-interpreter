a2406795abdda6bb5bde4757dd83a607d47c1b5d
Mateusz Klatecki <mateusz.klatecki@gc5.pl>
2019-04-11 15:26:37 +0200

    FindFATFS cmake

---
+++ CMake/Modules/FindFATFS.cmake
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2019 The nanoFramework project contributors
+# See LICENSE file in the project root for full license information.
+#
+
+
+# List of the required FatFs include files.
+list(APPEND FATFS_INCLUDE_DIRS ${PROJECT_BINARY_DIR}/FatFS_Source/source)
+
+set(FATFS_SRCS
+
+    # fatfs
+    ff.c
+    ffunicode.c
+)
+
+foreach(SRC_FILE ${FATFS_SRCS})
+    set(FATFS_SRC_FILE SRC_FILE -NOTFOUND)
+    find_file(FATFS_SRC_FILE ${SRC_FILE}
+        PATHS 
+            ${PROJECT_BINARY_DIR}/FatFS_Source/source
+
+        CMAKE_FIND_ROOT_PATH_BOTH
+    )
+    # message("${SRC_FILE} >> ${FATFS_SRC_FILE}") # debug helper
+    list(APPEND FATFS_SOURCES ${FATFS_SRC_FILE})
+endforeach()
+
+##
+if(EXISTS "${CMAKE_BINARY_DIR}/FatFS_Source/source/ffconf.h")
+    file(RENAME ${CMAKE_BINARY_DIR}/FatFS_Source/source/ffconf.h ${CMAKE_BINARY_DIR}/FatFS_Source/source/_ffconf.h)
+endif()
+
+include(FindPackageHandleStandardArgs)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(FATFS DEFAULT_MSG FATFS_INCLUDE_DIRS FATFS_SOURCES)
