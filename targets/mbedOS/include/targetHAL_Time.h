809503790f66beb8e02985244adf7425da6e8b2d
networkfusion <robin+github@networkfusion.co.uk>
2019-04-15 17:17:33 +0100

    add time and block storage headers

---
+++ targets/mbedOS/include/targetHAL_Time.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _TARGET_HAL_TIME_H_
+#define _TARGET_HAL_TIME_H_ 1
+
+#include <cmsis_os.h>
+
+#define HAL_Time_CurrentSysTicks           osKernelSysTick
+
+#endif //_TARGET_HAL_TIME_H_
