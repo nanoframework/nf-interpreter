2af5252e4f3f2c3a04ae3b30361d109b21b91651
José Simões <jose.simoes@eclo.solutions>
2019-04-16 09:37:05 +0100

    Enable COM ports on F7

--- targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_windows_devices_serialcommunication_config.cpp
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_windows_devices_serialcommunication_config.cpp
@@ -6,6 +6,52 @@
 #include "target_windows_devices_serialcommunication_config.h"
 #include <win_dev_serial_native.h>
 
+///////////
+// UART2 //
+///////////
+
+UART_CONFIG_PINS(2, GPIOD, GPIOD, 5, 6, 7)
+
+// buffers
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart2_TxBuffer[UART2_TX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart2_RxBuffer[UART2_RX_SIZE];
+
+// initialization for UART2
+UART_INIT(2, UART2_TX_SIZE, UART2_RX_SIZE)
+
+// un-initialization for UART2
+UART_UNINIT(2)
+
+
+///////////
+// UART3 //
+///////////
+
+// pin configuration for UART3
+UART_CONFIG_PINS(3, GPIOB, GPIOB, 10, 11, 7)
+
+// buffers
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart3_TxBuffer[UART3_TX_SIZE];
+#if defined(__GNUC__)
+__attribute__((aligned (32)))
+#endif
+uint8_t Uart3_RxBuffer[UART3_RX_SIZE];
+
+// initialization for UART3
+UART_INIT(3, UART3_TX_SIZE, UART3_RX_SIZE)
+
+// un-initialization for UART3
+UART_UNINIT(3)
+
 
 ///////////
 // UART6 //
@@ -37,3 +83,27 @@ UART_INIT(6, UART6_TX_SIZE, UART6_RX_SIZE)
 
 // un-initialization for UART6
 UART_UNINIT(6)
+
+// ///////////
+// // UART7 //
+// ///////////
+
+// // pin configuration for UART7
+// UART_CONFIG_PINS(7, GPIOF, GPIOF, 7, 6, 8)
+
+// // buffers
+// #if defined(__GNUC__)
+// __attribute__((aligned (32)))
+// #endif
+// uint8_t Uart7_TxBuffer[UART7_TX_SIZE];
+
+// #if defined(__GNUC__)
+// __attribute__((aligned (32)))
+// #endif
+// uint8_t Uart7_RxBuffer[UART7_RX_SIZE];
+
+// // initialization for UART7
+// UART_INIT(7, UART7_TX_SIZE, UART7_RX_SIZE)
+
+// // un-initialization for UART7
+// UART_UNINIT(7)
--- targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_windows_devices_serialcommunication_config.h
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_windows_devices_serialcommunication_config.h
@@ -2,6 +2,29 @@
 // Copyright (c) 2017 The nanoFramework project contributors
 // See LICENSE file in the project root for full license information.
 //
+///////////
+// UART2 //
+///////////
+
+// enable USART2
+#define NF_SERIAL_COMM_STM32_UART_USE_USART2    TRUE
+
+// buffers size
+#define UART2_TX_SIZE  256
+#define UART2_RX_SIZE  256
+
+
+///////////
+// UART3 //
+///////////
+
+// enable USART3
+#define NF_SERIAL_COMM_STM32_UART_USE_USART3    TRUE
+
+// buffers size
+#define UART3_TX_SIZE  256
+#define UART3_RX_SIZE  256
+
 
 ///////////
 // UART6 //
@@ -15,3 +38,14 @@
 #define UART6_TX_SIZE  256
 // rx buffer size: 256 bytes
 #define UART6_RX_SIZE  256
+
+// ///////////
+// // UART7 //
+// ///////////
+
+// // enable UART7
+// #define NF_SERIAL_COMM_STM32_UART_USE_UART7    TRUE
+
+// // buffers size
+// #define UART7_TX_SIZE  256
+// #define UART7_RX_SIZE  256
