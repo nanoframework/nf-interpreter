b71ca52bf9637e7763e6fff852d4ff777962e8f3
José Simões <jose.simoes@eclo.solutions>
2019-04-17 14:50:39 +0100

    Update win_storage_native_Windows_Storage_StorageFile.cpp

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Storage/win_storage_native_Windows_Storage_StorageFile.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Storage/win_storage_native_Windows_Storage_StorageFile.cpp
@@ -3,35 +3,36 @@
 // See LICENSE file in the project root for full license information.
 //
 
+#include <ff.h>
 #include "win_storage_native.h"
 
 HRESULT Library_win_storage_native_Windows_Storage_StorageFile::CheckFileNative___STATIC__VOID__STRING(CLR_RT_StackFrame& stack)
 {
-   	NANOCLR_HEADER();
+    NANOCLR_HEADER();
 
-	const char* filePath;
-	FRESULT fr;
-        FILINFO fileInfo;
+    const char* filePath;
+    FRESULT fr;
+    FILINFO fileInfo;
 
-	// get a pointer to the file path
-	filePath = stack.Arg1().DereferenceString()->StringText();
+    // get a pointer to the file path
+    filePath = stack.Arg1().DereferenceString()->StringText();
 
-	fr = f_stat(filePath, &fileInfo);
-	
-	if(fr == FR_OK)
-	{
-	    // file exists, we are good
-	}
-	else if(fr == FR_NO_FILE)
-	{
-            // file doesn't exist
-            NANOCLR_SET_AND_LEAVE(CLR_E_FILE_NOT_FOUND);		
-	}
-	else
-	{
-	    // IO error
-	    NANOCLR_SET_AND_LEAVE(CLR_E_FILE_IO);
-	}
+    fr = f_stat(filePath, &fileInfo);
 
-	NANOCLR_NOCLEANUP();
+    if(fr == FR_OK)
+    {
+        // file exists, we are good
+    }
+    else if(fr == FR_NO_FILE)
+    {
+        // file doesn't exist
+        NANOCLR_SET_AND_LEAVE(CLR_E_FILE_NOT_FOUND);		
+    }
+    else
+    {
+        // IO error
+	NANOCLR_SET_AND_LEAVE(CLR_E_FILE_IO);
+    }
+
+    NANOCLR_NOCLEANUP();
 }
