16849033d111c28a13cd7cc397477fd2cb747c43
José Simões <jose.simoes@eclo.solutions>
2019-04-17 14:37:41 +0100

    Implement CheckFileNative

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Storage/win_storage_native_Windows_Storage_StorageFile.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Storage/win_storage_native_Windows_Storage_StorageFile.cpp
@@ -3,21 +3,35 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include "win_storage_native_native.h"
-
+#include "win_storage_native.h"
 
 HRESULT Library_win_storage_native_Windows_Storage_StorageFile::CheckFileNative___STATIC__VOID__STRING(CLR_RT_StackFrame& stack)
 {
-	NANOCLR_HEADER();
+   	NANOCLR_HEADER();
 
 	const char* filePath;
+	FRESULT fr;
+        FILINFO fileInfo;
 
 	// get a pointer to the file path
 	filePath = stack.Arg1().DereferenceString()->StringText();
 
-	// TODO
-	NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
-
+	fr = f_stat(filePath, &fileInfo);
+	
+	if(fr == FR_OK)
+	{
+	    // file exists, we are good
+	}
+	else if(fr == FR_NO_FILE)
+	{
+            // file doesn't exist
+            NANOCLR_SET_AND_LEAVE(CLR_E_FILE_NOT_FOUND);		
+	}
+	else
+	{
+	    // IO error
+	    NANOCLR_SET_AND_LEAVE(CLR_E_FILE_IO);
+	}
 
 	NANOCLR_NOCLEANUP();
 }
