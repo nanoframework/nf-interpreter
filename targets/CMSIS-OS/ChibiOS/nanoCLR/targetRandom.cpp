16d0e16e18b8bab7787596b4e94cb2c987f62ee2
morali <jeremi.jasinski@gc5.pl>
2019-04-30 14:24:20 +0200

    restore chibios targetRandom file

--- targets/CMSIS-OS/ChibiOS/nanoCLR/targetRandom.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/targetRandom.cpp
@@ -3,39 +3,38 @@
 // See LICENSE file in the project root for full license information.
 //
 #include "Core.h"
-#include "fsl_trng.h"
-
-trng_config_t trngConfig;
+#include <hal.h>
+#include <hal_nf_community.h>
 
 void CLR_RT_Random::Initialize()
 {
-    TRNG_GetDefaultConfig(&trngConfig);
-    TRNG_Init(TRNG0, &trngConfig);
+    rngStart();
 }
 
 void CLR_RT_Random::Initialize( int seed )
 {
     (void)seed;
-    TRNG_GetDefaultConfig(&trngConfig);
-    TRNG_Init(TRNG0, &trngConfig);
+
+    rngStart();
 }
 
 uint32_t CLR_RT_Random::Next()
 {
-    uint32_t data = 0;
-    TRNG_GetRandomData(TRNG, &data, sizeof(data));
-    return data;
+    return rngGenerateRandomNumber();
 }
 
 double CLR_RT_Random::NextDouble()
 {
-    uint32_t data = 0;
-    TRNG_GetRandomData(TRNG, &data, sizeof(data));
     // the hardware generator returns a value between 0 - 0xFFFFFFFF
-    return ((double)data()) / ((double)0xFFFFFFFF);
+    return ((double)rngGenerateRandomNumber()) / ((double)0xFFFFFFFF);
 }
 
 void CLR_RT_Random::NextBytes(unsigned char* buffer, unsigned int count)
 {
-    TRNG_GetRandomData(TRNG, buffer, count);    
+    unsigned int i;
+
+    for(i = 0; i < count; i++)
+    {
+        buffer[i] = (unsigned char)rngGenerateRandomNumber();
+    }
 }
