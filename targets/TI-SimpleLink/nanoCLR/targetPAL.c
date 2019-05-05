c388990c7204e43af573e2487f1cb727f0d622f5
José Simões <jose.simoes@eclo.solutions>
2019-05-05 23:20:57 +0100

    Add workaround for linker optimizing functions called in asm code

--- targets/TI-SimpleLink/nanoCLR/targetPAL.c
+++ targets/TI-SimpleLink/nanoCLR/targetPAL.c
@@ -3,6 +3,15 @@
 // See LICENSE file in the project root for full license information.
 //
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+// Need to have calls to these two functions in C code.
+// Because they are called only on asm code, GCC linker with LTO option thinks they are not used and just removes them.
+// Having them called from a dummy function that is never called it a workaround for this.
+// The clean alternative would be to add the GCC attribute used in those functions, but that's not our code to touch.
+
+void dummyFunction(void) __attribute__((used));
+
+// Never called.
+void dummyFunction(void) {
+    vTaskSwitchContext();
+    localProgramStart();
+}
