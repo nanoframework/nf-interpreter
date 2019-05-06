088cc806cebb3c63956ae32a0bb4484601203650
José Simões <jose.simoes@eclo.solutions>
2019-05-06 14:53:12 +0100

    Add definition for SD Card detection line (#1301)

--- targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/target_windows_storage_config.h
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/target_windows_storage_config.h
@@ -5,3 +5,6 @@
 
 // maps the SD Card driver
 #define SD_CARD_DRIVER      SDCD2
+
+// maps the SD Card detect GPIO definition (in Target_Windows_Storage.c) to board GPIO line (in board.h)
+#define SDCARD_LINE_DETECT  LINE_SD_DETECT
