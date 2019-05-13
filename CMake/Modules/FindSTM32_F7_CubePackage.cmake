715a78868ac47a92a4ca45d94c629a1cec7a477c
José Simões <jose.simoes@eclo.solutions>
2019-05-13 09:58:11 +0100

    Rework CMakes for STM Cube package
    - Rename package and paths to include series name.
    - GitHub repo now access nF specific branch.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- CMake/Modules/FindSTM32_F7_CubePackage.cmake
+++ CMake/Modules/FindSTM32_F7_CubePackage.cmake
@@ -4,10 +4,10 @@
 #
 
 # set include directories
-list(APPEND STM32_F7_CubePackage_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/STM32_CubePackage_Source/Drivers/STM32F7xx_HAL_Driver/Inc")
+list(APPEND STM32F7_CubePackage_INCLUDE_DIRS "${PROJECT_BINARY_DIR}/STM32F7_CubePackage_Source/Drivers/STM32F7xx_HAL_Driver/Inc")
 
 # source files
-set(STM32_F7_CubePackage_SRCS
+set(STM32F7_CubePackage_SRCS
 
     # add HAL files here as required
 
@@ -15,19 +15,19 @@ set(STM32_F7_CubePackage_SRCS
     stm32f7xx_hal_qspi.c
 )
 
-foreach(SRC_FILE ${STM32_F7_CubePackage_SRCS})
-    set(STM32_F7_CubePackage_SRC_FILE SRC_FILE-NOTFOUND)
-    find_file(STM32_F7_CubePackage_SRC_FILE ${SRC_FILE}
+foreach(SRC_FILE ${STM32F7_CubePackage_SRCS})
+    set(STM32F7_CubePackage_SRC_FILE SRC_FILE-NOTFOUND)
+    find_file(STM32F7_CubePackage_SRC_FILE ${SRC_FILE}
         PATHS 
 
-            "${PROJECT_BINARY_DIR}/STM32_CubePackage_Source/Drivers/STM32F7xx_HAL_Driver/Src/"
+            "${PROJECT_BINARY_DIR}/STM32F7_CubePackage_Source/Drivers/STM32F7xx_HAL_Driver/Src/"
 
         CMAKE_FIND_ROOT_PATH_BOTH
     )
-    # message("${SRC_FILE} >> ${STM32_F7_CubePackage_SRC_FILE}") # debug helper
-    list(APPEND STM32_F7_CubePackage_SOURCES ${STM32_F7_CubePackage_SRC_FILE})
+    # message("${SRC_FILE} >> ${STM32F7_CubePackage_SRC_FILE}") # debug helper
+    list(APPEND STM32F7_CubePackage_SOURCES ${STM32F7_CubePackage_SRC_FILE})
 endforeach()
  
 include(FindPackageHandleStandardArgs)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(STM32_F7_CubePackage DEFAULT_MSG STM32_F7_CubePackage_INCLUDE_DIRS STM32_F7_CubePackage_SOURCES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(STM32F7_CubePackage DEFAULT_MSG STM32F7_CubePackage_INCLUDE_DIRS STM32F7_CubePackage_SOURCES)
