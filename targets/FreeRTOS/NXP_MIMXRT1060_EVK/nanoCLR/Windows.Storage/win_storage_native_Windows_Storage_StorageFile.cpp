c129e57e20a881b6a83c0eaa3c4323c158c67fdd
José Simões <jose.simoes@eclo.solutions>
2019-05-14 09:08:44 +0100

    Update Windows.Storage for NXP
    - Add missing stub calls for several classes.
    - Update assembly declarations.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ targets/FreeRTOS/NXP_MIMXRT1060_EVK/nanoCLR/Windows.Storage/win_storage_native_Windows_Storage_StorageFile.cpp
@@ -0,0 +1,36 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include "win_storage_native.h"
+
+HRESULT Library_win_storage_native_Windows_Storage_StorageFile::DeleteFileNative___VOID(CLR_RT_StackFrame& stack)
+{
+    NANOCLR_HEADER();
+    (void)stack;
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_win_storage_native_Windows_Storage_StorageFile::RenameFileNative___VOID__STRING(CLR_RT_StackFrame& stack)
+{
+    NANOCLR_HEADER();
+    (void)stack;
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_win_storage_native_Windows_Storage_StorageFile::GetFileFromPathNative___STATIC__WindowsStorageStorageFile__STRING__STRING( CLR_RT_StackFrame& stack )
+{
+    NANOCLR_HEADER();
+    (void)stack;
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
