5fb3243f67436898e3f04a4e0c0f41e6fc4e6e1f
José Simões <jose.simoes@eclo.solutions>
2019-05-17 09:24:28 +0100

    Update ADC config for F7 board

--- targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_windows_devices_adc_config.cpp
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_windows_devices_adc_config.cpp
@@ -8,14 +8,16 @@
 const NF_PAL_ADC_PORT_PIN_CHANNEL AdcPortPinConfig[] = {
     
     // ADC1
-    {1, GPIOA, 6, ADC_CHANNEL_IN6},
+    {1, GPIOA, 3, ADC_CHANNEL_IN3},
     {1, GPIOA, 4, ADC_CHANNEL_IN4},
+    {1, GPIOB, 0, ADC_CHANNEL_IN8},
+    {1, GPIOB, 1, ADC_CHANNEL_IN9},
     {1, GPIOC, 2, ADC_CHANNEL_IN12},
-    {1, GPIOF, 10, ADC_CHANNEL_IN8},
+    {1, GPIOC, 3, ADC_CHANNEL_IN13},
 
     // ADC3
     {3, GPIOF, 8, ADC_CHANNEL_IN6},
-    {3, GPIOB, 8, ADC_CHANNEL_IN7},
+    {3, GPIOF, 9, ADC_CHANNEL_IN7},
 
     // these are the internal sources, available only at ADC1
     {1, NULL, 0, ADC_CHANNEL_SENSOR},
