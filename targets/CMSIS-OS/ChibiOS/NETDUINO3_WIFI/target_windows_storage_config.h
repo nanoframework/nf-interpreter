d07a59b2fd8a5115369c5051752ce3fe2201e403
José Simões <jose.simoes@eclo.solutions>
2019-05-29 02:35:50 +0100

    Update storage config for NETDUINO3 (#1339)
    ***NO_CI***

--- targets/CMSIS-OS/ChibiOS/NETDUINO3_WIFI/target_windows_storage_config.h
+++ targets/CMSIS-OS/ChibiOS/NETDUINO3_WIFI/target_windows_storage_config.h
@@ -5,3 +5,9 @@
 
 // maps the SD Card driver
 #define SD_CARD_DRIVER      SDCD1
+
+// maps the SD Card detect GPIO definition (in Target_Windows_Storage.c) to board GPIO line (in board.h)
+#define SDCARD_LINE_DETECT  LINE_SD_DETECT
+
+// includes SPIFFS in storage
+#define USE_SPIFFS_FOR_STORAGE  FALSE
