d156d814bf6fe046d1c3da8a9dc6175cfca3fbc6
José Simões <jose.simoes@eclo.solutions>
2019-05-29 11:41:07 +0100

    Change serial flash to W25Q128
    - Update names, size configs, chip IDs and timmings.

--- targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_spiffs.h
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/target_spiffs.h
@@ -13,20 +13,20 @@
 
 #define SPIFFS_DRIVER_QSPI              (1)
 
-// @brief  W25Q32 Configuration   
-#define W25Q32_FLASH_SIZE                  0x400000  /* 32 MBits => 4MBytes */
-#define W25Q32_SECTOR_SIZE                 0x10000   /* 64 sectors of 64KBytes */
-#define W25Q32_SUBSECTOR_SIZE              0x1000    /* 1024 subsectors of 4kBytes */
-#define W25Q32_PAGE_SIZE                   0x100     /* 16384 pages of 256 bytes */
+// @brief  W25Q128 Configuration   
+#define W25Q128_FLASH_SIZE                  0x1000000 /* 128 MBits => 16MBytes */
+#define W25Q128_SECTOR_SIZE                 0x10000   /* 255 sectors of 64KBytes */
+#define W25Q128_SUBSECTOR_SIZE              0x1000    /* 4096 subsectors of 4kBytes */
+#define W25Q128_PAGE_SIZE                   0x100     /* 65536 pages of 256 bytes */
 
-#define W25Q32_DUMMY_CYCLES_READ           8
-#define W25Q32_DUMMY_CYCLES_READ_QUAD      8
+#define W25Q128_DUMMY_CYCLES_READ           8
+#define W25Q128_DUMMY_CYCLES_READ_QUAD      8
 
-#define W25Q32_BULK_ERASE_MAX_TIME         250000
-#define W25Q32_SECTOR_ERASE_MAX_TIME       3000
-#define W25Q32_SUBSECTOR_ERASE_MAX_TIME    800
+#define W25Q128_BULK_ERASE_MAX_TIME         250000
+#define W25Q128_SECTOR_ERASE_MAX_TIME       3000
+#define W25Q128_SUBSECTOR_ERASE_MAX_TIME    800
 
-// W25Q32 Commands   
+// W25Q128 Commands   
 /* Reset Operations */
 #define RESET_ENABLE_CMD                     0x66
 #define RESET_MEMORY_CMD                     0x99
@@ -87,55 +87,55 @@
 #define READ_OTP_ARRAY_CMD                   0x4B
 #define PROG_OTP_ARRAY_CMD                   0x42
 
-//  W25Q32 Registers   
+//  W25Q128 Registers   
 /* Status Register 1 */
-#define W25Q32_SR_WIP                      ((uint8_t)0x01)    /*!< Write in progress */
-#define W25Q32_SR_WREN                     ((uint8_t)0x02)    /*!< Write enable latch */
-#define W25Q32_SR_BLOCKPR                  ((uint8_t)0x5C)    /*!< Block protected against program and erase operations */
-#define W25Q32_SR_PRBOTTOM                 ((uint8_t)0x20)    /*!< Protected memory area defined by BLOCKPR starts from top or bottom */
-#define W25Q32_SR_SRWREN                   ((uint8_t)0x80)    /*!< Status register write enable/disable */
+#define W25Q128_SR_WIP                      ((uint8_t)0x01)    /*!< Write in progress */
+#define W25Q128_SR_WREN                     ((uint8_t)0x02)    /*!< Write enable latch */
+#define W25Q128_SR_BLOCKPR                  ((uint8_t)0x5C)    /*!< Block protected against program and erase operations */
+#define W25Q128_SR_PRBOTTOM                 ((uint8_t)0x20)    /*!< Protected memory area defined by BLOCKPR starts from top or bottom */
+#define W25Q128_SR_SRWREN                   ((uint8_t)0x80)    /*!< Status register write enable/disable */
 
 /* Status Register 2 */
-#define W25Q32_SR2_QE                      ((uint8_t)0x02)    /*!< Quad Enable */
+#define W25Q128_SR2_QE                      ((uint8_t)0x02)    /*!< Quad Enable */
 
 
 /* Nonvolatile Configuration Register */
-#define W25Q32_NVCR_LOCK                   ((uint16_t)0x0001) /*!< Lock nonvolatile configuration register */
-#define W25Q32_NVCR_DUAL                   ((uint16_t)0x0004) /*!< Dual I/O protocol */
-#define W25Q32_NVCR_QUAB                   ((uint16_t)0x0008) /*!< Quad I/O protocol */
-#define W25Q32_NVCR_RH                     ((uint16_t)0x0010) /*!< Reset/hold */
-#define W25Q32_NVCR_ODS                    ((uint16_t)0x01C0) /*!< Output driver strength */
-#define W25Q32_NVCR_XIP                    ((uint16_t)0x0E00) /*!< XIP mode at power-on reset */
-#define W25Q32_NVCR_NB_DUMMY               ((uint16_t)0xF000) /*!< Number of dummy clock cycles */
+#define W25Q128_NVCR_LOCK                   ((uint16_t)0x0001) /*!< Lock nonvolatile configuration register */
+#define W25Q128_NVCR_DUAL                   ((uint16_t)0x0004) /*!< Dual I/O protocol */
+#define W25Q128_NVCR_QUAB                   ((uint16_t)0x0008) /*!< Quad I/O protocol */
+#define W25Q128_NVCR_RH                     ((uint16_t)0x0010) /*!< Reset/hold */
+#define W25Q128_NVCR_ODS                    ((uint16_t)0x01C0) /*!< Output driver strength */
+#define W25Q128_NVCR_XIP                    ((uint16_t)0x0E00) /*!< XIP mode at power-on reset */
+#define W25Q128_NVCR_NB_DUMMY               ((uint16_t)0xF000) /*!< Number of dummy clock cycles */
 
 /* Volatile Configuration Register */
-#define W25Q32_VCR_WRAP                    ((uint8_t)0x03)    /*!< Wrap */
-#define W25Q32_VCR_XIP                     ((uint8_t)0x08)    /*!< XIP */
-#define W25Q32_VCR_NB_DUMMY                ((uint8_t)0xF0)    /*!< Number of dummy clock cycles */
+#define W25Q128_VCR_WRAP                    ((uint8_t)0x03)    /*!< Wrap */
+#define W25Q128_VCR_XIP                     ((uint8_t)0x08)    /*!< XIP */
+#define W25Q128_VCR_NB_DUMMY                ((uint8_t)0xF0)    /*!< Number of dummy clock cycles */
 
 /* Enhanced Volatile Configuration Register */
-#define W25Q32_EVCR_ODS                    ((uint8_t)0x07)    /*!< Output driver strength */
-#define W25Q32_EVCR_VPPA                   ((uint8_t)0x08)    /*!< Vpp accelerator */
-#define W25Q32_EVCR_RH                     ((uint8_t)0x10)    /*!< Reset/hold */
-#define W25Q32_EVCR_DUAL                   ((uint8_t)0x40)    /*!< Dual I/O protocol */
-#define W25Q32_EVCR_QUAD                   ((uint8_t)0x80)    /*!< Quad I/O protocol */
+#define W25Q128_EVCR_ODS                    ((uint8_t)0x07)    /*!< Output driver strength */
+#define W25Q128_EVCR_VPPA                   ((uint8_t)0x08)    /*!< Vpp accelerator */
+#define W25Q128_EVCR_RH                     ((uint8_t)0x10)    /*!< Reset/hold */
+#define W25Q128_EVCR_DUAL                   ((uint8_t)0x40)    /*!< Dual I/O protocol */
+#define W25Q128_EVCR_QUAD                   ((uint8_t)0x80)    /*!< Quad I/O protocol */
 
 /* Flag Status Register */
-#define W25Q32_FSR_PRERR                   ((uint8_t)0x02)    /*!< Protection error */
-#define W25Q32_FSR_PGSUS                   ((uint8_t)0x04)    /*!< Program operation suspended */
-#define W25Q32_FSR_VPPERR                  ((uint8_t)0x08)    /*!< Invalid voltage during program or erase */
-#define W25Q32_FSR_PGERR                   ((uint8_t)0x10)    /*!< Program error */
-#define W25Q32_FSR_ERERR                   ((uint8_t)0x20)    /*!< Erase error */
-#define W25Q32_FSR_ERSUS                   ((uint8_t)0x40)    /*!< Erase operation suspended */
-#define W25Q32_FSR_READY                   ((uint8_t)0x80)    /*!< Ready or command in progress */
+#define W25Q128_FSR_PRERR                   ((uint8_t)0x02)    /*!< Protection error */
+#define W25Q128_FSR_PGSUS                   ((uint8_t)0x04)    /*!< Program operation suspended */
+#define W25Q128_FSR_VPPERR                  ((uint8_t)0x08)    /*!< Invalid voltage during program or erase */
+#define W25Q128_FSR_PGERR                   ((uint8_t)0x10)    /*!< Program error */
+#define W25Q128_FSR_ERERR                   ((uint8_t)0x20)    /*!< Erase error */
+#define W25Q128_FSR_ERSUS                   ((uint8_t)0x40)    /*!< Erase operation suspended */
+#define W25Q128_FSR_READY                   ((uint8_t)0x80)    /*!< Ready or command in progress */
 
-#define W25Q32_MANUFACTURER_ID             ((uint8_t)0xEF)
-#define W25Q32_DEVICE_ID1                  ((uint8_t)0x40)
-#define W25Q32_DEVICE_ID2                  ((uint8_t)0x16)
+#define W25Q128_MANUFACTURER_ID             ((uint8_t)0xEF)
+#define W25Q128_DEVICE_ID1                  ((uint8_t)0x70)
+#define W25Q128_DEVICE_ID2                  ((uint8_t)0x18)
 
 ////////////////////////////////
 // remapping into SPIFFS defines
 
-#define SPIFFS_TOTAL_SIZE               W25Q32_FLASH_SIZE
-#define SPIFFS_ERASE_BLOCK_SIZE         W25Q32_SECTOR_SIZE
+#define SPIFFS_TOTAL_SIZE               W25Q128_FLASH_SIZE
+#define SPIFFS_ERASE_BLOCK_SIZE         W25Q128_SECTOR_SIZE
 #define SPIFFS_LOGICAL_BLOCK_SIZE       (64*1024)
