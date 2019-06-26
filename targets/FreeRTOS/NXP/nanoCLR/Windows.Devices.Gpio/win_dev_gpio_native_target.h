266f5b018c7574a278db8b4719ffced03ed48ab8
José Simões <jose.simoes@eclo.solutions>
2019-06-26 14:57:45 +0100

    Major rework on NXP platform files
    - Moved and distribute CLR and platform files to follow project general organization.
    - Add target config files for serial comm and storage.
    - Rework assemblies declaration to follow new common location.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ targets/FreeRTOS/NXP/nanoCLR/Windows.Devices.Gpio/win_dev_gpio_native_target.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2019 The nanoFramework project contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+
+#ifndef _WIN_DEV_GPIO_NATIVE_TARGET_H_
+#define _WIN_DEV_GPIO_NATIVE_TARGET_H_
+
+#include <win_dev_gpio_native.h>
+
+#endif  //_WIN_DEV_GPIO_NATIVE_TARGET_H_
