444a14971d2d5df94407bfd61cbfdd1bc617a384
morali <jeremi.jasinski@gmail.com>
2019-06-27 10:55:44 +0200

    Cleanup of UART driver for MIMXRT1062. (#1386)

--- targets/FreeRTOS/NXP/NXP_MIMXRT1060_EVK/target_windows_devices_serialcommunication_config.h
+++ targets/FreeRTOS/NXP/NXP_MIMXRT1060_EVK/target_windows_devices_serialcommunication_config.h
@@ -3,8 +3,6 @@
 // See LICENSE file in the project root for full license information.
 //
 
-// buffers size
-// tx buffer size: 256 bytes
+/* tx/rx buffer size: 256 bytes */
 #define UART_TX_BUFER_SIZE  256
-// rx buffer size: 256 bytes
 #define UART_RX_BUFER_SIZE  256
