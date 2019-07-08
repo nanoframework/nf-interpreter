fdb620cd93729449808f8366d9864fa1a222a283
José Simões <jose.simoes@eclo.solutions>
2019-07-08 16:02:55 +0100

    Rework PAL DAC config structure
    - Need to save driver and channel number.
    - Need to add PAL DAC structure to persist DAC configuration struct.
    - Add DAC config on all reference targets.
    - Update halconf for DAC on all reference targets.
    - Update mcuconf for DAC on all reference targets that support it.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_STM32F429I_DISCOVERY/target_system_devices_dac_config.cpp
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F429I_DISCOVERY/target_system_devices_dac_config.cpp
@@ -7,8 +7,11 @@
 
 const NF_PAL_DAC_PORT_PIN_CHANNEL DacPortPinConfig[] = {
 
-    // DAC1, channel 1, GPIO port A, pin 4 
+    // DAC1, channel 1, GPIO port A, pin 4
     {1, 1, GPIOA, 4},
+
+    // DAC1, channel 2, GPIO port A, pin 5
+    {1, 2, GPIOA, 5},
 };
 
 const int DacChannelCount = ARRAYSIZE(DacPortPinConfig);
