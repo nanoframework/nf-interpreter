dd67f776de10df4053edfbaf1c924fb5f76bd84f
Jakub Standarski <45733892+BusKetZz@users.noreply.github.com>
2019-07-11 13:27:17 +0200

    Add support for Windows.Devices.I2c (#1403)

---
+++ targets/FreeRTOS/NXP/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native_Windows_Devices_I2c_I2cController.cpp
@@ -0,0 +1,36 @@
+//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <string.h>
+#include "win_dev_i2c_native_target.h"
+
+
+HRESULT Library_win_dev_i2c_native_Windows_Devices_I2c_I2cController::NativeInit___VOID( CLR_RT_StackFrame& stack )
+{
+    (void)stack;
+
+    NANOCLR_HEADER();
+    {
+
+    }
+    NANOCLR_NOCLEANUP_NOLABEL();
+}
+
+
+HRESULT Library_win_dev_i2c_native_Windows_Devices_I2c_I2cController::GetDeviceSelector___STATIC__STRING( CLR_RT_StackFrame& stack )
+{
+    NANOCLR_HEADER();
+   {
+       // Declare the device selector string whose max size is "I2C1,I2C2,I2C3," + terminator and init with the terminator
+       char deviceSelectorString[ 15 + 1] = { 0 };
+
+       strcat(deviceSelectorString, "I2C1,I2C2,I2C3");
+    
+       // Because the caller is expecting a result to be returned
+       // we need set a return result in the stack argument using the appropriate SetResult according to the variable type (a string here)
+       stack.SetResult_String(deviceSelectorString);
+   }
+   NANOCLR_NOCLEANUP_NOLABEL();
+}
