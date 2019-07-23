68a0e004bc3522e13ea0ca442439145b5b20a373
José Simões <jose.simoes@eclo.solutions>
2019-07-23 23:01:42 +0100

    Update serial baud rate on both boards
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/nanoBooter/halconf.h
+++ targets/CMSIS-OS/ChibiOS/ORGPAL_PAL3_STM32F769_NF/nanoBooter/halconf.h
@@ -392,7 +392,7 @@
  *          default configuration.
  */
 #if !defined(SERIAL_DEFAULT_BITRATE) || defined(__DOXYGEN__)
-#define SERIAL_DEFAULT_BITRATE              115200
+#define SERIAL_DEFAULT_BITRATE              921600
 #endif
 
 /**
