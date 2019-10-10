894b1e50037bd7d3108baffbc83d2e535e88407b
José Simões <jose.simoes@eclo.solutions>
2019-10-10 10:29:41 +0100

    Add serial config for debugger port
    - Decrease baud rate and set OVER8 bit on USART CR1 to decrease baud rate clockin error.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/nanoBooter/main.c
+++ targets/CMSIS-OS/ChibiOS/ST_B_L072Z_LRWAN1/nanoBooter/main.c
@@ -16,6 +16,18 @@
 // need to declare the Receiver thread here
 osThreadDef(ReceiverThread, osPriorityHigh, 2048, "");
 
+// configuration for debugger serial port 
+// dev notes:
+// conservative baud rate value as 921600 has a high error percentage on baud rate clocking
+// OVER8 bit on CR1 to further decrease baud rate clocking error 
+static const SerialConfig uartConfig =
+{
+  460800,
+  USART_CR1_OVER8,
+  USART_CR2_STOP1_BITS,
+  0
+};
+
 // Application entry point.
 int main(void) {
 
@@ -43,7 +55,7 @@ int main(void) {
   }
 
   // starts the serial driver
-  sdStart(&SERIAL_DRIVER, NULL);
+  sdStart(&SERIAL_DRIVER, &uartConfig);
 
   // create the receiver thread
   osThreadCreate(osThread(ReceiverThread), NULL);
