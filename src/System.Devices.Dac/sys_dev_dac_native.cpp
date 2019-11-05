90cb49e6c32dd93c4a6778acfd5c19c31b2b34ce
José Simões <jose.simoes@eclo.solutions>
2019-11-05 11:11:41 +0000

    Update declaration of Devices.Dac
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- src/System.Devices.Dac/sys_dev_dac_native.cpp
+++ src/System.Devices.Dac/sys_dev_dac_native.cpp
@@ -31,10 +31,12 @@ static const CLR_RT_MethodHandler method_lookup[] =
     NULL,
     NULL,
     NULL,
+    NULL,
     Library_sys_dev_dac_native_System_Devices_Dac_DacController::NativeOpenChannel___VOID__I4,
     Library_sys_dev_dac_native_System_Devices_Dac_DacController::NativeGetChannelCount___I4,
     Library_sys_dev_dac_native_System_Devices_Dac_DacController::NativeGetResolutionInBits___I4,
     Library_sys_dev_dac_native_System_Devices_Dac_DacController::NativeInit___VOID,
+    NULL,
     NULL,
     Library_sys_dev_dac_native_System_Devices_Dac_DacController::GetDeviceSelector___STATIC__STRING,
     NULL,
@@ -45,7 +47,7 @@ static const CLR_RT_MethodHandler method_lookup[] =
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_Devices_Dac =
 {
     "System.Devices.Dac", 
-    0x12640AF3,
+    0x9B932004,
     method_lookup,
-    { 100, 0, 0, 4 }
+    { 100, 0, 0, 5 }
 };
