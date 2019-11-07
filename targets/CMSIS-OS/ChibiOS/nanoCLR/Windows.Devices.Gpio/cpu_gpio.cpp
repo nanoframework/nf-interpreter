a52e1a0db7e9b7088427f68becd6ec90c0c365c2
Adrian Soundy <adriansoundy@gmail.com>
2019-11-07 23:53:03 +1300

    Remove headers for STM32 from dummy cpu_gpio.cpp

--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/cpu_gpio.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Gpio/cpu_gpio.cpp
@@ -3,10 +3,6 @@
 // Portions Copyright (c) Microsoft Corporation.  All rights reserved.
 // See LICENSE file in the project root for full license information.
 //
-#include <targetHAL.h>
-#include <nanoCLR_Interop.h>
-#include <nanoCLR_Runtime.h>
-#include <nanoCLR_Checks.h>
 
 
 // TODO
