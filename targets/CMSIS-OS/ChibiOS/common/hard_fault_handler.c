2a26a86d9fe511fb1a1944df7a723aacca0b860c
António Fagundes <50989690+antoniofagundes@users.noreply.github.com>
2019-12-02 11:24:38 +0000

    Add Hard Fault handler for Cortex-M0 (#1520)
    ***NO_CI***

--- targets/CMSIS-OS/ChibiOS/common/hard_fault_handler.c
+++ targets/CMSIS-OS/ChibiOS/common/hard_fault_handler.c
@@ -31,7 +31,12 @@ void NMI_Handler(void) {
 #ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
+
+#if defined(STM32F4XX) || defined(STM32F7XX) || defined(STM32H7XX)
+
+// hard fault handler for Cortex-M3 & M4
 
 void HardFault_Handler(void) {                                                                                   
 
@@ -46,7 +51,7 @@ void HardFault_Handler(void) {
     volatile FaultType faultType = (FaultType)__get_IPSR();
 
     // these are not available in all the STM32 series
-#if defined(STM32F4XX) || defined(STM32F7XX)
+  #if defined(STM32F4XX) || defined(STM32F7XX)
 
     //Flags about hardfault / busfault
     //See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
@@ -74,7 +79,7 @@ void HardFault_Handler(void) {
     //For HardFault/BusFault this is the address that was accessed causing the error
     volatile uint32_t faultAddress = SCB->BFAR;
 
-#endif
+  #endif
     
     // forces a breakpoint causing the debugger to stop
     // if no debugger is attached this is ignored
@@ -83,6 +88,69 @@ void HardFault_Handler(void) {
     // If no debugger connected, just reset the board
     NVIC_SystemReset();
 }
+
+#elif defined(STM32F0XX) || defined(STM32L0XX) 
+
+// hard fault handler for Cortex-M0
+
+__attribute__((used))
+void HardFaultHandler_C(unsigned int * hardfault_args)
+{
+    volatile unsigned long stacked_r0;
+    volatile unsigned long stacked_r1;
+    volatile unsigned long stacked_r2;
+    volatile unsigned long stacked_r3;
+    volatile unsigned long stacked_r12;
+    volatile unsigned long stacked_lr;
+    volatile unsigned long stacked_pc;
+    volatile unsigned long stacked_psr;
+   
+    //Exception stack frame
+    stacked_r0 = ((volatile unsigned long) hardfault_args[0]);
+    stacked_r1 = ((volatile unsigned long) hardfault_args[1]);
+    stacked_r2 = ((volatile unsigned long) hardfault_args[2]);
+    stacked_r3 = ((volatile unsigned long) hardfault_args[3]);
+   
+    stacked_r12 = ((volatile unsigned long) hardfault_args[4]);
+    stacked_lr  = ((volatile unsigned long) hardfault_args[5]);
+    stacked_pc  = ((volatile unsigned long) hardfault_args[6]);
+    stacked_psr = ((volatile unsigned long) hardfault_args[7]);
+
+    volatile unsigned long BFAR = (*((volatile unsigned long *)(0xE000ED38)));
+    volatile unsigned long CFSR = (*((volatile unsigned long *)(0xE000ED28)));
+    volatile unsigned long HFSR = (*((volatile unsigned long *)(0xE000ED2C)));
+    volatile unsigned long DFSR = (*((volatile unsigned long *)(0xE000ED30)));
+    volatile unsigned long AFSR = (*((volatile unsigned long *)(0xE000ED3C)));
+
+    // forces a breakpoint causing the debugger to stop
+    // if no debugger is attached this is ignored
+    __asm volatile("BKPT #0\n");
+
+    // If no debugger connected, just reset the board
+    NVIC_SystemReset();
+}
+
+void __attribute__ (( naked )) HardFault_Handler(void)
+{
+    __asm volatile(
+        "movs r0, #4          \n"
+        "mov  r1, lr          \n"
+        "tst  r0, r1          \n"
+        "beq _MSP             \n"
+        "mrs r0, psp          \n"
+        "b _GetPC             \n"
+        "_MSP:                \n"
+        "mrs r0,msp           \n"
+        "_GetPC:              \n"
+        "ldr r1,[r0,#20]      \n"
+        "b HardFaultHandler_C \n"
+    );
+    HardFaultHandler_C(0);
+}
+
+#else
+#error "No hard fault handler implemented??? Check STM32 series"
+#endif
 
 void BusFault_Handler(void) __attribute__((alias("HardFault_Handler")));
 
@@ -99,7 +167,7 @@ void UsageFault_Handler(void) {
     (void)faultType;
 
     // these are not available in all the STM32 series
-#if defined(STM32F4XX) || defined(STM32F7XX)
+  #if defined(STM32F4XX) || defined(STM32F7XX)
     
     //Flags about hardfault / busfault
     //See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
@@ -110,7 +178,7 @@ void UsageFault_Handler(void) {
     volatile bool isUnalignedAccessFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 8) ? true : false);
     volatile bool isDivideByZeroFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 9) ? true : false);
 
-#endif
+  #endif
 
     // forces a breakpoint causing the debugger to stop
     // if no debugger is attached this is ignored
@@ -133,7 +201,7 @@ void MemManage_Handler(void) {
     (void)faultType;
 
     // these are not available in all the STM32 series
-#if defined(STM32F4XX) || defined(STM32F7XX)
+  #if defined(STM32F4XX) || defined(STM32F7XX)
     
     //For HardFault/BusFault this is the address that was accessed causing the error
     volatile uint32_t faultAddress = SCB->MMFAR;
@@ -146,7 +214,7 @@ void MemManage_Handler(void) {
     volatile bool isExceptionStackingFault = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 4) ? true : false);
     volatile bool isFaultAddressValid = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 7) ? true : false);
 
-#endif
+  #endif
 
     // forces a breakpoint causing the debugger to stop
     // if no debugger is attached this is ignored
@@ -159,3 +227,16 @@ void MemManage_Handler(void) {
 #ifdef __GNUC__
 #pragma GCC diagnostic pop
 #endif
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Hard Fault test code.
+// Call this to cause a hard fault by accessing a nonexistent memory address @ 0xCCCCCCCC.
+void HardFaultTest()
+{
+    volatile uint32_t*p;
+    uint32_t n;
+    p = (uint32_t*)0xCCCCCCCC;
+    n = *p;
+    (void)n;
+}
+///////////////////////////////////////////////////////////////////////////////////////////
