687604677f86f8bf8c1264a59c74827d05884906
José Simões <jose.simoes@eclo.solutions>
2020-01-29 15:54:14 +0000

    Rework CMake NF_DIRECTORY_EXISTS_NOT_EMPTY
    - Going throuhg the directory to find if it's not empty takes a lot of time because it sweeps all files, so simplifying this now to speed up local builds.
    ***NO_CI***
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- CMake/Modules/NF_Utils.cmake
+++ CMake/Modules/NF_Utils.cmake
@@ -3,21 +3,13 @@
 # See LICENSE file in the project root for full license information.
 #
 
-# check if a diretory exists and is not empty
+# check if a directory exists
+# going throuhg the directory to find if it's not empty takes a lot of time because it sweeps all files
+# simplifying this now to speed up local builds
 macro(NF_DIRECTORY_EXISTS_NOT_EMPTY path pathExists)
 
     if(EXISTS "${path}")
-
-        file(GLOB RESULT LIST_DIRECTORIES false "${path}")
-        list(LENGTH RESULT RES_LEN)
-        if(RES_LEN EQUAL 0)
-            # DIR is empty
-            set(${pathExists} OFF)
-        else()
-            # DIR exists and it's NOT empty
-            set(${pathExists} ON)
-        endif()
-
+        set(${pathExists} ON)
     else()
         set(${pathExists} OFF)
     endif()
