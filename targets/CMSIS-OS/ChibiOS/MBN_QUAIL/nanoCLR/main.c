0e0e3d8a0993eccf2997ab2a5648e62f0c089ee7
josesimoes <jose.simoes@eclo.solutions>
2020-02-24 17:26:07 +0000

    Fix hardware CRC32 module inclusion
    - Move CRC32 peripheral init to main (booter and CLR).
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- targets/CMSIS-OS/ChibiOS/MBN_QUAIL/nanoCLR/main.c
+++ targets/CMSIS-OS/ChibiOS/MBN_QUAIL/nanoCLR/main.c
@@ -7,6 +7,7 @@
 
 #include <ch.h>
 #include <hal.h>
+#include <hal_nf_community.h>
 #include <cmsis_os.h>
 
 #include "usbcfg.h"
@@ -67,6 +68,11 @@ int main(void) {
   // for STM32F4 family if watchdog is enabled can't use standby mode because the IWDG can't be stoped //
   ///////////////////////////////////////////////////////////////////////////////////////////////////////
   Watchdog_Init();
+
+  #if (HAL_NF_USE_STM32_CRC == TRUE)
+  // startup crc
+  crcStart(NULL);
+  #endif
 
   //  Initializes a serial-over-USB CDC driver.
   sduObjectInit(&SDU1);
