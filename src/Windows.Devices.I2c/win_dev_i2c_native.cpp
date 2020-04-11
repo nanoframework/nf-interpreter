bc840bd232f9615d10f248231da749c8f9855284
José Simões <jose.simoes@eclo.solutions>
2020-04-11 12:43:30 +0100

    Update I2C assembly declaration (#1604)

--- src/Windows.Devices.I2c/win_dev_i2c_native.cpp
+++ src/Windows.Devices.I2c/win_dev_i2c_native.cpp
@@ -60,7 +60,7 @@ static const CLR_RT_MethodHandler method_lookup[] =
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_Windows_Devices_I2c =
 {
     "Windows.Devices.I2c",
-    0x754E3590,
+    0x79EDBF71,
     method_lookup,
     { 100, 2, 0, 2 }
 };
