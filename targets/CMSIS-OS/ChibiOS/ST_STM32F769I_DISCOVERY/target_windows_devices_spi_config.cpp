0968f736f5edaef275193c46fe171bcca51105bd
Robin Jones <networkfusion@users.noreply.github.com>
2020-04-28 13:05:22 +0100

    Add config for SPI port ensures GPIO lines are allocated.

--- targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/target_windows_devices_spi_config.cpp
+++ targets/CMSIS-OS/ChibiOS/ST_STM32F769I_DISCOVERY/target_windows_devices_spi_config.cpp
@@ -3,6 +3,16 @@
 // See LICENSE file in the project root for full license information.
 //
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// THIS FILE IS BLANK ON PURPOSE BECAUSE THIS TARGET DOESN'T REQUIRE THIS SPECIFIC CONFIGURATION //
-///////////////////////////////////////////////////////////////////////////////////////////////////
+#include "win_dev_spi_native_target.h"
+
+//////////
+// SPI2 //
+//////////
+
+// pin configuration for SPI2
+// port for SCK pin is: LINE_ARD_D13
+// port for SDA pin is: LINE_ARD_D12
+// port for SDA pin is: LINE_ARD_D12
+
+// GPIO alternate pin function is 5 (see alternate function mapping table in device datasheet)
+SPI_CONFIG_PINS(2,LINE_ARD_D13, LINE_ARD_D12, LINE_ARD_D12, 5)
--- targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Spi/win_dev_spi_native_target.h
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/Windows.Devices.Spi/win_dev_spi_native_target.h
@@ -47,4 +47,39 @@ struct NF_PAL_SPI
     extern NF_PAL_SPI SPI6_PAL;
 #endif
 
+// the following macro defines a function that configures the GPIO pins for an STM32 SPI peripheral
+// it gets called in the Windows_Devices_SPi_SPiDevice::NativeInit function
+// this is required because the SPI peripherals can use multiple GPIO configuration combinations
+#define SPI_CONFIG_PINS(num, gpio_port_sck, gpio_port_miso, gpio_port_mosi, alternate_function) void ConfigPins_SPI##num() \
+{ \
+    palSetLineMode(gpio_port_sck, \
+                    PAL_MODE_ALTERNATE(alternate_function) | \
+                    PAL_STM32_OSPEED_HIGHEST); \
+    palSetLineMode(gpio_port_miso, \
+                    PAL_MODE_ALTERNATE(alternate_function) | \
+                    PAL_STM32_OSPEED_HIGHEST); \
+    palSetLineMode(gpio_port_mosi, \
+                    PAL_MODE_ALTERNATE(alternate_function) | \
+                    PAL_STM32_OSPEED_HIGHEST); \
+} 
+//the above `}` should be deleted if we include the below, but cannot have comments in the define.
+    // palSetLine(LINE_ARD_D15);
+    // palSetLineMode(LINE_ARD_D15, 
+    //                 PAL_MODE_OUTPUT_PUSHPULL);         /* CS0.                 */
+    // palSetLine(LINE_ARD_D14);
+    // palSetLineMode(LINE_ARD_D14,
+    //                 PAL_MODE_OUTPUT_PUSHPULL);         /* CS1.                 */
+//}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// when an SPI is defined the declarations below will have the real function/configuration //
+// in the target folder @ target_windows_devices_spi_config.cpp                             //
+//////////////////////////////////////////////////////////////////////////////////////////////
+void ConfigPins_SPI1();
+void ConfigPins_SPI2();
+void ConfigPins_SPI3();
+void ConfigPins_SPI4();
+void ConfigPins_SPI5();
+void ConfigPins_SPI6();
+
 #endif  //_WIN_DEV_SPI_NATIVE_TARGET_H_
