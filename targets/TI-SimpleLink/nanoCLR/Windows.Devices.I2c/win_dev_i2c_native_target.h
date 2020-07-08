3d599d0f2c18e3ef43fdcd13d4f7f447e2999db9
José Simões <jose.simoes@eclo.solutions>
2020-07-09 00:54:08 +0100

    Improve HAL Initialize/Uninitialize (#1677)

--- targets/TI-SimpleLink/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native_target.h
+++ targets/TI-SimpleLink/nanoCLR/Windows.Devices.I2c/win_dev_i2c_native_target.h
@@ -10,17 +10,17 @@
 #include "Board.h"
 #include <ti/drivers/I2C.h>
 
-// struct representing the I2C 
-struct NF_PAL_I2C
+// struct representing the I2C
+typedef struct
 {
-    I2C_Handle      i2c;
-    I2C_Params      i2cParams;
+    I2C_Handle i2c;
+    I2C_Params i2cParams;
     I2C_Transaction i2cTransaction;
-};
+} NF_PAL_I2C;
 
 ///////////////////////////////////////////
 // declaration of the the I2C PAL strucs //
 ///////////////////////////////////////////
 extern NF_PAL_I2C I2C1_PAL;
 
-#endif  //_WIN_DEV_I2C_NATIVE_TARGET_H_
+#endif //_WIN_DEV_I2C_NATIVE_TARGET_H_
--- targets/TI-SimpleLink/nanoCLR/Windows.Devices.Spi/win_dev_spi_native_target.h
+++ targets/TI-SimpleLink/nanoCLR/Windows.Devices.Spi/win_dev_spi_native_target.h
@@ -3,7 +3,6 @@
 // See LICENSE file in the project root for full license information.
 //
 
-
 #ifndef _WIN_DEV_SPI_NATIVE_TARGET_H_
 #define _WIN_DEV_SPI_NATIVE_TARGET_H_
 
@@ -11,18 +10,18 @@
 #include "Board.h"
 #include <ti/drivers/SPI.h>
 
-// struct representing the SPI 
-struct NF_PAL_SPI
+// struct representing the SPI
+typedef struct
 {
-    SPI_Handle          masterSpi;
-    SPI_Params          spiParams;
-    SPI_Transaction*    transactions;
-    uint8_t             transactionCount;
-};
+    SPI_Handle masterSpi;
+    SPI_Params spiParams;
+    SPI_Transaction *transactions;
+    uint8_t transactionCount;
+} NF_PAL_SPI;
 
 ///////////////////////////////////////////
 // declaration of the the SPI PAL strucs //
 ///////////////////////////////////////////
 extern NF_PAL_SPI SPI1_PAL;
 
-#endif  //_WIN_DEV_SPI_NATIVE_TARGET_H_
+#endif //_WIN_DEV_SPI_NATIVE_TARGET_H_
