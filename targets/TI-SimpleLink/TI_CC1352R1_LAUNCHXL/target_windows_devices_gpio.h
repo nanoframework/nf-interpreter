766bee88e4e2d47dcf262fc41eedbfaf46e74f2f
José Simões <jose.simoes@eclo.solutions>
2020-09-09 10:53:14 +0100

    Improvements on GPIO layer for TI SimpleLink (#1697)

---
+++ targets/TI-SimpleLink/TI_CC1352R1_LAUNCHXL/target_windows_devices_gpio.h
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2020 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _TARGET_WINDOWS_DEVICES_GPIO_H_
+#define _TARGET_WINDOWS_DEVICES_GPIO_H_
+
+// max number of GPIO pins
+#define GPIO_PINS_COUNT 32
+
+#endif // _TARGET_WINDOWS_DEVICES_GPIO_H_
