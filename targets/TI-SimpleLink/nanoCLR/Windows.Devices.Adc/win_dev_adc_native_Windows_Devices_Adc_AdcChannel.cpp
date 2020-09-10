efa8f4746b90bf5d34f9a09a5f2c9d70f248ba6e
nfbot <nanoframework@outlook.com>
2020-09-10 18:09:48 +0100

    Code style fixes (#35)
    Automated fixes for code style.

--- targets/TI-SimpleLink/nanoCLR/Windows.Devices.Adc/win_dev_adc_native_Windows_Devices_Adc_AdcChannel.cpp
+++ targets/TI-SimpleLink/nanoCLR/Windows.Devices.Adc/win_dev_adc_native_Windows_Devices_Adc_AdcChannel.cpp
@@ -24,12 +24,13 @@
 
 extern const ADC_Config ADC_config[];
 
-HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadValue___I4( CLR_RT_StackFrame& stack )
+HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadValue___I4(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
     {
         // get a pointer to the managed object instance and check that it's not NULL
-        CLR_RT_HeapBlock* pThis = stack.This();  FAULT_ON_NULL(pThis);
+        CLR_RT_HeapBlock *pThis = stack.This();
+        FAULT_ON_NULL(pThis);
 
         // Get channel from _channelNumber field
         int channelNumber = pThis[FIELD___channelNumber].NumericByRef().s4;
@@ -39,12 +40,13 @@ HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadVal
 
         ADC_Params params;
         ADC_Params_init(&params);
-        ADC_Handle adcHandler = ADC_open(channelNumber, &params); FAULT_ON_NULL(adcHandler);
+        ADC_Handle adcHandler = ADC_open(channelNumber, &params);
+        FAULT_ON_NULL(adcHandler);
 
         // sanity check
         if (adcHandler == NULL)
         {
-           NANOCLR_SET_AND_LEAVE(CLR_E_FAIL); 
+            NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
         }
 
         int_fast16_t res = ADC_convert(adcHandler, &adcValue0);
@@ -52,14 +54,16 @@ HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadVal
         // close ADC
         ADC_close(adcHandler);
 
-        if (res == ADC_STATUS_SUCCESS) 
+        if (res == ADC_STATUS_SUCCESS)
         {
             // get the raw value converted to micro volts
             adcValue0MicroVolt = ADC_convertToMicroVolts(adcHandler, adcValue0);
 
             // after the conversion need to get the value has if it was the raw one
             // the multiplier is 4096 (corresponding to 12bits)
-            double rawValue = (adcValue0MicroVolt / (double)((ADCCC26XX_HWAttrs*)ADC_config[channelNumber].hwAttrs)->refVoltage) * 4096;
+            double rawValue =
+                (adcValue0MicroVolt / (double)((ADCCC26XX_HWAttrs *)ADC_config[channelNumber].hwAttrs)->refVoltage) *
+                4096;
 
             // load back to integer to return a proper value
             adcValue0 = rawValue;
@@ -76,7 +80,7 @@ HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadVal
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeDisposeChannel___VOID( CLR_RT_StackFrame& stack )
+HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeDisposeChannel___VOID(CLR_RT_StackFrame &stack)
 {
     (void)stack;
 
--- targets/TI-SimpleLink/nanoCLR/Windows.Devices.Adc/win_dev_adc_native_target.h
+++ targets/TI-SimpleLink/nanoCLR/Windows.Devices.Adc/win_dev_adc_native_target.h
@@ -3,7 +3,6 @@
 // See LICENSE file in the project root for full license information.
 //
 
-
 #ifndef _WIN_DEV_ADC_NATIVE_TARGET_H_
 #define _WIN_DEV_ADC_NATIVE_TARGET_H_
 
@@ -12,4 +11,4 @@
 #include <ti/drivers/adc/ADCCC26XX.h>
 #include <win_dev_adc_native.h>
 
-#endif  //_WIN_DEV_ADC_NATIVE_TARGET_H_
+#endif //_WIN_DEV_ADC_NATIVE_TARGET_H_
