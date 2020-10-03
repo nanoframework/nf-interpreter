a9c118d0b7477159e8efa46b4cf43950dd202c10
Robin Jones <networkfusion@users.noreply.github.com>
2020-10-03 09:31:40 +0100

    Bring DAC native layer inline with managed (#1718)
    ***NO_CI***

--- targets/CMSIS-OS/ChibiOS/nanoCLR/System.Device.Dac/sys_dev_dac_native_System_Device_Dac_DacChannel.cpp
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/System.Device.Dac/sys_dev_dac_native_System_Device_Dac_DacChannel.cpp
@@ -14,25 +14,24 @@
 // not used, just left here if needed for debugging purposes
 static void dacerrorcallback(DACDriver *dacp, dacerror_t err)
 {
-  (void)dacp;
-  (void)err;
+    (void)dacp;
+    (void)err;
 }
 
 #ifdef __GNUC__
 #pragma GCC diagnostic pop
 #endif
 
-
-HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeWriteValue___VOID__U2( CLR_RT_StackFrame& stack )
+HRESULT Library_sys_dev_dac_native_System_Device_Dac_DacChannel::NativeWriteValue___VOID__U2(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
-    CLR_RT_HeapBlock* dacController = NULL;
+    CLR_RT_HeapBlock *dacController = NULL;
 
     // we are filling this below with the appropriate DAC port pin config and DAC driver
     NF_PAL_DAC_PORT_PIN_CHANNEL dacDefinition;
-    NF_PAL_DAC* palDac;
-    
+    NF_PAL_DAC *palDac;
+
     dacsample_t sample;
     int channelNumber;
     int controllerId;
@@ -41,7 +40,8 @@ HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeWriteVal
     sample = (dacsample_t)stack.Arg1().NumericByRef().u2;
 
     // get a pointer to the managed object instance and check that it's not NULL
-    CLR_RT_HeapBlock* pThis = stack.This();  FAULT_ON_NULL(pThis);
+    CLR_RT_HeapBlock *pThis = stack.This();
+    FAULT_ON_NULL(pThis);
 
     // Get channel from _channelNumber field
     channelNumber = pThis[FIELD___channelNumber].NumericByRef().s4;
@@ -51,43 +51,45 @@ HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeWriteVal
     dacController = pThis[FIELD___dacController].Dereference();
 
     // get pointer to _controllerId field in DacController
-    controllerId = dacController[Library_sys_dev_dac_native_System_Devices_Dac_DacController::FIELD___controllerId].NumericByRef().s4;
+    controllerId = dacController[Library_sys_dev_dac_native_System_Device_Dac_DacController::FIELD___controllerId]
+                       .NumericByRef()
+                       .s4;
 
     // only one DAC controller for now, but check it anyways
-    if(controllerId == 1)
+    if (controllerId == 1)
     {
         dacDefinition = DacPortPinConfig[channelNumber];
 
         // we should remove form the build the DAC controller & channel combinations that aren't implemented
         // plus we have to use the default to catch invalid DAC Ids
-        switch(ENCODED_DAC_REF(dacDefinition.dacIndex, dacDefinition.dacChannel))
+        switch (ENCODED_DAC_REF(dacDefinition.dacIndex, dacDefinition.dacChannel))
         {
-          #if STM32_DAC_USE_DAC1_CH1
+#if STM32_DAC_USE_DAC1_CH1
             case 11:
                 palDac = &Dac1_1_PAL;
                 break;
-          #endif
+#endif
 
-          #if STM32_DAC_USE_DAC1_CH2
+#if STM32_DAC_USE_DAC1_CH2
             case 12:
                 palDac = &Dac1_2_PAL;
                 break;
-          #endif
+#endif
 
-          #if STM32_DAC_USE_DAC2_CH1
+#if STM32_DAC_USE_DAC2_CH1
             case 21:
                 palDac = &Dac2_1_PAL;
                 break;
-          #endif
+#endif
 
-          #if STM32_DAC_USE_DAC2_CH2
+#if STM32_DAC_USE_DAC2_CH2
             case 22:
                 palDac = &Dac2_2_PAL;
                 break;
-          #endif
+#endif
 
-            default: 
-                NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);            
+            default:
+                NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
         }
     }
     else
@@ -101,7 +103,7 @@ HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeWriteVal
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeDispose___VOID__BOOLEAN( CLR_RT_StackFrame& stack )
+HRESULT Library_sys_dev_dac_native_System_Device_Dac_DacChannel::NativeDispose___VOID__BOOLEAN(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -110,7 +112,8 @@ HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeDispose_
     bool disposeController = false;
 
     // get a pointer to the managed object instance and check that it's not NULL
-    CLR_RT_HeapBlock* pThis = stack.This();  FAULT_ON_NULL(pThis);
+    CLR_RT_HeapBlock *pThis = stack.This();
+    FAULT_ON_NULL(pThis);
 
     // get disposeController
     disposeController = (bool)stack.Arg0().NumericByRef().u1;
@@ -118,39 +121,39 @@ HRESULT Library_sys_dev_dac_native_System_Devices_Dac_DacChannel::NativeDispose_
     // Get channel from _channelNumber field
     channelNumber = pThis[FIELD___channelNumber].NumericByRef().s4;
 
-    if(disposeController)
+    if (disposeController)
     {
         dacDefinition = DacPortPinConfig[channelNumber];
 
-        switch(ENCODED_DAC_REF(dacDefinition.dacIndex, dacDefinition.dacChannel))
+        switch (ENCODED_DAC_REF(dacDefinition.dacIndex, dacDefinition.dacChannel))
         {
-          #if STM32_DAC_USE_DAC1_CH1
+#if STM32_DAC_USE_DAC1_CH1
             case 11:
                 dacStop(&DACD1);
                 break;
-          #endif
+#endif
 
-          #if STM32_DAC_USE_DAC1_CH2
+#if STM32_DAC_USE_DAC1_CH2
             case 12:
                 dacStop(&DACD2);
                 break;
-          #endif
+#endif
 
-          #if STM32_DAC_USE_DAC2_CH1
+#if STM32_DAC_USE_DAC2_CH1
             case 21:
                 dacStop(&DACD3);
                 break;
-          #endif
+#endif
 
-          #if STM32_DAC_USE_DAC2_CH2
+#if STM32_DAC_USE_DAC2_CH2
             case 22:
                 dacStop(&DACD4);
                 break;
-          #endif
+#endif
 
-            default: 
-                NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);            
-        }     
+            default:
+                NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
+        }
     }
 
     NANOCLR_NOCLEANUP();
