3063f6c1930b13f72c1fef889cb52b3296aa4aff
José Simões <jose.simoes@eclo.solutions>
2020-10-06 20:24:13 +0100

    Work TI SimpleLink (#1725)
    ***NO_CI***

--- targets/TI-SimpleLink/nanoCLR/nanoFramework.TI.EasyLink/nf_ti_easylink.cpp
+++ targets/TI-SimpleLink/nanoCLR/nanoFramework.TI.EasyLink/nf_ti_easylink.cpp
@@ -5,6 +5,8 @@
 
 #include "nf_ti_easylink.h"
 
+// clang-format off
+
 static const CLR_RT_MethodHandler method_lookup[] =
 {
     NULL,
@@ -32,6 +34,7 @@ static const CLR_RT_MethodHandler method_lookup[] =
     NULL,
     NULL,
     NULL,
+    NULL,
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::GetConfiguration___U4__nanoFrameworkTIEasyLinkControlOption,
     NULL,
     NULL,
@@ -41,17 +44,19 @@ static const CLR_RT_MethodHandler method_lookup[] =
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::DisposeNative___VOID,
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::UpdateRxAddressFilterNative___VOID,
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::InitNative___U1,
-    Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::ReceiveNative___U1__BYREF_nanoFrameworkTIEasyLinkReceivedPacket__I4,
+    Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::ReceiveNative___U1__BYREF_nanoFrameworkTIEasyLinkReceivedPacket__SystemTimeSpan,
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::SetConfigurationNative___U1__nanoFrameworkTIEasyLinkControlOption__U4,
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::SetFrequencyNative___U1__U4,
     Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::SetRfPowerNative___U1__I1,
-    Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::TransmitNative___U1__nanoFrameworkTIEasyLinkTransmitPacket__I4__I4,
+    Library_nf_ti_easylink_nanoFramework_TI_EasyLink_EasyLinkController::TransmitNative___U1__nanoFrameworkTIEasyLinkTransmitPacket__SystemTimeSpan__SystemTimeSpan,
 };
 
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_nanoFramework_TI_EasyLink =
 {
     "nanoFramework.TI.EasyLink",
-    0xEC93F5DE,
+    0x948DBCF9,
     method_lookup,
-    { 100, 0, 0, 2 }
+    { 100, 0, 0, 3 }
 };
+
+// clang-format on
