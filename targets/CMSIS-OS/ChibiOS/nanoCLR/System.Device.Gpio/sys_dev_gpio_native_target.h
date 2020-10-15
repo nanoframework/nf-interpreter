1842d9d159a7e995761d96ca0cf7d49a64476ecd
nfbot <nanoframework@outlook.com>
2020-10-15 14:34:52 +0100

    Code style fixes (#44)
    Automated fixes for code style.

--- targets/CMSIS-OS/ChibiOS/nanoCLR/System.Device.Gpio/sys_dev_gpio_native_target.h
+++ targets/CMSIS-OS/ChibiOS/nanoCLR/System.Device.Gpio/sys_dev_gpio_native_target.h
@@ -3,10 +3,9 @@
 // See LICENSE file in the project root for full license information.
 //
 
-
 #ifndef _SYS_DEV_GPIO_NATIVE_TARGET_H_
 #define _SYS_DEV_GPIO_NATIVE_TARGET_H_
 
 #include <sys_dev_gpio_native.h>
 
-#endif  //_SYS_DEV_GPIO_NATIVE_TARGET_H_
+#endif //_SYS_DEV_GPIO_NATIVE_TARGET_H_
