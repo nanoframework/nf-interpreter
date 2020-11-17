7e4683774bcf0e7a4cb09c98d03a6eb8864c056e
josesimoes <jose.simoes@eclo.solutions>
2020-11-17 13:43:02 +0000

    Work CI-CD
    - Fix inclusion of target board header.

--- targets/CMSIS-OS/ChibiOS/NESHTEC_C2STM32F4/common/Device_BlockStorage-DEBUG.c
+++ targets/CMSIS-OS/ChibiOS/NESHTEC_C2STM32F4/common/Device_BlockStorage-DEBUG.c
@@ -20,8 +20,8 @@ const BlockRange BlockRange2[] = {
 
 // 128kB blocks
 const BlockRange BlockRange3[] = {
-    {BlockRange_BLOCKTYPE_CODE, 0, 1},       // 0x08020000 nanoCLR
-    {BlockRange_BLOCKTYPE_DEPLOYMENT, 2, 6}, // 0x08060000 deployment
+    {BlockRange_BLOCKTYPE_CODE, 0, 2},       // 0x08020000 nanoCLR
+    {BlockRange_BLOCKTYPE_DEPLOYMENT, 3, 6}, // 0x08080000 deployment
 };
 
 const BlockRegionInfo BlockRegions[] = {
