6f934e413f8b4b9aebaec25ae9b409672c0b744c
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 16:30:13 +0000

    Add platform common files

---
+++ targets/AzureRTOS/_common/include/targetHAL_Power.h
@@ -0,0 +1,18 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _TARGET_HAL_POWER_H_
+#define _TARGET_HAL_POWER_H_ 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_TARGET_HAL_POWER_H_
---
+++ targets/AzureRTOS/_common/include/targetHAL_Time.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _TARGET_HAL_TIME_H_
+#define _TARGET_HAL_TIME_H_
+
+#include <stm32l4xx_hal.h>
+
+#define HAL_Time_CurrentSysTicks    HAL_GetTick           
+
+#endif //_TARGET_HAL_TIME_H_
