12847baeb92aae23456f63419b3720d1176f195c
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 16:30:13 +0000

    Add platform common files

---
+++ targets/AzureRTOS/_common/include/LaunchCLR.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _LAUNCHCLR_H_
+#define _LAUNCHCLR_H_
+
+void LaunchCLR(uint32_t address);
+bool CheckValidCLRImage(uint32_t address);
+
+#endif //_LAUNCHCLR_H_
+
