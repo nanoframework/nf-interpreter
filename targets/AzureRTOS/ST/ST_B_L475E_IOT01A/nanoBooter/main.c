46274d1ff00b60df9bc553f62cce95a67e58cb50
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 18:32:52 +0000

    Add test code to nanoBooter

--- targets/AzureRTOS/ST/ST_B_L475E_IOT01A/nanoBooter/main.c
+++ targets/AzureRTOS/ST/ST_B_L475E_IOT01A/nanoBooter/main.c
@@ -4,19 +4,34 @@
 //
 
 #include <stdio.h>
+#include <stm32l4xx_hal.h>
+#include <stm32l475e_iot01.h>
 
 // #include "tx_api.h"
 
 // #include "board_init.h"
 // #include "cmsis_utils.h"
 
+void SystemClock_Config(void);
+
 //  Application entry point.
 int main(void)
 {
+    HAL_Init();
+
+    SystemClock_Config();
+
+    BSP_LED_Init(LED2);
+
     // Initialize the board
     // board_init();
 
     // Enter the ThreadX kernel
     //tx_kernel_enter();
-    return 0;
+
+    while (1)
+    {
+        BSP_LED_Toggle(LED2);
+        HAL_Delay(500);
+    }
 }
