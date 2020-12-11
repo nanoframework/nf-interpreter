4b06553c9683f2f237232b0eb98b7a4c64c449b0
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 18:31:35 +0000

    Improvements in STM32L4 package

--- CMake/Modules/FindSTM32L4_CubePackage.cmake
+++ CMake/Modules/FindSTM32L4_CubePackage.cmake
@@ -7,15 +7,29 @@
 list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/CMSIS/Device/ST/STM32L4xx/Include)
 list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/CMSIS/Core/Include)
 list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/STM32L4xx_HAL_Driver/Inc)
+list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01)
 
 # source files
 set(STM32L4_CubePackage_SRCS
 
     # add HAL files here as required
-    stm32l4xx_hal.c
-    
-    # SPIFFS
+    stm32l4xx_hal_cortex.c
+    stm32l4xx_hal_crc_ex.c
+    stm32l4xx_hal_crc.c
+    stm32l4xx_hal_i2c_ex.c
+    stm32l4xx_hal_i2c.c
+    stm32l4xx_hal_flash_ex.c
+    stm32l4xx_hal_flash.c
+    stm32l4xx_hal_gpio.c
+    stm32l4xx_hal_pwr_ex.c
+    stm32l4xx_hal_pwr.c
+    stm32l4xx_hal_rcc_ex.c
+    stm32l4xx_hal_rcc.c
     stm32l4xx_hal_qspi.c
+    stm32l4xx_hal.c
+
+    # BSP
+    stm32l475e_iot01.c
 )
 
 foreach(SRC_FILE ${STM32L4_CubePackage_SRCS})
@@ -23,13 +37,20 @@ foreach(SRC_FILE ${STM32L4_CubePackage_SRCS})
     find_file(STM32L4_CubePackage_SRC_FILE ${SRC_FILE}
         PATHS 
 
+        # HAL
         ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/STM32L4xx_HAL_Driver/Src
+
+        # BSP
+        ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01
 
         CMAKE_FIND_ROOT_PATH_BOTH
     )
     # message("${SRC_FILE} >> ${STM32L4_CubePackage_SRC_FILE}") # debug helper
     list(APPEND STM32L4_CubePackage_SOURCES ${STM32L4_CubePackage_SRC_FILE})
 endforeach()
+
+# unset these warnings as errors because this is a 3rd party library 
+SET_SOURCE_FILES_PROPERTIES( ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01/stm32l475e_iot01.c PROPERTIES COMPILE_FLAGS -Wno-unused-parameter)
 
 include(FindPackageHandleStandardArgs)
 
