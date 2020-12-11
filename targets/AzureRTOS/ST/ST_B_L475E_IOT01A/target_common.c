df7b624cc42084e66196ad136af41f363b8668a8
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 09:59:22 +0000

    Several fixes in ST_B_L475E_IOT01A common files

--- targets/AzureRTOS/ST/ST_B_L475E_IOT01A/target_common.c
+++ targets/AzureRTOS/ST/ST_B_L475E_IOT01A/target_common.c
@@ -22,3 +22,7 @@ HAL_SYSTEM_CONFIG HalSystemConfig =
 };
 
 HAL_TARGET_CONFIGURATION  g_TargetConfiguration;
+
+// this target can use JTAG for updates
+inline GET_TARGET_CAPABILITIES(TargetCapabilities_JtagUpdate);
+inline TARGET_HAS_PROPRIETARY_BOOTER(true);
