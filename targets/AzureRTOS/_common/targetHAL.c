908ba3afd55bd938ced866d733e86fa3f3657038
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 16:30:13 +0000

    Add platform common files

---
+++ targets/AzureRTOS/_common/targetHAL.c
@@ -0,0 +1,34 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <nanoHAL_v2.h>
+#include <platform_target_capabilities.h>
+
+void HAL_AssertEx()
+{
+    __asm("BKPT #0\n");
+    while (true)
+    { /*nop*/
+    }
+}
+
+#if !defined(BUILD_RTM)
+
+void HARD_Breakpoint()
+{
+    __asm("BKPT #0\n");
+    while (true)
+    { /*nop*/
+    }
+};
+
+#endif // !defined(BUILD_RTM)
+
+// provide platform level "weak" implementations for all capabilities
+__nfweak TARGET_IFU_CAPABLE(false);
+
+// STM32 default capability is JTAG update
+// declared as "weak" to allow targets to provide hard implementation
+__nfweak GET_TARGET_CAPABILITIES(TargetCapabilities_JtagUpdate);
