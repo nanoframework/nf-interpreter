dcc763fa9b38e13aa2e359f6ee2c6be903c63c08
josesimoes <jose.simoes@eclo.solutions>
2020-12-12 08:36:26 +0000

    Rework module for STM Cube for L4
    - Add BSP and sensor files.
    - Made inclusions depending on target board.

--- CMake/Modules/FindSTM32L4_CubePackage.cmake
+++ CMake/Modules/FindSTM32L4_CubePackage.cmake
@@ -10,13 +10,15 @@ FetchContent_GetProperties(stm32l4_cubepackage)
 list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${stm32l4_cubepackage_SOURCE_DIR}/Drivers/CMSIS/Device/ST/STM32L4xx/Include)
 list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${stm32l4_cubepackage_SOURCE_DIR}/Drivers/STM32L4xx_HAL_Driver/Inc)
 
-# source files
+# HAL source files
 set(STM32L4_CubePackage_SRCS
 
     # add HAL files here as required
     stm32l4xx_hal_cortex.c
     stm32l4xx_hal_crc_ex.c
     stm32l4xx_hal_crc.c
+    stm32l4xx_hal_dma_ex.c
+    stm32l4xx_hal_dma.c
     stm32l4xx_hal_i2c_ex.c
     stm32l4xx_hal_i2c.c
     stm32l4xx_hal_flash_ex.c
@@ -24,13 +26,15 @@ set(STM32L4_CubePackage_SRCS
     stm32l4xx_hal_gpio.c
     stm32l4xx_hal_pwr_ex.c
     stm32l4xx_hal_pwr.c
+    stm32l4xx_hal_qspi.c
     stm32l4xx_hal_rcc_ex.c
     stm32l4xx_hal_rcc.c
+    stm32l4xx_hal_rtc_ex.c
+    stm32l4xx_hal_rtc.c
     stm32l4xx_hal_qspi.c
+    stm32l4xx_hal_uart_ex.c
+    stm32l4xx_hal_uart.c
     stm32l4xx_hal.c
-
-    # BSP
-    stm32l475e_iot01.c
 )
 
 foreach(SRC_FILE ${STM32L4_CubePackage_SRCS})
@@ -41,9 +45,6 @@ foreach(SRC_FILE ${STM32L4_CubePackage_SRCS})
         PATHS 
 
         ${stm32l4_cubepackage_SOURCE_DIR}/Drivers/STM32L4xx_HAL_Driver/Src
-
-        # BSP
-        ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01
 
         CMAKE_FIND_ROOT_PATH_BOTH
     )
@@ -56,8 +57,59 @@ foreach(SRC_FILE ${STM32L4_CubePackage_SRCS})
     
 endforeach()
 
-# unset these warnings as errors because this is a 3rd party library 
-SET_SOURCE_FILES_PROPERTIES( ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01/stm32l475e_iot01.c PROPERTIES COMPILE_FLAGS -Wno-unused-parameter)
+# BSP
+if(${TARGET_BOARD} STREQUAL "ST_B_L475E_IOT01A")
+
+    # ST_B_L475E_IOT01A
+    list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01)
+    list(APPEND STM32L4_CubePackage_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/hts221)
+
+    set(BSP_CubePackage_SRCS
+
+        # BSP
+        stm32l475e_iot01_accelero.c
+        stm32l475e_iot01_gyro.c
+        stm32l475e_iot01_hsensor.c
+        stm32l475e_iot01_magneto.c
+        stm32l475e_iot01_psensor.c
+        stm32l475e_iot01_qspi.c
+        stm32l475e_iot01_tsensor.c
+        stm32l475e_iot01.c
+
+        # Sensors
+        hts221.c
+        lis3mdl.c
+        lsm6dsl.c
+        lps22hb.c
+    )
+
+    foreach(SRC_FILE ${BSP_CubePackage_SRCS})
+        set(BSP_CubePackage_SRC_FILE SRC_FILE-NOTFOUND)
+        find_file(BSP_CubePackage_SRC_FILE ${SRC_FILE}
+            PATHS 
+
+            # BSP
+            ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01
+
+            # Sensors
+            ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/hts221
+            ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/lis3mdl
+            ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/lsm6dsl
+            ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/lps22hb
+
+            CMAKE_FIND_ROOT_PATH_BOTH
+        )
+        # message("${SRC_FILE} >> ${BSP_CubePackage_SRC_FILE}") # debug helper
+        list(APPEND STM32L4_CubePackage_SOURCES ${BSP_CubePackage_SRC_FILE})
+    endforeach()
+
+    # unset these warnings as errors because these come from a 3rd party library 
+    SET_SOURCE_FILES_PROPERTIES( ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01/stm32l475e_iot01.c PROPERTIES COMPILE_FLAGS -Wno-unused-parameter)
+    SET_SOURCE_FILES_PROPERTIES( ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/B-L475E-IOT01/stm32l475e_iot01_qspi.c PROPERTIES COMPILE_FLAGS -Wno-unused-parameter)
+    SET_SOURCE_FILES_PROPERTIES( ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/hts221/hts221.c PROPERTIES COMPILE_FLAGS -Wno-unused-parameter)
+    SET_SOURCE_FILES_PROPERTIES( ${CMAKE_BINARY_DIR}/STM32L4_CubePackage_Source/Drivers/BSP/Components/lps22hb/lps22hb.c PROPERTIES COMPILE_FLAGS -Wno-unused-parameter)
+ 
+endif()
 
 include(FindPackageHandleStandardArgs)
 
