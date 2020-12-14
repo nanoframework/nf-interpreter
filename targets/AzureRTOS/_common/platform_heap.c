cdf6675f74f96d3b4a1f892c411227fe25fd696b
josesimoes <jose.simoes@eclo.solutions>
2020-12-14 10:32:35 +0000

    Code cleanup and formating

--- targets/AzureRTOS/_common/platform_heap.c
+++ targets/AzureRTOS/_common/platform_heap.c
@@ -5,15 +5,28 @@
 
 #include <nanoCLR_Headers.h>
 
-void* platform_malloc(size_t size) {
-    return malloc(size);
+#include <tx_api.h>
+
+// the byte pool for memory allocation is defined in main()
+extern TX_BYTE_POOL byte_pool_0;
+
+void *platform_malloc(size_t size)
+{
+    uint8_t *pointer = TX_NULL;
+
+    /* Allocate the stack for thread 0.  */
+    tx_byte_allocate(&byte_pool_0, (VOID **)&pointer, size, TX_NO_WAIT);
+
+    return pointer;
 }
 
-void  platform_free(void*  ptr) {
-    free(ptr);
+void platform_free(void *ptr)
+{
+    tx_byte_release(ptr);
 }
 
-void* platform_realloc(void*  ptr, size_t size) {
+void *platform_realloc(void *ptr, size_t size)
+{
     (void)size;
 
     return ptr;
