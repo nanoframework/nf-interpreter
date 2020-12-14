793a8a853cf24a400cf60104b8f7cbdd95824043
josesimoes <jose.simoes@eclo.solutions>
2020-12-14 10:32:35 +0000

    Code cleanup and formating

--- targets/AzureRTOS/ST/ST_B_L475E_IOT01A/common/stm32l4xx_hal_msp.c
+++ targets/AzureRTOS/ST/ST_B_L475E_IOT01A/common/stm32l4xx_hal_msp.c
@@ -7,75 +7,65 @@
 #include <stm32l4xx_hal.h>
 #include <tx_api.h>
 
-// void HAL_TIM_OC_MspInit(TIM_HandleTypeDef *htim)
-// { 
-//   /* TIM1 Peripheral clock enable */
-//   __HAL_RCC_TIM1_CLK_ENABLE();
-
-//   /*  Enable the TIM1 global Interrupt & set priority */
-//   HAL_NVIC_SetPriority(TIM1_CC_IRQn, 0x8, 0);
-//   HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
-// }
-
 void HAL_RTC_MspInit(RTC_HandleTypeDef *hrtc)
 {
-  (void)hrtc;
+    (void)hrtc;
 
-  RCC_OscInitTypeDef        RCC_OscInitStruct;
-  RCC_PeriphCLKInitTypeDef  PeriphClkInitStruct;
-  
-  /* To change the source clock of the RTC feature (LSE, LSI), You have to:
-     - Enable the power clock using __HAL_RCC_PWR_CLK_ENABLE()
-     - Enable write access using HAL_PWR_EnableBkUpAccess() function before to 
-       configure the RTC clock source (to be done once after reset).
-     - Reset the Back up Domain using __HAL_RCC_BACKUPRESET_FORCE() and 
-       __HAL_RCC_BACKUPRESET_RELEASE().
-     - Configure the needed RTc clock source */
-  
-  /* Configue LSE as RTC clock source */ 
-  RCC_OscInitStruct.OscillatorType =  RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_LSE;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  RCC_OscInitStruct.LSIState = RCC_LSI_OFF;
-  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK){ 
-    while(1);
-  }
-  
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-  PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-  if(HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK){
-    while(1);
-  }
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
-  /* Enable RTC Clock */ 
-  __HAL_RCC_RTC_ENABLE(); 
+    // Configue LSE as RTC clock source
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI | RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.LSIState = RCC_LSI_OFF;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        while (1)
+        {
+        }
+    }
+
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+    PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+        while (1)
+        {
+        }
+    }
+
+    // Enable RTC Clock
+    __HAL_RCC_RTC_ENABLE();
 }
 
 void HAL_RTC_MspDeInit(RTC_HandleTypeDef *hrtc)
 {
-  (void)hrtc;
+    (void)hrtc;
 
-  /*##-1- Reset peripherals ##################################################*/
-   __HAL_RCC_RTC_DISABLE();
+    // Reset peripherals
+    __HAL_RCC_RTC_DISABLE();
 }
 
-void HAL_RNG_MspInit(RNG_HandleTypeDef* hrng)
+void HAL_RNG_MspInit(RNG_HandleTypeDef *hrng)
 {
-  if(hrng->Instance==RNG) {
-    /* Peripheral clock enable */
-    __HAL_RCC_RNG_CLK_ENABLE();
-  }
+    if (hrng->Instance == RNG)
+    {
+        // Peripheral clock enable
+        __HAL_RCC_RNG_CLK_ENABLE();
+    }
 }
 
-void HAL_RNG_MspDeInit(RNG_HandleTypeDef* hrng)
+void HAL_RNG_MspDeInit(RNG_HandleTypeDef *hrng)
 {
-  if(hrng->Instance==RNG) {
-    /* Peripheral clock disable */
-    __HAL_RCC_RNG_CLK_DISABLE();
-  }
+    if (hrng->Instance == RNG)
+    {
+        // Peripheral clock disable
+        __HAL_RCC_RNG_CLK_DISABLE();
+    }
 }
 
 uint32_t HAL_GetTick(void)
 {
-  return tx_time_get() * 10;
+    return tx_time_get() * 10;
 }
