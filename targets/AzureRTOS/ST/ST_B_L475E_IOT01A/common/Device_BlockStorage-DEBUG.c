b83717dcf7567a302d01984b497a2d05ee171d43
josesimoes <jose.simoes@eclo.solutions>
2020-12-14 20:16:17 +0000

    Fix block storage definitions for debug

--- targets/AzureRTOS/ST/ST_B_L475E_IOT01A/common/Device_BlockStorage-DEBUG.c
+++ targets/AzureRTOS/ST/ST_B_L475E_IOT01A/common/Device_BlockStorage-DEBUG.c
@@ -13,10 +13,10 @@ const BlockRange BlockRange1[] = {
     {BlockRange_BLOCKTYPE_BOOTSTRAP, 0, 16},
 
     // 08008800 nanoCLR
-    {BlockRange_BLOCKTYPE_CODE, 17, 399},
+    {BlockRange_BLOCKTYPE_CODE, 17, 108},
 
-    // 080C7800 deployment
-    {BlockRange_BLOCKTYPE_DEPLOYMENT, 400, 510},
+    // 08036800 deployment
+    {BlockRange_BLOCKTYPE_DEPLOYMENT, 109, 510},
 
     ///////////////////////////////////////////////////////////////////////////////////////
     // because this target is using a configuration block need to add the
