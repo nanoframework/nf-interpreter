50e9406d55d73620ab3d599f1fbfcb814f54b203
josesimoes <jose.simoes@eclo.solutions>
2020-12-14 12:33:27 +0000

    Update linker files to include bootloader clipboard region
    - Increase booter block size.
    - Fix check for nanoCLR.

--- CMake/binutils.AzureRTOS.cmake
+++ CMake/binutils.AzureRTOS.cmake
@@ -24,7 +24,7 @@ function(NF_SET_LINK_MAP TARGET)
     string(SUBSTRING ${TARGET} 0 ${TARGET_EXTENSION_DOT_INDEX} TARGET_SHORT)
     
     # add linker flags to generate map file
-    set_property(TARGET ${TARGET_SHORT}.elf APPEND_STRING PROPERTY LINK_FLAGS " -Wl,-Map=${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.map")
+    set_property(TARGET ${TARGET_SHORT}.elf APPEND_STRING PROPERTY LINK_FLAGS " -Wl,-Map=${PROJECT_SOURCE_DIR}/build/${TARGET_SHORT}.map,--library-path=${CMAKE_SOURCE_DIR}/targets/AzureRTOS/_common")
 
 endfunction()
 
