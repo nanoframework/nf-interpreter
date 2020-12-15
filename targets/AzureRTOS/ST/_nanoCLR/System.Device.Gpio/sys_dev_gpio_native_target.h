9d57ca4463070e87b0823d046f7570c9e0eda15d
josesimoes <jose.simoes@eclo.solutions>
2020-12-15 14:24:55 +0000

    Work on CPU GPIO
    - Add ST GPIO helpers.
    - Add target PAL.
    - Implement CPU GPIO.
    - Add interrupt handlers for all EXTI lines.

---
+++ targets/AzureRTOS/ST/_nanoCLR/System.Device.Gpio/sys_dev_gpio_native_target.h
@@ -0,0 +1,11 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _SYS_DEV_GPIO_NATIVE_TARGET_H_
+#define _SYS_DEV_GPIO_NATIVE_TARGET_H_
+
+#include <sys_dev_gpio_native.h>
+
+#endif //_SYS_DEV_GPIO_NATIVE_TARGET_H_
