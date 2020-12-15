3394124b673c21ffeacafecba8eda79afbd5406b
josesimoes <jose.simoes@eclo.solutions>
2020-12-15 12:12:06 +0000

    Rework target PAL Events

--- targets/AzureRTOS/_common/nanoCLR/targetPAL_Events.cpp
+++ targets/AzureRTOS/_common/nanoCLR/targetPAL_Events.cpp
@@ -10,6 +10,52 @@
 #include <nanoPAL.h>
 #include <target_platform.h>
 
+uint64_t CPU_MillisecondsToTicks(uint64_t ticks);
+
+// timer for bool events
+TX_TIMER boolEventsTimer;
+static bool *saveTimerCompleteFlag = 0;
+
+static void local_Events_SetBoolTimer_Callback(uint32_t id)
+{
+    (void)id;
+
+    *saveTimerCompleteFlag = true;
+}
+
+bool Events_Initialize_Platform()
+{
+    return tx_timer_create(
+               &boolEventsTimer,
+               (char *)"BoolEvents Timer",
+               local_Events_SetBoolTimer_Callback,
+               0,
+               0,
+               1,
+               TX_NO_ACTIVATE) == TX_SUCCESS;
+}
+
+bool Events_Uninitialize_Platform()
+{
+    tx_timer_delete(&boolEventsTimer);
+
+    return true;
+}
+
+void Events_SetBoolTimer(bool *timerCompleteFlag, uint32_t millisecondsFromNow)
+{
+    if (timerCompleteFlag != NULL)
+    {
+        // As only one timer running at a time we will just save it
+        saveTimerCompleteFlag = timerCompleteFlag;
+
+        // need to stop the timer first
+        tx_timer_deactivate(&boolEventsTimer);
+        tx_timer_change(&boolEventsTimer, 0, millisecondsFromNow / 10);
+        tx_timer_activate(&boolEventsTimer);
+    }
+}
+
 uint32_t Events_WaitForEvents(uint32_t powerLevel, uint32_t wakeupSystemEvents, uint32_t timeoutMilliseconds)
 {
     // schedule an interrupt for this far in the future
