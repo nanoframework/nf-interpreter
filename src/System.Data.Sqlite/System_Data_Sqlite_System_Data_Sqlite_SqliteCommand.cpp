c9b1f252bb6be8e7e6df82f0c627b21aac7605b9
Robin Jones <networkfusion@users.noreply.github.com>
2021-02-08 09:51:44 +0000

    Adds SQLite souce (#1648)
    * Adds SQLite souce
    
    * Add the initial stubs
    
    * Enable build options
    (only enabled on STM32F769I for the moment - good test for both cases)
    Make lib non target specific
    
    * Remove interop layer
    
    * Add some (commented out) glue
    
    * remove unused file
    
    * Code style fixes
    
    Automated fixes for code style.
    
    * Code style fixes
    
    Automated fixes for code style.
    
    Co-authored-by: nfbot <nanoframework@outlook.com>
    Co-authored-by: Adrian Soundy <adriansoundy@gmail.com>

---
+++ src/System.Data.Sqlite/System_Data_Sqlite_System_Data_Sqlite_SqliteCommand.cpp
@@ -0,0 +1,156 @@
+
+#include "nf_system_data_sqlite.h"
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeOpen___I4__STRING(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+    //    rc = sqlite3_open("test.db", &db);
+    //    if( rc ) {
+    //       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+    //       return(0);
+    //    } else {
+    //       fprintf(stdout, "Opened database successfully\n");
+    //    }
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeClose___I4(CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+    // sqlite3_close(db);
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativePrepare___I4__STRING__I4__BYREF_I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+    // rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    // if( rc != SQLITE_OK ){
+    //     fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    //     sqlite3_free(zErrMsg);
+    // } else {
+    //     fprintf(stdout, "Table created successfully\n");
+    // }
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeErrorMessage___STRING(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeStep___I4__I4(CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeFinalize___I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnCount___I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnName___STRING__I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnType___I4__I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnLong___I8__I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnText___STRING__I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnDouble___R8__I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnBlobLength___I4__I4__I4(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_system_data_sqlite_System_Data_Sqlite_SqliteCommand::NativeColumnBlobData___VOID__I4__I4__SZARRAY_U1(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
