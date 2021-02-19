71fbe9905df45615b01b518df89ab3f3132cb275
josesimoes <jose.simoes@eclo.solutions>
2021-02-19 21:30:25 +0000

    Initial work on adding support for Nordic and micro::bit

---
+++ targets/AzureRTOS/Nordic/MICRO_BIT_2/tx_user.h
@@ -0,0 +1,11 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifdef I_AM_NANOBOOTER
+   #include "nanoBooter/tx_user_nanoBooter.h"
+#endif
+#ifdef I_AM_NANOCLR
+   #include "nanoCLR/tx_user_nanoCLR.h"
+#endif
