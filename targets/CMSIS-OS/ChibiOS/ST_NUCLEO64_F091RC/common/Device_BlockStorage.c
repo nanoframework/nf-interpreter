99eaa4e4b901c1a139da49b4780267a3950cb292
josesimoes <jose.simoes@eclo.solutions>
2021-02-23 11:07:46 +0000

    Work CD-CI
    - Fixes in ST_NUCLEO64_F091RC linker files and block storage definitions.
    
    ***NO_CI***

--- targets/CMSIS-OS/ChibiOS/ST_NUCLEO64_F091RC/common/Device_BlockStorage.c
+++ targets/CMSIS-OS/ChibiOS/ST_NUCLEO64_F091RC/common/Device_BlockStorage.c
@@ -6,61 +6,63 @@
 #include <nanoHAL_Types.h>
 #include <nanoPAL_BlockStorage.h>
 
-//2kB block
-const BlockRange BlockRange1[] = 
-{
-    { BlockRange_BLOCKTYPE_BOOTSTRAP ,    0, 4    },            // 0x08000000 nanoBooter          
-    { BlockRange_BLOCKTYPE_CODE      ,    5, 83   },            // 0x08002800 nanoCLR    
-    { BlockRange_BLOCKTYPE_DEPLOYMENT,   84, 127  }             // 0x0802A000 deployment  
-};
+// 2kB block
+const BlockRange BlockRange1[] = {
+    // 0x08000000 nanoBooter
+    {BlockRange_BLOCKTYPE_BOOTSTRAP, 0, 4},
 
-const BlockRegionInfo BlockRegions[] = 
-{
+    // 0x08002800 nanoCLR
+    {BlockRange_BLOCKTYPE_CODE, 5, 83},
+
+    // 0x0802A000 deployment
+    {BlockRange_BLOCKTYPE_DEPLOYMENT, 84, 127}};
+
+const BlockRegionInfo BlockRegions[] = {
     {
-        (0),                                // no attributes for this region
-        0x08000000,                         // start address for block region
-        128,                                // total number of blocks in this region
-        0x800,                              // total number of bytes per block
+        (0),        // no attributes for this region
+        0x08000000, // start address for block region
+        128,        // total number of blocks in this region
+        0x800,      // total number of bytes per block
         ARRAYSIZE_CONST_EXPR(BlockRange1),
         BlockRange1,
     },
 };
 
-const DeviceBlockInfo Device_BlockInfo =
-{
-    (MediaAttribute_SupportsXIP),           // STM32 flash memory is XIP
-    2,                                      // UINT32 BytesPerSector
-    ARRAYSIZE_CONST_EXPR(BlockRegions),     // UINT32 NumRegions;
-    (BlockRegionInfo*)BlockRegions,         // const BlockRegionInfo* pRegions;
+const DeviceBlockInfo Device_BlockInfo = {
+    (MediaAttribute_SupportsXIP),       // STM32 flash memory is XIP
+    2,                                  // UINT32 BytesPerSector
+    ARRAYSIZE_CONST_EXPR(BlockRegions), // UINT32 NumRegions;
+    (BlockRegionInfo *)BlockRegions,    // const BlockRegionInfo* pRegions;
 };
 
-MEMORY_MAPPED_NOR_BLOCK_CONFIG Device_BlockStorageConfig =
-{
-    { // BLOCK_CONFIG
+MEMORY_MAPPED_NOR_BLOCK_CONFIG Device_BlockStorageConfig = {
+    {
+        // BLOCK_CONFIG
         {
-            0,          // GPIO_PIN             Pin;
-            false,      // BOOL                 ActiveState;
+            0,     // GPIO_PIN             Pin;
+            false, // BOOL                 ActiveState;
         },
 
-        (DeviceBlockInfo*)&Device_BlockInfo,    // BlockDeviceinfo
+        (DeviceBlockInfo *)&Device_BlockInfo, // BlockDeviceinfo
     },
 
-    { // CPU_MEMORY_CONFIG
-        0,                          // UINT8  CPU_MEMORY_CONFIG::ChipSelect;
-        true,                       // UINT8  CPU_MEMORY_CONFIG::ReadOnly;
-        0,                          // UINT32 CPU_MEMORY_CONFIG::WaitStates;
-        0,                          // UINT32 CPU_MEMORY_CONFIG::ReleaseCounts;
-        16,                         // UINT32 CPU_MEMORY_CONFIG::BitWidth;
-        0x08000000,                 // UINT32 CPU_MEMORY_CONFIG::BaseAddress;
-        0x00040000,                 // UINT32 CPU_MEMORY_CONFIG::SizeInBytes;
-        0,                          // UINT8  CPU_MEMORY_CONFIG::XREADYEnable 
-        0,                          // UINT8  CPU_MEMORY_CONFIG::ByteSignalsForRead 
-        0,                          // UINT8  CPU_MEMORY_CONFIG::ExternalBufferEnable
+    {
+        // CPU_MEMORY_CONFIG
+        0,          // UINT8  CPU_MEMORY_CONFIG::ChipSelect;
+        true,       // UINT8  CPU_MEMORY_CONFIG::ReadOnly;
+        0,          // UINT32 CPU_MEMORY_CONFIG::WaitStates;
+        0,          // UINT32 CPU_MEMORY_CONFIG::ReleaseCounts;
+        16,         // UINT32 CPU_MEMORY_CONFIG::BitWidth;
+        0x08000000, // UINT32 CPU_MEMORY_CONFIG::BaseAddress;
+        0x00040000, // UINT32 CPU_MEMORY_CONFIG::SizeInBytes;
+        0,          // UINT8  CPU_MEMORY_CONFIG::XREADYEnable
+        0,          // UINT8  CPU_MEMORY_CONFIG::ByteSignalsForRead
+        0,          // UINT8  CPU_MEMORY_CONFIG::ExternalBufferEnable
     },
 
-    0,                              // UINT32 ChipProtection;
-    0,                              // UINT32 ManufacturerCode;
-    0,                              // UINT32 DeviceCode;
+    0, // UINT32 ChipProtection;
+    0, // UINT32 ManufacturerCode;
+    0, // UINT32 DeviceCode;
 };
 
-__attribute__((__used__))  BlockStorageDevice    Device_BlockStorage;
+__attribute__((__used__)) BlockStorageDevice Device_BlockStorage;
