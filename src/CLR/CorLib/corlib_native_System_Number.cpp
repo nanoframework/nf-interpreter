a3a994d7e7b94a9dc367b207ac946e42ebdbe1fd
SandorDobos <37699515+SandorDobos@users.noreply.github.com>
2021-02-23 21:39:28 +0100

    TinyPrintf replaced with nanoprintf (#1841)
    ***NO_CI***

--- src/CLR/CorLib/corlib_native_System_Number.cpp
+++ src/CLR/CorLib/corlib_native_System_Number.cpp
@@ -6,16 +6,32 @@
 #include "CorLib.h"
 
 // compose sprintf format string according to requested parameters
-void nf_GetFormatString(char* formatStr, char formatCh, int precision, bool isLong, bool isFloat, bool isSigned)
+void nf_GetFormatString(
+    char *formatStr,
+    size_t formatStrLen,
+    char formatCh,
+    int precision,
+    bool isLong,
+    bool isFloat,
+    bool isSigned)
 {
-    if(formatCh == 'X' || formatCh=='x')
-		sprintf(formatStr, "%%0%d%sX", precision, isLong ? "ll" : "");
+
+    if (formatCh == 'X' || formatCh == 'x')
+        snprintf(formatStr, formatStrLen, "%%0%d%sX", precision, isLong ? "ll" : "");
     else
-        sprintf(formatStr, "%%%s%d%s%s", isFloat ? "." : isSigned ? "-0" : "0", precision, isLong ? "ll" : isFloat ? "f" : "", isFloat ? "" : isSigned ? "d" : "u");
+        snprintf(
+            formatStr,
+            formatStrLen,
+            "%%%s%d%s%s",
+            isFloat ? "." : isSigned ? "-0" : "0",
+            precision,
+            isLong ? "ll" : isFloat ? "f" : "",
+            isFloat ? "" : isSigned ? "d" : "u");
 }
 
-// remove the prepended zeros and (if possible) the decimal point in a float that's formated as string. e.g. "47.1100815000000" => "47.1100815" or "8.0000E-12" => "8E-12"
-void nf_RemovePrependedZeros(char* floatStr)
+// remove the prepended zeros and (if possible) the decimal point in a float that's formated as string. e.g.
+// "47.1100815000000" => "47.1100815" or "8.0000E-12" => "8E-12"
+void nf_RemovePrependedZeros(char *floatStr)
 {
     int length = hal_strlen_s(floatStr);
     // flag for finding the decimal point
@@ -96,171 +112,171 @@ void nf_RemovePrependedZeros(char* floatStr)
     }
 }
 
-HRESULT Library_corlib_native_System_Number::FormatNative___STATIC__STRING__OBJECT__CHAR__I4( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Number::FormatNative___STATIC__STRING__OBJECT__CHAR__I4(CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    CLR_RT_HeapBlock*     pArgs = &(stack.Arg0());
-    CLR_RT_HeapBlock*     value = pArgs;
-    CLR_DataType          dt;
+    CLR_RT_HeapBlock *pArgs = &(stack.Arg0());
+    CLR_RT_HeapBlock *value = pArgs;
+    CLR_DataType dt;
     CLR_RT_TypeDescriptor desc;
-    char result[ 24 ];
-    // This is temporary buffer to create proper format string.  
-    char formatStr[ 8 ];
-    char formatCh  = (char)pArgs[ 1 ].NumericByRef().u1;
-    int  precision =       pArgs[ 2 ].NumericByRef().s4;
+    char result[24];
+    // This is temporary buffer to create proper format string.
+    char formatStr[8];
+    char formatCh = (char)pArgs[1].NumericByRef().u1;
+    int precision = pArgs[2].NumericByRef().s4;
     bool shouldRemovePrependedZeros = false;
 
-    NANOCLR_CHECK_HRESULT(desc.InitializeFromObject( *value ));
-    NANOCLR_CHECK_HRESULT(value->PerformUnboxing( desc.m_handlerCls ));
+    NANOCLR_CHECK_HRESULT(desc.InitializeFromObject(*value));
+    NANOCLR_CHECK_HRESULT(value->PerformUnboxing(desc.m_handlerCls));
 
     dt = value->DataType();
 
-    if(formatCh == 'G' && precision == 0)
+    if (formatCh == 'G' && precision == 0)
     {
         precision = 1;
         shouldRemovePrependedZeros = true;
     }
 
-    switch(dt)
+    switch (dt)
     {
         case DATATYPE_I1:
             // get format string
-            nf_GetFormatString(formatStr, formatCh, precision, false, false, true);
-            sprintf(result, formatStr, value->NumericByRef().s1 );
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, false, true);
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().s1);
             break;
 
         case DATATYPE_U1:
             // get format string
-            nf_GetFormatString(formatStr, formatCh, precision, false, false, false);
-            sprintf(result, formatStr, value->NumericByRef().u1 );
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, false, false);
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().u1);
             break;
 
         case DATATYPE_I2:
             // get format string
-            nf_GetFormatString(formatStr, formatCh, precision, false, false, true);
-            sprintf(result, formatStr, value->NumericByRef().s2 );
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, false, true);
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().s2);
             break;
 
         case DATATYPE_U2:
             // get format string
-            nf_GetFormatString(formatStr, formatCh, precision, false, false, false);
-            sprintf(result, formatStr, value->NumericByRef().u2 );
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, false, false);
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().u2);
             break;
 
         case DATATYPE_I4:
             // get format string
-            nf_GetFormatString(formatStr, formatCh, precision, false, false, true);
-            sprintf(result, formatStr, value->NumericByRef().s4);
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, false, true);
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().s4);
             break;
 
         case DATATYPE_U4:
             // get format string
-            nf_GetFormatString(formatStr, formatCh, precision, false, false, false);
-            sprintf(result, formatStr, value->NumericByRef().u4);
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, false, false);
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().u4);
             break;
 
         case DATATYPE_I8:
-            {  
-              #if defined(_WIN32)
-                hal_snprintf( result, ARRAYSIZE(result), "%I64d",(CLR_INT64_TEMP_CAST)value->NumericByRef().s8 ); 
-              #else
-                // get format string
-                nf_GetFormatString(formatStr, formatCh, precision, true, false, true);
-                sprintf(result, formatStr, (CLR_INT64_TEMP_CAST)value->NumericByRef().s8); 
-              #endif // defined(_WIN32)
-            }
-            break;
+        {
+#if defined(_WIN32)
+            hal_snprintf(result, ARRAYSIZE(result), "%I64d", (CLR_INT64_TEMP_CAST)value->NumericByRef().s8);
+#else
+            // get format string
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, true, false, true);
+            snprintf(result, ARRAYSIZE(result), formatStr, (CLR_INT64_TEMP_CAST)value->NumericByRef().s8);
+#endif // defined(_WIN32)
+        }
+        break;
 
         case DATATYPE_U8:
-            {
-              #if defined(_WIN32)
-                hal_snprintf( result, ARRAYSIZE(result), "%I64u",(CLR_UINT64_TEMP_CAST)value->NumericByRef().u8 ); 
-              #else
-                // get format string
-                nf_GetFormatString(formatStr, formatCh, precision, true, false, false);
-                sprintf(result, formatStr, (CLR_UINT64_TEMP_CAST)value->NumericByRef().u8 );
-              #endif // defined(_WIN32)
-            }
-            break;
+        {
+#if defined(_WIN32)
+            hal_snprintf(result, ARRAYSIZE(result), "%I64u", (CLR_UINT64_TEMP_CAST)value->NumericByRef().u8);
+#else
+            // get format string
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, true, false, false);
+            snprintf(result, ARRAYSIZE(result), formatStr, (CLR_UINT64_TEMP_CAST)value->NumericByRef().u8);
+#endif // defined(_WIN32)
+        }
+        break;
 
         case DATATYPE_R4:
+        {
+            // All the formatCh have been converted to upper case in the managed layer
+            _ASSERTE(formatCh == 'G' || formatCh == 'N' || formatCh == 'F' || formatCh == 'D');
+
+            if (precision < 0 || precision > 99)
             {
-                // All the formatCh have been converted to upper case in the managed layer
-                _ASSERTE(formatCh == 'G' || formatCh == 'N' || formatCh == 'F' || formatCh == 'D');
-
-                if (precision < 0 || precision > 99) 
-                {
-                    NANOCLR_SET_AND_LEAVE( CLR_E_INVALID_PARAMETER );
-                }
-
-                // DATATYPE_R4 is float and uses a precision of 9, standard sprintf format is "%.9g"
-
-              #if defined(_WIN32) || defined(WIN32) || defined(_WIN32_WCE)
-                hal_snprintf( result, ARRAYSIZE(result), "%.9g", value->NumericByRef().r4 );
-              #else
-
-              #if !defined(NANOCLR_EMULATED_FLOATINGPOINT)
-                // use default precision is none is specfied AND if format in not generic
-                precision = (formatCh == 'G' && precision == 1) ? 9 : precision;
-
-                // get format string
-                nf_GetFormatString(formatStr, formatCh, precision, false, true, false);
-
-                sprintf(result, formatStr, value->NumericByRef().r4);
-                if (shouldRemovePrependedZeros)
-                {
-                    nf_RemovePrependedZeros(result);
-                }
-              #else
-                CLR_INT32 f = value->NumericByRef().r4;
-                NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
-              #endif // !defined(NANOCLR_EMULATED_FLOATINGPOINT)
-              #endif // defined(_WIN32)
+                NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
             }
-            break;
+
+            // DATATYPE_R4 is float and uses a precision of 9, standard sprintf format is "%.9g"
+
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN32_WCE)
+            hal_snprintf(result, ARRAYSIZE(result), "%.9g", value->NumericByRef().r4);
+#else
+
+#if !defined(NANOCLR_EMULATED_FLOATINGPOINT)
+            // use default precision is none is specfied AND if format in not generic
+            precision = (formatCh == 'G' && precision == 1) ? 9 : precision;
+
+            // get format string
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, true, false);
+
+            snprintf(result, ARRAYSIZE(result), formatStr, value->NumericByRef().r4);
+            if (shouldRemovePrependedZeros)
+            {
+                nf_RemovePrependedZeros(result);
+            }
+#else
+            CLR_INT32 f = value->NumericByRef().r4;
+            NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
+#endif // !defined(NANOCLR_EMULATED_FLOATINGPOINT)
+#endif // defined(_WIN32)
+        }
+        break;
 
         case DATATYPE_R8:
+        {
+            // All the formatCh have been converted to upper case in the managed layer
+            _ASSERTE(formatCh == 'G' || formatCh == 'N' || formatCh == 'F' || formatCh == 'D');
+
+            if (precision < 0 || precision > 99)
             {
-                // All the formatCh have been converted to upper case in the managed layer
-                _ASSERTE(formatCh == 'G' || formatCh == 'N' || formatCh == 'F' || formatCh == 'D');
-
-                if (precision < 0 || precision > 99) 
-                {
-                    NANOCLR_SET_AND_LEAVE( CLR_E_INVALID_PARAMETER );
-                }
-
-                // DATATYPE_R8 is double and uses a precision of 15, standard sprintf format is "%.15g"
-
-              #if defined(_WIN32) || defined(WIN32) || defined(_WIN32_WCE)
-                hal_snprintf( result, ARRAYSIZE(result), "%.15g", (CLR_DOUBLE_TEMP_CAST)value->NumericByRef().r8 );
-              #else
-              #if !defined(NANOCLR_EMULATED_FLOATINGPOINT)
-                // use default precision is none is specfied AND format is not generic
-                precision = (formatCh == 'G' && precision == 1) ? 15 : precision;
-
-                // get format string
-                nf_GetFormatString(formatStr, formatCh, precision, false, true, false);
-                
-                sprintf(result, formatStr, (CLR_DOUBLE_TEMP_CAST)value->NumericByRef().r8);
-                if (shouldRemovePrependedZeros)
-                {
-                    nf_RemovePrependedZeros(result);
-                }
-              #else
-                CLR_INT64 d = (CLR_DOUBLE_TEMP_CAST)value->NumericByRef().r8;
-                NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
-              #endif // !defined(NANOCLR_EMULATED_FLOATINGPOINT)
-              #endif // defined(_WIN32)
+                NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
             }
-            break;
+
+            // DATATYPE_R8 is double and uses a precision of 15, standard sprintf format is "%.15g"
+
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN32_WCE)
+            hal_snprintf(result, ARRAYSIZE(result), "%.15g", (CLR_DOUBLE_TEMP_CAST)value->NumericByRef().r8);
+#else
+#if !defined(NANOCLR_EMULATED_FLOATINGPOINT)
+            // use default precision is none is specfied AND format is not generic
+            precision = (formatCh == 'G' && precision == 1) ? 15 : precision;
+
+            // get format string
+            nf_GetFormatString(formatStr, ARRAYSIZE(formatStr), formatCh, precision, false, true, false);
+
+            snprintf(result, ARRAYSIZE(result), formatStr, (CLR_DOUBLE_TEMP_CAST)value->NumericByRef().r8);
+            if (shouldRemovePrependedZeros)
+            {
+                nf_RemovePrependedZeros(result);
+            }
+#else
+            CLR_INT64 d = (CLR_DOUBLE_TEMP_CAST)value->NumericByRef().r8;
+            NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
+#endif // !defined(NANOCLR_EMULATED_FLOATINGPOINT)
+#endif // defined(_WIN32)
+        }
+        break;
 
         default:
             NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
     }
 
-    NANOCLR_SET_AND_LEAVE(stack.SetResult_String( result ));
+    NANOCLR_SET_AND_LEAVE(stack.SetResult_String(result));
 
     NANOCLR_NOCLEANUP();
 }
