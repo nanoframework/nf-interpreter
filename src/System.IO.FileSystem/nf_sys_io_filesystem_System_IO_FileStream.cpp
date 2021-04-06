4f4f1a25cbb1bd038deea742ab3459672065ae57
nfbot <nanoframework@outlook.com>
2021-04-06 06:55:24 +0000

    Code style fixes
    Automated fixes for code style.

--- src/System.IO.FileSystem/nf_sys_io_filesystem_System_IO_FileStream.cpp
+++ src/System.IO.FileSystem/nf_sys_io_filesystem_System_IO_FileStream.cpp
@@ -118,11 +118,11 @@ HRESULT Library_nf_sys_io_filesystem_System_IO_FileStream::OpenFileNative___VOID
             NANOCLR_SET_AND_LEAVE(CLR_E_FILE_NOT_FOUND);
         }
         if (operationResult == FR_OK)
-            {
-                // file created (or opened) succesfully
-                // OK to close it
-                f_close(&file);
-            }
+        {
+            // file created (or opened) succesfully
+            // OK to close it
+            f_close(&file);
+        }
         else
         {
             // failed to create the file
@@ -223,7 +223,7 @@ HRESULT Library_nf_sys_io_filesystem_System_IO_FileStream::ReadNative___I4__STRI
                 // failed to read the file
                 NANOCLR_SET_AND_LEAVE(CLR_E_FILE_IO);
             }
-            
+
             // OK to close it
             f_close(&file);
         }
