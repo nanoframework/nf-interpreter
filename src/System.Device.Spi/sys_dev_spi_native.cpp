6593283d49bac781d612e52f9b3da0ee28f34a6b
Laurent Ellerbach <laurelle@microsoft.com>
2021-04-18 15:51:21 +0200

    adding basic system.device.spi

---
+++ src/System.Device.Spi/sys_dev_spi_native.cpp
@@ -0,0 +1,80 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include "sys_dev_spi_native.h"
+
+// clang-format off
+
+static const CLR_RT_MethodHandler method_lookup[] =
+{
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiBusInfo::NativeChipSelectLineCount___I4,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiBusInfo::NativeMaxClockFrequency___I4,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiBusInfo::NativeMinClockFrequency___I4,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiDevice::DisposeNative___VOID,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiDevice::NativeTransfer___VOID__SZARRAY_U1__SZARRAY_U1__BOOLEAN,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiDevice::NativeTransfer___VOID__SZARRAY_U2__SZARRAY_U2__BOOLEAN,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiDevice::NativeInit___VOID,
+    Library_sys_dev_spi_native_System_Device_Spi_SpiDevice::NativeOpenDevice___I4,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
+
+const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_Device_Spi =
+{
+    "System.Device.Spi",
+    0x360239F1,
+    method_lookup,
+    { 100, 1, 4, 2 }
+};
+
+// clang-format on
---
+++ src/System.Device.Spi/sys_dev_spi_native.h
@@ -0,0 +1,78 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef _SYS_DEV_SPI_NATIVE_H_
+#define _SYS_DEV_SPI_NATIVE_H_
+
+#include <nanoCLR_Interop.h>
+#include <nanoCLR_Runtime.h>
+#include <nanoCLR_Checks.h>
+
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+struct Library_sys_dev_spi_native_System_Device_Spi_SpiBusInfo
+{
+    static const int FIELD___controllerId = 1;
+
+    NANOCLR_NATIVE_DECLARE(NativeChipSelectLineCount___I4);
+    NANOCLR_NATIVE_DECLARE(NativeMaxClockFrequency___I4);
+    NANOCLR_NATIVE_DECLARE(NativeMinClockFrequency___I4);
+
+    //--//
+};
+
+struct Library_sys_dev_spi_native_System_Device_Spi_SpiConnectionSettings
+{
+    static const int FIELD___csLine = 1;
+    static const int FIELD___clockFrequency = 2;
+    static const int FIELD___databitLength = 3;
+    static const int FIELD___spiMode = 4;
+    static const int FIELD___spiSharingMode = 5;
+    static const int FIELD___dataFlow = 6;
+    static const int FIELD___busId = 7;
+    //--//
+};
+
+struct Library_sys_dev_spi_native_System_Device_Spi_SpiController
+{
+    static const int FIELD___syncLock = 1;
+    static const int FIELD___controllerId = 2;
+
+    NANOCLR_NATIVE_DECLARE(GetDeviceSelector___STATIC__STRING);
+
+    //--//
+};
+
+struct Library_sys_dev_spi_native_System_Device_Spi_SpiControllerManager
+{
+    static const int FIELD_STATIC__s_syncLock = 0;
+    static const int FIELD_STATIC__s_controllersCollection = 1;
+
+    //--//
+};
+
+struct Library_sys_dev_spi_native_System_Device_Spi_SpiDevice
+{
+    static const int FIELD___deviceId = 1;
+    static const int FIELD___connectionSettings = 2;
+    static const int FIELD___spiController = 3;
+    static const int FIELD___syncLock = 4;
+    static const int FIELD___disposedValue = 5;
+
+    NANOCLR_NATIVE_DECLARE(DisposeNative___VOID);
+    NANOCLR_NATIVE_DECLARE(NativeTransfer___VOID__SZARRAY_U1__SZARRAY_U1__BOOLEAN);
+    NANOCLR_NATIVE_DECLARE(NativeTransfer___VOID__SZARRAY_U2__SZARRAY_U2__BOOLEAN);
+    NANOCLR_NATIVE_DECLARE(NativeInit___VOID);
+    NANOCLR_NATIVE_DECLARE(NativeOpenDevice___I4);
+
+    //--//
+
+    static HRESULT NativeTransfer(CLR_RT_StackFrame &stack, bool bufferIs16bits);
+};
+
+extern const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_Device_Spi;
+
+#endif //_SYS_DEV_SPI_NATIVE_H_
