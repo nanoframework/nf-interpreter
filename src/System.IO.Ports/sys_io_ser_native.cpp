54494de90bbc853c84bc2a6fbfab23d6dbcff8ff
Laurent Ellerbach <laurelle@microsoft.com>
2021-05-20 09:09:48 +0200

    Adjusting build pipeline

--- src/System.IO.Ports/sys_io_ser_native.cpp
+++ src/System.IO.Ports/sys_io_ser_native.cpp
@@ -89,13 +89,12 @@ static const CLR_RT_MethodHandler method_lookup[] =
     NULL,
     NULL,
     NULL,
-    NULL,
 };
 
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_IO_Ports =
 {
     "System.IO.Ports",
-    0x4EA5273D,
+    0x5292BAB2,
     method_lookup,
     { 100, 1, 1, 2 }
 };
