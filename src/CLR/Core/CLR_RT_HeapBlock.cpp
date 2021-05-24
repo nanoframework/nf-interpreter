c29ea8824cfc429240702abab7060c56e2865564
edleno2 <edleno@comcast.net>
2021-05-24 11:17:22 -0700

    Use invalid cast exception for bad casts
    Unit tests of boxing/unboxing are expecting invalid cast exception.  Code was returning generic System.Exception with HR of CLR_E_WRONG_TYPE.  Changed code since invalid cast exception is more descriptive of the problem.

--- src/CLR/Core/CLR_RT_HeapBlock.cpp
+++ src/CLR/Core/CLR_RT_HeapBlock.cpp
@@ -878,10 +878,11 @@ HRESULT CLR_RT_HeapBlock::PerformBoxing(const CLR_RT_TypeDef_Instance &cls)
  *  Function parameters:
  *  1. this - Heap block at the top of evaluation stack.
  *  2. cls  - Runtime Type Definition of the type specified after instruction.
- *  The functoin takes the object pointed by top of ev. stack. Then it does 3 operatioins:
+ * 
+ *  The function takes the object pointed by top of ev. stack. Then it does 3 operatioins:
  *  1. Dereferences the object
  *  2. Validates the type of data kept by object corresponds to type in cls.
- *  3. Moves de-referenced date to top of evaluation stack.
+ *  3. Moves de-referenced data to top of evaluation stack.
  *******************************************************************************************/
 
 HRESULT CLR_RT_HeapBlock::PerformUnboxing(const CLR_RT_TypeDef_Instance &cls)
@@ -893,7 +894,7 @@ HRESULT CLR_RT_HeapBlock::PerformUnboxing(const CLR_RT_TypeDef_Instance &cls)
 
     if (this->DataType() != DATATYPE_OBJECT)
     {
-        NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
+        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_CAST);
     }
 
     // Finds the object that keeps the boxed type.
@@ -903,7 +904,7 @@ HRESULT CLR_RT_HeapBlock::PerformUnboxing(const CLR_RT_TypeDef_Instance &cls)
     // Validates that src keeps something boxed and the boxed value is VALUE type.
     if (src->IsBoxed() == false || src->DataType() != DATATYPE_VALUETYPE)
     {
-        NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
+        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_CAST); 
     }
 
     // Validates the type of data kept by object corresponds to type in cls.
@@ -915,7 +916,7 @@ HRESULT CLR_RT_HeapBlock::PerformUnboxing(const CLR_RT_TypeDef_Instance &cls)
         // This "if" compares underlying type in object and cls. Should be equal in order to continue.
         if (!(src->DataSize() > 1 && (src[1].DataType() == cls.m_target->dataType)))
         {
-            // No luck. The types in src object and specified by cls are different. Need to throw exceptioin.
+            // No luck. The types in src object and specified by cls are different. Need to throw exception.
             NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_CAST);
         }
     }
