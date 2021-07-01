ed5e1a896a5bef1c84df444f17b6da23a02f8b9f
Roman Mashta <roman.mashta@gmail.com>
2021-07-01 21:25:42 +0300

    Use stdafx instead of pch

--- targets/netcore/nanoCLR/nanoCLR.Native/stdafx.cpp
+++ targets/netcore/nanoCLR/nanoCLR.Native/stdafx.cpp
@@ -1,5 +1,5 @@
 // pch.cpp: source file corresponding to the pre-compiled header
 
-#include "pch.h"
+#include "stdafx.h"
 
 // When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
