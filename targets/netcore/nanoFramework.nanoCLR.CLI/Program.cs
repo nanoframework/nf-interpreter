e2f95050dafc124b6adf4a7d7cdb65c315d33e20
Roman Mashta <roman.mashta@gmail.com>
2021-07-02 21:55:38 +0300

    Add Host Class Library Project and make Libraries names consistent

---
+++ targets/netcore/nanoFramework.nanoCLR.CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+using nanoFramework.nanoCLR.Host;
+
+namespace nanoFramework.nanoCLR.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interop.nanoclr_run(args.Length, args);
+        }
+    }
+}
---
+++ targets/netcore/nanoFramework.nanoCLR.Host/Interop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace nanoFramework.nanoCLR.Host
+{
+    public class Interop
+    {
+        public const string NativeLibraryName = "nanoFramework.nanoCLR.Native";
+
+        [DllImport(NativeLibraryName, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void nanoclr_run(int argc, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPWStr)] string[] argv);
+    }
+}
