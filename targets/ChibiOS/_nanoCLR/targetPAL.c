f0c7ef57b3c2b6817e0f1e9f06b9b20e0596cf44
nfbot <nanoframework@outlook.com>
2021-08-09 15:31:23 +0100

    Code style fixes (#92)
    Automated fixes for code style.

--- targets/ChibiOS/_nanoCLR/targetPAL.c
+++ targets/ChibiOS/_nanoCLR/targetPAL.c
@@ -5,35 +5,47 @@
 
 #include <hal.h>
 
-#if defined(STM32L0XX) || defined(STM32L4XX) || defined(STM32F0XX) || defined(STM32F4XX) || defined(STM32F7XX) || defined(STM32H7XX)
+#if defined(STM32L0XX) || defined(STM32L4XX) || defined(STM32F0XX) || defined(STM32F4XX) || defined(STM32F7XX) ||      \
+    defined(STM32H7XX)
 
-stm32_gpio_t* gpioPort[] = { GPIOA, GPIOB
+stm32_gpio_t *gpioPort[] = {
+    GPIOA,
+    GPIOB
 #if STM32_HAS_GPIOC
-, GPIOC
+    ,
+    GPIOC
 #endif
 #if STM32_HAS_GPIOD
-, GPIOD
+    ,
+    GPIOD
 #endif
 #if STM32_HAS_GPIOE
-, GPIOE
+    ,
+    GPIOE
 #endif
 #if STM32_HAS_GPIOF
-, GPIOF
+    ,
+    GPIOF
 #endif
 #if STM32_HAS_GPIOG
-, GPIOG
+    ,
+    GPIOG
 #endif
 #if STM32_HAS_GPIOH
-, GPIOH
+    ,
+    GPIOH
 #endif
 #if STM32_HAS_GPIOI
-, GPIOI
+    ,
+    GPIOI
 #endif
 #if STM32_HAS_GPIOJ
-, GPIOJ
+    ,
+    GPIOJ
 #endif
 #if STM32_HAS_GPIOK
-, GPIOK
+    ,
+    GPIOK
 #endif
 };
 
