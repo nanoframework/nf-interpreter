cd05f468a36e8c031c97b83005fa4fdf5b9d1906
José Simões <jose.simoes@eclo.solutions>
2021-08-10 09:48:40 +0100

    Add ExitCode and  VerbosityLevel
    - Rework progam main to grab version and other informational stuff to show on prompt.
    ** WIP **

---
+++ targets/netcore/nanoFramework.nanoCLR.CLI/ExitCode.cs
@@ -0,0 +1,29 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System.ComponentModel.DataAnnotations;
+
+namespace nanoFramework.nanoCLR.CLI
+{
+    public enum ExitCode
+    {
+        /// <summary>
+        /// Execution terminated without any error
+        /// </summary>
+        [Display(Name = "")]
+        OK = 0,
+
+        //////////////////////////////////////
+        // Virtual Serial Port Tools errors //
+        //////////////////////////////////////
+
+        /// <summary>
+        /// Virtual Serial Port Tools software not installed.
+        /// </summary>
+        [Display(Name = "Virtual Serial Port Tools software not installed. Make sure it's connected and has booted in DFU mode")]
+        E1000 = 1000,
+
+    }
+}
--- targets/netcore/nanoFramework.nanoCLR.CLI/Program.cs
+++ targets/netcore/nanoFramework.nanoCLR.CLI/Program.cs
@@ -1,19 +1,72 @@
 ﻿//
-// Copyright (c) 2017 The nanoFramework project contributors
+// Copyright (c) 2021 The nanoFramework project contributors
 // See LICENSE file in the project root for full license information.
 //
 
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using CommandLine;
+using CommandLine.Text;
 using nanoFramework.nanoCLR.Host;
 using nanoFramework.nanoCLR.VirtualCom;
 
 namespace nanoFramework.nanoCLR.CLI
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static ExitCode _exitCode;
+        private static string _extraMessage;
+        private static VerbosityLevel _verbosityLevel = VerbosityLevel.Normal;
+        private static AssemblyInformationalVersionAttribute _informationalVersionAttribute;
+        private static string _headerInfo;
+        private static CopyrightInfo _copyrightInfo;
+        internal static string ExecutingPath;
+
+        static int Main(string[] args)
         {
+            // take care of static fields
+            _informationalVersionAttribute = Attribute.GetCustomAttribute(
+                Assembly.GetEntryAssembly()!,
+                typeof(AssemblyInformationalVersionAttribute))
+            as AssemblyInformationalVersionAttribute;
+
+            _headerInfo = $".NET nanoFramework CLR CLI v{_informationalVersionAttribute.InformationalVersion}";
+
+            _copyrightInfo = new CopyrightInfo(true, $".NET Foundation and nanoFramework project contributors", 2019);
+
+            // need this to be able to use ProcessStart at the location where the .NET Core CLI tool is running from
+            string codeBase = Assembly.GetExecutingAssembly().Location;
+            var uri = new UriBuilder(codeBase);
+            var fullPath = Uri.UnescapeDataString(uri.Path);
+            ExecutingPath = Path.GetDirectoryName(fullPath);
+            
+            // check for empty argument collection
+            if (!args.Any())
+            {
+                // no argument provided, show help text and usage examples
+
+                // because of short-comings in CommandLine parsing 
+                // need to customize the output to provide a consistent output
+                var parser = new Parser(config => config.HelpWriter = null);
+                var result = parser.ParseArguments<RunCommandLineOptions>(new[] { "", "" });
+
+                var helpText = new HelpText(
+                    new HeadingInfo(_headerInfo),
+                    _copyrightInfo)
+                        .AddPreOptionsLine("No command was provided.")
+                        .AddPreOptionsLine("")
+                        .AddPreOptionsLine(HelpText.RenderUsageText(result))
+                        .AddPreOptionsLine("")
+                        .AddOptions(result);
+
+                Console.WriteLine(helpText.ToString());
+
+                return (int)ExitCode.OK;
+            }
+
             LogErrors(() =>
             {
                 VirtualComManager virtualComManager = new VirtualComManager();
@@ -22,7 +75,7 @@ namespace nanoFramework.nanoCLR.CLI
                 NanoClrHostBuilder hostBuilder = NanoClrHost.CreateBuilder();
                 hostBuilder.UseConsoleDebugPrint();
 
-                Parser.Default.ParseArguments<RunCommandLineOptions, VirtualComCommandLineOptions>(args)
+                var parsedArguments = Parser.Default.ParseArguments<RunCommandLineOptions, VirtualComCommandLineOptions>(args)
                     .MapResult(
                         (RunCommandLineOptions opts) =>
                             RunCommandProcessor.ProcessVerb(opts, hostBuilder, virtualComManager),
@@ -30,7 +83,53 @@ namespace nanoFramework.nanoCLR.CLI
                             VirtualComCommandProcessor.ProcessVerb(opts, virtualComManager),
                         errs => 1);
             });
+
+            if (_verbosityLevel > VerbosityLevel.Quiet)
+            {
+                OutputError(_exitCode, _verbosityLevel > VerbosityLevel.Normal, _extraMessage);
+            }
+
+            return (int)_exitCode;
         }
+
+        private static void OutputError(ExitCode errorCode, bool outputMessage, string extraMessage = null)
+        {
+            if (errorCode == ExitCode.OK)
+            {
+                return;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            if (outputMessage)
+            {
+                Console.Write($"Error {errorCode}");
+
+                var exitCodeDisplayName = errorCode.GetAttribute<DisplayAttribute>();
+
+                if (!string.IsNullOrEmpty(exitCodeDisplayName.Name))
+                {
+                    Console.Write($": { exitCodeDisplayName.Name }");
+                }
+
+                if (string.IsNullOrEmpty(extraMessage))
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($" ({ extraMessage })");
+                }
+            }
+            else
+            {
+                Console.Write($"{errorCode}");
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
 
         private static void LogErrors(Action scope)
         {
