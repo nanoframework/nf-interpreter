eb2fa45f220b8fb6f44fd94a9b4677a27d1b1f62
José Simões <jose.simoes@eclo.solutions>
2021-08-12 15:48:23 +0100

    Replace ChibiOS ELF driver with nano HAL

--- targets/AzureRTOS/ST/ST_B_L475E_IOT01A/nanoBooter/halconf_nf.h
+++ targets/AzureRTOS/ST/ST_B_L475E_IOT01A/nanoBooter/halconf_nf.h
@@ -7,8 +7,13 @@
 #define _HALCONF_NF_H_
 
 // Enables the ChibiOS community overlay.
-#if !defined(HAL_USE_COMMUNITY) 
-#define HAL_USE_COMMUNITY           TRUE
+#if !defined(HAL_USE_COMMUNITY)
+#define HAL_USE_COMMUNITY TRUE
+#endif
+
+// enables STM32 Flash driver
+#if !defined(HAL_NF_USE_STM32_FLASH)
+#define HAL_NF_USE_STM32_FLASH TRUE
 #endif
 
 #endif // _HALCONF_NF_H_
--- targets/AzureRTOS/ST/_common/Target_BlockStorage_STM32FlashDriver.c
+++ targets/AzureRTOS/ST/_common/Target_BlockStorage_STM32FlashDriver.c
@@ -3,101 +3,63 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include <hal.h>
 #include <Target_BlockStorage_STM32FlashDriver.h>
 
 // -- //
 
-bool STM32FlashDriver_InitializeDevice(void *context)
+bool STM32FlashDriver_InitializeDevice(void* context)
 {
     (void)context;
 
-    // nothing to do here
+    // ChibiOS driver takes care of this, so always true
     return true;
 }
 
-bool STM32FlashDriver_UninitializeDevice(void *context)
+bool STM32FlashDriver_UninitializeDevice(void* context)
 {
     (void)context;
 
-    // nothing to do here
+    // ChibiOS driver takes care of this, so always true
     return true;
 }
 
-DeviceBlockInfo *STM32FlashDriver_GetDeviceInfo(void *context)
+DeviceBlockInfo* STM32FlashDriver_GetDeviceInfo(void* context)
 {
-
-    MEMORY_MAPPED_NOR_BLOCK_CONFIG *config = context;
-
-    return config->BlockConfig.BlockDeviceInformation;
+    
+    MEMORY_MAPPED_NOR_BLOCK_CONFIG* config = context;
+    
+    return config->BlockConfig.BlockDeviceInformation;  
 }
 
-bool STM32FlashDriver_Read(void *context, ByteAddress startAddress, unsigned int numBytes, unsigned char *buffer)
+bool STM32FlashDriver_Read(void* context, ByteAddress startAddress, unsigned int numBytes, unsigned char* buffer)
 {
     (void)context;
 
-    volatile uint8_t *cursor = (volatile uint8_t *)startAddress;
-    volatile uint8_t *endAddress = (volatile uint8_t *)(startAddress + numBytes);
+    // driver doesn't return anything so: we have to perform the operation here...
+    stm32FlashReadBytes(startAddress, numBytes, buffer);
 
-    // copy contents from flash to buffer starting from the start address
-    while (cursor < endAddress)
-    {
-        *buffer++ = *cursor++;
-    }
-
+    // ... and always return true
     return true;
 }
 
-bool STM32FlashDriver_Write(
-    void *context,
-    ByteAddress startAddress,
-    unsigned int numBytes,
-    unsigned char *buffer,
-    bool readModifyWrite)
+bool STM32FlashDriver_Write(void* context, ByteAddress startAddress, unsigned int numBytes, unsigned char* buffer, bool readModifyWrite)
 {
     (void)context;
     (void)readModifyWrite;
 
-    return flashProgram(
-        &EFLD1, 
-        startAddress,
-        numBytes,
-        buffer) == FLASH_NO_ERROR;
+    return stm32FlashWrite(startAddress, numBytes, buffer);
 }
 
-bool STM32FlashDriver_IsBlockErased(void *context, ByteAddress blockAddress, unsigned int length)
-{
-    (void)context;
-(void)length;
-    
-    return flashVerifyErase(
-        &EFLD1,
-        blockAddress) == FLASH_NO_ERROR;
-}
-
-bool STM32FlashDriver_EraseBlock(void *context, ByteAddress address)
+bool STM32FlashDriver_IsBlockErased(void* context, ByteAddress blockAddress, unsigned int length)
 {
     (void)context;
 
-#ifdef DEBUG    
-    flash_error_t flashError;
-#endif
+    return stm32FlashIsErased(blockAddress, length);
+}
 
-#ifdef DEBUG    
-    flashError = flashStartEraseSector(&EFLD1, address);
-#else
-    if(flashStartEraseSector(&EFLD1, address) == FLASH_NO_ERROR)
-#endif
-    {
+bool STM32FlashDriver_EraseBlock(void* context, ByteAddress address)
+{
+    (void)context;
 
-#ifdef DEBUG    
-        flashError = flashQueryErase(&EFLD1, NULL);
-
-        return flashError == FLASH_NO_ERROR;
-#else
-        return flashQueryErase(&EFLD1, NULL) == FLASH_NO_ERROR;
-#endif
-    }
-
-    return false;
+    return stm32FlashErase(address);
 }
