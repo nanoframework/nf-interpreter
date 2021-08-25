57795af9a86284a122a662dfccc50a39233de43f
José Simões <jose.simoes@eclo.solutions>
2021-08-25 04:55:45 +0100

    Rework Azure RTOS build system

--- CMake/Modules/AzureRTOS_RP2040_GCC_options.cmake
+++ CMake/Modules/AzureRTOS_RP2040_GCC_options.cmake
@@ -13,34 +13,52 @@ set(CMAKE_CXX_FLAGS " -mthumb -mcpu=cortex-m0plus -Wextra -Werror -ffunction-sec
 # need to specify linker flags here
 set(CMAKE_EXE_LINKER_FLAGS " -Wl,--gc-sections -Wl,--no-wchar-size-warning -Wl,--print-memory-usage -mthumb -mcpu=cortex-m0plus -nostartfiles " CACHE INTERNAL "executable linker flags")
 
-function(NF_SET_COMPILER_OPTIONS TARGET)
+# TARGET parameter to set the target that's setting them for
+# optional EXTRA_COMPILE_OPTIONS with compile options to be added
+macro(nf_set_compile_options)
+
+    # parse arguments
+    cmake_parse_arguments(NFSCO "" "TARGET" "EXTRA_COMPILE_OPTIONS" ${ARGN})
+    
+    if(NOT NFSCO_TARGET OR "${NFSCO_TARGET}" STREQUAL "")
+        message(FATAL_ERROR "Need to set TARGET argument when calling nf_set_compile_options()")
+    endif()
 
     # include any extra options coming from any extra args?
-    target_compile_options(${TARGET} PUBLIC  ${ARGN} -mthumb -mcpu=cortex-m0plus -mtune=cortex-m0plus -nostdlib -Wall -Wextra -Werror -Wundef -Wshadow -Wimplicit-fallthrough -fshort-wchar -fno-builtin -fno-common -mno-long-calls -fno-exceptions -fcheck-new )
+    target_compile_options(${NFSCO_TARGET} PUBLIC  ${NFSCO__EXTRA_COMPILE_OPTIONS} -mthumb -mcpu=cortex-m0plus -mtune=cortex-m0plus -nostdlib -Wall -Wextra -Werror -Wundef -Wshadow -Wimplicit-fallthrough -fshort-wchar -fno-builtin -fno-common -mno-long-calls -fno-exceptions -fcheck-new )
 
     # this series doesn't have FPU 
-    target_compile_definitions(${TARGET} PUBLIC -DUSE_FPU=FALSE)
+    target_compile_definitions(${NFSCO_TARGET} PUBLIC -DPLATFORM_ARM -DUSE_FPU=FALSE)
 
-endfunction()
+endmacro()
 
 
-function(NF_SET_LINKER_OPTIONS TARGET)
+# TARGET parameter to set the target that's setting them for
+# optional EXTRA_LINK_FLAGS with link flags to be added
+macro(nf_set_linker_options)
+
+    # parse arguments
+    cmake_parse_arguments(NFSLO "" "TARGET;EXTRA_LINK_FLAGS" "" ${ARGN})
+    
+    if(NOT NFSLO_TARGET OR "${NFSLO_TARGET}" STREQUAL "")
+        message(FATAL_ERROR "Need to set TARGET argument when calling nf_set_linker_options()")
+    endif()
 
     # request specs from newlib nano
-    set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " --specs=nano.specs ")
-
-    # set extra linker flags
-    set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " ${ARGN}")
+    set_property(TARGET ${NFSLO_TARGET} APPEND_STRING PROPERTY LINK_FLAGS " --specs=nano.specs ")
 
     # set optimization linker flags for RELEASE and MinSizeRel
     if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
-        set_property(TARGET ${TARGET} APPEND_STRING PROPERTY LINK_FLAGS " -flto -Os -fstrict-aliasing -fomit-frame-pointer -fno-unroll-loops -frounding-math -fsignaling-nans -ffloat-store -fno-math-errno -ftree-vectorize -fno-default-inline -finline-functions-called-once -fno-defer-pop ")
+        set_property(TARGET ${NFSLO_TARGET} APPEND_STRING PROPERTY LINK_FLAGS " -flto -Os -fstrict-aliasing -fomit-frame-pointer -fno-unroll-loops -frounding-math -fsignaling-nans -ffloat-store -fno-math-errno -ftree-vectorize -fno-default-inline -finline-functions-called-once -fno-defer-pop ")
     endif()
 
-    # set optimization flags
-    nf_set_optimization_options(${TARGET})
-    
-    # set link map
-    nf_set_link_map(${TARGET})
+    # include libraries in build
+    nf_include_libraries_in_build(${NFSLO_TARGET})
 
-endfunction()
+    # set extra linker flags
+    set_property(TARGET ${NFSLO_TARGET} APPEND_STRING PROPERTY LINK_FLAGS " ${NFSLO__EXTRA_LINK_FLAGS} ")
+      
+    # set optimization flags
+    nf_set_optimization_options(${NFSLO_TARGET})
+
+endmacro()
