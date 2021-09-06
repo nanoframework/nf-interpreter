04bdc0e0bb7135d39ac7a7344148dcb6f3cee4fa
Richard <doingnz@gmail.com>
2021-09-07 02:17:27 +1200

    Update STM32_IRQ_USART1_PRIORITY  to 3
    Raising the IRQ priority reduces the opportunity for other interrupts to preempt the USART ISR which needs to keep up with the incoming serial characters as 460800 baud as tis CPU does not have a FIFO queue to buffer the characters in hardware.

--- targets/AzureRTOS/ST/ST_B_L475E_IOT01A/nanoCLR/mcuconf.h
+++ targets/AzureRTOS/ST/ST_B_L475E_IOT01A/nanoCLR/mcuconf.h
@@ -122,7 +122,7 @@
 #define STM32_IRQ_TIM8_UP_PRIORITY          7
 #define STM32_IRQ_TIM8_CC_PRIORITY          7
 
-#define STM32_IRQ_USART1_PRIORITY           12
+#define STM32_IRQ_USART1_PRIORITY           3
 #define STM32_IRQ_USART2_PRIORITY           12
 #define STM32_IRQ_USART3_PRIORITY           12
 #define STM32_IRQ_UART4_PRIORITY            12
