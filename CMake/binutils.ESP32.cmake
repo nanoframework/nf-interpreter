010b9e36fe15a07cfba2b3386945b9144aaf1f75
José Simões <jose.simoes@eclo.solutions>
2021-09-10 13:18:18 +0100

    Rework ESP32 bin utils

--- CMake/binutils.ESP32.cmake
+++ CMake/binutils.ESP32.cmake
@@ -93,6 +93,8 @@ macro(nf_add_platform_packages)
     # parse arguments
     cmake_parse_arguments(NFAPP "" "TARGET" "" ${ARGN})
 
+    find_package(ESP32_IDF REQUIRED QUIET)
+
     # packages specific for nanoCRL
     if("${NFAPP_TARGET}" STREQUAL "${NANOCLR_PROJECT_NAME}")
 
@@ -155,22 +157,6 @@ endmacro()
 macro(nf_add_platform_include_directories target)
 
     target_include_directories(${target}.elf PUBLIC
-                
-        # target path (both source and binary)
-        ${CMAKE_CURRENT_BINARY_DIR}
-        ${CMAKE_CURRENT_SOURCE_DIR}
-        # target common path
-        ${CMAKE_CURRENT_SOURCE_DIR}/common
-
-        # path for CMake target (both source and binary)
-        ${CMAKE_CURRENT_BINARY_DIR}/${target}
-        ${CMAKE_CURRENT_SOURCE_DIR}/${target}
-
-        ${NF_CoreCLR_INCLUDE_DIRS}
-        ${NF_NativeAssemblies_INCLUDE_DIRS}
-
-        ${WireProtocol_INCLUDE_DIRS}
-    )
 
     target_include_directories(${target}.elf PUBLIC
 
@@ -182,9 +168,9 @@ macro(nf_add_platform_include_directories target)
         ${NF_Network_INCLUDE_DIRS}
 
     # includes specific to nanoCRL
-    if(${TARGET} STREQUAL ${NANOCLR_PROJECT_NAME})
+    if(${target} STREQUAL ${NANOCLR_PROJECT_NAME})
 
-        target_include_directories(${TARGET}.elf PUBLIC
+        target_include_directories(${target}.elf PUBLIC
 
             ${TARGET_ESP32_IDF_NANOCLR_INCLUDE_DIRS}
         )
@@ -204,25 +190,20 @@ macro(nf_add_platform_sources target)
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/target_common.h.in
                    ${CMAKE_BINARY_DIR}/targets/${RTOS}/${TARGET_BOARD}/target_common.h @ONLY)
 
-    configure_file(${CMAKE_SOURCE_DIR}/CMake/ESP32_target_os.h.in
-                   ${CMAKE_BINARY_DIR}/targets/${RTOS}/${TARGET_BOARD}/target_os.h @ONLY)
+    # sources common to both builds
+    target_sources(${target}.elf PUBLIC
+    
+        ${TARGET_ESP32_IDF_COMMON_SOURCES}
 
     target_sources(${target}.elf PUBLIC
 
-        ${CMAKE_CURRENT_SOURCE_DIR}/target_common.c
-        ${CMAKE_CURRENT_SOURCE_DIR}/target_BlockStorage.c
-        ${CMAKE_SOURCE_DIR}/src/PAL/BlockStorage/nanoPAL_BlockStorage.c
-
-        ${COMMON_PROJECT_SOURCES}
-        ${WireProtocol_SOURCES}
+    # sources specific to nanoCRL
+    if(${target} STREQUAL ${NANOCLR_PROJECT_NAME})
 
         configure_file(${CMAKE_CURRENT_SOURCE_DIR}/nanoCLR/target_board.h.in
                     ${CMAKE_CURRENT_BINARY_DIR}/nanoCLR/target_board.h @ONLY)
 
-        ${NF_HALCore_SOURCES}
-        ${NF_CoreCLR_SOURCES}
-        ${NF_Debugger_SOURCES}
-        ${NF_Diagnostics_SOURCES}
+        target_sources(${target}.elf PUBLIC
 
             ${TARGET_ESP32_IDF_NANOCLR_SOURCES}
             ${ESP32_IDF_SOURCES}
@@ -230,64 +211,104 @@ macro(nf_add_platform_sources target)
 
     endif()
 
-    # mbed TLS requires a config file
-    if(USE_SECURITY_MBEDTLS_OPTION)
-        # this seems to be only option to properly set a compiler define through the command line that needs to be a string literal
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DMBEDTLS_CONFIG_FILE=\"<${CMAKE_SOURCE_DIR}/src/PAL/COM/sockets/ssl/mbedTLS/nf_mbedtls_config.h>\"")
-        SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMBEDTLS_CONFIG_FILE=\"<${CMAKE_SOURCE_DIR}/src/PAL/COM/sockets/ssl/mbedTLS/nf_mbedtls_config.h>\"")
-    endif()
+    # # mbed TLS requires a config file
+    # if(USE_SECURITY_MBEDTLS_OPTION)
+    #     # this seems to be only option to properly set a compiler define through the command line that needs to be a string literal
+    #     SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DMBEDTLS_CONFIG_FILE=\"<${CMAKE_SOURCE_DIR}/src/PAL/COM/sockets/ssl/mbedTLS/nf_mbedtls_config.h>\"")
+    #     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMBEDTLS_CONFIG_FILE=\"<${CMAKE_SOURCE_DIR}/src/PAL/COM/sockets/ssl/mbedTLS/nf_mbedtls_config.h>\"")
+    # endif()
 
 endmacro()
 
 # macro to setup the build for a target
-# mandatory HAS_NANOBOOTER specifing if the target implements nanoBooter
-# BOOTER_LINKER_FILE with the path to the linker file for nanoBooter (if the target has it)
 # mandatory CLR_LINKER_FILE with the path to the linker file for nanoCLR
-# optional BOOTER_EXTRA_SOURCE_FILES with paths to extra files to be added to the nanoBooter build target
 # optional CLR_EXTRA_SOURCE_FILES with paths to extra files to be added to the nanoCLR build target
-# optional BOOTER_EXTRA_COMPILE_DEFINITIONS extra nanoBooter compile definitions to pass to nf_set_compile_definitions() 
 # optional CLR_EXTRA_COMPILE_DEFINITIONS extra nanoCLR compile definitions to pass to nf_set_compile_definitions() 
-# optional BOOTER_EXTRA_LINKMAP_PROPERTIES extra nanoBooter link map properties to pass to nf_set_link_map() 
 # optional CLR_EXTRA_LINKMAP_PROPERTIES extra nanoCLR link map properties to pass to nf_set_link_map() 
-# optional BOOTER_EXTRA_LINK_FLAGS extra nanoBooter link flags to pass to nf_set_link_options() 
 # optional CLR_EXTRA_LINK_FLAGS extra nanoCLR link flags to pass to nf_set_link_options() 
-
-# optional ESP32_EXTRA_COMPONENTS extra IDF components to add to the project
 macro(nf_setup_target_build)
 
+    # parse arguments
+    cmake_parse_arguments(
+        NFSTBC 
+        "" 
+        "CLR_LINKER_FILE;CLR_EXTRA_LINKMAP_PROPERTIES;CLR_EXTRA_COMPILE_DEFINITIONS;CLR_EXTRA_COMPILE_OPTIONS;CLR_EXTRA_LINK_FLAGS" 
+        "CLR_EXTRA_SOURCE_FILES;CLR_EXTRA_LIBRARIES" 
+        ${ARGN})
 
-    # # Create idf::esp32 and idf::freertos static libraries
-    # idf_build_process(esp32
-    #     # try and trim the build; additional components
-    #     # will be included as needed based on dependency tree
-    #     #
-    #     # although esptool_py does not generate static library,
-    #     # processing the component is needed for flashing related
-    #     # targets and file generation
-    #     COMPONENTS 
-    #         esp32s2
-    #         freertos
-    #         esptool_py
-        
-    #     SDKCONFIG ${CMAKE_CURRENT_DIR}/sdkconfig
-    #     BUILD_DIR ${CMAKE_BINARY_DIR})
+    #######################################
+    # now the actual calls for building a target
 
-    #         set(elf_file ${CMAKE_PROJECT_NAME}.elf)
-    #         add_executable(${elf_file} nanoCLR/app_main.c)
+    # add packages
+    nf_add_common_packages()
+    nf_add_platform_packages()
 
+    #######################################
 
-    # # OK to pass ARGN, to have it perform it's parsings and validation 
-    # # nf_setup_target_build_common(${ARGN})
+    add_subdirectory("common")
 
-    # target_link_libraries(${elf_file} idf::esp32 idf::freertos idf::spi_flash)
+    #######################
+    # nanoCLR executable has already been created on main CMakeLists
+    # just add any extra source files here
+    target_sources(
+        ${NANOCLR_PROJECT_NAME}.elf PUBLIC
+
+        # extra build files 
+        ${NFSTBC_CLR_EXTRA_SOURCE_FILES}
+    )
+
+    nf_add_platform_packages(TARGET ${NANOCLR_PROJECT_NAME})
+    nf_add_platform_dependencies(${NANOCLR_PROJECT_NAME})
     
-    # # Attach additional targets to the executable file for flashing,
-    # # linker script generation, partition_table generation, etc.
-    # idf_build_executable(${elf_file})
+    # can't call  nf_add_common_sources() just like with the other platforms
+    # so need to replicate the code here  
+    target_sources(${NANOCLR_PROJECT_NAME}.elf PUBLIC
+    
+        ${CMAKE_CURRENT_SOURCE_DIR}/target_common.c
+        ${CMAKE_CURRENT_SOURCE_DIR}/target_BlockStorage.c
+        ${CMAKE_SOURCE_DIR}/src/PAL/BlockStorage/nanoPAL_BlockStorage.c
+        ${COMMON_PROJECT_SOURCES}
+        ${NF_HALCore_SOURCES}
+    )
 
+    target_link_libraries(${NANOCLR_PROJECT_NAME}.elf
+        NF_CoreCLR
+        NF_NativeAssemblies
+        NF_Debugger
+        WireProtocol
+        
+        ${NFACS_EXTRA_LIBRARIES}
+    )
+
+    target_sources(${NANOCLR_PROJECT_NAME}.elf PUBLIC
+
+        ${NANOCLR_PROJECT_SOURCES}
+
+        # sources for nanoFramework libraries
+        ${Graphics_Sources}
+    )
+
+    nf_add_platform_sources(${NANOCLR_PROJECT_NAME})
+
+    # include directories for nanoCLR
+    nf_add_common_include_directories(${NANOCLR_PROJECT_NAME})
+    nf_add_platform_include_directories(${NANOCLR_PROJECT_NAME})
+
+    # set compile options
+    nf_set_compile_options(TARGET ${NANOCLR_PROJECT_NAME}.elf EXTRA_COMPILE_OPTIONS ${NFSTBC_BOOTER_EXTRA_COMPILE_OPTIONS})
+
+    # set compile definitions
+    nf_set_compile_definitions(TARGET ${NANOCLR_PROJECT_NAME}.elf EXTRA_COMPILE_DEFINITIONS ${NFSTBC_CLR_EXTRA_COMPILE_DEFINITIONS} BUILD_TARGET ${NANOCLR_PROJECT_NAME} )
+
+    # set link options
+    nf_set_link_options(TARGET ${NANOCLR_PROJECT_NAME}.elf EXTRA_LINK_FLAGS ${NFSTBC_CLR_EXTRA_LINK_FLAGS})
+
+    # this where all the **magic** happens with the IDF build
+    idf_build_executable(${NANOCLR_PROJECT_NAME}.elf)
 
 endmacro()
 
+# macro to add IDF as a library to the build and add the IDF components according to variant and options
 macro(nf_add_idf_as_library)
 
     include($ENV{IDF_PATH}/tools/cmake/idf.cmake)
@@ -302,8 +323,6 @@ macro(nf_add_idf_as_library)
         # SDKCONFIG ${CMAKE_SOURCE_DIR}/targets/FreeRTOS_ESP32/_IDF/sdkconfig
         BUILD_DIR ${CMAKE_BINARY_DIR}
     )
-
-    add_executable(${NANOCLR_PROJECT_NAME}.elf ${CMAKE_SOURCE_DIR}/targets/FreeRTOS_ESP32/_IDF/${TARGET_SERIES_SHORT}/app_main.c)
 
     # Link the static libraries to the executable
     target_link_libraries(${NANOCLR_PROJECT_NAME}.elf 
