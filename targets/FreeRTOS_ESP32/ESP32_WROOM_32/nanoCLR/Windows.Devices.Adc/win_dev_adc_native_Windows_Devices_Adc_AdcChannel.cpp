3f5612293c6ba2c2a629587df17d2b66bb7d56bf
Adrian Soundy <adriansoundy@gmail.com>
2021-09-13 03:05:44 +1200

    Fix ADC2 and tidy up (#2043)

--- targets/FreeRTOS_ESP32/ESP32_WROOM_32/nanoCLR/Windows.Devices.Adc/win_dev_adc_native_Windows_Devices_Adc_AdcChannel.cpp
+++ targets/FreeRTOS_ESP32/ESP32_WROOM_32/nanoCLR/Windows.Devices.Adc/win_dev_adc_native_Windows_Devices_Adc_AdcChannel.cpp
@@ -6,9 +6,9 @@
 
 #include "win_dev_adc_native_target.h"
 
-extern "C" uint8_t temprature_sens_read(); 
+extern "C" uint8_t temprature_sens_read();
 
-HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadValue___I4( CLR_RT_StackFrame& stack )
+HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadValue___I4(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
     {
@@ -16,50 +16,55 @@ HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeReadVal
         esp_err_t result;
 
         // get a pointer to the managed object instance and check that it's not NULL
-        CLR_RT_HeapBlock* pThis = stack.This();  FAULT_ON_NULL(pThis);
+        CLR_RT_HeapBlock *pThis = stack.This();
+        FAULT_ON_NULL(pThis);
 
         // Get channel from _channelNumber field
         int channelNumber = pThis[FIELD___channelNumber].NumericByRef().s4;
 
-        // need to get the controllerId for the ADC controller of this channel
-        // get pointer to AdcController field
-        //CLR_RT_HeapBlock* adcController = pThis[FIELD___adcController].Dereference();
-
-        // get pointer to _controllerId field
-//       int adcNumber = adcController[Library_win_dev_adc_native_Windows_Devices_Adc_AdcController::FIELD___controllerId].NumericByRef().s4;
+        // Calculate internal ADC number based on channel number
         int adcNumber = channelNumber <= 9 ? 1 : 2;
 
-        if ( adcNumber == 1)
+        if (adcNumber == 1)
         {
-            switch(channelNumber)
+            switch (channelNumber)
             {
                 case 8:
-                    reading = temprature_sens_read(); break;
+                    reading = temprature_sens_read();
+                    break;
                 case 9:
-                    reading = hall_sensor_read(); break;
+                    reading = hall_sensor_read();
+                    break;
                 default:
-                     reading = adc1_get_raw( (adc1_channel_t)channelNumber); break;
+                    reading = adc1_get_raw((adc1_channel_t)channelNumber);
+                    break;
             }
         }
-        else if ( adcNumber == 2)
+        else if (adcNumber == 2)
         {
-            result = adc2_get_raw( (adc2_channel_t)channelNumber, ADC_WIDTH_12Bit, &reading); 
-            if ( result != ESP_OK)
+            // Adjust channel number for ADC2
+            channelNumber -= 10;
+            result = adc2_get_raw((adc2_channel_t)channelNumber, ADC_WIDTH_12Bit, &reading);
+
+            if (result != ESP_OK)
+            {
                 NANOCLR_SET_AND_LEAVE(CLR_E_PIN_UNAVAILABLE);
+            }
         }
         else
+        {
             NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
-        
-       stack.SetResult_I4(reading);
- 
+        }
+
+        stack.SetResult_I4(reading);
     }
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeDisposeChannel___VOID( CLR_RT_StackFrame& stack )
+HRESULT Library_win_dev_adc_native_Windows_Devices_Adc_AdcChannel::NativeDisposeChannel___VOID(CLR_RT_StackFrame &stack)
 {
     (void)stack;
-    
+
     NANOCLR_HEADER();
 
     // left empty on purpose, nothing to do here
