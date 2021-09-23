01916a75b115584296648e87d4508ef18f7693f1
José Simões <jose.simoes@eclo.solutions>
2021-09-23 11:11:24 +0100

    Fix call to set native pin configuration (#2072)

--- targets/FreeRTOS_ESP32/ESP32_WROOM_32/nanoCLR/nanoFramework.Hardware.ESP32/nanoFramework_hardware_esp32_native_Hardware_Esp32_Configuration.cpp
+++ targets/FreeRTOS_ESP32/ESP32_WROOM_32/nanoCLR/nanoFramework.Hardware.ESP32/nanoFramework_hardware_esp32_native_Hardware_Esp32_Configuration.cpp
@@ -9,30 +9,29 @@
 #include <esp32_os.h>
 #include "Esp32_DeviceMapping.h"
 
-HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_Configuration::NativeSetPinFunction___STATIC__VOID__I4__I4( CLR_RT_StackFrame& stack )
-{
-	NANOCLR_HEADER();
-	{
-		CLR_RT_HeapBlock*  pThis = stack.This();  FAULT_ON_NULL(pThis);
-
-		uint32_t pin = (int32_t)stack.Arg0().NumericByRef().s4;
-		int32_t function = (int32_t)stack.Arg1().NumericByRef().s4;
-
-		Esp32_SetMappedDevicePins(pin, function);
-	}
-	NANOCLR_NOCLEANUP();
-}
-
-HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_Configuration::NativeGetPinFunction___STATIC__I4__I4( CLR_RT_StackFrame& stack )
+HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_Configuration::
+    NativeSetPinFunction___STATIC__VOID__I4__I4(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
-	{
-		CLR_RT_HeapBlock*  pThis = stack.This();  FAULT_ON_NULL(pThis);
-		int32_t function = (int32_t)stack.Arg0().NumericByRef().s4;
 
-		// Return current pin used for function
-		int32_t pin = (int32_t)Esp32_GetMappedDevicePinsWithFunction(function);
-		stack.SetResult_I4(pin);
-	}
-    NANOCLR_NOCLEANUP();
+    uint32_t pin = (int32_t)stack.Arg0().NumericByRef().s4;
+    int32_t function = (int32_t)stack.Arg1().NumericByRef().s4;
+
+    Esp32_SetMappedDevicePins(pin, function);
+
+    NANOCLR_NOCLEANUP_NOLABEL();
+}
+
+HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_Configuration::
+    NativeGetPinFunction___STATIC__I4__I4(CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    int32_t function = (int32_t)stack.Arg0().NumericByRef().s4;
+
+    // Return current pin used for function
+    int32_t pin = (int32_t)Esp32_GetMappedDevicePinsWithFunction(function);
+    stack.SetResult_I4(pin);
+
+    NANOCLR_NOCLEANUP_NOLABEL();
 }
