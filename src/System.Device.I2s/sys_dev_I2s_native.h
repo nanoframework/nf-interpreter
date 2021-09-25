17da758ee0782e02b71c0541b78dfbd17f823f04
Laurent Ellerbach <laurelle@microsoft.com>
2021-09-25 11:45:18 +0300

    adjust stubs, add target

--- src/System.Device.I2s/sys_dev_I2s_native.h
+++ src/System.Device.I2s/sys_dev_I2s_native.h
@@ -11,6 +11,63 @@
 #include <nanoPackStruct.h>
 #include <corlib_native.h>
 
+typedef enum __nfpack I2sBitsPerSample
+{
+    I2sBitsPerSample_Bit8 = 8,
+    I2sBitsPerSample_Bit16 = 16,
+    I2sBitsPerSample_Bit24 = 24,
+    I2sBitsPerSample_Bit32 = 32,
+} I2sBitsPerSample;
+
+typedef enum __nfpack I2sChannel
+{
+    I2sChannel_Mono = 1,
+    I2sChannel_Stereo = 2,
+} I2sChannel;
+
+typedef enum __nfpack I2sChannelFormat
+{
+    I2sChannelFormat_RightLeft = 0,
+    I2sChannelFormat_AllRight = 1,
+    I2sChannelFormat_AllLeft = 2,
+    I2sChannelFormat_OnlyRight = 3,
+    I2sChannelFormat_OnlyLeft = 4,
+} I2sChannelFormat;
+
+typedef enum __nfpack I2sCommunicationFormat
+{
+    I2sCommunicationFormat_StandardI2s = 1,
+    I2sCommunicationFormat_Lsb = 2,
+    I2sCommunicationFormat_Msb = 3,
+    I2sCommunicationFormat_PcmShort = 4,
+    I2sCommunicationFormat_PcmLong = 12,
+    I2sCommunicationFormat_StandMax = 13,
+} I2sCommunicationFormat;
+
+typedef enum __nfpack I2sMode
+{
+    I2sMode_Master = 1,
+    I2sMode_Slave = 2,
+    I2sMode_Tx = 4,
+    I2sMode_Rx = 8,
+    I2sMode_DacBuiltIn = 16,
+    I2sMode_AdcBuiltIn = 32,
+    I2sMode_Pdm = 64,
+} I2sMode;
+
+struct Library_sys_dev_I2s_native_System_Device_I2s_I2sConnectionSettings
+{
+    static const int FIELD___busId = 1;
+    static const int FIELD___sampleRate = 2;
+    static const int FIELD___i2sMode = 3;
+    static const int FIELD___i2sBitsPerSample = 4;
+    static const int FIELD___i2sChannelFormat = 5;
+    static const int FIELD___i2sConnectionFormat = 6;
+
+    //--//
+
+};
+
 struct Library_sys_dev_I2s_native_System_Device_I2s_I2sDevice
 {
     static const int FIELD___connectionSettings = 1;
