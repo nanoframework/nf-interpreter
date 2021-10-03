4838cc0379a88f6008458d019bbc858aef13f88b
doingnz <richard.scott@healthsystemslimited.com>
2021-10-04 02:17:23 +1300

    various utility & lib files /ST/core

---
+++ targets/AzureRTOS/ST/core/src/azure_iot_cert.h
@@ -0,0 +1,10 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+   
+#ifndef _AZURE_IOT_CERT_H
+#define _AZURE_IOT_CERT_H
+
+extern const unsigned char azure_iot_root_ca[];
+extern const unsigned int azure_iot_root_ca_len;
+
+#endif
---
+++ targets/AzureRTOS/ST/core/src/newlib_nano.c
@@ -0,0 +1,76 @@
+/* Copyright (c) Microsoft Corporation.
+   Licensed under the MIT License. */
+
+#ifdef __GNUC__
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include <sys/stat.h>
+
+extern int errno;
+extern int _end;
+
+void* _sbrk(int incr)
+{
+    static unsigned char* heap = NULL;
+    unsigned char* prev_heap;
+
+    if (heap == NULL)
+    {
+        heap = (unsigned char*)&_end;
+    }
+    prev_heap = heap;
+
+    heap += incr;
+
+    return prev_heap;
+}
+
+int _close(int file)
+{
+    return -1;
+}
+
+int _fstat(int file, struct stat* st)
+{
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _isatty(int file)
+{
+    return 1;
+}
+
+int _lseek(int file, int ptr, int dir)
+{
+    return 0;
+}
+
+void _exit(int status)
+{
+    printf("Exiting with status %d.\n", status);
+    while (1)
+        ;
+}
+
+void _kill(int pid, int sig)
+{
+    return;
+}
+
+int _getpid(void)
+{
+    return -1;
+}
+
+// function aliases to support different runtimes
+int lseek(int file, int ptr, int dir) __attribute__((weak, alias("_lseek")));
+int fstat(int file, struct stat* st) __attribute__((weak, alias("_fstat")));
+int close(int file) __attribute__((weak, alias("_close")));
+int isatty(int file) __attribute__((weak, alias("_isatty")));
+
+#endif
