bc16e0e3fc9439b60e45d0d1739bbd9745d5a10d
José Simões <jose.simoes@eclo.solutions>
2021-10-06 23:10:29 +0100

    Improve check for SDK config file

--- CMake/binutils.ESP32.cmake
+++ CMake/binutils.ESP32.cmake
@@ -420,11 +420,25 @@ macro(nf_add_idf_as_library)
     # check for SDK config from build options
     if(${SDK_CONFIG_FILE})
         # got an SDK config CONFIG on the build options
-        set(SDKCONFIG_DEFAULTS_FILE ${SDK_CONFIG_FILE})
+
+        # check if the file exists
+        if(EXISTS ${SDK_CONFIG_FILE})
+            set(SDKCONFIG_DEFAULTS_FILE ${SDK_CONFIG_FILE})
+        else()
+            # no, try the IDF folder
+            if(EXISTS targets/FreeRTOS_ESP32/_IDF/${SDK_CONFIG_FILE})
+                set(SDKCONFIG_DEFAULTS_FILE targets/FreeRTOS_ESP32/_IDF/${SDK_CONFIG_FILE})
+            else()
+                message(FATAL_ERROR "Couldn't find IDF SDK CONFIG file '${ESP32_IDF_SOURCE}'. Please check the SDK_CONFIG_FILE build option.")
+            endif()
+        endif()
     else()
         # get default SDK CONFIG file
         set(SDKCONFIG_DEFAULTS_FILE ${CMAKE_SOURCE_DIR}/targets/FreeRTOS_ESP32/_IDF/sdkconfig.default)
     endif()
+
+    message(STATUS "\n--SDK CONFIG is: '${SDKCONFIG_DEFAULTS_FILE}'.")
+
 
     # create IDF static libraries
     idf_build_process(${TARGET_SERIES_SHORT}
