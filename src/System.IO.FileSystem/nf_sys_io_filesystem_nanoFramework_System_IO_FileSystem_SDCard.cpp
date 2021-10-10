5e0a218195814336aa45245d455ddd2830e8c3f3
Adrian Soundy <adriansoundy@gmail.com>
2021-10-10 23:44:37 +1300

    Another fix

--- src/System.IO.FileSystem/nf_sys_io_filesystem_nanoFramework_System_IO_FileSystem_SDCard.cpp
+++ src/System.IO.FileSystem/nf_sys_io_filesystem_nanoFramework_System_IO_FileSystem_SDCard.cpp
@@ -35,11 +35,21 @@ __nfweak HRESULT Library_nf_sys_io_filesystem_nanoFramework_System_IO_FileSystem
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nf_sys_io_filesystem_nanoFramework_System_IO_FileSystem_SDCard::PollCardDetectNative___BOOLEAN(
+__nfweak HRESULT Library_nf_sys_io_filesystem_nanoFramework_System_IO_FileSystem_SDCard::PollCardDetectNative___BOOLEAN(
     CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+__nfweak HRESULT Library_nf_sys_io_filesystem_nanoFramework_System_IO_FileSystem_SDCard::NativeDispose___VOID(
+    CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+ 
     NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
 
     NANOCLR_NOCLEANUP();
