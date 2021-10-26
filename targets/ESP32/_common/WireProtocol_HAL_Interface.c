571c8771ec032e96f4894a31fd8c18d19f9a4d5e
José Simões <jose.simoes@eclo.solutions>
2021-10-26 11:40:50 +0100

    Rework ESP32 WP HAL interface (#2132)
    ***NO_CI***

--- targets/ESP32/_common/WireProtocol_HAL_Interface.c
+++ targets/ESP32/_common/WireProtocol_HAL_Interface.c
@@ -18,34 +18,99 @@
 #endif
 
 static bool WP_Port_Intitialised = false;
-static uart_port_t WP_Port = UART_NUM_0;
 
-#define ESP32_UART_RXD_PINS                                                                                            \
-    {                                                                                                                  \
-        3, 32, 16                                                                                                      \
-    }
-#define ESP32_UART_TXD_PINS                                                                                            \
-    {                                                                                                                  \
-        1, 33, 17                                                                                                      \
-    }
-#define ESP32_UART_CTS_PINS                                                                                            \
-    {                                                                                                                  \
-        19, 6, 8                                                                                                       \
-    }
-#define ESP32_UART_RTS_PINS                                                                                            \
-    {                                                                                                                  \
-        22, 11, 7                                                                                                      \
-    }
+#ifdef CONFIG_IDF_TARGET_ESP32
 
-// Pins
-static const char g_ESP32_Uart_RxD_Pins[] = ESP32_UART_RXD_PINS;
-static const char g_ESP32_Uart_TxD_Pins[] = ESP32_UART_TXD_PINS;
+// WP uses UART0
+static uart_port_t ESP32_WP_UART = UART_NUM_0;
 
+// UART pins for ESP32
+// U0RXD GPIO1
+// U0TXD GPIO3
+// U1RXD GPIO32
+// U1TXD GPIO33
+// U2RXD GPIO16
+// U2TXD GPIO17
+
+#define ESP32_WP_RX_PIN GPIO_NUM_3
+#define ESP32_WP_TX_PIN GPIO_NUM_1
+
+#elif CONFIG_IDF_TARGET_ESP32S2
+
+// WP uses UART0
+static uart_port_t ESP32_WP_UART = UART_NUM_0;
+
+// UART pins for ESP32-S2
+// U0RXD GPIO44
+// U0TXD GPIO43
+// U1RXD GPIO17
+// U1TXD GPIO18
+
+// we are using the same GPIOs as IDF UART0, so no need to reconfigure
+#define ESP32_WP_RX_PIN GPIO_NUM_44
+#define ESP32_WP_TX_PIN GPIO_NUM_43
+
+#elif CONFIG_IDF_TARGET_ESP32S3
+
+// NOT IMPLEMENTED (YET)
+
+#elif CONFIG_IDF_TARGET_ESP32C3
+
+// NOT IMPLEMENTED (YET)
+
+#endif
+
+#if CONFIG_USB_CDC_ENABLED
+
+extern TaskHandle_t ReceiverTask;
+
+void WP_Cdc_Rx_Callback(int itf, cdcacm_event_t *event)
+{
+    BaseType_t shouldWakeHigherPriorityTask = pdFALSE;
+
+    // signal data received
+    vTaskNotifyGiveFromISR(&ReceiverTask, &shouldWakeHigherPriorityTask);
+
+    portYIELD_FROM_ISR(shouldWakeHigherPriorityTask);
+}
+
+// WP using embedded USB CDC
 bool WP_Initialise(COM_HANDLE port)
 {
     (void)port;
 
-    ASSERT(WP_Port <= SOC_UART_NUM);
+    // get configuration with default values
+    tinyusb_config_t tusb_cfg = {};
+
+    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+    tinyusb_config_cdcacm_t amc_cfg = {
+        .usb_dev = TINYUSB_USBDEV_0,
+        .cdc_port = TINYUSB_CDC_ACM_0,
+        .rx_unread_buf_sz = 64,
+        .callback_rx = &WP_Cdc_Rx_Callback,
+        .callback_rx_wanted_char = NULL,
+        .callback_line_state_changed = NULL,
+        .callback_line_coding_changed = NULL};
+
+    ESP_ERROR_CHECK(tusb_cdc_acm_init(&amc_cfg));
+
+    WP_Port_Intitialised = true;
+
+    return true;
+}
+
+#else
+
+// WP using UART
+bool WP_Initialise(COM_HANDLE port)
+{
+    (void)port;
+
+    ASSERT(ESP32_WP_UART <= SOC_UART_NUM);
+
+    // uninstall driver for console
+    // ESP_ERROR_CHECK(uart_driver_delete(ESP32_WP_UART));
 
     uart_config_t uart_config = {// baudrate
                                  .baud_rate = TARGET_SERIAL_BAUDRATE,
@@ -58,29 +123,27 @@ bool WP_Initialise(COM_HANDLE port)
                                  // hardware flow control(cts/rts)
                                  .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
 
-    uart_param_config(WP_Port, &uart_config);
+    ESP_ERROR_CHECK(uart_param_config(ESP32_WP_UART, &uart_config));
 
-    uart_set_pin(
-        WP_Port,
-        g_ESP32_Uart_TxD_Pins[WP_Port],
-        g_ESP32_Uart_RxD_Pins[WP_Port],
-        UART_PIN_NO_CHANGE,
-        UART_PIN_NO_CHANGE);
+    ESP_ERROR_CHECK(
+        uart_set_pin(ESP32_WP_UART, ESP32_WP_TX_PIN, ESP32_WP_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
     // setup UART driver with UART queue
-    uart_driver_install(WP_Port, 256, 256, 0, NULL, ESP_INTR_FLAG_IRAM);
+    ESP_ERROR_CHECK(uart_driver_install(ESP32_WP_UART, 256, 256, 0, NULL, ESP_INTR_FLAG_IRAM));
 
     WP_Port_Intitialised = true;
 
     return true;
 }
 
+#endif // CONFIG_USB_CDC_ENABLED
+
 void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
 {
     // TODO: Initialise Port if not already done, Wire Protocol should be calling this directly at startup
     if (!WP_Port_Intitialised)
     {
-        WP_Initialise(WP_Port);
+        WP_Initialise(ESP32_WP_UART);
     }
 
     // save for later comparison
@@ -89,8 +152,21 @@ void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
     // check for request with 0 size
     if (*size)
     {
+
+#if CONFIG_USB_CDC_ENABLED
+
+        // reset read count
+        size_t read = 0;
+
+        // wait to try reading from CDC buffer
+        ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(100));
+
+        tinyusb_cdcacm_read(TINYUSB_CDC_ACM_0, *ptr, requestedSize, &read);
+
+#else
         // non blocking read from serial port with 100ms timeout
-        size_t read = uart_read_bytes(WP_Port, *ptr, (uint32_t)requestedSize, pdMS_TO_TICKS(100));
+        size_t read = uart_read_bytes(ESP32_WP_UART, *ptr, (uint32_t)requestedSize, pdMS_TO_TICKS(250));
+#endif
 
         *ptr += read;
         *size -= read;
@@ -101,12 +177,13 @@ uint8_t WP_TransmitMessage(WP_Message *message)
 {
     if (!WP_Port_Intitialised)
     {
-        WP_Initialise(WP_Port);
+        WP_Initialise(ESP32_WP_UART);
     }
 
-    // TODO Check if timeout required
+#if CONFIG_USB_CDC_ENABLED
+
     // write header to output stream
-    if (uart_write_bytes(WP_Port, (const char *)&message->m_header, sizeof(message->m_header)) !=
+    if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, (uint8_t *)&message->m_header, sizeof(message->m_header)) !=
         sizeof(message->m_header))
     {
         return false;
@@ -115,12 +192,36 @@ uint8_t WP_TransmitMessage(WP_Message *message)
     // if there is anything on the payload send it to the output stream
     if (message->m_header.m_size && message->m_payload)
     {
-        if (uart_write_bytes(WP_Port, (const char *)message->m_payload, message->m_header.m_size) !=
+        if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, (uint8_t *)message->m_payload, message->m_header.m_size) !=
+            message->m_header.m_size)
+        {
+            return false;
+        }
+    }
+
+    tinyusb_cdcacm_write_flush(TINYUSB_CDC_ACM_0, 0);
+
+#else
+
+    // TODO Check if timeout required
+    // write header to output stream
+    if (uart_write_bytes(ESP32_WP_UART, (const char *)&message->m_header, sizeof(message->m_header)) !=
+        sizeof(message->m_header))
+    {
+        return false;
+    }
+
+    // if there is anything on the payload send it to the output stream
+    if (message->m_header.m_size && message->m_payload)
+    {
+        if (uart_write_bytes(ESP32_WP_UART, (const char *)message->m_payload, message->m_header.m_size) !=
             (int)message->m_header.m_size)
         {
             return false;
         }
     }
+
+#endif
 
     return true;
 }
