cb416906ab2cef842fd749dff66c9cc3d79b0605
José Simões <jose.simoes@eclo.solutions>
2021-10-26 12:02:56 +0100

    Increase stack size for ReceiverThread task

--- targets/ESP32/_IDF/esp32s2/app_main.c
+++ targets/ESP32/_IDF/esp32s2/app_main.c
@@ -62,7 +62,7 @@ void app_main()
     ESP_ERROR_CHECK(nvs_flash_init());
 
     // start receiver task
-    xTaskCreate(&receiver_task, "ReceiverThread", 2048, NULL, 5, &ReceiverTask);
+    xTaskCreate(&receiver_task, "ReceiverThread", 3072, NULL, 5, &ReceiverTask);
 
     // start the CLR main task
     xTaskCreate(&main_task, "main_task", 15000, NULL, 5, NULL);
