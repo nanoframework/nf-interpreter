43c84ee67d0ad35e90bc42a595ededd01aa1c233
Adrian Soundy <adriansoundy@gmail.com>
2021-10-31 22:46:29 +1300

    Missed change in commit

--- targets/ESP32/_nanoCLR/nanoFramework.Hardware.ESP32/nanoFramework_hardware_esp32_native_Hardware_Esp32_NativeMemory.cpp
+++ targets/ESP32/_nanoCLR/nanoFramework.Hardware.ESP32/nanoFramework_hardware_esp32_native_Hardware_Esp32_NativeMemory.cpp
@@ -5,7 +5,7 @@
 //
 
 #include "nanoFramework_hardware_esp32_native.h"
-#include <esp32_idf.h>
+#include <nanoHAL.h>
 
 
 uint32_t GetCaps(CLR_RT_StackFrame &stack)
@@ -13,23 +13,21 @@ uint32_t GetCaps(CLR_RT_StackFrame &stack)
     uint32_t caps = MALLOC_CAP_32BIT | MALLOC_CAP_8BIT;
     int memoryType = (int)stack.Arg0().NumericByRef().u4;
 
-    if ( memoryType == NativeMemory_MemoryType::NativeMemory_MemoryType_All )
+    if (memoryType == NativeMemory_MemoryType::NativeMemory_MemoryType_Internal)
     {
-        caps | = (MALLOC_CAP_INTERNAL + MALLOC_CAP_SPIRAM);
+         caps |= MALLOC_CAP_INTERNAL;
     }
-    else if (memoryType == NativeMemory_MemoryType::NativeMemory_MemoryType_Internal)
+    else if (memoryType == NativeMemory_MemoryType::NativeMemory_MemoryType_SpiRam)
     {
-         caps | = MALLOC_CAP_INTERNAL;
-    }
-    else
-    {
-         caps | = MALLOC_CAP_SPIRAM;
+         caps |= MALLOC_CAP_SPIRAM;
     }
     
     return caps;
 }
 
-
+//
+// Get the total size of all the regions. 
+//
 HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_NativeMemory::NativeGetMemoryTotalSize___STATIC__U4__I4( CLR_RT_StackFrame &stack )
 {
     NANOCLR_HEADER();
@@ -39,6 +37,9 @@ HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32
     NANOCLR_NOCLEANUP_NOLABEL();
 }
 
+//
+// Get the total free size of all the regions. 
+//
 HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_NativeMemory::NativeGetMemoryTotalFreeSize___STATIC__U4__I4( CLR_RT_StackFrame &stack )
 {
     NANOCLR_HEADER();
@@ -48,11 +49,14 @@ HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32
     NANOCLR_NOCLEANUP_NOLABEL();
 }
 
+//
+// Get the largest free block of memory able to be allocated.
+// 
 HRESULT Library_nanoFramework_hardware_esp32_native_nanoFramework_Hardware_Esp32_NativeMemory::NativeGetMemoryLargestFreeBlock___STATIC__U4__I4( CLR_RT_StackFrame &stack )
 {
     NANOCLR_HEADER();
     {
-        stack.SetResult_U4((uint32_t)heap_caps_get_largest_free_blockcaps(GetCaps(stack)));
+        stack.SetResult_U4((uint32_t)heap_caps_get_largest_free_block(GetCaps(stack)));
     }
     NANOCLR_NOCLEANUP_NOLABEL();
 }
