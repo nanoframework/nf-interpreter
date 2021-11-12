43230dd7c83a7d430bcdae61bc0dfb6f2056b9af
José Simões <jose.simoes@eclo.solutions>
2021-11-12 13:17:18 +0000

    Update System.Math
    - Add support for Cbrt().
    - Fix Max().
    - Update assembly declaration.

--- src/CLR/System.Math/nf_native_system_math.cpp
+++ src/CLR/System.Math/nf_native_system_math.cpp
@@ -10,25 +10,31 @@
 static const CLR_RT_MethodHandler method_lookup[] =
 {
     NULL,
-    Library_nf_native_system_math_System_Math::Max___STATIC__R8__R8__R8,
-    NULL,
-    Library_nf_native_system_math_System_Math::Min___STATIC__R8__R8__R8,
-    NULL,
-    NULL,
     Library_nf_native_system_math_System_Math::Abs___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Abs___STATIC__R4__R4,
     Library_nf_native_system_math_System_Math::Acos___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Asin___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Atan___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Atan2___STATIC__R8__R8__R8,
+    Library_nf_native_system_math_System_Math::Cbrt___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Ceiling___STATIC__R8__R8,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
     Library_nf_native_system_math_System_Math::Cos___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Cosh___STATIC__R8__R8,
-    Library_nf_native_system_math_System_Math::IEEERemainder___STATIC__R8__R8__R8,
     Library_nf_native_system_math_System_Math::Exp___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Floor___STATIC__R8__R8,
+    Library_nf_native_system_math_System_Math::IEEERemainder___STATIC__R8__R8__R8,
     Library_nf_native_system_math_System_Math::Log___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Log10___STATIC__R8__R8,
+    NULL,
+    Library_nf_native_system_math_System_Math::Max___STATIC__R8__R8__R8,
+    NULL,
+    NULL,
+    Library_nf_native_system_math_System_Math::Min___STATIC__R8__R8__R8,
+    NULL,
     Library_nf_native_system_math_System_Math::Pow___STATIC__R8__R8__R8,
     Library_nf_native_system_math_System_Math::Round___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Sign___STATIC__I4__R8,
@@ -39,24 +45,15 @@ static const CLR_RT_MethodHandler method_lookup[] =
     Library_nf_native_system_math_System_Math::Tan___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Tanh___STATIC__R8__R8,
     Library_nf_native_system_math_System_Math::Truncate___STATIC__R8__R8,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
 };
 
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_Math =
 {
     "System.Math",
-    0xD6F277F6,
+    0x46092CB1,
     method_lookup,
-    { 100, 0, 5, 3 }
+    { 100, 0, 5, 4 }
 };
+
 
 // clang-format on
