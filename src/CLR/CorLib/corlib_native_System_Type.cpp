38be21025e1bffb7ce9cfea1d3bc09b99654a1b8
josesimoes <jose.simoes@eclo.solutions>
2022-01-27 17:06:09 +0000

    Add missing open & close brackets

--- src/CLR/CorLib/corlib_native_System_Type.cpp
+++ src/CLR/CorLib/corlib_native_System_Type.cpp
@@ -539,10 +539,15 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
     bool staticInstanceOnly = false;
 
     if (bindingFlags == c_BindingFlags_Default)
+    {
         bindingFlags = c_BindingFlags_DefaultLookup;
+    }
+
     // in default lookup mode we want the static methods only from the instance not from the base classes
     if (bindingFlags == c_BindingFlags_DefaultLookup)
+    {
         staticInstanceOnly = true;
+    }
 
     NANOCLR_CHECK_HRESULT(Library_corlib_native_System_RuntimeType::GetTypeDescriptor(*hbType, tdArg));
 
@@ -581,23 +586,31 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
                 if (md->flags & CLR_RECORD_METHODDEF::MD_Static)
                 {
                     if ((bindingFlags & c_BindingFlags_Static) == 0)
+                    {
                         continue;
+                    }
                 }
                 else
                 {
                     if ((bindingFlags & c_BindingFlags_Instance) == 0)
+                    {
                         continue;
+                    }
                 }
 
                 if ((md->flags & CLR_RECORD_METHODDEF::MD_Scope_Mask) == CLR_RECORD_METHODDEF::MD_Scope_Public)
                 {
                     if ((bindingFlags & c_BindingFlags_Public) == 0)
+                    {
                         continue;
+                    }
                 }
                 else
                 {
                     if ((bindingFlags & c_BindingFlags_NonPublic) == 0)
+                    {
                         continue;
+                    }
                 }
 
                 //--//
@@ -605,15 +618,21 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
                 if (md->flags & CLR_RECORD_METHODDEF::MD_Constructor)
                 {
                     if ((bindingFlags & c_BindingFlags_CreateInstance) == 0)
+                    {
                         continue;
+                    }
                 }
                 else
                 {
                     if ((bindingFlags & c_BindingFlags_CreateInstance) != 0)
+                    {
                         continue;
+                    }
 
                     if (szText != NULL && !strcmp(assm->GetString(md->name), szText) == false)
+                    {
                         continue;
+                    }
                 }
 
                 if (pParams)
@@ -630,7 +649,9 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
                     NANOCLR_CHECK_HRESULT(parserLeft.Advance(res));
 
                     if (CLR_RT_TypeSystem::MatchSignatureDirect(parserLeft, parserRight, true) == false)
+                    {
                         continue;
+                    }
                 }
 
                 CLR_RT_MethodDef_Index idx;
@@ -704,9 +725,14 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
                                 //  matches pParams
 
                                 if (fLeftBetterMatchT && !fRightBetterMatchT)
+                                {
                                     fLeftBetterMatch = true;
+                                }
+
                                 if (!fLeftBetterMatchT && fRightBetterMatchT)
+                                {
                                     fRightBetterMatch = true;
+                                }
                             }
 
                             if (fLeftBetterMatch && fRightBetterMatch)
@@ -719,7 +745,9 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
                             // If they are identical signatures, the first one wins (subclass takes precendence)
                             // Only if Right is better do we have a strictly better match
                             if (!fRightBetterMatch)
+                            {
                                 continue;
+                            }
 
                             // Found a better match
                         }
@@ -735,13 +763,18 @@ HRESULT Library_corlib_native_System_Type::GetMethods(
             }
 
             if (bindingFlags & (c_BindingFlags_DeclaredOnly | c_BindingFlags_CreateInstance))
+            {
                 break;
+            }
+
         } while (td.SwitchToParent());
 
         if (pass == 0)
         {
             if (!fAllMatches)
+            {
                 NANOCLR_SET_AND_LEAVE(S_OK);
+            }
 
             NANOCLR_CHECK_HRESULT(
                 CLR_RT_HeapBlock_Array::CreateInstance(top, iMethod, g_CLR_RT_WellKnownTypes.m_MethodInfo));
