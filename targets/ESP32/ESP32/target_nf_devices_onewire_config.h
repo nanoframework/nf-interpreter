318fe1db6e1c877f8c0e6540302f657438da72da
José Simões <jose.simoes@eclo.solutions>
2022-02-25 10:13:11 +0000

    Fix 1-Wire init for ESP32 (#2264)
    ***NO_CI***

--- targets/ESP32/ESP32/target_nf_devices_onewire_config.h
+++ targets/ESP32/ESP32/target_nf_devices_onewire_config.h
@@ -3,5 +3,7 @@
 // See LICENSE file in the project root for full license information.
 //
 
+#include <uart.h>
+
 // use UART 2 for the 1-wire interface
 #define NF_ONEWIRE_ESP32_UART_NUM UART_NUM_2
