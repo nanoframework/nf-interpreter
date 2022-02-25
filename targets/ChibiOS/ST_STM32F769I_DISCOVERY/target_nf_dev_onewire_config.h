8cd7f4d8d9b49719f63a20c9b839afc7cb87dd7b
nfbot <nanoframework@outlook.com>
2022-02-25 12:55:45 +0000

    Code style fixes
    Automated fixes for code style.

--- targets/ChibiOS/ST_STM32F769I_DISCOVERY/target_nf_dev_onewire_config.h
+++ targets/ChibiOS/ST_STM32F769I_DISCOVERY/target_nf_dev_onewire_config.h
@@ -8,10 +8,10 @@
 ///////////
 
 // enable USART4
-#define NF_ONEWIRE_STM32_UART_USE_USART4    TRUE
+#define NF_ONEWIRE_STM32_UART_USE_USART4 TRUE
 
 // buffers size
 // tx buffer size: 32 bytes
-#define UART4_TX_SIZE  32
+#define UART4_TX_SIZE 32
 // rx buffer size: 32 bytes
-#define UART4_RX_SIZE  32
+#define UART4_RX_SIZE 32
