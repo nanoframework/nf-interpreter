eaba3393638b0864e2766749e8862fe892655963
Robin Jones <networkfusion@users.noreply.github.com>
2022-06-27 19:42:22 +0100

    Add cmake module.

---
+++ CMake/Modules/FindnanoFramework.System.Data.Sqlite.cmake
@@ -0,0 +1,43 @@
+#
+# Copyright (c) .NET Foundation and Contributors
+# See LICENSE file in the project root for full license information.
+#
+
+
+# set include directories
+list(APPEND nanoFramework.System.Data.Sqlite_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/src/nanoFramework.System.Data.Sqlite")
+
+# source files
+set(nanoFramework.System.Data.Sqlite_SRCS
+
+    # source files of the assembly
+    nf_system_data_sqllite_native_System_Data_Sqlite_SqliteCommand_stubs.cpp
+    shell.c
+    sqlite3.c
+)
+
+foreach(SRC_FILE ${nanoFramework.System.Data.Sqlite_SRCS})
+
+    set(nanoFramework.System.Data.Sqlite_SRC_FILE SRC_FILE-NOTFOUND)
+
+    find_file(nanoFramework.System.Data.Sqlite_SRC_FILE ${SRC_FILE}
+        PATHS
+
+            # path for source files of this module
+            ${CMAKE_SOURCE_DIR}/src/nanoFramework.System.Data.Sqlite
+
+        CMAKE_FIND_ROOT_PATH_BOTH
+    )
+
+    if (BUILD_VERBOSE)
+        message("${SRC_FILE} >> ${nanoFramework.System.Data.SqLite_SRC_FILE}")
+    endif()
+
+    list(APPEND nanoFramework.System.Text_SOURCES ${nanoFramework.System.Data.Sqlite_SRC_FILE})
+    
+endforeach()
+
+
+include(FindPackageHandleStandardArgs)
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(nanoFramework.System.Data.Sqlite DEFAULT_MSG nanoFramework.System.Data.Sqlite_INCLUDE_DIRS nanoFramework.System.Data.Sqlite_SOURCES)
