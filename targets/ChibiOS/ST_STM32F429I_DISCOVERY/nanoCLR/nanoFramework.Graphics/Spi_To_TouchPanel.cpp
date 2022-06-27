e93ec91a5442708a42e0d7ac815c168052f1d819
Robin Jones <networkfusion@users.noreply.github.com>
2022-06-27 12:59:38 +0100

    Manually apply codestyle fixes
    So the PR can continue.

--- targets/ChibiOS/ST_STM32F429I_DISCOVERY/nanoCLR/nanoFramework.Graphics/Spi_To_TouchPanel.cpp
+++ targets/ChibiOS/ST_STM32F429I_DISCOVERY/nanoCLR/nanoFramework.Graphics/Spi_To_TouchPanel.cpp
@@ -3,7 +3,7 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#define UNUSED(x)  (void)x
+#define UNUSED(x) (void)x
 
 #ifndef SPI_TO_TOUCHPANEL_H
 #define SPI_TO_TOUCHPANEL_H
@@ -14,7 +14,7 @@
 //#include <esp32_idf.h>
 
 #include "TouchInterface.h"
-             
+
 bool TouchInterface::Initialize()
 {
     // Setup SPI configuration
@@ -22,16 +22,17 @@ bool TouchInterface::Initialize()
     return true;
 }
 
-CLR_UINT8* TouchInterface::Write_Read(CLR_UINT8* valuesToSend, CLR_UINT16 numberOfValuesToSend, CLR_UINT16 numberValuesExpected)
+CLR_UINT8* TouchInterface::Write_Read(
+    CLR_UINT8* valuesToSend,
+    CLR_UINT16 numberOfValuesToSend,
+    CLR_UINT16 numberValuesExpected)
 {
 
     UNUSED(valuesToSend);
     UNUSED(numberOfValuesToSend);
     UNUSED(numberValuesExpected);
 
-
     return 0;
 }
-
 
 #endif //SPI_TO_TOUCHPANEL_H
