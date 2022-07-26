7c58f47b4325d999f5722e89ebee99a52bf8eb9d
José Simões <jose.simoes@eclo.solutions>
2022-07-26 16:42:56 +0100

    Add support for ADC

--- targets/AzureRTOS/SiliconLabs/SL_STK3701A/README.md
+++ targets/AzureRTOS/SiliconLabs/SL_STK3701A/README.md
@@ -9,3 +9,13 @@ https://community.silabs.com/s/article/kba-bt-1208-using-virtual-com-port-vcom-x
 
 :warning:
 Only possible for revC hardware.
+
+## ADC channels
+
+ADC has enabled the following channels:
+0 - reading from PE11 (pin 6 on the expansion header)
+1 - internal temperature
+2 - VCC
+
+:warning:
+Vref is 2.5V for all channels, except for VCC which is 5V.
---
+++ targets/AzureRTOS/SiliconLabs/SL_STK3701A/target_system_device_adc_config.cpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <sys_dev_adc_native_target.h>
+
+// PE11- ADC0 Port 4X Channel 11 (Expansion Header Pin 6)
+
+const NF_PAL_ADC_PORT_PIN_CHANNEL AdcPortPinConfig[] = {
+
+    // ADC0
+    {0, adcPosSelAPORT4XCH11},
+
+    // these are the internal sources
+    {0, adcPosSelTEMP},
+    {0, adcPosSelAVDD},    
+};
+
+const int AdcChannelCount = ARRAYSIZE(AdcPortPinConfig);
