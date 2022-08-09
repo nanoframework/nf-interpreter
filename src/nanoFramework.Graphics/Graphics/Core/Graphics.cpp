2084d903d84d231f8b77ce7fb38f7cf748b5cef7
Terry Fogg <Terry.Fogg@outlook.com>
2022-08-09 22:39:06 +1000

    Update

--- src/nanoFramework.Graphics/Graphics/Core/Graphics.cpp
+++ src/nanoFramework.Graphics/Graphics/Core/Graphics.cpp
@@ -376,6 +376,9 @@ void CLR_GFX_Bitmap::Decompress(const CLR_UINT8 *data, CLR_UINT32 size)
 
 CLR_UINT32 CLR_GFX_Bitmap::ConvertToNative1BppHelper(int x, int y, CLR_UINT32 flags, CLR_UINT16 &opacity, void *param)
 {
+    (void)x;
+    (void)y;
+
     ConvertToNativeHelperParam *myParam = (ConvertToNativeHelperParam *)param;
     if (flags & PAL_GFX_Bitmap::c_SetPixels_NewRow)
     {
@@ -396,6 +399,9 @@ CLR_UINT32 CLR_GFX_Bitmap::ConvertToNative1BppHelper(int x, int y, CLR_UINT32 fl
 
 CLR_UINT32 CLR_GFX_Bitmap::ConvertToNative16BppHelper(int x, int y, CLR_UINT32 flags, CLR_UINT16 &opacity, void *param)
 {
+    (void)x;
+    (void)y;
+
     ConvertToNativeHelperParam *myParam = (ConvertToNativeHelperParam *)param;
     if (flags & PAL_GFX_Bitmap::c_SetPixels_NewRow)
     {
