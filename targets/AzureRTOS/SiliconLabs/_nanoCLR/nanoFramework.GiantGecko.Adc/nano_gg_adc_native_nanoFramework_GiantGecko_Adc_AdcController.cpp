78aec862ab3198f05f932e1c80b13eb747cbc776
José Simões <jose.simoes@eclo.solutions>
2022-09-20 09:31:01 +0100

    More work on Gecko ADC implementation

--- targets/AzureRTOS/SiliconLabs/_nanoCLR/nanoFramework.GiantGecko.Adc/nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController.cpp
+++ targets/AzureRTOS/SiliconLabs/_nanoCLR/nanoFramework.GiantGecko.Adc/nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController.cpp
@@ -7,9 +7,22 @@
 #include "nano_gg_adc_native.h"
 #include "nano_gg_adc_native_target.h"
 
-#define ADC_FREQ 16000000
+#define ADC_IMPOSSIBLE_CLOCK 90000000UL
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeInit___VOID( CLR_RT_StackFrame &stack )
+#if GECKO_USE_ADC0
+ADC_Init_TypeDef adc0Init;
+bool adc0Initialized = false;
+#endif
+#if GECKO_USE_ADC1
+ADC_Init_TypeDef adc1Init;
+bool adc1Initialized = false;
+#endif
+
+// add typedef for AdcConfiguration to ease access and improve readability
+typedef Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcConfiguration AdcConfiguration;
+
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeInit___VOID(
+    CLR_RT_StackFrame &stack)
 {
     (void)stack;
 
@@ -20,17 +33,19 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP_NOLABEL();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeOpenChannel___VOID__I4( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeOpenChannel___VOID__I4(
+    CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
     int channel;
-    NF_PAL_ADC_PORT_PIN_CHANNEL adcDefinition;
-    ADC_Init_TypeDef adcInit;
+    NF_PAL_ADC_PORT_PIN_CHANNEL adcChannelDefinition;
     CMU_Clock_TypeDef adcClock;
     ADC_TypeDef *adcDriver = NULL;
+    ADC_Init_TypeDef *adcInit = NULL;
+    bool *adcInitialized = NULL;
 
-    adcInit = ADC_INIT_DEFAULT;
+    CLR_RT_HeapBlock *adcConfiguration;
 
     // get a pointer to the managed object instance and check that it's not NULL
     CLR_RT_HeapBlock *pThis = stack.This();
@@ -39,17 +54,26 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     // Get channel from argument
     channel = stack.Arg1().NumericByRef().s4;
 
-    adcDefinition = AdcPortPinConfig[channel];
+    // sanity check for existing configuration
+    if (channel >= AdcChannelCount)
+    {
+        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
+    }
+
+    // grab ADC channel definition
+    adcChannelDefinition = AdcPortPinConfig[channel];
 
     // we should remove from the build the ADC options that aren't implemented
     // plus we have to use the default to catch invalid ADC Ids
-    switch (adcDefinition.adcIndex)
+    switch (adcChannelDefinition.adcIndex)
     {
 
 #if GECKO_USE_ADC0
         case 0:
             adcDriver = ADC0;
             adcClock = cmuClock_ADC0;
+            adcInit = &adc0Init;
+            adcInitialized = &adc0Initialized;
             break;
 #endif
 
@@ -57,6 +81,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
         case 1:
             adcDriver = ADC1;
             adcClock = cmuClock_ADC1;
+            adcInit = &adc1Init;
+            adcInitialized = &adc1Initialized;
             break;
 #endif
 
@@ -64,21 +90,63 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
             NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
     }
 
-    // Enable ADC clock
-    CMU_ClockEnable(cmuClock_HFPER, true);
-    CMU_ClockEnable(adcClock, true);
+    // is this ADC already initialized?
+    if (
+#if GECKO_USE_ADC0
+        (adcDriver == ADC0 && !adcInitialized)
+#endif
+#if GECKO_USE_ADC0 && GECKO_USE_ADC1
+        ||
+#endif
+#if GECKO_USE_ADC1
+        (adcDriver == ADC1 && !adcInitialized)
+#endif
+    )
+    {
+        // get a pointer to the managed AdcConfiguration object instance
+        adcConfiguration = pThis[FIELD___acdConfiguration].Dereference();
 
-    // Init to max ADC clock for Series 1
-    adcInit.prescale = ADC_PrescaleCalc(ADC_FREQ, 0);
-    adcInit.timebase = ADC_TimebaseCalc(0);
+        // init with default values
+        *adcInit = ADC_INIT_DEFAULT;
 
-    // start ADC
-    ADC_Init(adcDriver, &adcInit);
+        // now map the AdcConfiguration object to the native structure
+        adcInit->ovsRateSel =
+            (ADC_OvsRateSel_TypeDef)adcConfiguration[AdcConfiguration::FIELD___oversampleRate].NumericByRef().s4;
+
+#if defined(_ADC_CTRL_LPFMODE_MASK)
+        adcInit->lpfMode =
+            (ADC_LPFilter_TypeDef)adcConfiguration[AdcConfiguration::FIELD___lowpassFilterMode].NumericByRef().s4;
+#endif
+
+        adcInit->warmUpMode =
+            (ADC_Warmup_TypeDef)adcConfiguration[AdcConfiguration::FIELD___warmUpMode].NumericByRef().s4;
+        adcInit->tailgate = (bool)adcConfiguration[AdcConfiguration::FIELD___tailgating].NumericByRef().u1;
+
+        // Enable ADC clock
+        CMU_ClockEnable(cmuClock_HFPER, true);
+        CMU_ClockEnable(adcClock, true);
+
+        // init to max possible ADC clock
+        adcInit->prescale = ADC_PrescaleCalc(ADC_IMPOSSIBLE_CLOCK, 0);
+        // let the drivers compute the appropriate value for the reference clock
+        adcInit->timebase = ADC_TimebaseCalc(0);
+
+#if defined(_ADC_CTRL_ADCCLKMODE_MASK)
+        adcInit->em2ClockConfig = adcEm2ClockOnDemand;
+#endif
+
+        // start ADC
+        ADC_Init(adcDriver, adcInit);
+
+        // set ADC initialized flag
+        *adcInitialized = true;
+    }
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeGetChannelCount___I4( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeGetChannelCount___I4(
+    CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -87,7 +155,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeIsChannelModeSupported___BOOLEAN__I4( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::
+    NativeIsChannelModeSupported___BOOLEAN__I4(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -96,7 +165,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeGetSupportedResolutionsInBits___SZARRAY_nanoFrameworkGiantGeckoAdcSampleResolution( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::
+    NativeGetSupportedResolutionsInBits___SZARRAY_nanoFrameworkGiantGeckoAdcSampleResolution(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -105,7 +175,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeStartContinuousConversion___BOOLEAN( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::
+    NativeStartContinuousConversion___BOOLEAN(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -114,7 +185,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeStoptContinuousConversion___BOOLEAN( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::
+    NativeStoptContinuousConversion___BOOLEAN(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -123,7 +195,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeGetLastContinuousSamples___SZARRAY_I4( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::
+    NativeGetLastContinuousSamples___SZARRAY_I4(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
@@ -132,7 +205,8 @@ HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::N
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::NativeGetLastScanSampleForChannel___I4__I4( CLR_RT_StackFrame &stack )
+HRESULT Library_nano_gg_adc_native_nanoFramework_GiantGecko_Adc_AdcController::
+    NativeGetLastScanSampleForChannel___I4__I4(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
