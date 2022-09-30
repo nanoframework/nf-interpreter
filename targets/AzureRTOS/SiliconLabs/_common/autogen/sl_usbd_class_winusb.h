86e606fc075e92e7f98b9ef642401b46b8f4e907
José Simões <jose.simoes@eclo.solutions>
2022-09-30 17:51:07 +0100

    Add missing Microsoft extended property
    - Enumeration requires DeviceInterfaceGUID to have device properly accessible.
    - Add configs and setting the property.

---
+++ targets/AzureRTOS/SiliconLabs/_common/autogen/sl_usbd_class_winusb.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright 2021 Silicon Laboratories Inc. All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef SL_USBD_CLASS_WINUSB_H
+#define SL_USBD_CLASS_WINUSB_H
+
+#define DEVICEINTERFACE_GUID_PROP_NAME L"DeviceInterfaceGUID"
+
+/// Length of GUID Property Name
+#define DEVICEINTERFACE_GUID_PROP_NAME_LEN  sizeof (DEVICEINTERFACE_GUID_PROP_NAME)
+
+#endif // SL_USBD_CLASS_WINUSB_H
