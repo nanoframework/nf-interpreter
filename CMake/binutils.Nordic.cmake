3f021ce97cdb5ea583d0bfa5a4d790b807e3db13
José Simões <jose.simoes@eclo.solutions>
2022-10-19 17:34:35 +0100

    Rework Nordic CMake and binutils
    - Full setup nRF Connect (either existing local or download).
    - Set Zephyr SDK and toolchain version.
    - Setup CMake vars.
    - Add macro to setup Zephyr west.
    - Remove unnecessary sets.
    - Simplification of some CMake vars for paths.
    - Add compile definitions common to nanoFramework.
    - Remove unused macros.

--- CMake/binutils.Nordic.cmake
+++ CMake/binutils.Nordic.cmake
@@ -3,6 +3,26 @@
 # See LICENSE file in the project root for full license information.
 #
 
+# setup Zephyr west tool
+# this is required to be able to execute west commands from CMake
+# requires Python and Zephyr west to be installed
+macro(setup_west)
+
+    # make sure Python is installed and accessible
+    find_package(Python 3.10 COMPONENTS Interpreter REQUIRED)
+
+    # install west, if not already installed
+    execute_process(
+        COMMAND pip3 install west
+        COMMAND_ECHO NONE
+        OUTPUT_QUIET
+    )
+
+    # set west "executable"
+    set(WEST_EXE ${Python_EXECUTABLE} -m west CACHE INTERNAL "West executable in CMake callable format")
+
+endmacro()
+
 # Add platform packages specific to Zephyr
 # To be called from target CMakeList.txt
 # optional TARGET argument with target name
@@ -83,17 +103,18 @@ endmacro()
 macro(nf_add_platform_sources target)
 
     # add header files with common OS definitions and board definitions 
-    configure_file(${TARGET_RTOS_ROOT}/${TARGET_BOARD}/target_common.h.in
+    configure_file(${TARGET_BASE_LOCATION}/target_common.h.in
                    ${CMAKE_BINARY_DIR}/targets/${RTOS}/${TARGET_BOARD}/target_common.h @ONLY)
 
     # sources specific to nanoCLR
     if(${target} STREQUAL ${NANOCLR_PROJECT_NAME})
 
         # add header files with common OS definitions and board definitions 
-        configure_file(${TARGET_RTOS_ROOT}/${TARGET_BOARD}/nanoCLR/target_board.h.in
-                    ${CMAKE_BINARY_DIR}/targets/${RTOS}/${TARGET_BOARD}/nanoCLR/target_board.h @ONLY)
+        configure_file(${TARGET_BASE_LOCATION}/nanoCLR/target_board.h.in
+                       ${CMAKE_BINARY_DIR}/targets/${RTOS}/${TARGET_BOARD}/nanoCLR/target_board.h @ONLY)
 
     endif()
+
 endmacro()
 
 # macro to setup the build for a target
@@ -101,14 +122,11 @@ endmacro()
 # build system takes care of all the linking and other complexities
 macro(nf_setup_target_build)
 
-    # Get the zephyr base 
-    string(REPLACE "\\" "/" ZBASE $ENV{ZEPHYR_BASE})
-    # Add zephyr tree to CMAKE path.
-    list(APPEND CMAKE_MODULE_PATH ${ZBASE}/cmake)
+    # points Zephyr to nanoCLR lib module
+    list(APPEND ZEPHYR_EXTRA_MODULES ${CMAKE_SOURCE_DIR}/targets/Nordic/_nanoCLR_module)
 
-    # This tells the Zephyr RTOS where to look to add the NF lib as a module.
-    list(APPEND ZEPHYR_EXTRA_MODULES
-        ${TARGET_RTOS_ROOT}/module)
+    # add our location for board directories search
+    list(APPEND BOARD_ROOT ${CMAKE_SOURCE_DIR}/targets/Nordic/_zephyr_boards)
 
     # HACK::
     # We need to parse the build version in to seperate elements.
@@ -122,46 +140,70 @@ macro(nf_setup_target_build)
     list(GET BVLIST 2 nanoFramework_VERSION_PATCH)
     list(GET BVLIST 3 nanoFramework_VERSION_TWEAK)
 
-    #       
-    # Continue build with standard NRF/Zephyr build 
-    #
-    # During this invocation, the NF will be built as a zephyr 'module'. 
-    # This invokes .\tarrgets\Zephyr\module\cmakelists.txt which does the 
-    # work to build the nanoframework.
-    #
-    message(STATUS "========= Building Zephyr board=${BOARD}   ZEPHYR_BASE >$ENV{ZEPHYR_BASE}<   build_version >${BUILD_VERSION}<")
-    find_package(Zephyr REQUIRED HINTS $ENV{ZEPHYR_BASE})
-    message(STATUS "========= Zephyr cmakelist.txt return.")
-
-    # At this point, the nanoframework module (library) has been built. What remains is to build
-    # a trivial app that invokes the NF runtime.
-
-    zephyr_include_directories("../module/nf_lib/include")
-
-    # The Zephyr build system requires project to be defined here
-    project(nanoFramework VERSION ${BUILD_VERSION} LANGUAGES C CXX ASM)
-
-    zephyr_compile_definitions(PROJECT_NAME=${PROJECT_NAME})
-
-    # need this for NF build
-    zephyr_compile_definitions(PLATFORM_ARM=1)
-
-    if(APP_VERSION)
-        zephyr_compile_definitions(APP_VERSION=${APP_VERSION})
+    # During this invocation nanoCLR will be built as a Zephyr 'module'.
+    find_package(Zephyr REQUIRED QUIET HINTS ${NCS_BASE_LOCATION})
+    
+    if(NOT Zephyr_FOUND)
+        # Zephyr SDK not found report error
+        message(FATAL_ERROR "\n\nCouldn't find Zephyr SDK v${ZEPHYR_SDK_VERSION}!\n\nMake sure to install it using Toolchain Manager from Nordic Connect SDK or manually from the Zephyr SDK GitHub repository.\n\n")
     endif()
 
-endmacro()
+    # At this point nanoCLR module (library) has been built. 
+    # Add a trivial app that invokes nanoCLR.
 
-# macro to clear binary files related with nanoBooter from output
-# to make sure that the build file it's up to date
-macro(nf_clear_output_files_nanobooter)
-    nf_clear_common_output_files_nanobooter()
-    # other files specific to this platform should go here
-endmacro()
+    zephyr_include_directories(${CMAKE_SOURCE_DIR}/targets/Nordic/nf_lib_module/zephyr/include)
 
-# macro to clear binary files related with nanoCLR from output
-# to make sure that the build file it's up to date
-macro(nf_clear_output_files_nanoclr)
-    nf_clear_common_output_files_nanoclr()
-    # other files specific to this platform should go here
+    # The Zephyr build system requires that CMake project has to be defined here
+    project(nanoFramework VERSION ${BUILD_VERSION} LANGUAGES C CXX ASM)
+
+    zephyr_compile_definitions(PROJECT_NAME=nanoCLR)
+
+    zephyr_compile_definitions(APP_VERSION=${BUILD_VERSION})
+
+    #######################################################
+    # the following are nanoCLR common compile definitions
+    # for "regular" targets they are set in nf_common_compiler_definitions
+    
+    # build types that have debugging capabilities AND are NOT RTM have to have the define 'NANOCLR_ENABLE_SOURCELEVELDEBUGGING'
+    if((NOT NF_BUILD_RTM) OR NF_FEATURE_DEBUGGER)
+        zephyr_compile_definitions(NANOCLR_ENABLE_SOURCELEVELDEBUGGING)
+    endif()
+
+    # set compiler definition for RTM build option
+    if(NF_BUILD_RTM)
+        zephyr_compile_definitions(BUILD_RTM)
+    endif()
+
+    # set compiler definition for using Application Domains feature
+    if(NF_FEATURE_USE_APPDOMAINS)
+        zephyr_compile_definitions(NANOCLR_USE_APPDOMAINS)
+    endif()
+
+    # set compiler definition for implementing (or not) CRC32 in Wire Protocol
+    if(NF_WP_IMPLEMENTS_CRC32)
+        zephyr_compile_definitions(WP_IMPLEMENTS_CRC32)
+    endif()
+
+    # set definition for Wire Protocol trace mask
+    zephyr_compile_definitions(TRACE_MASK=${WP_TRACE_MASK})
+
+    # set compiler definition regarding inclusion of trace messages and checks on CLR
+    if(NF_PLATFORM_NO_CLR_TRACE)
+        zephyr_compile_definitions(PLATFORM_NO_CLR_TRACE=1)
+    endif()
+
+    # set compiler definition regarding CLR IL inlining
+    if(NF_CLR_NO_IL_INLINE)
+        zephyr_compile_definitions(NANOCLR_NO_IL_INLINE=1)
+    endif()
+
+    # set compiler definition for implementing (or not) TRACE to stdio
+    if(NF_TRACE_TO_STDIO)
+        zephyr_compile_definitions(NF_TRACE_TO_STDIO)
+    endif()
+
+    zephyr_compile_definitions(PLATFORM_ARM)
+
+    #######################################################
+
 endmacro()
