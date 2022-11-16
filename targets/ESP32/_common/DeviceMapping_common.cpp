f5983cc592c0f3364b12f164817b12f1a0b1d4cb
José Simões <jose.simoes@eclo.solutions>
2022-11-16 11:03:24 +0000

    Add support for ESP32_C3 (#2471)

--- targets/ESP32/_common/DeviceMapping_common.cpp
+++ targets/ESP32/_common/DeviceMapping_common.cpp
@@ -49,8 +49,10 @@ int Esp32_GetMappedDevicePins(Esp32_MapDeviceType deviceType, int busIndex, int 
             case DEV_TYPE_ADC:
                 return (int)Esp32_ADC_DevicePinMap[pinIndex];
 
+#if !defined(CONFIG_IDF_TARGET_ESP32C3)
             case DEV_TYPE_DAC:
                 return (int)Esp32_DAC_DevicePinMap[pinIndex];
+#endif
 
             case DEV_TYPE_I2S:
                 return (int)Esp32_I2S_DevicePinMap[busIndex][pinIndex];
@@ -88,9 +90,11 @@ void Esp32_SetMappedDevicePins(Esp32_MapDeviceType deviceType, int busIndex, int
                 Esp32_ADC_DevicePinMap[pinIndex] = ioPinNumber;
                 break;
 
+#if !defined(CONFIG_IDF_TARGET_ESP32C3)
             case DEV_TYPE_DAC:
                 Esp32_DAC_DevicePinMap[pinIndex] = ioPinNumber;
                 break;
+#endif
 
             case DEV_TYPE_I2S:
                 Esp32_I2S_DevicePinMap[busIndex][pinIndex] = ioPinNumber;
