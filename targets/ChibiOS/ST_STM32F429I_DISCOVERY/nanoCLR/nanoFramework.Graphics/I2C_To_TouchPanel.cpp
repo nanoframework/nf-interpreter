733da94875698db2fa0fce889e5bea3e9fc18e30
José Simões <jose.simoes@eclo.solutions>
2023-01-16 18:32:50 +0000

    Code style fixes

--- targets/ChibiOS/ST_STM32F429I_DISCOVERY/nanoCLR/nanoFramework.Graphics/I2C_To_TouchPanel.cpp
+++ targets/ChibiOS/ST_STM32F429I_DISCOVERY/nanoCLR/nanoFramework.Graphics/I2C_To_TouchPanel.cpp
@@ -15,15 +15,14 @@
 #include "Core_Cm7.h"
 #include "STM32f769xx.h"
 
-#define UNUSED(x)  (void)x
+#define UNUSED(x) (void)x
 
 // This code module is written for a STM32F429I DISCOVERY board with display 240x320 TFT and touch screen
 // The touch screen driver IC is a STMPE811QTR
-#define STMPE811QTR_I2C_Address  0x41
+#define STMPE811QTR_I2C_Address 0x41
 
-extern  I2CDriver touchDriver;
+extern I2CDriver touchDriver;
 static CLR_UINT16 I2C_Address;
-
 
 bool TouchInterface::Initialize()
 {
@@ -33,26 +32,24 @@ bool TouchInterface::Initialize()
 
     // #define STM32_I2C_USE_I2C4                  TRUE
 
-
     I2C_Address = STMPE811QTR_I2C_Address;
-
 
     return true;
 }
-CLR_UINT8* TouchInterface::Write_Read(CLR_UINT8* valuesToSend, CLR_UINT16 numberOfValuesToSend, CLR_UINT16 numberValuesExpected)
+CLR_UINT8 *TouchInterface::Write_Read(
+    CLR_UINT8 *valuesToSend,
+    CLR_UINT16 numberOfValuesToSend,
+    CLR_UINT16 numberValuesExpected)
 {
     // TO BE DEVELOPED
     UNUSED(valuesToSend);
     UNUSED(numberOfValuesToSend);
     UNUSED(numberValuesExpected);
 
-    //CLR_UINT8* receivedValues = NULL;
-    //msg_t result = i2cMasterTransmitTimeout(&touchDriver, I2C_Address, valuesToSend, numberOfValuesToSend, receivedValues, numberValuesExpected, TIME_MS2I(20));
-    //return receivedValues;
+    // CLR_UINT8* receivedValues = NULL;
+    // msg_t result = i2cMasterTransmitTimeout(&touchDriver, I2C_Address, valuesToSend, numberOfValuesToSend,
+    // receivedValues, numberValuesExpected, TIME_MS2I(20)); return receivedValues;
 
     return 0U;
-
 }
 #endif // I2C_TO_TOUCHPANEL_H
-
-
