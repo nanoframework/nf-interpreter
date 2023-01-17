a83d0babc79932452a7a1917eb1fba8e6e71cefa
MrCSharp <rafael.yousuf@outlook.com>
2023-01-17 23:56:37 +1100

    add typedefs to transmitter channel for better readability

--- targets/ESP32/_nanoCLR/nanoFramework.Hardware.Esp32.Rmt/nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel.cpp
+++ targets/ESP32/_nanoCLR/nanoFramework.Hardware.Esp32.Rmt/nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel.cpp
@@ -9,6 +9,8 @@
 #define DEFAULT_DIVIDER 4
 
 typedef Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannel RmtChannel;
+typedef Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings RmtChannelSettings;
+typedef Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings TransmitterChannelSettings;
 
 HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
     NativeTxInit___I4(CLR_RT_StackFrame &stack)
@@ -23,22 +25,10 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
         FAULT_ON_NULL(pThis);
 
         // get a reference to the configs in the managed code instance
-        transmitter_channel_settings =
-            pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                      FIELD___transmitterChannelSettings]
-                .Dereference();
-        channel =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___channel]
-                    .NumericByRef()
-                    .s4;
-        pin_number =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___pinNumber]
-                    .NumericByRef()
-                    .s4;
+        transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
+
+        channel = transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
+        pin_number = transmitter_channel_settings[RmtChannelSettings::FIELD___pinNumber].NumericByRef().s4;
 
         if (channel < 0)
         {
@@ -50,72 +40,22 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
         }
 
         rmt_config_t rmt_tx_config = RMT_DEFAULT_CONFIG_TX((gpio_num_t)pin_number, (rmt_channel_t)channel);
-        rmt_tx_config.clk_div =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___clockDivider]
-                    .NumericByRef()
-                    .u1;
-        rmt_tx_config.mem_block_num =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___numberOfMemoryBlocks]
-                    .NumericByRef()
-                    .u1;
-        rmt_tx_config.tx_config.carrier_en =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___enableCarrierWave]
-                    .NumericByRef()
-                    .u1;
-        rmt_tx_config.tx_config.carrier_freq_hz =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___carrierWaveFrequency]
-                    .NumericByRef()
-                    .u4;
+        rmt_tx_config.clk_div = transmitter_channel_settings[RmtChannelSettings::FIELD___clockDivider].NumericByRef().u1;
+        rmt_tx_config.mem_block_num = transmitter_channel_settings[RmtChannelSettings::FIELD___numberOfMemoryBlocks].NumericByRef().u1;
+        rmt_tx_config.tx_config.carrier_en = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___enableCarrierWave].NumericByRef().u1;
+        rmt_tx_config.tx_config.carrier_freq_hz = transmitter_channel_settings[TransmitterChannelSettings::FIELD___carrierWaveFrequency].NumericByRef().u4;
 
-        bool carrier_level =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___carrierLevel]
-                    .NumericByRef()
-                    .u1;
+        bool carrier_level = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___carrierLevel].NumericByRef().u1;
         rmt_tx_config.tx_config.carrier_level = carrier_level ? RMT_CARRIER_LEVEL_HIGH : RMT_CARRIER_LEVEL_LOW;
 
-        rmt_tx_config.tx_config.carrier_duty_percent =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___carrierWaveDutyPercentage]
-                    .NumericByRef()
-                    .u1;
-        rmt_tx_config.tx_config.loop_en =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___enableLooping]
-                    .NumericByRef()
-                    .u1;
-        rmt_tx_config.tx_config.idle_output_en =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___enableIdleLevelOutput]
-                    .NumericByRef()
-                    .u1;
+        rmt_tx_config.tx_config.carrier_duty_percent = transmitter_channel_settings[TransmitterChannelSettings::FIELD___carrierWaveDutyPercentage].NumericByRef().u1;
+        rmt_tx_config.tx_config.loop_en = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___enableLooping].NumericByRef().u1;
+        rmt_tx_config.tx_config.idle_output_en = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___enableIdleLevelOutput].NumericByRef().u1;
 
-        bool idle_level =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___idleLevel]
-                    .NumericByRef()
-                    .u1;
+        bool idle_level = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___idleLevel].NumericByRef().u1;
         rmt_tx_config.tx_config.idle_level = idle_level ? RMT_IDLE_LEVEL_HIGH : RMT_IDLE_LEVEL_LOW;
 
-        bool invert_signal =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___signalInverterEnabled]
-                    .NumericByRef()
-                    .u1;
+        bool invert_signal = (bool)transmitter_channel_settings[RmtChannelSettings::FIELD___signalInverterEnabled].NumericByRef().u1;
         if (invert_signal)
         {
             rmt_tx_config.flags = RMT_CHANNEL_FLAGS_INVERT_SIG;
@@ -156,15 +96,8 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
     FAULT_ON_NULL(pThis);
 
     // get a reference to the configs in the managed code instance
-    transmitter_channel_settings =
-        pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                  FIELD___transmitterChannelSettings]
-            .Dereference();
-    channel = transmitter_channel_settings
-                  [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                       FIELD___channel]
-                      .NumericByRef()
-                      .s4;
+    transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
+    channel = transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
 
     if (!RmtChannel::CheckChannel(channel))
     {
@@ -191,22 +124,9 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
         FAULT_ON_NULL(pThis);
 
         // get a reference to the configs in the managed code instance
-        transmitter_channel_settings =
-            pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                      FIELD___transmitterChannelSettings]
-                .Dereference();
-        channel =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___channel]
-                    .NumericByRef()
-                    .s4;
-        enabled =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___enableLooping]
-                    .NumericByRef()
-                    .u1;
+        transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
+        channel = transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
+        enabled = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___enableLooping].NumericByRef().u1;
 
         if (!RmtChannel::CheckChannel(channel))
         {
@@ -238,40 +158,12 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
         FAULT_ON_NULL(pThis);
 
         // get a reference to the configs in the managed code instance
-        transmitter_channel_settings =
-            pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                      FIELD___transmitterChannelSettings]
-                .Dereference();
-        channel =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___channel]
-                    .NumericByRef()
-                    .s4;
-        carrier_en =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___enableCarrierWave]
-                    .NumericByRef()
-                    .u1;
-        carrier_freq_hz =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___carrierWaveFrequency]
-                    .NumericByRef()
-                    .u4;
-        carrier_duty_percent =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___carrierWaveDutyPercentage]
-                    .NumericByRef()
-                    .u1;
-        carrier_level =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___carrierLevel]
-                    .NumericByRef()
-                    .u1;
+        transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
+        channel = transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
+        carrier_en = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___enableCarrierWave].NumericByRef().u1;
+        carrier_freq_hz = transmitter_channel_settings[TransmitterChannelSettings::FIELD___carrierWaveFrequency].NumericByRef().u4;
+        carrier_duty_percent = transmitter_channel_settings[TransmitterChannelSettings::FIELD___carrierWaveDutyPercentage].NumericByRef().u1;
+        carrier_level = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___carrierLevel].NumericByRef().u1;
 
         if (!RmtChannel::CheckChannel(channel))
         {
@@ -312,28 +204,10 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
         FAULT_ON_NULL(pThis);
 
         // get a reference to the configs in the managed code instance
-        transmitter_channel_settings =
-            pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                      FIELD___transmitterChannelSettings]
-                .Dereference();
-        channel =
-            (rmt_channel_t)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___channel]
-                    .NumericByRef()
-                    .s4;
-        idle_out_en =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___enableIdleLevelOutput]
-                    .NumericByRef()
-                    .u1;
-        idle_level =
-            (bool)transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitChannelSettings::
-                     FIELD___idleLevel]
-                    .NumericByRef()
-                    .u1;
+        transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
+        channel = (rmt_channel_t)transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
+        idle_out_en = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___enableIdleLevelOutput].NumericByRef().u1;
+        idle_level = (bool)transmitter_channel_settings[TransmitterChannelSettings::FIELD___idleLevel].NumericByRef().u1;
 
         if (!RmtChannel::CheckChannel(channel))
         {
@@ -359,18 +233,10 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
     FAULT_ON_NULL(pThis);
 
     // get a reference to the configs in the managed code instance
-    transmitter_channel_settings =
-        pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                  FIELD___transmitterChannelSettings]
-            .Dereference();
+    transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
 
     {
-        int32_t channel =
-            transmitter_channel_settings
-                [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                     FIELD___channel]
-                    .NumericByRef()
-                    .s4;
+        int32_t channel = transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
         bool wait_tx_done = (bool)stack.Arg2().NumericByRef().u1;
 
         CLR_RT_HeapBlock_Array *data = stack.Arg1().DereferenceArray();
@@ -412,16 +278,9 @@ HRESULT Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_E
     FAULT_ON_NULL(pThis);
 
     // get a reference to the configs in the managed code instance
-    transmitter_channel_settings =
-        pThis[Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_TransmitterChannel::
-                  FIELD___transmitterChannelSettings]
-            .Dereference();
+    transmitter_channel_settings = pThis[FIELD___transmitterChannelSettings].Dereference();
 
-    channel = (int32_t)transmitter_channel_settings
-                  [Library_nanoFramework_hardware_esp32_rmt_native_nanoFramework_Hardware_Esp32_Rmt_RmtChannelSettings::
-                       FIELD___channel]
-                      .NumericByRef()
-                      .s4;
+    channel = (int32_t)transmitter_channel_settings[RmtChannelSettings::FIELD___channel].NumericByRef().s4;
 
     if (!RmtChannel::CheckChannel(channel))
     {
