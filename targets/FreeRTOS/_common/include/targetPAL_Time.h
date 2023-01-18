dc4040b436bdf3f7581011c3a7c210632b6cefcb
José Simões <jose.simoes@eclo.solutions>
2023-01-18 16:16:20 +0000

    Fix wrong implementation for FreeRTOS

--- targets/FreeRTOS/_common/include/targetPAL_Time.h
+++ targets/FreeRTOS/_common/include/targetPAL_Time.h
@@ -6,25 +6,4 @@
 #ifndef TARGET_PAL_TIME_H
 #define TARGET_PAL_TIME_H
 
-// Converts FreeRTOS Tickcount to .NET ticks (100 nanoseconds)
-inline __attribute__((always_inline)) uint64_t HAL_Time_SysTicksToTime(uint64_t sysTicks)
-{
-    return sysTicks * configTICK_RATE_HZ * 10;
-}
-
-// because HAL_Time_SysTicksToTime needs to be called from C we need a proxy to allow it to be called from 'C' code
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-    inline __attribute__((always_inline)) uint64_t HAL_Time_SysTicksToTime_C(uint64_t sysTicks)
-    {
-        return sysTicks * configTICK_RATE_HZ * 10;
-    }
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif // TARGET_PAL_TIME_H
