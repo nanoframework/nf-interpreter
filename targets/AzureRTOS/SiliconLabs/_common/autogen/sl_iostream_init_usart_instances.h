0aad4d396123f00a10050a08360337cd97473b33
nfbot <nanoframework@outlook.com>
2023-02-17 19:37:49 +0000

    Code style fixes
    Automated fixes for code style.

--- targets/AzureRTOS/SiliconLabs/_common/autogen/sl_iostream_init_usart_instances.h
+++ targets/AzureRTOS/SiliconLabs/_common/autogen/sl_iostream_init_usart_instances.h
@@ -11,33 +11,33 @@
 #include <target_platform.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #if HAL_WP_USE_SERIAL == TRUE
-extern sl_iostream_t *sl_iostream_vcom_handle;
-extern sl_iostream_uart_t *sl_iostream_uart_vcom_handle;
-extern sl_iostream_instance_info_t sl_iostream_instance_vcom_info;
+    extern sl_iostream_t *sl_iostream_vcom_handle;
+    extern sl_iostream_uart_t *sl_iostream_uart_vcom_handle;
+    extern sl_iostream_instance_info_t sl_iostream_instance_vcom_info;
 #endif
 #if HAL_USE_ONEWIRE == TRUE
-extern sl_iostream_t *sl_iostream_onewire_handle;
-extern sl_iostream_uart_t *sl_iostream_uart_onewire_handle;
-extern sl_iostream_instance_info_t sl_iostream_instance_onewire_info;
+    extern sl_iostream_t *sl_iostream_onewire_handle;
+    extern sl_iostream_uart_t *sl_iostream_uart_onewire_handle;
+    extern sl_iostream_instance_info_t sl_iostream_instance_onewire_info;
 #endif
 
-
-// Initialize only iostream usart instance(s)
-void sl_iostream_usart_init_instances(void);
+    // Initialize only iostream usart instance(s)
+    void sl_iostream_usart_init_instances(void);
 
 #if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
 
 #if HAL_WP_USE_SERIAL == TRUE
-sl_power_manager_on_isr_exit_t sl_iostream_usart_vcom_sleep_on_isr_exit(void);
-bool sl_iostream_usart_vcom_is_ok_to_sleep(void);
+    sl_power_manager_on_isr_exit_t sl_iostream_usart_vcom_sleep_on_isr_exit(void);
+    bool sl_iostream_usart_vcom_is_ok_to_sleep(void);
 #endif
 #if HAL_USE_ONEWIRE == TRUE
-sl_power_manager_on_isr_exit_t sl_iostream_usart_onewire_sleep_on_isr_exit(void);
-bool sl_iostream_usart_onewire_is_ok_to_sleep(void);
+    sl_power_manager_on_isr_exit_t sl_iostream_usart_onewire_sleep_on_isr_exit(void);
+    bool sl_iostream_usart_onewire_is_ok_to_sleep(void);
 #endif
 
 #endif
