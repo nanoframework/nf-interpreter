c4350308872dcb9d563aa55603186a8346c0c608
MATSUOKA Takashi <matsujirushi@live.jp>
2023-03-21 20:52:22 +0900

    Added HAL_WP_USE_USB_CDC for ESP32

--- targets/ESP32/_common/WireProtocol_HAL_Interface.c
+++ targets/ESP32/_common/WireProtocol_HAL_Interface.c
@@ -2,13 +2,13 @@
 // Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
+#include <target_platform.h>
 #include <esp32_idf.h>
 
 #include <nanoHAL_v2.h>
 #include <WireProtocol.h>
 #include <WireProtocol_Message.h>
 #include <WireProtocol_HAL_Interface.h>
-#include "UsbSerial.h"
 
 ////////////////////////////////////////////////////////////////////
 // Baudrate for the serial port                                   //
@@ -17,8 +17,6 @@
 #ifndef TARGET_SERIAL_BAUDRATE
 #define TARGET_SERIAL_BAUDRATE 921600
 #endif
-
-static bool WP_Port_Intitialised = false;
 
 #ifdef CONFIG_IDF_TARGET_ESP32
 
@@ -69,22 +67,127 @@ static uart_port_t ESP32_WP_UART = UART_NUM_0;
 
 #endif
 
-#if CONFIG_USB_CDC_ENABLED
+#if CONFIG_IDF_TARGET_ESP32C3 && HAL_WP_USE_USB_CDC
 
-extern TaskHandle_t ReceiverTask;
+#include <hal/usb_serial_jtag_ll.h>
 
-void WP_Cdc_Rx_Callback(int itf, cdcacm_event_t *event)
+static size_t UsbSerialWrite(const uint8_t *data, size_t dataSize, TickType_t xTicksToWait)
+{
+    ASSERT(data);
+    ASSERT(dataSize >= 1);
+
+    const TickType_t startTicks = xTaskGetTickCount();
+
+    size_t writtenTotalSize = 0;
+    do
+    {
+        if (usb_serial_jtag_ll_txfifo_writable())
+        {
+            const size_t writtenSize = usb_serial_jtag_ll_write_txfifo(data, dataSize);
+            usb_serial_jtag_ll_txfifo_flush();
+            ASSERT(writtenSize <= dataSize);
+
+            data += writtenSize;
+            dataSize -= writtenSize;
+            writtenTotalSize += writtenSize;
+
+            if (dataSize <= 0)
+                break;
+        }
+        else
+        {
+            taskYIELD();
+        }
+    } while (xTaskGetTickCount() - startTicks < xTicksToWait);
+
+    return writtenTotalSize;
+}
+
+static size_t UsbSerialRead(uint8_t *data, size_t dataSize, TickType_t xTicksToWait)
+{
+    ASSERT(data);
+    ASSERT(dataSize >= 1);
+
+    const TickType_t startTicks = xTaskGetTickCount();
+
+    size_t readTotalSize = 0;
+    do
+    {
+        if (usb_serial_jtag_ll_rxfifo_data_available())
+        {
+            const size_t readSize = usb_serial_jtag_ll_read_rxfifo(data, dataSize);
+            ASSERT(readSize <= dataSize);
+
+            data += readSize;
+            dataSize -= readSize;
+            readTotalSize += readSize;
+
+            if (dataSize <= 0)
+                break;
+        }
+        else
+        {
+            taskYIELD();
+        }
+    } while (xTaskGetTickCount() - startTicks < xTicksToWait);
+
+    return readTotalSize;
+}
+
+void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
+{
+    (void)ESP32_WP_UART;
+
+    ASSERT(size);
+    if (*size == 0)
+        return;
+    ASSERT(ptr);
+    ASSERT(*ptr);
+
+    const size_t read = UsbSerialRead(*ptr, *size, pdMS_TO_TICKS(250));
+    ASSERT(read <= *size);
+
+    *ptr += read;
+    *size -= read;
+}
+
+uint8_t WP_TransmitMessage(WP_Message *message)
+{
+    (void)ESP32_WP_UART;
+
+    ASSERT(message);
+
+    if (UsbSerialWrite((const uint8_t *)&message->m_header, sizeof(message->m_header), pdMS_TO_TICKS(250)) !=
+        sizeof(message->m_header))
+        return false;
+
+    if (message->m_header.m_size && message->m_payload)
+    {
+        if (UsbSerialWrite(message->m_payload, message->m_header.m_size, pdMS_TO_TICKS(250)) !=
+            message->m_header.m_size)
+            return false;
+    }
+
+    return true;
+}
+
+#elif CONFIG_USB_CDC_ENABLED
+
+static bool WP_Port_Intitialised = false;
+
+static void WP_Cdc_Rx_Callback(int itf, cdcacm_event_t *event)
 {
     BaseType_t shouldWakeHigherPriorityTask = pdFALSE;
 
     // signal data received
+    extern TaskHandle_t ReceiverTask;
     vTaskNotifyGiveFromISR(ReceiverTask, &shouldWakeHigherPriorityTask);
 
     portYIELD_FROM_ISR(shouldWakeHigherPriorityTask);
 }
 
 // WP using embedded USB CDC
-bool WP_Initialise(COM_HANDLE port)
+static bool WP_Initialise(COM_HANDLE port)
 {
     (void)port;
 
@@ -109,22 +212,6 @@ bool WP_Initialise(COM_HANDLE port)
     return true;
 }
 
-#else
-
-// WP using UART
-bool WP_Initialise(COM_HANDLE port)
-{
-    (void)port;
-
-    UsbSerialIntitialise();
-
-    WP_Port_Intitialised = true;
-
-    return true;
-}
-
-#endif // CONFIG_USB_CDC_ENABLED
-
 void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
 {
     // TODO: Initialise Port if not already done, Wire Protocol should be calling this directly at startup
@@ -139,9 +226,6 @@ void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
     // check for request with 0 size
     if (*size)
     {
-
-#if CONFIG_USB_CDC_ENABLED
-
         // reset read count
         size_t read = 0;
 
@@ -149,10 +233,6 @@ void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
         ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(100));
 
         tinyusb_cdcacm_read(TINYUSB_CDC_ACM_0, *ptr, requestedSize, &read);
-
-#else
-        const size_t read = UsbSerialRead(*ptr, requestedSize, pdMS_TO_TICKS(250));
-#endif
 
         *ptr += read;
         *size -= read;
@@ -165,8 +245,6 @@ uint8_t WP_TransmitMessage(WP_Message *message)
     {
         WP_Initialise(ESP32_WP_UART);
     }
-
-#if CONFIG_USB_CDC_ENABLED
 
     // write header to output stream
     if (tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, (uint8_t *)&message->m_header, sizeof(message->m_header)) !=
@@ -187,20 +265,95 @@ uint8_t WP_TransmitMessage(WP_Message *message)
 
     tinyusb_cdcacm_write_flush(TINYUSB_CDC_ACM_0, 0);
 
+    return true;
+}
+
 #else
 
-    if (UsbSerialWrite((const uint8_t *)&message->m_header, sizeof(message->m_header), pdMS_TO_TICKS(250)) !=
-        sizeof(message->m_header))
-        return false;
+static bool WP_Port_Intitialised = false;
 
-    if (message->m_header.m_size && message->m_payload)
-    {
-        if (UsbSerialWrite(message->m_payload, message->m_header.m_size, pdMS_TO_TICKS(250)) !=
-            message->m_header.m_size)
-            return false;
-    }
+// WP using UART
+static bool WP_Initialise(COM_HANDLE port)
+{
+    (void)port;
 
-#endif
+    ASSERT(ESP32_WP_UART <= SOC_UART_NUM);
+
+    // uninstall driver for console
+    // ESP_ERROR_CHECK(uart_driver_delete(ESP32_WP_UART));
+
+    uart_config_t uart_config = {// baudrate
+                                 .baud_rate = TARGET_SERIAL_BAUDRATE,
+                                 // baudrate
+                                 .data_bits = UART_DATA_8_BITS,
+                                 // parity mode
+                                 .parity = UART_PARITY_DISABLE,
+                                 // stop bit mode
+                                 .stop_bits = UART_STOP_BITS_1,
+                                 // hardware flow control(cts/rts)
+                                 .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
+
+    ESP_ERROR_CHECK(uart_param_config(ESP32_WP_UART, &uart_config));
+
+    ESP_ERROR_CHECK(
+        uart_set_pin(ESP32_WP_UART, ESP32_WP_TX_PIN, ESP32_WP_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+    // setup UART driver with UART queue
+    ESP_ERROR_CHECK(uart_driver_install(ESP32_WP_UART, 256, 256, 0, NULL, ESP_INTR_FLAG_IRAM));
+
+    WP_Port_Intitialised = true;
 
     return true;
 }
+
+void WP_ReceiveBytes(uint8_t **ptr, uint32_t *size)
+{
+    // TODO: Initialise Port if not already done, Wire Protocol should be calling this directly at startup
+    if (!WP_Port_Intitialised)
+    {
+        WP_Initialise(ESP32_WP_UART);
+    }
+
+    // save for later comparison
+    uint32_t requestedSize = *size;
+
+    // check for request with 0 size
+    if (*size)
+    {
+        // non blocking read from serial port with 100ms timeout
+        size_t read = uart_read_bytes(ESP32_WP_UART, *ptr, (uint32_t)requestedSize, pdMS_TO_TICKS(250));
+
+        *ptr += read;
+        *size -= read;
+    }
+}
+
+uint8_t WP_TransmitMessage(WP_Message *message)
+{
+    if (!WP_Port_Intitialised)
+    {
+        WP_Initialise(ESP32_WP_UART);
+    }
+
+    // TODO Check if timeout required
+    // write header to output stream
+    if (uart_write_bytes(ESP32_WP_UART, (const char *)&message->m_header, sizeof(message->m_header)) !=
+        sizeof(message->m_header))
+    {
+        return false;
+    }
+
+    // if there is anything on the payload send it to the output stream
+    if (message->m_header.m_size && message->m_payload)
+    {
+        if (uart_write_bytes(ESP32_WP_UART, (const char *)message->m_payload, message->m_header.m_size) !=
+            (int)message->m_header.m_size)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+#endif
