f7050a7f1fa0bc71cae02a66ca4f37a61f3f9864
Mike Muegel <mike.muegel@skyworksinc.com>
2023-04-03 17:12:41 -0500

    Fix YAML script

--- .github/workflows/make-sky-yamls.pl
+++ .github/workflows/make-sky-yamls.pl
@@ -5,11 +5,11 @@ use File::Slurp;
 
 my $opt = shift;
 
-my $yaml;
+my $yaml_file;
 if ($opt eq "--main") {
-   $yaml = read_file("build_skyworks_evb.main.yml.template");
+   $yaml_file = "build_skyworks_evb.main.yml.template";
 } elsif ($opt eq "--branch") {
-   $yaml = read_file("build_skyworks_evb.yml.template");
+   $yaml_file = "build_skyworks_evb.yml.template";
 } else {
    die "bad usage\n";
 }
@@ -20,6 +20,7 @@ my @targets = split /\r\n|\n|\r/, $targets_buff;
 foreach my $target (@targets)
 {
    print("$target ...\n");
+   my $yaml = read_file($yaml_file);
    $yaml =~ s/\@TARGET\@/$target/g;
    write_file("Build_$target.yaml", $yaml);
 }
