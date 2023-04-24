fe3dbecbd6f9859fafd786da8d36e68bef2e7fb6
nfbot <nanoframework@outlook.com>
2023-04-24 22:13:45 +0000

    Code style fixes
    Automated fixes for code style.

--- targets/ChibiOS/_common/platform_heap.c
+++ targets/ChibiOS/_common/platform_heap.c
@@ -5,8 +5,8 @@
 
 #include <ch.h>
 #include <chmemheaps.h> // Required for platform_realloc heap_header
-#include <string.h>  // Required for platform_realloc memcpy
-//#include <nanoHAL_v2.h> // Not required as we are fully using the platform specific implementation.
+#include <string.h>     // Required for platform_realloc memcpy
+// #include <nanoHAL_v2.h> // Not required as we are fully using the platform specific implementation.
 
 void *platform_malloc(size_t size)
 {
@@ -15,19 +15,21 @@ void *platform_malloc(size_t size)
 
 void platform_free(void *ptr)
 {
-    if (ptr) {
+    if (ptr)
+    {
         chHeapFree(ptr);
     }
 }
 
-void *platform_realloc(void *ptr, size_t size) //ptr is the memory address
+void *platform_realloc(void *ptr, size_t size) // ptr is the memory address
 {
     union heap_header *hp;
     uint32_t prev_size, new_size;
 
     void *new_ptr;
 
-    if(ptr == NULL) {
+    if (ptr == NULL)
+    {
         return chHeapAlloc(NULL, size);
     }
 
@@ -36,19 +38,23 @@ void *platform_realloc(void *ptr, size_t size) //ptr is the memory address
     prev_size = hp->used.size; /* size is always multiple of 8 */
 
     /* check new size memory alignment */
-    if(size % 8 == 0) {
+    if (size % 8 == 0)
+    {
         new_size = size;
     }
-    else {
-        new_size = ((int) (size / 8)) * 8 + 8;
+    else
+    {
+        new_size = ((int)(size / 8)) * 8 + 8;
     }
 
-    if(prev_size >= new_size) {
+    if (prev_size >= new_size)
+    {
         return ptr;
     }
 
     ptr = chHeapAlloc(NULL, size);
-    if(new_ptr == NULL) {
+    if (new_ptr == NULL)
+    {
         return NULL;
     }
 
