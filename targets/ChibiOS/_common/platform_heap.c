c6c01a16218aca23bc1b64f70d0e1afb6408f7e4
Robin Jones <networkfusion@users.noreply.github.com>
2023-04-24 23:11:13 +0100

    Update platform_heap.c
    Ensure compatibility.

--- targets/ChibiOS/_common/platform_heap.c
+++ targets/ChibiOS/_common/platform_heap.c
@@ -3,12 +3,10 @@
 // See LICENSE file in the project root for full license information.
 //
 
-// This file provides the platform specific implementation of memory functions for ChibiOS
-
 #include <ch.h>
 #include <chmemheaps.h> // Required for platform_realloc heap_header
-#include <string.h>     // Required for platform_realloc memcpy
-// #include <nanoHAL_v2.h> // Not required as we are fully using the platform specific implementation.
+#include <string.h>  // Required for platform_realloc memcpy
+//#include <nanoHAL_v2.h> // Not required as we are fully using the platform specific implementation.
 
 void *platform_malloc(size_t size)
 {
@@ -17,52 +15,46 @@ void *platform_malloc(size_t size)
 
 void platform_free(void *ptr)
 {
-    if (ptr)
-    {
+    if (ptr) {
         chHeapFree(ptr);
     }
 }
 
-void *platform_realloc(void *addr, size_t size)
+void *platform_realloc(void *ptr, size_t size) //ptr is the memory address
 {
     union heap_header *hp;
     uint32_t prev_size, new_size;
 
-    void *ptr;
+    void *new_ptr;
 
-    if (addr == NULL)
-    {
+    if(ptr == NULL) {
         return chHeapAlloc(NULL, size);
     }
 
     /* previous allocated segment is preceded by an heap_header */
-    hp = addr - sizeof(union heap_header);
+    hp = ptr - sizeof(union heap_header);
     prev_size = hp->used.size; /* size is always multiple of 8 */
 
     /* check new size memory alignment */
-    if (size % 8 == 0)
-    {
+    if(size % 8 == 0) {
         new_size = size;
     }
-    else
-    {
-        new_size = ((int)(size / 8)) * 8 + 8;
+    else {
+        new_size = ((int) (size / 8)) * 8 + 8;
     }
 
-    if (prev_size >= new_size)
-    {
-        return addr;
+    if(prev_size >= new_size) {
+        return ptr;
     }
 
     ptr = chHeapAlloc(NULL, size);
-    if (ptr == NULL)
-    {
+    if(new_ptr == NULL) {
         return NULL;
     }
 
-    memcpy(ptr, addr, prev_size);
+    memcpy(new_ptr, ptr, prev_size);
 
-    chHeapFree(addr);
+    chHeapFree(ptr);
 
-    return ptr;
+    return new_ptr;
 }
