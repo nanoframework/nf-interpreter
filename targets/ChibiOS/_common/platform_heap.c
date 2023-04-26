8cc5e0e16ea9f994f4080e20e913a80094cf349b
Robin Jones <networkfusion@users.noreply.github.com>
2023-04-26 15:35:26 +0100

    remove realloc

--- targets/ChibiOS/_common/platform_heap.c
+++ targets/ChibiOS/_common/platform_heap.c
@@ -4,8 +4,6 @@
 //
 
 #include <ch.h>
-#include <chmemheaps.h> // Required for platform_realloc heap_header
-#include <string.h>     // Required for platform_realloc memcpy
 // #include <nanoHAL_v2.h> // Not required as we are fully using the platform specific implementation.
 
 void *platform_malloc(size_t size)
@@ -19,48 +17,4 @@ void platform_free(void *ptr)
     {
         chHeapFree(ptr);
     }
-}
-
-void *platform_realloc(void *ptr, size_t size)
-{
-    union heap_header *hp;
-    uint32_t prev_size, new_size;
-
-    void *new_ptr = NULL;
-
-    if (ptr == NULL)
-    {
-        return chHeapAlloc(NULL, size);
-    }
-
-    /* previous allocated segment is preceded by an heap_header */
-    hp = ptr - sizeof(union heap_header);
-    prev_size = hp->used.size; /* size is always multiple of 8 */
-
-    /* check new size memory alignment */
-    if (size % 8 == 0)
-    {
-        new_size = size;
-    }
-    else
-    {
-        new_size = ((int)(size / 8)) * 8 + 8;
-    }
-
-    if (prev_size >= new_size)
-    {
-        return ptr;
-    }
-
-    ptr = chHeapAlloc(NULL, size);
-    if (new_ptr == NULL)
-    {
-        return NULL;
-    }
-
-    memcpy(new_ptr, ptr, prev_size);
-
-    chHeapFree(ptr);
-
-    return new_ptr;
 }
