010097c56874b35c951962055261b2b471256bce
nfbot <nanoframework@outlook.com>
2023-04-26 10:24:26 +0000

    Code style fixes
    Automated fixes for code style.

--- src/HAL/Include/nanoHAL_v2.h
+++ src/HAL/Include/nanoHAL_v2.h
@@ -15,8 +15,8 @@
 // defines to prevent use of malloc, free
 // the platform implementations: platform_malloc(), platform_free
 // are the preferred calls to use as they ensure thread safety and RTOS integration
-#define malloc  YOU_SHALL_NOT_USE_malloc
-#define free    YOU_SHALL_NOT_USE_free
+#define malloc YOU_SHALL_NOT_USE_malloc
+#define free   YOU_SHALL_NOT_USE_free
 
 #endif
 
