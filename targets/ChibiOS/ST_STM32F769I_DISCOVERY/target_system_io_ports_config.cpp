56f1528e5660028036580c27c862322093659c9f
Robin Jones <networkfusion@users.noreply.github.com>
2023-05-02 01:31:50 +0100

    Update target_system_io_ports_config.cpp

--- targets/ChibiOS/ST_STM32F769I_DISCOVERY/target_system_io_ports_config.cpp
+++ targets/ChibiOS/ST_STM32F769I_DISCOVERY/target_system_io_ports_config.cpp
@@ -10,78 +10,31 @@
 // // UART1 //
 // ///////////
 
-// // pin configuration for UART1
-// // port for TX pin is: GPIOD
-// // port for RX pin is: GPIOC
-// // TX pin: is GPIOD_2
-// // RX pin: is GPIOC_12
-// // GPIO alternate pin function is 8 (see "Table 13. STM32F765xx, STM32F767xx, STM32F768Ax and STM32F769xx alternate
-// function mapping" in STM32F769I datasheet) UART_CONFIG_PINS(1, GPIOD, GPIOC, 2, 12, 8)
-
-// // initialization for UART1
-// UART_INIT(1)
-
-// // un-initialization for UART1
-// UART_UNINIT(1)
+// NOT AVAILABLE!
+// USED FOR NF_WIREPROTOCOL
 
 // ///////////
 // // UART2 //
 // ///////////
 
-// // pin configuration for UART2
-// // port for TX pin is: GPIOD
-// // port for RX pin is: GPIOC
-// // TX pin: is GPIOD_2
-// // RX pin: is GPIOC_12
-// // GPIO alternate pin function is 8 (see "Table 13. STM32F765xx, STM32F767xx, STM32F768Ax and STM32F769xx alternate
-// function mapping" in STM32F769I datasheet) UART_CONFIG_PINS(2, GPIOD, GPIOC, 2, 12, 8)
-
-// // initialization for UART2
-// UART_INIT(2)
-
-// // un-initialization for UART2
-// UART_UNINIT(2)
+// NOT AVAILABLE!
 
 // ///////////
 // // UART3 //
 // ///////////
 
-// // pin configuration for UART3
-// // port for TX pin is: GPIOD
-// // port for RX pin is: GPIOC
-// // TX pin: is GPIOD_2
-// // RX pin: is GPIOC_12
-// // GPIO alternate pin function is 8 (see "Table 13. STM32F765xx, STM32F767xx, STM32F768Ax and STM32F769xx alternate
-// function mapping" in STM32F769I datasheet) UART_CONFIG_PINS(3, GPIOD, GPIOC, 2, 12, 8)
-
-// // initialization for UART3
-// UART_INIT(3)
-
-// // un-initialization for UART3
-// UART_UNINIT(3)
+// NOT AVAILABLE!
 
 // ///////////
 // // UART4 //
 // ///////////
 
-// // pin configuration for UART4
-// // port for TX pin is: GPIOD
-// // port for RX pin is: GPIOC
-// // TX pin: is GPIOD_2
-// // RX pin: is GPIOC_12
-// // GPIO alternate pin function is 8 (see "Table 13. STM32F765xx, STM32F767xx, STM32F768Ax and STM32F769xx alternate
-// function mapping" in STM32F769I datasheet) UART_CONFIG_PINS(4, GPIOD, GPIOC, 2, 12, 8)
-
-// // initialization for UART4
-// UART_INIT(4)
-
-// // un-initialization for UART4
-// UART_UNINIT(4)
-
+// NOT AVAILABLE!
 ///////////
 // UART5 //
 ///////////
 
+// Located on CN2, the WiFi/I2C daughter board connector pins 1 and 8
 // pin configuration for UART5
 // port for TX pin is: GPIOD
 // port for RX pin is: GPIOC
@@ -101,6 +54,7 @@ UART_UNINIT(5)
 // UART6 //
 ///////////
 
+// Located on CN13, the Arduino connector pins D0 and D1
 // pin configuration for UART6
 // port for TX pin is: GPIOC
 // port for RX pin is: GPIOC
@@ -119,35 +73,9 @@ UART_UNINIT(6)
 // ///////////
 // // UART7 //
 // ///////////
-
-// // pin configuration for UART7
-// // port for TX pin is: GPIOF
-// // port for RX pin is: GPIOF
-// // TX pin: is GPIOF_7
-// // RX pin: is GPIOF_6
-// // GPIO alternate pin function is 8 (see "Table 13. STM32F765xx, STM32F767xx, STM32F768Ax and STM32F769xx alternate
-// function mapping" in STM32F769I datasheet) UART_CONFIG_PINS(7, GPIOF, GPIOF, 7, 6, 8)
-
-// // initialization for UART7
-// UART_INIT(7)
-
-// // un-initialization for UART7
-// UART_UNINIT(7)
+// NOT AVAILABLE!
 
 // ///////////
 // // UART8 //
 // ///////////
-
-// // pin configuration for UART8
-// // port for TX pin is: GPIOC
-// // port for RX pin is: GPIOC
-// // TX pin: is GPIOC_6
-// // RX pin: is GPIOC_7
-// // GPIO alternate pin function is 8 (see "Table 13. STM32F765xx, STM32F767xx, STM32F768Ax and STM32F769xx alternate
-// function mapping" in STM32F769I datasheet) UART_CONFIG_PINS(8, GPIOC, GPIOC, 6, 7, 8)
-
-// // initialization for UART8
-// UART_INIT(8)
-
-// // un-initialization for UART8
-// UART_UNINIT(8)
+// NOT AVAILABLE!
