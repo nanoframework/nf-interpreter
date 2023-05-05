ad7e929051a0d0681d460d65df9a4c8101a98b7d
José Simões <jose.simoes@eclo.solutions>
2023-05-05 12:10:49 +0100

    Remove gcc pragmas to ignore undefined macros
    - Fix upstream with latest USB release in Gecko SDK.

--- targets/AzureRTOS/SiliconLabs/SKY_EVB/autogen/sl_usbd_class_hid_instances.c
+++ targets/AzureRTOS/SiliconLabs/SKY_EVB/autogen/sl_usbd_class_hid_instances.c
@@ -7,19 +7,9 @@
 //****************************************************************************
 // Includes.
 
-#include "sl_status.h"
-
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wundef"
-#endif
-
+#include <sl_status.h>
 #include <sl_usbd_core.h>
 #include "sl_usbd_class_hid.h"
-
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
 
 /* template headers */
 #include "sl_usbd_configuration_instances.h"
