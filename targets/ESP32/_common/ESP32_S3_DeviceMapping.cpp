4b2be8ebb259e1c9bd066702771731942e62b517
Laurentiu <laurentiu_tonea@microbilt.com>
2023-05-15 15:58:17 -0400

    Apply suggestions from code review

--- targets/ESP32/_common/ESP32_S3_DeviceMapping.cpp
+++ targets/ESP32/_common/ESP32_S3_DeviceMapping.cpp
@@ -5,6 +5,7 @@
 
 #include <esp32_idf.h>
 #include <Esp32_DeviceMapping.h>
+#include <adc_channel.h>
 
 //  SPI
 //  2 devices
@@ -57,17 +58,17 @@ int8_t Esp32_LED_DevicePinMap[16] = {
     -1, // 14
     -1, // 15
     -1, // 16
-    // 5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18,    19,
-    //                                  21, 22, 23, 24, 25, 26, 27, 28, 30, 31, 32, 33, 34, 35 38, 39,
-    //                                  40, 41, 42, 43, 44, 45, 47, 48, 49, 50, 51, 52, 37, 36
 };
 
 // Mapped to ESP32 controllers
-// ESP32 ADC1 channels 0 - 7
-//  "    ADC1 channel  8 - Internal Temperature sensor (VP)
-//  "    ADC1 channel  9 - Internal Hall Sensor (VN)
-//  "    ADC2 channels 10 - 19
-int8_t Esp32_ADC_DevicePinMap[20] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26};
+// ESP32 ADC1 channels 0 - 9
+//       ADC2 channels 10 - 19
+int8_t Esp32_ADC_DevicePinMap[20] = {
+    ADC1_CHANNEL_0_GPIO_NUM, ADC1_CHANNEL_1_GPIO_NUM, ADC1_CHANNEL_2_GPIO_NUM, ADC1_CHANNEL_3_GPIO_NUM,
+    ADC1_CHANNEL_4_GPIO_NUM, ADC1_CHANNEL_5_GPIO_NUM, ADC1_CHANNEL_6_GPIO_NUM, ADC1_CHANNEL_7_GPIO_NUM,
+    ADC1_CHANNEL_8_GPIO_NUM, ADC1_CHANNEL_9_GPIO_NUM, ADC2_CHANNEL_0_GPIO_NUM, ADC2_CHANNEL_1_GPIO_NUM,
+    ADC2_CHANNEL_2_GPIO_NUM, ADC2_CHANNEL_3_GPIO_NUM, ADC2_CHANNEL_4_GPIO_NUM, ADC2_CHANNEL_5_GPIO_NUM,
+    ADC2_CHANNEL_6_GPIO_NUM, ADC2_CHANNEL_7_GPIO_NUM, ADC2_CHANNEL_8_GPIO_NUM, ADC2_CHANNEL_9_GPIO_NUM};
 
 //  I2S
 //  2 device I2S1
