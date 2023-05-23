11d52a0555fc09272d827438fbb09b8f82ef665b
José Simões <jose.simoes@eclo.solutions>
2023-05-23 12:46:49 +0100

    Add CPU_SPI_Initialize_Extended (#2667)
    ***NO_CI***

--- targets/AzureRTOS/SiliconLabs/_nanoCLR/System.Device.Spi/sys_dev_spi_native_target.h
+++ targets/AzureRTOS/SiliconLabs/_nanoCLR/System.Device.Spi/sys_dev_spi_native_target.h
@@ -63,6 +63,12 @@
 #define GECKO_USE_SPI5 FALSE
 #endif
 
+// adjust number of buses
+#if defined(NUM_SPI_BUSES)
+#undef NUM_SPI_BUSES
+#endif
+#define NUM_SPI_BUSES 6
+
 // struct representing the SPI bus
 struct NF_PAL_SPI
 {
@@ -84,6 +90,7 @@ struct NF_PAL_SPI
     int32_t ChipSelect;
 
     NF_SpiDriver_Handle_t Handle;
+    NF_SpiDriver_Init_t *InitSpiData;
 };
 
 ////////////////////////////////////////////
