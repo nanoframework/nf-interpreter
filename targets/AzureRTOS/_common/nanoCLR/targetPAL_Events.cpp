54f00ac27019ba1bd82193ae21574980032e4693
José Simões <jose.simoes@eclo.solutions>
2023-06-16 14:26:29 +0100

    Fix Events_SetBoolTimer for Azure RTOS
    - Timer change was being called with the wrong parameters.

--- targets/AzureRTOS/_common/nanoCLR/targetPAL_Events.cpp
+++ targets/AzureRTOS/_common/nanoCLR/targetPAL_Events.cpp
@@ -51,7 +51,7 @@ void Events_SetBoolTimer(bool *timerCompleteFlag, uint32_t millisecondsFromNow)
 
         // need to stop the timer first
         tx_timer_deactivate(&boolEventsTimer);
-        tx_timer_change(&boolEventsTimer, 0, TX_TICKS_PER_MILLISEC(millisecondsFromNow));
+        tx_timer_change(&boolEventsTimer, TX_TICKS_PER_MILLISEC(millisecondsFromNow), 0);
         tx_timer_activate(&boolEventsTimer);
     }
 }
