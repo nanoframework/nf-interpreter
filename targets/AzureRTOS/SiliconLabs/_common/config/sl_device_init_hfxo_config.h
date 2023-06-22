918f5a50da92a78c09a16cd22ff0589f507cb8c0
nfbot <nanoframework@outlook.com>
2023-06-22 18:27:40 +0100

    Code style fixes (#98)
    Automated fixes for code style.

--- targets/AzureRTOS/SiliconLabs/_common/config/sl_device_init_hfxo_config.h
+++ targets/AzureRTOS/SiliconLabs/_common/config/sl_device_init_hfxo_config.h
@@ -9,32 +9,30 @@
 // <cmuOscMode_AcCoupled=> AC-coupled buffer
 // <cmuOscMode_External=> External digital clock
 // <i> Default: cmuOscMode_Crystal
-#define SL_DEVICE_INIT_HFXO_MODE           cmuOscMode_Crystal
+#define SL_DEVICE_INIT_HFXO_MODE cmuOscMode_Crystal
 
 // <o SL_DEVICE_INIT_HFXO_FREQ> Frequency <4000000-48000000>
 // <i> Default: 50000000
-#define SL_DEVICE_INIT_HFXO_FREQ           50000000
+#define SL_DEVICE_INIT_HFXO_FREQ 50000000
 
 // <o SL_DEVICE_INIT_HFXO_PRECISION> HFXO precision in PPM <0-65535>
 // <i> Default: 500
-#define SL_DEVICE_INIT_HFXO_PRECISION      500
+#define SL_DEVICE_INIT_HFXO_PRECISION 500
 
 // <o SL_DEVICE_INIT_HFXO_CTUNE> CTUNE <0-511>
 // <i> Default: 360
-#define SL_DEVICE_INIT_HFXO_CTUNE          132
+#define SL_DEVICE_INIT_HFXO_CTUNE 132
 
 // <h> Advanced Configurations
-// <o SL_DEVICE_INIT_HFXO_AUTOSTART> Auto-start HFXO. This feature is incompatible with Power Manager and can only be enabled in applications that do not use Power Manager or a radio protocol stack.  - DEPRECATED
-// <true=> True
-// <false=> False
-// <i> Default: false
-#define SL_DEVICE_INIT_HFXO_AUTOSTART      false
+// <o SL_DEVICE_INIT_HFXO_AUTOSTART> Auto-start HFXO. This feature is incompatible with Power Manager and can only be
+// enabled in applications that do not use Power Manager or a radio protocol stack.  - DEPRECATED <true=> True <false=>
+// False <i> Default: false
+#define SL_DEVICE_INIT_HFXO_AUTOSTART false
 
-// <o SL_DEVICE_INIT_HFXO_AUTOSELECT> Auto-select HFXO. This feature is incompatible with Power Manager and can only be enabled in applications that do not use Power Manager or a radio protocol stack.  - DEPRECATED
-// <true=> True
-// <false=> False
-// <i> Default: false
-#define SL_DEVICE_INIT_HFXO_AUTOSELECT      false
+// <o SL_DEVICE_INIT_HFXO_AUTOSELECT> Auto-select HFXO. This feature is incompatible with Power Manager and can only be
+// enabled in applications that do not use Power Manager or a radio protocol stack.  - DEPRECATED <true=> True <false=>
+// False <i> Default: false
+#define SL_DEVICE_INIT_HFXO_AUTOSELECT false
 
 // </h>
 
