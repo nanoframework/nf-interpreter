4ff742b92b6b0a9f712803b75efb9ebff4b0ca5d
António Fagundes <antonio.fagundes@eclo.solutions>
2023-07-24 16:48:29 +0100

    Fix ADC config for Si5575

--- targets/AzureRTOS/SiliconLabs/Si5575_CEVB/target_nano_gg_adc_config.h
+++ targets/AzureRTOS/SiliconLabs/Si5575_CEVB/target_nano_gg_adc_config.h
@@ -5,7 +5,5 @@
 
 // No ADC on Si5575 CEVB" CMakePresets.json sets API_nanoFramework.GiantGecko.Adc=OFF
 
-/*
-#define GECKO_USE_ADC0 TRUE
+#define GECKO_USE_ADC0 FALSE
 #define GECKO_USE_ADC1 FALSE
-*/
