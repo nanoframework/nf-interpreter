a6ce791a6e8bd5d099b279017a24a59ef4f72e5c
Anthony Burns <anthony.burns@skyworksinc.com>
2023-07-31 11:14:13 -0500

    Revert "Added missing configuration to disable SPI in the STB Interposer CLR."
    Leaving SPI enabled as the pins it uses will not be connected to anything on the Interposer
    
    This reverts commit 1804bb55a52f3d75cbc91db3949e62d3817e86d5.

--- targets/AzureRTOS/SiliconLabs/STB_Interposer/target_system_device_i2c_config.cpp
+++ targets/AzureRTOS/SiliconLabs/STB_Interposer/target_system_device_i2c_config.cpp
@@ -5,8 +5,6 @@
 
 #include <sys_dev_i2c_native_target.h>
 
-/*
-
 //////////
 // I2C0 //
 //////////
@@ -28,5 +26,3 @@ I2C_CONFIG_PINS(0, gpioPortA, gpioPortA, 1, 0, 0, 0)
 
 // GPIO alternate pin function is 0 for both pins (see alternate function mapping table in device datasheet)
 I2C_CONFIG_PINS(1, gpioPortC, gpioPortC, 5, 4, 0, 0)
-
-*/
--- targets/AzureRTOS/SiliconLabs/STB_Interposer/target_system_device_spi_config.h
+++ targets/AzureRTOS/SiliconLabs/STB_Interposer/target_system_device_spi_config.h
@@ -2,6 +2,5 @@
 // Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
-
-#define GECKO_USE_SPI1 FALSE
-#define GECKO_USE_SPI2 FALSE
+#define GECKO_USE_SPI1 TRUE
+#define GECKO_USE_SPI2 TRUE
