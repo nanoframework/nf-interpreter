0a1d5ef31033a4ca5d71f528294442e10236c2cc
Robin Jones <networkfusion@users.noreply.github.com>
2023-09-10 20:25:17 +0100

    Minor fixes

--- targets/ChibiOS/ST_NUCLEO144_F767ZI/stm32f7xx_hal_conf.h
+++ targets/ChibiOS/ST_NUCLEO144_F767ZI/stm32f7xx_hal_conf.h
@@ -43,7 +43,7 @@
   * @retval None
   */
   #define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
-  void assert_failed(uint8_t* file, uint32_t line);
+  void assert_failed(uint8_t *file, uint32_t line);
 #else
   #define assert_param(expr) ((void)0U)
 #endif /* USE_FULL_ASSERT */
