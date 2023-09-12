03506496b061e98f560dab6730b0f01d514b2911
Robin Jones <networkfusion@users.noreply.github.com>
2023-09-12 12:49:12 +0100

    Turn on ADC

--- targets/ChibiOS/ST_NUCLEO144_F767ZI/nanoCLR/mcuconf.h
+++ targets/ChibiOS/ST_NUCLEO144_F767ZI/nanoCLR/mcuconf.h
@@ -146,9 +146,9 @@
  * ADC driver system settings.
  */
 #define STM32_ADC_ADCPRE                    ADC_CCR_ADCPRE_DIV4
-#define STM32_ADC_USE_ADC1                  FALSE
-#define STM32_ADC_USE_ADC2                  FALSE
-#define STM32_ADC_USE_ADC3                  FALSE
+#define STM32_ADC_USE_ADC1                  TRUE
+#define STM32_ADC_USE_ADC2                  TRUE
+#define STM32_ADC_USE_ADC3                  TRUE
 #define STM32_ADC_ADC1_DMA_STREAM           STM32_DMA_STREAM_ID(2, 4)
 #define STM32_ADC_ADC2_DMA_STREAM           STM32_DMA_STREAM_ID(2, 2)
 #define STM32_ADC_ADC3_DMA_STREAM           STM32_DMA_STREAM_ID(2, 1)
