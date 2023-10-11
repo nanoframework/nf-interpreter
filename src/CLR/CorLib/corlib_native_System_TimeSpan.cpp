4b07c7651ccb7900c83494a3a139f861a496306f
José Simões <jose.simoes@eclo.solutions>
2023-10-11 14:06:18 +0100

    DateTime and TimeSpan are not handled as primitive types
    - Remove these throughout the code base.
    - Add DateTime::NewObject and rework code accordingly.
    - Update data type lookup for DateTime and TimeSpan so they end up as value type types.

--- src/CLR/CorLib/corlib_native_System_TimeSpan.cpp
+++ src/CLR/CorLib/corlib_native_System_TimeSpan.cpp
@@ -180,12 +180,18 @@ HRESULT Library_corlib_native_System_TimeSpan::Equals___STATIC__BOOLEAN__SystemT
 CLR_INT64 *Library_corlib_native_System_TimeSpan::NewObject(CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
+
+    CLR_RT_TypeDescriptor dtType;
+
     CLR_RT_HeapBlock &ref = stack.PushValue();
 
-    ref.SetDataId(CLR_RT_HEAPBLOCK_RAW_ID(DATATYPE_TIMESPAN, 0, 1));
-    ref.ClearData();
+    // initialize <TimeSpan> type descriptor
+    dtType.InitializeFromType(g_CLR_RT_WellKnownTypes.m_TimeSpan);
 
-    return (CLR_INT64 *)&ref.NumericByRef().s8;
+    // create an instance of <TimeSpan>
+    g_CLR_RT_ExecutionEngine.NewObject(ref, dtType.m_handlerCls);
+
+    return GetValuePtr(ref);
 }
 
 CLR_INT64 *Library_corlib_native_System_TimeSpan::GetValuePtr(CLR_RT_StackFrame &stack)
@@ -207,11 +213,6 @@ CLR_INT64 *Library_corlib_native_System_TimeSpan::GetValuePtr(CLR_RT_HeapBlock &
             return NULL;
 
         dt = obj->DataType();
-    }
-
-    if (dt == DATATYPE_TIMESPAN)
-    {
-        return (CLR_INT64 *)&obj->NumericByRef().s8;
     }
 
     if (dt == DATATYPE_I8)
