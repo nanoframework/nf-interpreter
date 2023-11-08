a23310b974cb4fe47edb29d3376335953001792c
José Simões <jose.simoes@eclo.solutions>
2023-11-08 18:45:41 +0000

    Add implementation for I2C Slave (#2824)
    ***NO_CI***

--- targets/ESP32/_nanoCLR/System.Device.I2c.Slave/sys_dev_i2c_slave_native_target.h
+++ targets/ESP32/_nanoCLR/System.Device.I2c.Slave/sys_dev_i2c_slave_native_target.h
@@ -3,13 +3,14 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#ifndef SYS_DEV_I2C_NATIVE_TARGET_H
-#define SYS_DEV_I2C_NATIVE_TARGET_H
+#ifndef SYS_DEV_I2C_SLAVE_NATIVE_TARGET_H
+#define SYS_DEV_I2C_SLAVE_NATIVE_TARGET_H
 
-#include <sys_dev_i2c_native.h>
+#include <sys_dev_i2c_slave_native.h>
 #include <string.h>
 #include <targetPAL.h>
 #include <targetPAL_I2c.h>
 #include <Esp32_DeviceMapping.h>
+#include <target_system_device_i2c_slave_config.h>
 
-#endif // SYS_DEV_I2C_NATIVE_TARGET_H
+#endif // SYS_DEV_I2C_SLAVE_NATIVE_TARGET_H
