97b12c832301a3125c4da0cf284d650b329201ec
José Simões <jose.simoes@eclo.solutions>
2023-12-21 01:36:25 +0000

    Update mcuconf for ChibiOS community (#2850)

--- targets/ChibiOS/ORGPAL_PALTHREE/nanoCLR/mcuconf_community.h
+++ targets/ChibiOS/ORGPAL_PALTHREE/nanoCLR/mcuconf_community.h
@@ -4,21 +4,19 @@
 // See LICENSE file in the project root for full license information.
 //
 
-/*
- * USBH driver system settings.
- */
-#define STM32_OTG1_CHANNELS_NUMBER          8
-#define STM32_OTG2_CHANNELS_NUMBER          12
+// USBH driver system settings
+#define STM32_OTG_FS_CHANNELS_NUMBER 8
+#define STM32_OTG_HS_CHANNELS_NUMBER 12
 
-#define STM32_USBH_USE_OTG1                 FALSE
-#define STM32_OTG1_RXFIFO_SIZE              1024
-#define STM32_OTG1_PTXFIFO_SIZE             128
-#define STM32_OTG1_NPTXFIFO_SIZE            128
+#define STM32_USBH_USE_OTG1      FALSE
+#define STM32_OTG1_RXFIFO_SIZE   1024
+#define STM32_OTG1_PTXFIFO_SIZE  128
+#define STM32_OTG1_NPTXFIFO_SIZE 128
 
-#define STM32_USBH_USE_OTG2                 TRUE
-#define STM32_OTG2_RXFIFO_SIZE              2048
-#define STM32_OTG2_PTXFIFO_SIZE             1024
-#define STM32_OTG2_NPTXFIFO_SIZE            1024
+#define STM32_USBH_USE_OTG2      TRUE
+#define STM32_OTG2_RXFIFO_SIZE   2048
+#define STM32_OTG2_PTXFIFO_SIZE  1024
+#define STM32_OTG2_NPTXFIFO_SIZE 1024
 
-#define STM32_USBH_MIN_QSPACE               4
-#define STM32_USBH_CHANNELS_NP              4
+#define STM32_USBH_MIN_QSPACE  4
+#define STM32_USBH_CHANNELS_NP 4
