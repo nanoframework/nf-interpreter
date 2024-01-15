9398f004f030abe128438d7a4e66ca0e80e306d7
Robin Jones <networkfusion@users.noreply.github.com>
2024-01-15 19:08:08 +0000

    Add extra improvements
    Can be used as future reference.

--- src/HAL/Include/nanoHAL_Persistent_Storage.h
+++ src/HAL/Include/nanoHAL_Persistent_Storage.h
@@ -15,30 +15,46 @@
 
 // driver letters and paths
 
+typedef enum {
+    SD,
+    USB,
+    SPI,
+    __STORAGE_TYPE_COUNT
+} storage_type_t;
+
+typedef struct {
+    char drive_letter[1];
+    uint8_t drive_index;
+    storage_type_t drive_type;
+    bool enabled;
+} storage_info_t;
+
+#define DRIVE_PATH_STR ":\\"
+
 // SD Card
 // #define SDCARD_DRIVE_NAME "SD"
 #define INDEX0_DRIVE_LETTER "D"
-#define INDEX0_DRIVE_PATH   INDEX0_DRIVE_LETTER ":\\"
+#define INDEX0_DRIVE_PATH   INDEX0_DRIVE_LETTER DRIVE_PATH_STR
 
 // USB mass storage device
 // #define USB_DRIVE_NAME "USB"
 #define INDEX1_DRIVE_LETTER "E"
-#define INDEX1_DRIVE_PATH   INDEX1_DRIVE_LETTER ":\\"
+#define INDEX1_DRIVE_PATH   INDEX1_DRIVE_LETTER DRIVE_PATH_STR
 
 // USB mass storage device 2
 // #define USB2_DRIVE_NAME "USB2"
 #define INDEX2_DRIVE_LETTER "F"
-#define INDEX2_DRIVE_PATH   INDEX2_DRIVE_LETTER ":\\"
+#define INDEX2_DRIVE_PATH   INDEX2_DRIVE_LETTER DRIVE_PATH_STR
 
 // Internal drive 0 (SPIFFS)
 // #define INTERNAL_DRIVE0_NAME "SPI"
 #define INDEX3_DRIVE_LETTER "I"
-#define INDEX3_DRIVE_PATH   INDEX3_DRIVE_LETTER ":\\"
+#define INDEX3_DRIVE_PATH   INDEX3_DRIVE_LETTER DRIVE_PATH_STR
 
 // Internal drive 1 (SPIFFS)
 // #define INTERNAL_DRIVE1_NAME "SPI2"
 #define INDEX4_DRIVE_LETTER "J"
-#define INDEX4_DRIVE_PATH   INDEX4_DRIVE_LETTER ":\\"
+#define INDEX4_DRIVE_PATH   INDEX4_DRIVE_LETTER DRIVE_PATH_STR
 
 //////////////////////////////////////////////////////////////////////////
 // Keep in sync with StorageEventManager.StorageEventType in managed code
@@ -59,8 +75,8 @@ typedef enum StorageEventType
 
 // constants to be used throughout the code
 #define SUPPORTED_DRIVES_COUNT (2)
-#define DRIVE_LETTER_LENGTH    sizeof(INDEX0_DRIVE_LETTER)
-#define DRIVE_PATH_LENGTH      sizeof(INDEX0_DRIVE_PATH)
+#define DRIVE_LETTER_LENGTH    (1)
+#define DRIVE_PATH_LENGTH      (DRIVE_LETTER_LENGTH + sizeof(DRIVE_PATH_STR))
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // !!! KEEP IN SYNC WITH Windows.Storage.CreationCollisionOption (in managed code) !!! //
