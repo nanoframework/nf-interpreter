3679ae39fc0b2da66b61dfb45b4e816a998c7b02
José Simões <jose.simoes@eclo.solutions>
2024-01-15 13:36:15 +0000

    Update declaration with bumped version number

--- src/DeviceInterfaces/System.Net/sys_net_native.cpp
+++ src/DeviceInterfaces/System.Net/sys_net_native.cpp
@@ -344,6 +344,6 @@ const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_Net =
     "System.Net",
     0xA6875261,
     method_lookup,
-    { 100, 1, 5, 0 }
+    { 100, 2, 0, 0 }
 };
 // clang-format on
