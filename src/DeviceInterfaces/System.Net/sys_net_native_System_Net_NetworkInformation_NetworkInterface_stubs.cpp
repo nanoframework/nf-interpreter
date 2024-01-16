3566af2c8ec252d8dddaaf3ee50a318fdf073981
Adrian Soundy <adriansoundy@gmail.com>
2024-01-16 16:20:09 +1300

    Add support for IPV6 sockets (#2863)
    Co-authored-by: nfbot <nanoframework@outlook.com>
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- src/DeviceInterfaces/System.Net/sys_net_native_System_Net_NetworkInformation_NetworkInterface_stubs.cpp
+++ src/DeviceInterfaces/System.Net/sys_net_native_System_Net_NetworkInformation_NetworkInterface_stubs.cpp
@@ -55,8 +55,18 @@ HRESULT Library_sys_net_native_System_Net_NetworkInformation_NetworkInterface::
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_sys_net_native_System_Net_NetworkInformation_NetworkInterface::IPAddressFromString___STATIC__I8__STRING(
-    CLR_RT_StackFrame &stack)
+HRESULT Library_sys_net_native_System_Net_NetworkInformation_NetworkInterface::
+    IPV4AddressFromString___STATIC__I8__STRING(CLR_RT_StackFrame &stack)
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_sys_net_native_System_Net_NetworkInformation_NetworkInterface::
+    IPV6AddressFromString___STATIC__SZARRAY_U2__STRING(CLR_RT_StackFrame &stack)
 {
     NANOCLR_HEADER();
 
