e7f86c3ea7d1966ab4f4c533a0249ab87f4ed0af
alberk8 <alberk@gmail.com>
2024-02-01 13:41:47 +0800

    Fix ESP32 PWM when dutycycle and frequency is zero

--- targets/ESP32/_nanoCLR/System.Device.Pwm/sys_dev_pwm_native_System_Device_Pwm_PwmChannel.cpp
+++ targets/ESP32/_nanoCLR/System.Device.Pwm/sys_dev_pwm_native_System_Device_Pwm_PwmChannel.cpp
@@ -54,6 +54,8 @@ esp_err_t SetDutyCycle(ledc_mode_t speed_mode, ledc_channel_t channel, uint32_t 
 
 using namespace sys_dev_pwm_native_System_Device_Pwm_PwmChannelHelpers;
 
+bool isStarted;
+
 //
 //  Look up Pin number to find channel, if create true and not present then add pin
 //  return channel number or -1 if error
@@ -180,6 +182,8 @@ HRESULT sys_dev_pwm_native_System_Device_Pwm_PwmChannelHelpers::ConfigureAndStar
     {
         ledc_stop(mode, channel, polarity);
     }
+    
+    isStarted = true;
 
     NANOCLR_NOCLEANUP();
 }
@@ -286,18 +290,18 @@ HRESULT Library_sys_dev_pwm_native_System_Device_Pwm_PwmChannel::NativeSetDesire
     channel = (ledc_channel_t)GetChannel(pinNumber, timerId, false);
     dutyCycle = pThis[FIELD___dutyCycle].NumericByRef().u4;
 
+    // parameter check
+    if (desiredFrequency <= 0)
+    {
+        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
+    }
+
     // Set the backing frequency field when it is zero and return
     // We need this as this native function is called before Init
-    if (channel == -1)
+    if (!isStarted || channel == -1)
     {
         pThis[FIELD___frequency].NumericByRef().s4 = desiredFrequency;
         NANOCLR_SET_AND_LEAVE(S_OK);
-    }
-
-    // parameter check
-    if (desiredFrequency < 0)
-    {
-        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
     }
 
     timer = (ledc_timer_t)(timerId & 0x03);
@@ -351,7 +355,6 @@ HRESULT Library_sys_dev_pwm_native_System_Device_Pwm_PwmChannel::NativeSetActive
     ledc_mode_t speed_mode;
     int32_t desiredFrequency;
     int32_t optimumDutyResolution;
-    int32_t privateDutyCycle;
     uint32_t calDutyCycle;
 
     NANOCLR_HEADER();
@@ -363,7 +366,6 @@ HRESULT Library_sys_dev_pwm_native_System_Device_Pwm_PwmChannel::NativeSetActive
     // Retrieves the needed parameters from private class properties or method parameters
     timerId = pThis[FIELD___pwmTimer].NumericByRef().s4;
     pinNumber = pThis[FIELD___pinNumber].NumericByRef().s4;
-    privateDutyCycle = pThis[FIELD___dutyCycle].NumericByRef().u4;
     desiredFrequency = pThis[FIELD___frequency].NumericByRef().s4;
 
     // parameter check
@@ -378,7 +380,7 @@ HRESULT Library_sys_dev_pwm_native_System_Device_Pwm_PwmChannel::NativeSetActive
 
     // Set the backing duty cycle field when it is zero and return
     // We need this as this native function is called before Init
-    if (privateDutyCycle == 0)
+    if (!isStarted)
     {
         pThis[FIELD___dutyCycle].NumericByRef().u4 = dutyCycle;
         NANOCLR_SET_AND_LEAVE(S_OK);
@@ -442,6 +444,8 @@ HRESULT Library_sys_dev_pwm_native_System_Device_Pwm_PwmChannel::NativeStop___VO
     channel = (ledc_channel_t)GetChannel(pinNumber, timerId, false);
 
     ledc_stop(speed_mode, channel, (uint32_t)polarity);
+
+    isStarted = false;
 
     NANOCLR_NOCLEANUP();
 }
