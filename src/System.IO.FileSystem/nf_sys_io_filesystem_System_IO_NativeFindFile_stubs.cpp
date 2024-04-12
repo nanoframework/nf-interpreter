c5d52cf075d130817488f9d613283357eb40f0c6
josesimoes <jose.simoes@eclo.solutions>
2024-04-12 10:11:06 +0100

    Add vcxp project for Sys.IO.Filesystem

---
+++ src/System.IO.FileSystem/nf_sys_io_filesystem_System_IO_NativeFindFile_stubs.cpp
@@ -0,0 +1,42 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include "nf_sys_io_filesystem.h"
+
+HRESULT Library_nf_sys_io_filesystem_System_IO_NativeFindFile::_ctor___VOID__STRING__STRING( CLR_RT_StackFrame &stack )
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_sys_io_filesystem_System_IO_NativeFindFile::GetNext___SystemIONativeFileInfo( CLR_RT_StackFrame &stack )
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_sys_io_filesystem_System_IO_NativeFindFile::Close___VOID( CLR_RT_StackFrame &stack )
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
+
+HRESULT Library_nf_sys_io_filesystem_System_IO_NativeFindFile::GetFileInfo___STATIC__SystemIONativeFileInfo__STRING( CLR_RT_StackFrame &stack )
+{
+    NANOCLR_HEADER();
+
+    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+
+    NANOCLR_NOCLEANUP();
+}
