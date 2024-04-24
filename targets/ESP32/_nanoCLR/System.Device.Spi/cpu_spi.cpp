96ce46a854d644d797ef1eb5c3b10fc4705302f0
Cory Charlton <CoryCharlton@users.noreply.github.com>
2024-04-24 01:01:22 -0700

    Properly offset managed SPI bus ID to native for ESP32C3 and S3 (#2915)

--- targets/ESP32/_nanoCLR/System.Device.Spi/cpu_spi.cpp
+++ targets/ESP32/_nanoCLR/System.Device.Spi/cpu_spi.cpp
@@ -189,8 +189,10 @@ bool CPU_SPI_Initialize(uint8_t busIndex, const SPI_DEVICE_CONFIGURATION &spiDev
     if (ret != ESP_OK)
     {
 #if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32S3)
-        ESP_LOGE(TAG, "Unable to init SPI bus %d esp_err %d", busIndex, ret);
+        // First available bus on ESP32_C3/S3 is SPI2_HOST
+        ESP_LOGE(TAG, "Unable to init SPI bus %d esp_err %d", busIndex + SPI2_HOST, ret);
 #else
+        // First available bus on ESP32 is HSPI_HOST(1)
         ESP_LOGE(TAG, "Unable to init SPI bus %d esp_err %d", busIndex + HSPI_HOST, ret);
 #endif
         return false;
@@ -208,16 +210,20 @@ bool CPU_SPI_Initialize(uint8_t busIndex, const SPI_DEVICE_CONFIGURATION &spiDev
 bool CPU_SPI_Uninitialize(uint8_t busIndex)
 {
 #if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32S3)
-    esp_err_t ret = spi_bus_free((spi_host_device_t)(busIndex));
+    // First available bus on ESP32_C3/S3 is SPI2_HOST
+    esp_err_t ret = spi_bus_free((spi_host_device_t)(busIndex + SPI2_HOST));
 #else
+    // First available bus on ESP32 is HSPI_HOST(1)
     esp_err_t ret = spi_bus_free((spi_host_device_t)(busIndex + HSPI_HOST));
 #endif
 
     if (ret != ESP_OK)
     {
 #if defined(CONFIG_IDF_TARGET_ESP32C3) || defined(CONFIG_IDF_TARGET_ESP32S3)
-        ESP_LOGE(TAG, "spi_bus_free bus %d esp_err %d", busIndex, ret);
+        // First available bus on ESP32_C3/S3 is SPI2_HOST
+        ESP_LOGE(TAG, "spi_bus_free bus %d esp_err %d", busIndex + SPI2_HOST, ret);
 #else
+        // First available bus on ESP32 is HSPI_HOST(1)
         ESP_LOGE(TAG, "spi_bus_free bus %d esp_err %d", busIndex + HSPI_HOST, ret);
 #endif
 
