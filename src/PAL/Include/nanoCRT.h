0c51f8ed319c0cdac12ac515fb22c6aab77671bc
josesimoes <jose.simoes@eclo.solutions>
2024-05-09 19:41:57 +0100

    Fix declarations and formatting
    ***NO_CI***

--- src/PAL/Include/nanoCRT.h
+++ src/PAL/Include/nanoCRT.h
@@ -7,15 +7,17 @@
 #ifndef NANOCRT_DECL_H
 #define NANOCRT_DECL_H
 
+#include <stddef.h>
+
 #if defined(PLATFORM_EMULATED_FLOATINGPOINT)
-int hal_snprintf_float( char* buffer, size_t len, const char* format, int32_t f );
-int hal_snprintf_double( char* buffer, size_t len, const char* format, int64_t& d );
+int hal_snprintf_float(char *buffer, size_t len, const char *format, int32_t f);
+int hal_snprintf_double(char *buffer, size_t len, const char *format, int64_t &d);
 #else
-int hal_snprintf_float( char* buffer, size_t len, const char* format, float f );
-int hal_snprintf_double( char* buffer, size_t len, const char* format, double d );
+int hal_snprintf_float(char *buffer, size_t len, const char *format, float f);
+int hal_snprintf_double(char *buffer, size_t len, const char *format, double d);
 #endif
 
 // Compares 2 ASCII strings case insensitive. Always defined in our code ( nanoCRT.cpp )
-int hal_stricmp( const char * dst, const char * src );
+int hal_stricmp(const char *dst, const char *src);
 
 #endif // NANOCRT_DECL_H
