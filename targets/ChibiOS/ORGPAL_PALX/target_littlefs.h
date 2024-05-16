21b6083736393e3ff568f9873100b8dafa537f97
josesimoes <jose.simoes@eclo.solutions>
2024-05-16 18:36:56 +0100

    Remove unused commands

--- targets/ChibiOS/ORGPAL_PALX/target_littlefs.h
+++ targets/ChibiOS/ORGPAL_PALX/target_littlefs.h
@@ -46,52 +46,24 @@
 // Identification Operations
 #define READ_ID_CMD                       0x90
 #define READ_ID_CMD2                      0x9F
-#define MULTIPLE_IO_READ_ID_CMD           0xAF
-#define READ_SERIAL_FLASH_DISCO_PARAM_CMD 0x5A
 
 // Read Operations
-#define READ_CMD                 0x03
-#define FAST_READ_CMD            0x0B
-#define DUAL_OUT_FAST_READ_CMD   0x3B
-#define DUAL_INOUT_FAST_READ_CMD 0xBB
 #define QUAD_OUT_FAST_READ_CMD   0x6B
-#define QUAD_INOUT_FAST_READ_CMD 0xEB
 
 // Write Operations
 #define WRITE_ENABLE_CMD  0x06
-#define WRITE_DISABLE_CMD 0x04
 
 // Register Operations
 #define READ_STATUS_REG1_CMD           0x05
-#define WRITE_STATUS_REG1_CMD          0x01
 #define READ_STATUS_REG2_CMD           0x35
 #define WRITE_STATUS_REG2_CMD          0x31
-#define READ_LOCK_REG_CMD              0xE8
-#define WRITE_LOCK_REG_CMD             0xE5
-#define READ_FLAG_STATUS_REG_CMD       0x70
-#define CLEAR_FLAG_STATUS_REG_CMD      0x50
-#define READ_NONVOL_CFG_REG_CMD        0xB5
-#define WRITE_NONVOL_CFG_REG_CMD       0xB1
-#define READ_ENHANCED_VOL_CFG_REG_CMD  0x65
-#define WRITE_ENHANCED_VOL_CFG_REG_CMD 0x61
 
 // Program Operations
 #define PAGE_PROG_CMD             0x02
-#define DUAL_IN_FAST_PROG_CMD     0xA2
-#define EXT_DUAL_IN_FAST_PROG_CMD 0xD2
 #define QUAD_IN_FAST_PROG_CMD     0x32
-#define EXT_QUAD_IN_FAST_PROG_CMD 0x12
 
 // Erase Operations
 #define SUBSECTOR_ERASE_CMD    0x20
-#define SECTOR_ERASE_CMD       0xD8
-#define BULK_ERASE_CMD         0xC7
-#define PROG_ERASE_RESUME_CMD  0x7A
-#define PROG_ERASE_SUSPEND_CMD 0x75
-
-// One-Time Programmable Operations
-#define READ_OTP_ARRAY_CMD 0x4B
-#define PROG_OTP_ARRAY_CMD 0x42
 
 //  W25Q128 Registers
 // Status Register 1
