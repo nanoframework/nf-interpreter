2ec8242685f743c0c8e9f42dbbf7ece063cc796c
josesimoes <jose.simoes@eclo.solutions>
2024-06-04 13:24:52 +0100

    Fix define checks

--- targets/ChibiOS/_include/Target_System_IO_FileSystem.h
+++ targets/ChibiOS/_include/Target_System_IO_FileSystem.h
@@ -29,23 +29,19 @@
 // and those have to follow the sequence that is used in ChibiOS FatFS wrappers
 // SD Card (or SPI) is 1st and USB MAS is 2nd (if SD Card is enabled)
 // this is also mapped in the FatFS configuration
-#if (HAL_USE_SDC == TRUE)
-
+#if (HAL_USE_SDC == TRUE) && (HAL_USBH_USE_MSD == TRUE)
 #define SD_CARD_DRIVE_INDEX         "0:"
 #define SD_CARD_DRIVE_INDEX_NUMERIC (0)
-
-#endif
-
-#if (HAL_USE_SDC == TRUE) && defined(HAL_USBH_USE_MSD)
-
 #define USB_MSD_DRIVE_INDEX         "1:"
 #define USB_MSD_DRIVE_INDEX_NUMERIC (1)
-
-#elif (HAL_USE_SDC == FALSE) && defined(HAL_USBH_USE_MSD)
-
+#elif (HAL_USE_SDC == FALSE) && (HAL_USBH_USE_MSD == TRUE)
 #define USB_MSD_DRIVE_INDEX         "0:"
 #define USB_MSD_DRIVE_INDEX_NUMERIC (0)
-
+#elif (HAL_USE_SDC == TRUE) && (HAL_USBH_USE_MSD == FALSE)
+#define SD_CARD_DRIVE_INDEX         "0:"
+#define SD_CARD_DRIVE_INDEX_NUMERIC (0)
+#else
+#error "FatFS being configured without SDCard or USB Mass Storage Device enabled. Check configuration."
 #endif
 
 #ifdef __cplusplus
