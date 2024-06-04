64a8fe746107c3f45455339a421d073022710859
josesimoes <jose.simoes@eclo.solutions>
2024-06-04 15:29:43 +0100

    Fix File System stubs

--- src/PAL/FileSystem/nanoPAL_FileSystem_stubs.cpp
+++ src/PAL/FileSystem/nanoPAL_FileSystem_stubs.cpp
@@ -4,78 +4,26 @@
 // See LICENSE file in the project root for full license information.
 //
 
-#include <nanoHAL_v2.h>
 #include <nanoPAL.h>
 
-// //--//
+__nfweak void FS_Initialize()
+{
+}
 
-// void FS_MountVolume( LPCSTR rootName, UINT32 serialNumber, UINT32 deviceFlags, char * fileSystemDriver )
-// {
-// }
+__nfweak void FS_AddVolumes()
+{
+}
 
-// void FS_UnmountVolume( BlockStorageDevice* blockStorageDevice )
-// {
-// }
+__nfweak void FileSystemVolumeList::Initialize()
+{
+}
 
-// //--//
+__nfweak bool FileSystemVolumeList::InitializeVolumes()
+{
+    return TRUE;
+}
 
-// __nfweak void FS_Initialize()
-// {
-// }
-
-// // //--//
-
-// // HAL_DblLinkedList<FileSystemVolume> FileSystemVolumeList::s_volumeList;
-
-// // //--//
-
-// __nfweak void FileSystemVolumeList::Initialize()
-// {
-// }
-
-// __nfweak bool FileSystemVolumeList::InitializeVolumes()
-// {
-//     return TRUE;
-// }
-
-// __nfweak bool FileSystemVolumeList::UninitializeVolumes()
-// {
-//     return TRUE;
-// }
-
-// BOOL FileSystemVolumeList::AddVolume( FileSystemVolume* fsv, LPCSTR rootName, UINT32 serialNumber, UINT32 deviceFlags,
-//                                       STREAM_DRIVER_INTERFACE* streamDriver, FILESYSTEM_DRIVER_INTERFACE* fsDriver,
-//                                       BlockStorageDevice* blockStorageDevice, UINT32 volumeId, BOOL init )
-// {
-//     return TRUE;
-// }
-
-// BOOL FileSystemVolumeList::RemoveVolume( FileSystemVolume* fsv, BOOL uninit )
-// {
-//     return TRUE;
-// }
-
-// FileSystemVolume* FileSystemVolumeList::GetFirstVolume()
-// { 
-//     return NULL;
-// }
-
-// FileSystemVolume* FileSystemVolumeList::GetNextVolume( FileSystemVolume& volume )
-// { 
-//     return NULL;
-// }
-
-// UINT32 FileSystemVolumeList::GetNumVolumes()
-// {
-//     return 0;
-// }
-
-// FileSystemVolume* FileSystemVolumeList::FindVolume( LPCSTR rootName, UINT32 rootNameLength )
-// {
-//     return NULL;
-// }
-
-// BOOL FileSystemVolumeList::Contains( FileSystemVolume* fsv )
-// {
-//     return FALSE;
-// }
+__nfweak bool FileSystemVolumeList::UninitializeVolumes()
+{
+    return TRUE;
+}
