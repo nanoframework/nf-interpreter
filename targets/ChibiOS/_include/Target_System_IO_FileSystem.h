004edc4ba1de5dc2695366da498fe4961861f926
josesimoes <jose.simoes@eclo.solutions>
2024-06-04 15:27:44 +0100

    Fix compiler def usage
    - HAL_USBH_USE_MSD is coming from community contrib repo which is not always pulled in, so need to check if it's defined before accessing it.

--- targets/ChibiOS/_include/Target_System_IO_FileSystem.h
+++ targets/ChibiOS/_include/Target_System_IO_FileSystem.h
@@ -29,15 +29,15 @@
 // and those have to follow the sequence that is used in ChibiOS FatFS wrappers
 // SD Card (or SPI) is 1st and USB MAS is 2nd (if SD Card is enabled)
 // this is also mapped in the FatFS configuration
-#if (HAL_USE_SDC == TRUE) && (HAL_USBH_USE_MSD == TRUE)
+#if (HAL_USE_SDC == TRUE) && (defined(HAL_USBH_USE_MSD) && (HAL_USBH_USE_MSD == TRUE))
 #define SD_CARD_DRIVE_INDEX         "0:"
 #define SD_CARD_DRIVE_INDEX_NUMERIC (0)
 #define USB_MSD_DRIVE_INDEX         "1:"
 #define USB_MSD_DRIVE_INDEX_NUMERIC (1)
-#elif (HAL_USE_SDC == FALSE) && (HAL_USBH_USE_MSD == TRUE)
+#elif (HAL_USE_SDC == FALSE) && (defined(HAL_USBH_USE_MSD) && (HAL_USBH_USE_MSD == TRUE))
 #define USB_MSD_DRIVE_INDEX         "0:"
 #define USB_MSD_DRIVE_INDEX_NUMERIC (0)
-#elif (HAL_USE_SDC == TRUE) && (HAL_USBH_USE_MSD == FALSE)
+#elif (HAL_USE_SDC == TRUE) && (defined(HAL_USBH_USE_MSD) && (HAL_USBH_USE_MSD == FALSE))
 #define SD_CARD_DRIVE_INDEX         "0:"
 #define SD_CARD_DRIVE_INDEX_NUMERIC (0)
 #endif
