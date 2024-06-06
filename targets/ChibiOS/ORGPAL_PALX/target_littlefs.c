1e951f09dd7f19b28a91bc87ad75505a2f8c1ad8
josesimoes <jose.simoes@eclo.solutions>
2024-06-06 15:04:48 +0100

    Setting up MPU for QSPI region
    - Need to use this despite the indirect access to the QSPI.
    - Known explained at https://community.st.com/t5/stm32-mcus-products/qspi-flag-qspi-flag-busy-sometimes-stays-set/td-p/365865.

--- targets/ChibiOS/ORGPAL_PALX/target_littlefs.c
+++ targets/ChibiOS/ORGPAL_PALX/target_littlefs.c
@@ -504,6 +504,32 @@ int8_t target_lfs_init()
 {
     uint8_t dataBuffer[3];
 
+    // need to adjust MPU in order to use QSPI memory, despite using indirect access
+    // this is a know issue with STM32F7
+    // https://community.st.com/t5/stm32-mcus-products/qspi-flag-qspi-flag-busy-sometimes-stays-set/td-p/365865
+
+    // need to disable MPU in order to change regions
+    HAL_MPU_Disable();
+
+    MPU_Region_InitTypeDef MPU_InitStruct;
+
+    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+    MPU_InitStruct.Number = MPU_REGION_NUMBER1;
+    MPU_InitStruct.BaseAddress = 0x90000000;
+    MPU_InitStruct.Size = MPU_REGION_SIZE_64MB;
+    MPU_InitStruct.SubRegionDisable = 0x0;
+    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+    MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+    MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+
+    HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+    // Enable the MPU
+    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
     // QSPI initialization
     QSPID1.Init.ClockPrescaler = 1;
     QSPID1.Init.FifoThreshold = 4;
