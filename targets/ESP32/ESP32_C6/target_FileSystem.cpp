d3e211f9bbb6873162298b4dac0c60cbb54f02d7
josesimoes <jose.simoes@eclo.solutions>
2024-06-07 20:18:38 +0100

    More fixes to AZDO build

--- targets/ESP32/ESP32_C6/target_FileSystem.cpp
+++ targets/ESP32/ESP32_C6/target_FileSystem.cpp
@@ -12,10 +12,10 @@
 #include "Target_System_IO_FileSystem.h"
 
 // extern FILESYSTEM_DRIVER_INTERFACE g_LITTLEFS_FILE_SYSTEM_DriverInterface;
-extern STREAM_DRIVER_INTERFACE g_FATFS_STREAM_DriverInterface;
+// extern STREAM_DRIVER_INTERFACE g_FATFS_STREAM_DriverInterface;
 
 // extern STREAM_DRIVER_INTERFACE g_LITTLEFS_STREAM_DriverInterface;
-extern FILESYSTEM_DRIVER_INTERFACE g_FATFS_FILE_SYSTEM_DriverInterface;
+// extern FILESYSTEM_DRIVER_INTERFACE g_FATFS_FILE_SYSTEM_DriverInterface;
 
 // #if HAL_USE_SDC
 // #include <target_windows_storage_config.h>
