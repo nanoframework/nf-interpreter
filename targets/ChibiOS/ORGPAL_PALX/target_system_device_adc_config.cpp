f84b7309710d4fc32f3cb85228ff375797ae5db4
Robin Jones <networkfusion@users.noreply.github.com>
2024-06-13 01:07:28 +0100

    Fix ADC config

--- targets/ChibiOS/ORGPAL_PALX/target_system_device_adc_config.cpp
+++ targets/ChibiOS/ORGPAL_PALX/target_system_device_adc_config.cpp
@@ -8,21 +8,16 @@
 const NF_PAL_ADC_PORT_PIN_CHANNEL AdcPortPinConfig[] = {
 
     // ADC1
-    {1, GPIOA, 3, ADC_CHANNEL_IN3},
-    {1, GPIOA, 4, ADC_CHANNEL_IN4},
-    {1, GPIOB, 0, ADC_CHANNEL_IN8},
-    {1, GPIOB, 1, ADC_CHANNEL_IN9},
-    {1, GPIOC, 2, ADC_CHANNEL_IN12},
-    {1, GPIOC, 3, ADC_CHANNEL_IN13},
+    {1, GPIOA, 2, ADC_CHANNEL_IN2}, // Channel 0, Thermistor port Input
+    {1, GPIOB, 0, ADC_CHANNEL_IN8}, // Channel 1, Battery port input.
+    {1, GPIOC, 2, ADC_CHANNEL_IN12}, // Channel 2, 420MA Current sensor port input.
+    {1, GPIOC, 3, ADC_CHANNEL_IN13}, // Channel 3, Onboard PCB temperature.
 
-    // ADC3
-    {3, GPIOF, 8, ADC_CHANNEL_IN6},
-    {3, GPIOF, 9, ADC_CHANNEL_IN7},
-
-    // these are the internal sources, available only at ADC1
-    {1, NULL, 0, ADC_CHANNEL_SENSOR},
-    {1, NULL, 0, ADC_CHANNEL_VREFINT},
-    {1, NULL, 0, ADC_CHANNEL_VBAT},
+    // These are the internal sources, available only at ADC1
+    // and are added to the end.
+    {1, NULL, 0, ADC_CHANNEL_SENSOR}, // Channel 4, internal MCU temperature.
+    {1, NULL, 0, ADC_CHANNEL_VREFINT}, //Channel 5, internal MCU voltage reference
+    {1, NULL, 0, ADC_CHANNEL_VBAT}, // Channel 6, internal MCU vbat reference.
 };
 
 const int AdcChannelCount = ARRAYSIZE(AdcPortPinConfig);
