61d4c51891bda240a742de1fd254d8380761fbba
nfbot <nanoframework@outlook.com>
2024-06-14 14:14:25 +0100

    Code style fixes (#18)
    Automated fixes for code style.

--- targets/ChibiOS/ORGPAL_PALX/target_system_device_adc_config.cpp
+++ targets/ChibiOS/ORGPAL_PALX/target_system_device_adc_config.cpp
@@ -8,16 +8,16 @@
 const NF_PAL_ADC_PORT_PIN_CHANNEL AdcPortPinConfig[] = {
 
     // ADC1
-    {1, GPIOA, 2, ADC_CHANNEL_IN2}, // Channel 0, Thermistor port Input
-    {1, GPIOB, 0, ADC_CHANNEL_IN8}, // Channel 1, Battery port input.
+    {1, GPIOA, 2, ADC_CHANNEL_IN2},  // Channel 0, Thermistor port Input
+    {1, GPIOB, 0, ADC_CHANNEL_IN8},  // Channel 1, Battery port input.
     {1, GPIOC, 2, ADC_CHANNEL_IN12}, // Channel 2, 420MA Current sensor port input.
     {1, GPIOC, 3, ADC_CHANNEL_IN13}, // Channel 3, Onboard PCB temperature.
 
     // These are the internal sources, available only at ADC1
     // and are added to the end.
-    {1, NULL, 0, ADC_CHANNEL_SENSOR}, // Channel 4, internal MCU temperature.
-    {1, NULL, 0, ADC_CHANNEL_VREFINT}, //Channel 5, internal MCU voltage reference
-    {1, NULL, 0, ADC_CHANNEL_VBAT}, // Channel 6, internal MCU vbat reference.
+    {1, NULL, 0, ADC_CHANNEL_SENSOR},  // Channel 4, internal MCU temperature.
+    {1, NULL, 0, ADC_CHANNEL_VREFINT}, // Channel 5, internal MCU voltage reference
+    {1, NULL, 0, ADC_CHANNEL_VBAT},    // Channel 6, internal MCU vbat reference.
 };
 
 const int AdcChannelCount = ARRAYSIZE(AdcPortPinConfig);
