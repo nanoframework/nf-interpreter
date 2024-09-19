78828b5a7b2c4f3c2e3e85992f829288adb5be22
josesimoes <jose.simoes@eclo.solutions>
2024-09-19 12:00:02 +0100

    Remove wrong call to GC
    - This shouldn't be here as the CLR setting is only available in Virtual Device and the RTM check was also invalid.

--- src/CLR/Core/Execution.cpp
+++ src/CLR/Core/Execution.cpp
@@ -1621,16 +1621,6 @@ CLR_RT_HeapBlock *CLR_RT_ExecutionEngine::ExtractHeapBlocks(
         g_CLR_RT_EventCache.EventCache_Cleanup();
         PerformGarbageCollection();
     }
-#else
-
-#if !defined(BUILD_RTM) || defined(VIRTUAL_DEVICE)
-    if (g_CLR_RT_ExecutionEngine.m_fPerformGarbageCollection)
-    {
-        g_CLR_RT_EventCache.EventCache_Cleanup();
-        PerformGarbageCollection();
-    }
-#endif
-
 #endif
 
     for (int phase = 0;; phase++)
