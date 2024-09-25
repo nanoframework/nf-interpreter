0c5844ed529263415751bbb3a347c92c533122c2
josesimoes <jose.simoes@eclo.solutions>
2024-09-25 09:47:39 +0100

    Add back LocalInstance option
    - Required to offer a smooth migration path to developers and tools currently using this option.
    - Marked as Obsolete.

--- targets/netcore/nanoFramework.nanoCLR.CLI/CommonOptions.cs
+++ targets/netcore/nanoFramework.nanoCLR.CLI/CommonOptions.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using CommandLine;
 
 namespace nanoFramework.nanoCLR.CLI
@@ -13,6 +14,22 @@ namespace nanoFramework.nanoCLR.CLI
             Default = null,
             HelpText = "Path to the directory from which to load a specific version of nanoFramework.nanoCLR.dll.")]
         public string PathToCLRInstance { get; set; }
+
+        [Obsolete("This option is deprecated and will be removed in a future version. Use --clrpath instead.")]
+        [Option(
+            "localinstance",
+            Required = false,
+            Default = null,
+            Hidden = true,
+            HelpText = "Path to a local instance of the nanoCLR.")]
+        public string LocalInstance
+        {
+            set
+            {
+                PathToCLRInstance = value;
+            }
+        }
+
 
         /// <summary>
         /// Allowed values:
