4cdfb12f57fe3658810e096c01ff1ba401543c52
nfbot <nanoframework@outlook.com>
2024-09-26 11:46:01 +0000

    Code style fixes
    Automated fixes for code style.

--- targets/netcore/nanoFramework.nanoCLR/nanoCLR_native.cpp
+++ targets/netcore/nanoFramework.nanoCLR/nanoCLR_native.cpp
@@ -55,22 +55,22 @@ bool _wireProtocolStopProcess;
 /////////////////////////////////////////////////////////////////////////////
 
 // All solutions are expected to provide an implementation of this
-bool Target_GetReleaseInfo (NFReleaseInfo &releaseInfo)
+bool Target_GetReleaseInfo(NFReleaseInfo &releaseInfo)
 {
-	NFReleaseInfo::Init (
-		releaseInfo,
-		VERSION_MAJOR,
-		VERSION_MINOR,
-		VERSION_BUILD,
-		VERSION_REVISION,
-		OEMSYSTEMINFOSTRING,
-		hal_strlen_s (OEMSYSTEMINFOSTRING),
-		TARGETNAMESTRING,
-		hal_strlen_s (TARGETNAMESTRING),
-		PLATFORMNAMESTRING,
-		hal_strlen_s (PLATFORMNAMESTRING));
+    NFReleaseInfo::Init(
+        releaseInfo,
+        VERSION_MAJOR,
+        VERSION_MINOR,
+        VERSION_BUILD,
+        VERSION_REVISION,
+        OEMSYSTEMINFOSTRING,
+        hal_strlen_s(OEMSYSTEMINFOSTRING),
+        TARGETNAMESTRING,
+        hal_strlen_s(TARGETNAMESTRING),
+        PLATFORMNAMESTRING,
+        hal_strlen_s(PLATFORMNAMESTRING));
 
-	return true; // alternatively, return false if you didn't initialize the releaseInfo structure.
+    return true; // alternatively, return false if you didn't initialize the releaseInfo structure.
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -80,152 +80,152 @@ bool Target_GetReleaseInfo (NFReleaseInfo &releaseInfo)
 // and the declarations @ nanoCLR_native.h
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void nanoCLR_Run (NANO_CLR_SETTINGS nanoClrSettings)
+void nanoCLR_Run(NANO_CLR_SETTINGS nanoClrSettings)
 {
 
 #if _DEBUG
-	// only show this in debug build
-	DWORD pid = GetCurrentProcessId ();
-	CLR_Debug::Printf ("Process ID: %d\r\n", pid);
+    // only show this in debug build
+    DWORD pid = GetCurrentProcessId();
+    CLR_Debug::Printf("Process ID: %d\r\n", pid);
 #endif
 
-	CLR_Debug::Printf (
-		"\r\nLoading nanoCLR v%d.%d.%d.%d\r\n...",
-		VERSION_MAJOR,
-		VERSION_MINOR,
-		VERSION_BUILD,
-		VERSION_REVISION);
+    CLR_Debug::Printf(
+        "\r\nLoading nanoCLR v%d.%d.%d.%d\r\n...",
+        VERSION_MAJOR,
+        VERSION_MINOR,
+        VERSION_BUILD,
+        VERSION_REVISION);
 
-	// initialize nanoHAL
-	nanoHAL_Initialize ();
+    // initialize nanoHAL
+    nanoHAL_Initialize();
 
-	// take care of block storage here as we are RAM based
-	BlockStorageList_Initialize ();
-	BlockStorage_AddDevices ();
-	BlockStorageList_InitializeDevices ();
+    // take care of block storage here as we are RAM based
+    BlockStorageList_Initialize();
+    BlockStorage_AddDevices();
+    BlockStorageList_InitializeDevices();
 
-	CLR_SETTINGS clrSettings;
-	ZeroMemory (&clrSettings, sizeof (CLR_SETTINGS));
+    CLR_SETTINGS clrSettings;
+    ZeroMemory(&clrSettings, sizeof(CLR_SETTINGS));
 
-	clrSettings.MaxContextSwitches = nanoClrSettings.MaxContextSwitches;
-	clrSettings.WaitForDebugger = nanoClrSettings.WaitForDebugger;
-	clrSettings.EnterDebuggerLoopAfterExit = nanoClrSettings.EnterDebuggerLoopAfterExit;
-	clrSettings.PerformGarbageCollection = nanoClrSettings.PerformGarbageCollection;
-	clrSettings.PerformHeapCompaction = nanoClrSettings.PerformHeapCompaction;
+    clrSettings.MaxContextSwitches = nanoClrSettings.MaxContextSwitches;
+    clrSettings.WaitForDebugger = nanoClrSettings.WaitForDebugger;
+    clrSettings.EnterDebuggerLoopAfterExit = nanoClrSettings.EnterDebuggerLoopAfterExit;
+    clrSettings.PerformGarbageCollection = nanoClrSettings.PerformGarbageCollection;
+    clrSettings.PerformHeapCompaction = nanoClrSettings.PerformHeapCompaction;
 
-	ClrStartup (clrSettings);
+    ClrStartup(clrSettings);
 
 #if !defined(BUILD_RTM)
-	CLR_Debug::Printf ("Exiting.\r\n");
+    CLR_Debug::Printf("Exiting.\r\n");
 #endif
 }
 
-void nanoCLR_SetDebugPrintCallback (DebugPrintCallback debugPrintCallback)
+void nanoCLR_SetDebugPrintCallback(DebugPrintCallback debugPrintCallback)
 {
-	gDebugPrintCallback = debugPrintCallback;
+    gDebugPrintCallback = debugPrintCallback;
 }
 
-void nanoCLR_SetWireProtocolReceiveCallback (WireReceiveCallback receiveCallback)
+void nanoCLR_SetWireProtocolReceiveCallback(WireReceiveCallback receiveCallback)
 {
-	WireProtocolReceiveCallback = receiveCallback;
+    WireProtocolReceiveCallback = receiveCallback;
 }
 
-void nanoCLR_SetWireProtocolTransmitCallback (WireTransmitCallback transmitCallback)
+void nanoCLR_SetWireProtocolTransmitCallback(WireTransmitCallback transmitCallback)
 {
-	WireProtocolTransmitCallback = transmitCallback;
+    WireProtocolTransmitCallback = transmitCallback;
 }
 
-void nanoCLR_WireProtocolProcess ()
+void nanoCLR_WireProtocolProcess()
 {
-	while (!_wireProtocolStopProcess)
-	{
-		WP_Message_Process ();
-	}
+    while (!_wireProtocolStopProcess)
+    {
+        WP_Message_Process();
+    }
 }
 
-void nanoCLR_WireProtocolOpen ()
+void nanoCLR_WireProtocolOpen()
 {
-	WP_Message_PrepareReception ();
+    WP_Message_PrepareReception();
 
-	_wireProtocolStopProcess = false;
+    _wireProtocolStopProcess = false;
 }
 
-void nanoCLR_WireProtocolClose ()
+void nanoCLR_WireProtocolClose()
 {
-	// request to stop
-	_wireProtocolStopProcess = true;
+    // request to stop
+    _wireProtocolStopProcess = true;
 }
 
-const char *nanoCLR_GetVersion ()
+const char *nanoCLR_GetVersion()
 {
-	char buffer[128];
+    char buffer[128];
 
-	char *pszVersion = nullptr;
-	pszVersion = (char *)CoTaskMemAlloc (std::size (buffer));
+    char *pszVersion = nullptr;
+    pszVersion = (char *)CoTaskMemAlloc(std::size(buffer));
 
-	if (pszVersion != nullptr)
-	{
-		const auto result = std::format_to_n (
-			buffer,
-			std::size (buffer) - 1,
-			"{}.{}.{}.{}",
-			VERSION_MAJOR,
-			VERSION_MINOR,
-			VERSION_BUILD,
-			VERSION_REVISION);
-		*result.out = '\0';
+    if (pszVersion != nullptr)
+    {
+        const auto result = std::format_to_n(
+            buffer,
+            std::size(buffer) - 1,
+            "{}.{}.{}.{}",
+            VERSION_MAJOR,
+            VERSION_MINOR,
+            VERSION_BUILD,
+            VERSION_REVISION);
+        *result.out = '\0';
 
-		const std::string_view str{ buffer, result.out };
+        const std::string_view str{buffer, result.out};
 
-		std::memcpy (pszVersion, buffer, result.size);
-	}
+        std::memcpy(pszVersion, buffer, result.size);
+    }
 
-	return pszVersion;
+    return pszVersion;
 }
 
-uint16_t nanoCLR_GetNativeAssemblyCount ()
+uint16_t nanoCLR_GetNativeAssemblyCount()
 {
-	return g_CLR_InteropAssembliesCount;
+    return g_CLR_InteropAssembliesCount;
 }
 
-bool nanoCLR_GetNativeAssemblyInformation (const CLR_UINT8 *data, size_t size)
+bool nanoCLR_GetNativeAssemblyInformation(const CLR_UINT8 *data, size_t size)
 {
-	if (data == nullptr)
-	{
-		return false;
-	}
+    if (data == nullptr)
+    {
+        return false;
+    }
 
-	const size_t requiredSize = g_CLR_InteropAssembliesCount * (5 * sizeof (CLR_UINT16) + 128);
-	if (size < requiredSize)
-	{
-		// Buffer too small
-		return false;
-	}
+    const size_t requiredSize = g_CLR_InteropAssembliesCount * (5 * sizeof(CLR_UINT16) + 128);
+    if (size < requiredSize)
+    {
+        // Buffer too small
+        return false;
+    }
 
-	// clear buffer memory
-	memset ((void *)data, 0, size);
+    // clear buffer memory
+    memset((void *)data, 0, size);
 
-	// fill the array
-	for (uint32_t i = 0; i < g_CLR_InteropAssembliesCount; i++)
-	{
-		memcpy ((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_checkSum, sizeof (CLR_UINT32));
-		data += sizeof (CLR_UINT32);
+    // fill the array
+    for (uint32_t i = 0; i < g_CLR_InteropAssembliesCount; i++)
+    {
+        memcpy((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_checkSum, sizeof(CLR_UINT32));
+        data += sizeof(CLR_UINT32);
 
-		memcpy ((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iMajorVersion, sizeof (CLR_UINT16));
-		data += sizeof (CLR_UINT16);
+        memcpy((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iMajorVersion, sizeof(CLR_UINT16));
+        data += sizeof(CLR_UINT16);
 
-		memcpy ((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iMinorVersion, sizeof (CLR_UINT16));
-		data += sizeof (CLR_UINT16);
+        memcpy((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iMinorVersion, sizeof(CLR_UINT16));
+        data += sizeof(CLR_UINT16);
 
-		memcpy ((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iBuildNumber, sizeof (CLR_UINT16));
-		data += sizeof (CLR_UINT16);
+        memcpy((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iBuildNumber, sizeof(CLR_UINT16));
+        data += sizeof(CLR_UINT16);
 
-		memcpy ((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iRevisionNumber, sizeof (CLR_UINT16));
-		data += sizeof (CLR_UINT16);
+        memcpy((void *)data, &g_CLR_InteropAssembliesNativeData[i]->m_Version.iRevisionNumber, sizeof(CLR_UINT16));
+        data += sizeof(CLR_UINT16);
 
-		hal_strcpy_s ((char *)data, 128, g_CLR_InteropAssembliesNativeData[i]->m_szAssemblyName);
-		data += 128;
-	}
+        hal_strcpy_s((char *)data, 128, g_CLR_InteropAssembliesNativeData[i]->m_szAssemblyName);
+        data += 128;
+    }
 
-	return true; // Success
+    return true; // Success
 }
