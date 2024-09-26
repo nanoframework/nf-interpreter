77924264505da6787b574e9ee9a2c57fb2897f1a
josesimoes <jose.simoes@eclo.solutions>
2024-09-26 12:19:18 +0100

    Update Sys.Collections to use key hash code as parameter
    - Adjust code as needed.
    - Update assembly declaration.

--- src/nanoFramework.System.Collections/nf_system_collections.h
+++ src/nanoFramework.System.Collections/nf_system_collections.h
@@ -31,16 +31,16 @@ struct Library_nf_system_collections_System_Collections_Hashtable
     static const int FIELD___version = 5;
 
     NANOCLR_NATIVE_DECLARE(Clear___VOID);
-    NANOCLR_NATIVE_DECLARE(Contains___BOOLEAN__OBJECT);
-    NANOCLR_NATIVE_DECLARE(Remove___VOID__OBJECT);
-    NANOCLR_NATIVE_DECLARE(InsertNative___VOID__OBJECT__OBJECT__BOOLEAN);
-    NANOCLR_NATIVE_DECLARE(GetNative___OBJECT__OBJECT);
+    NANOCLR_NATIVE_DECLARE(InsertNative___VOID__OBJECT__OBJECT__BOOLEAN__I4);
+    NANOCLR_NATIVE_DECLARE(GetNative___OBJECT__OBJECT__I4);
+    NANOCLR_NATIVE_DECLARE(ContainsNative___BOOLEAN__OBJECT__I4);
+    NANOCLR_NATIVE_DECLARE(RemoveNative___VOID__OBJECT__I4);
     NANOCLR_NATIVE_DECLARE(GetPrimeNative___STATIC__I4__I4);
 
     //--//
 
     static HRESULT Expand(CLR_RT_StackFrame &stack);
-    static uint32_t InitHash(CLR_RT_HeapBlock *key, int32_t hashsize, uint32_t *seed, uint32_t *incr);
+    static uint32_t InitHash(const int32_t keyHashCode, int32_t hashsize, uint32_t *seed, uint32_t *incr);
 };
 
 struct Library_nf_system_collections_System_Collections_Hashtable__HashtableEnumerator
