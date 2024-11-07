b8c9a48f8cfdb0e689edbec2b5911466c538427f
josesimoes <jose.simoes@eclo.solutions>
2024-11-07 14:00:15 +0000

    Move USB init to target PAL
    - Remove all USB stack init from CLR main.
    - Update code accordingly.

--- targets/AzureRTOS/SiliconLabs/_nanoCLR/targetPAL.c
+++ targets/AzureRTOS/SiliconLabs/_nanoCLR/targetPAL.c
@@ -6,5 +6,41 @@
 #include <em_device.h>
 #include <em_gpio.h>
 
+#include <targetHAL.h>
+#include <tx_api.h>
+
+// need to declare these as extern instead of including the header files
+extern void usb_device_hid_app_init(void);
+extern void sli_usbd_configuration_config0_init(void);
+extern void sli_usbd_hid_hid0_init(void);
+extern void sli_usbd_init(void);
+extern void sli_usbd_configuration_config0_init(void);
+extern void sli_usbd_cdc_acm_acm0_init(void);
+extern void usb_device_cdc_acm_app_init(void);
+
 // required for Azure RTOS TX_INTERRUPT_SAVE_AREA implementation at platform level
 unsigned int interrupt_save;
+
+// common code used to start USB stack and various components
+void UsbStackInit()
+{
+
+#if GECKO_FEATURE_USBD_HID == TRUE || HAL_WP_USE_USB_CDC == TRUE || GECKO_FEATURE_USBD_WINUSB == TRUE
+    // wait a couple of seconds to allow all other threads to init
+    tx_thread_sleep(TX_TICKS_PER_MILLISEC(4000));
+
+    // can't call USBD init twice
+    sli_usbd_init();
+    sli_usbd_configuration_config0_init();
+#endif
+
+#if GECKO_FEATURE_USBD_HID == TRUE
+    sli_usbd_hid_hid0_init();
+    usb_device_hid_app_init();
+#endif
+
+#if HAL_WP_USE_USB_CDC == TRUE
+    sli_usbd_cdc_acm_acm0_init();
+    usb_device_cdc_acm_app_init();
+#endif
+}
