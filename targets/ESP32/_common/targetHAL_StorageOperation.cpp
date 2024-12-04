f292301b71b9f967d69ac0309fce9585cb44d923
Laurent Ellerbach <laurent@ellerbach.net>
2024-12-04 10:09:14 +0000

    fixing deployment when folder doesn't exist

--- targets/ESP32/_common/targetHAL_StorageOperation.cpp
+++ targets/ESP32/_common/targetHAL_StorageOperation.cpp
@@ -22,6 +22,42 @@ void EnsureStorageInitialized()
     }
 }
 
+// Function to create all necessary intermediate directories
+int create_directories(const char *path)
+{
+    char temp[256];
+    char *pos = NULL;
+    size_t len = 0;
+
+    len = hal_strlen_s(path);
+
+    snprintf(temp, sizeof(temp), "%s", path);
+    if (temp[len - 1] == '/')
+    {
+        temp[len - 1] = 0;
+    }
+
+    for (pos = temp + 1; *pos; pos++)
+    {
+        if (*pos == '/')
+        {
+            *pos = 0;
+            if (mkdir(temp, 0700) && errno != EEXIST)
+            {
+                return -1;
+            }
+            *pos = '/';
+        }
+    }
+
+    if (mkdir(temp, 0700) && errno != EEXIST)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
 uint32_t HAL_StorageOperation(
     uint8_t operation,
     uint32_t nameLength,
@@ -68,17 +104,47 @@ uint32_t HAL_StorageOperation(
         // Remove the file if already exists
         remove(storageNameChar);
 
-        // Open the file in read mode
-        FILE *file = fopen(storageNameChar, "w");
+        // Extract directory path from file path
+        char dir_path[256];
+        snprintf(dir_path, sizeof(dir_path), "%s", storageNameChar);
+        char *last_slash = strrchr(dir_path, '/');
+        if (last_slash != NULL)
+        {
+            *last_slash = '\0';
+        }
+        else
+        {
+            // No directory part, file is in the root
+            dir_path[0] = '\0';
+        }
 
-        // append more data
-        result = fwrite((const void *)(data + nameLength), 1, (size_t)dataLength, file);
-        fclose(file);
+        // Check if there is only one slash in the path
+        if (strchr(dir_path, '/') == strrchr(dir_path, '/'))
+        {
+            // Only one slash, skip creating the directory
+            dir_path[0] = '\0';
+        }
 
-        // check if the data was written
-        if (result != (size_t)dataLength)
+        // Create all necessary intermediate directories
+        if (dir_path[0] != '\0' && create_directories(dir_path) != 0)
         {
             errorCode = StorageOperationErrorCode::WriteError;
+        }
+
+        if (errorCode != StorageOperationErrorCode::WriteError)
+        {
+            // Open the file in read mode
+            FILE *file = fopen(storageNameChar, "w");
+
+            // append more data
+            result = fwrite((const void *)(data + nameLength), 1, (size_t)dataLength, file);
+            fclose(file);
+
+            // check if the data was written
+            if (result != (size_t)dataLength)
+            {
+                errorCode = StorageOperationErrorCode::WriteError;
+            }
         }
     }
     else if (operation == StorageOperation_Monitor::StorageOperation_Append)
