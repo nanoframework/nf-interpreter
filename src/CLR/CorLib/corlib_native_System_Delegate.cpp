1ba3a866e4b6559eaebae3d3b73f7f6bb2305dcc
José Simões <jose.simoes@eclo.solutions>
2025-03-31 19:09:09 +0100

    Fix formatting
    ***NO_CI***

--- src/CLR/CorLib/corlib_native_System_Delegate.cpp
+++ src/CLR/CorLib/corlib_native_System_Delegate.cpp
@@ -5,20 +5,19 @@
 //
 #include "CorLib.h"
 
-
-HRESULT Library_corlib_native_System_Delegate::Equals___BOOLEAN__OBJECT( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::Equals___BOOLEAN__OBJECT(CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    stack.SetResult_Boolean( CLR_RT_HeapBlock::Compare_Unsigned_Values( stack.Arg0(), stack.Arg1() ) == 0 );
+    stack.SetResult_Boolean(CLR_RT_HeapBlock::Compare_Unsigned_Values(stack.Arg0(), stack.Arg1()) == 0);
 
     NANOCLR_NOCLEANUP_NOLABEL();
 }
 
 #if (NANOCLR_REFLECTION == TRUE)
 
-HRESULT Library_corlib_native_System_Delegate::GetInvocationList___SZARRAY_SystemDelegate( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::GetInvocationList___SZARRAY_SystemDelegate(CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
@@ -32,41 +31,53 @@ HRESULT Library_corlib_native_System_Delegate::GetInvocationList___SZARRAY_Syste
     // put the return array on the stack
     CLR_RT_HeapBlock &top = stack.PushValueAndClear();
 
-    if (dlg) {
-        if (dlg->DataType() == DATATYPE_DELEGATELIST_HEAD) {
+    if (dlg)
+    {
+        if (dlg->DataType() == DATATYPE_DELEGATELIST_HEAD)
+        {
             delegatesCount = lst->m_length;
-        } else {
+        }
+        else
+        {
             delegatesCount = 1;
         }
     }
 
     // create the result array
-    NANOCLR_CHECK_HRESULT(CLR_RT_HeapBlock_Array::CreateInstance(top, delegatesCount, g_CLR_RT_WellKnownTypes.m_Delegate));
+    NANOCLR_CHECK_HRESULT(
+        CLR_RT_HeapBlock_Array::CreateInstance(top, delegatesCount, g_CLR_RT_WellKnownTypes.m_Delegate));
 
-    if (delegatesCount > 0) {
+    if (delegatesCount > 0)
+    {
 
         // get the pointer to the first element
         returnArray = (CLR_RT_HeapBlock *)top.DereferenceArray()->GetFirstElement();
-       
-        if (delegatesCount > 1) {
+
+        if (delegatesCount > 1)
+        {
             CLR_RT_HeapBlock *ptr = lst->GetDelegates();
             // fill the array with the delegates
-            for (int i = 0; i < delegatesCount; i++) {
+            for (int i = 0; i < delegatesCount; i++)
+            {
 
                 // create an instance of delegate
-                NANOCLR_CHECK_HRESULT(g_CLR_RT_ExecutionEngine.NewObjectFromIndex(*returnArray, g_CLR_RT_WellKnownTypes.m_Delegate));
+                NANOCLR_CHECK_HRESULT(
+                    g_CLR_RT_ExecutionEngine.NewObjectFromIndex(*returnArray, g_CLR_RT_WellKnownTypes.m_Delegate));
 
-                //fetch delegate from list
+                // fetch delegate from list
                 dlg = ptr[i].DereferenceDelegate();
-                //set delegate reference to return element
+                // set delegate reference to return element
                 returnArray->SetObjectReference(dlg);
 
                 returnArray++;
             }
-        } else {
+        }
+        else
+        {
             // create an instance of delegate
-            NANOCLR_CHECK_HRESULT(g_CLR_RT_ExecutionEngine.NewObjectFromIndex(*returnArray, g_CLR_RT_WellKnownTypes.m_Delegate));
-            //set delegate reference to return element
+            NANOCLR_CHECK_HRESULT(
+                g_CLR_RT_ExecutionEngine.NewObjectFromIndex(*returnArray, g_CLR_RT_WellKnownTypes.m_Delegate));
+            // set delegate reference to return element
             returnArray->SetObjectReference(dlg);
         }
     }
@@ -74,39 +85,43 @@ HRESULT Library_corlib_native_System_Delegate::GetInvocationList___SZARRAY_Syste
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_corlib_native_System_Delegate::get_Method___SystemReflectionMethodInfo( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::get_Method___SystemReflectionMethodInfo(CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    CLR_RT_HeapBlock_Delegate* dlg = stack.Arg0().DereferenceDelegate();
+    CLR_RT_HeapBlock_Delegate *dlg = stack.Arg0().DereferenceDelegate();
 
-    dlg = GetLastDelegate( dlg ); if(!dlg) NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
+    dlg = GetLastDelegate(dlg);
+    if (!dlg)
+        NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
 
     {
-        CLR_RT_HeapBlock& top = stack.PushValue();
-        CLR_RT_HeapBlock* hbObj;
-        
+        CLR_RT_HeapBlock &top = stack.PushValue();
+        CLR_RT_HeapBlock *hbObj;
+
         NANOCLR_CHECK_HRESULT(g_CLR_RT_ExecutionEngine.NewObjectFromIndex(top, g_CLR_RT_WellKnownTypes.m_MethodInfo));
         hbObj = top.Dereference();
-        
-        hbObj->SetReflection( dlg->DelegateFtn() ); 
+
+        hbObj->SetReflection(dlg->DelegateFtn());
     }
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_corlib_native_System_Delegate::get_Target___OBJECT( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::get_Target___OBJECT(CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    CLR_RT_HeapBlock_Delegate* dlg = stack.Arg0().DereferenceDelegate();
+    CLR_RT_HeapBlock_Delegate *dlg = stack.Arg0().DereferenceDelegate();
 
-    dlg = GetLastDelegate( dlg ); if(!dlg) NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
+    dlg = GetLastDelegate(dlg);
+    if (!dlg)
+        NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
 
     {
-        stack.PushValueAndAssign( dlg->m_object );
+        stack.PushValueAndAssign(dlg->m_object);
     }
 
     NANOCLR_NOCLEANUP();
@@ -114,42 +129,48 @@ HRESULT Library_corlib_native_System_Delegate::get_Target___OBJECT( CLR_RT_Stack
 
 #endif // NANOCLR_REFLECTION
 
-HRESULT Library_corlib_native_System_Delegate::Combine___STATIC__SystemDelegate__SystemDelegate__SystemDelegate( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::Combine___STATIC__SystemDelegate__SystemDelegate__SystemDelegate(
+    CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    NANOCLR_SET_AND_LEAVE(CLR_RT_HeapBlock_Delegate_List::Combine( stack.PushValue(), stack.Arg0(), stack.Arg1(), false ));
+    NANOCLR_SET_AND_LEAVE(
+        CLR_RT_HeapBlock_Delegate_List::Combine(stack.PushValue(), stack.Arg0(), stack.Arg1(), false));
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_corlib_native_System_Delegate::Remove___STATIC__SystemDelegate__SystemDelegate__SystemDelegate( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::Remove___STATIC__SystemDelegate__SystemDelegate__SystemDelegate(
+    CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    NANOCLR_SET_AND_LEAVE(CLR_RT_HeapBlock_Delegate_List::Remove( stack.PushValue(), stack.Arg0(), stack.Arg1() ));
+    NANOCLR_SET_AND_LEAVE(CLR_RT_HeapBlock_Delegate_List::Remove(stack.PushValue(), stack.Arg0(), stack.Arg1()));
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_corlib_native_System_Delegate::op_Equality___STATIC__BOOLEAN__SystemDelegate__SystemDelegate( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::op_Equality___STATIC__BOOLEAN__SystemDelegate__SystemDelegate(
+    CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    NANOCLR_SET_AND_LEAVE(Library_corlib_native_System_Delegate::Equals___BOOLEAN__OBJECT( stack ));
+    NANOCLR_SET_AND_LEAVE(Library_corlib_native_System_Delegate::Equals___BOOLEAN__OBJECT(stack));
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT Library_corlib_native_System_Delegate::op_Inequality___STATIC__BOOLEAN__SystemDelegate__SystemDelegate( CLR_RT_StackFrame& stack )
+HRESULT Library_corlib_native_System_Delegate::op_Inequality___STATIC__BOOLEAN__SystemDelegate__SystemDelegate(
+    CLR_RT_StackFrame &stack)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    NANOCLR_CHECK_HRESULT(Library_corlib_native_System_Delegate::op_Equality___STATIC__BOOLEAN__SystemDelegate__SystemDelegate( stack ));
+    NANOCLR_CHECK_HRESULT(
+        Library_corlib_native_System_Delegate::op_Equality___STATIC__BOOLEAN__SystemDelegate__SystemDelegate(stack));
 
     stack.NegateResult();
 
@@ -158,24 +179,24 @@ HRESULT Library_corlib_native_System_Delegate::op_Inequality___STATIC__BOOLEAN__
 
 //--//
 
-CLR_RT_HeapBlock_Delegate* Library_corlib_native_System_Delegate::GetLastDelegate( CLR_RT_HeapBlock_Delegate* dlg )
+CLR_RT_HeapBlock_Delegate *Library_corlib_native_System_Delegate::GetLastDelegate(CLR_RT_HeapBlock_Delegate *dlg)
 {
     NATIVE_PROFILE_CLR_CORE();
-    if(dlg)
+    if (dlg)
     {
-        if(dlg->DataType() == DATATYPE_DELEGATELIST_HEAD)
+        if (dlg->DataType() == DATATYPE_DELEGATELIST_HEAD)
         {
-            CLR_RT_HeapBlock_Delegate_List* lst = (CLR_RT_HeapBlock_Delegate_List*)dlg;
+            CLR_RT_HeapBlock_Delegate_List *lst = (CLR_RT_HeapBlock_Delegate_List *)dlg;
 
-            if(lst->m_length == 0)
+            if (lst->m_length == 0)
             {
                 dlg = NULL;
             }
             else
             {
-                CLR_RT_HeapBlock* ptr = lst->GetDelegates();
+                CLR_RT_HeapBlock *ptr = lst->GetDelegates();
 
-                dlg = ptr[ lst->m_length-1 ].DereferenceDelegate();
+                dlg = ptr[lst->m_length - 1].DereferenceDelegate();
             }
         }
     }
--- src/CLR/Core/Various.cpp
+++ src/CLR/Core/Various.cpp
@@ -7,103 +7,122 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void CLR_RT_GetVersion( unsigned short int* pMajor, unsigned short int* pMinor, unsigned short int* pBuild, unsigned short int* pRevision )
+void CLR_RT_GetVersion(
+    unsigned short int *pMajor,
+    unsigned short int *pMinor,
+    unsigned short int *pBuild,
+    unsigned short int *pRevision)
 {
     NATIVE_PROFILE_CLR_CORE();
-    if (pMajor) *pMajor = VERSION_MAJOR;
-    if (pMinor) *pMinor = VERSION_MINOR;
-    if (pBuild) *pBuild = VERSION_BUILD;
-    if (pRevision) *pRevision = VERSION_REVISION;
+    if (pMajor)
+        *pMajor = VERSION_MAJOR;
+    if (pMinor)
+        *pMinor = VERSION_MINOR;
+    if (pBuild)
+        *pBuild = VERSION_BUILD;
+    if (pRevision)
+        *pRevision = VERSION_REVISION;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-HRESULT CLR_RT_ArrayListHelper::PrepareArrayList( CLR_RT_HeapBlock& thisRef, int count, int capacity )
+HRESULT CLR_RT_ArrayListHelper::PrepareArrayList(CLR_RT_HeapBlock &thisRef, int count, int capacity)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    CLR_RT_HeapBlock* pThis = thisRef.Dereference(); FAULT_ON_NULL(pThis);
+    CLR_RT_HeapBlock *pThis = thisRef.Dereference();
+    FAULT_ON_NULL(pThis);
 
-    if(count > capacity || capacity < 1)
+    if (count > capacity || capacity < 1)
     {
         NANOCLR_SET_AND_LEAVE(CLR_E_OUT_OF_RANGE);
     }
 
-    NANOCLR_CHECK_HRESULT(CLR_RT_HeapBlock_Array::CreateInstance( pThis[ FIELD___items ], capacity, g_CLR_RT_WellKnownTypes.m_Object ));
+    NANOCLR_CHECK_HRESULT(
+        CLR_RT_HeapBlock_Array::CreateInstance(pThis[FIELD___items], capacity, g_CLR_RT_WellKnownTypes.m_Object));
 
-    pThis[ FIELD___size ].NumericByRef().s4 = count;
+    pThis[FIELD___size].NumericByRef().s4 = count;
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT CLR_RT_ArrayListHelper::ExtractArrayFromArrayList( CLR_RT_HeapBlock & thisRef, CLR_RT_HeapBlock_Array* & array, int& count, int& capacity )
+HRESULT CLR_RT_ArrayListHelper::ExtractArrayFromArrayList(
+    CLR_RT_HeapBlock &thisRef,
+    CLR_RT_HeapBlock_Array *&array,
+    int &count,
+    int &capacity)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    CLR_RT_HeapBlock* pThis;
-    CLR_RT_HeapBlock* items;
+    CLR_RT_HeapBlock *pThis;
+    CLR_RT_HeapBlock *items;
 
-    pThis = thisRef.Dereference(); FAULT_ON_NULL(pThis);
-    items = &pThis[ FIELD___items ];
+    pThis = thisRef.Dereference();
+    FAULT_ON_NULL(pThis);
+    items = &pThis[FIELD___items];
 
-    if(items->DataType() != DATATYPE_OBJECT)
+    if (items->DataType() != DATATYPE_OBJECT)
     {
         NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
     }
 
-    array = items->DereferenceArray(); FAULT_ON_NULL(array);
+    array = items->DereferenceArray();
+    FAULT_ON_NULL(array);
 
-    if(array->DataType() != DATATYPE_SZARRAY)
+    if (array->DataType() != DATATYPE_SZARRAY)
     {
         NANOCLR_SET_AND_LEAVE(CLR_E_WRONG_TYPE);
     }
 
     capacity = array->m_numOfElements;
-    count    = pThis[ FIELD___size ].NumericByRef().s4;
+    count = pThis[FIELD___size].NumericByRef().s4;
 
     NANOCLR_NOCLEANUP();
 }
 
 //--//
 
-HRESULT CLR_RT_ByteArrayReader::Init( const unsigned char* src, unsigned int srcSize )
+HRESULT CLR_RT_ByteArrayReader::Init(const unsigned char *src, unsigned int srcSize)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    if (src == NULL || srcSize == 0) NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
+    if (src == NULL || srcSize == 0)
+        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
 
-    source     = src;
+    source = src;
     sourceSize = srcSize;
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT CLR_RT_ByteArrayReader::Read( void* dst, unsigned int size )
+HRESULT CLR_RT_ByteArrayReader::Read(void *dst, unsigned int size)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    if (size > sourceSize) NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
+    if (size > sourceSize)
+        NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
 
-    memcpy( dst, source, size );
+    memcpy(dst, source, size);
 
-    source     += size;
+    source += size;
     sourceSize -= size;
 
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT CLR_RT_ByteArrayReader::Read1Byte( void* dst )
+HRESULT CLR_RT_ByteArrayReader::Read1Byte(void *dst)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    if (1 > sourceSize) NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
+    if (1 > sourceSize)
+        NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
 
-    *(unsigned char*)dst = *source;
+    *(unsigned char *)dst = *source;
 
     source++;
     sourceSize--;
@@ -111,16 +130,16 @@ HRESULT CLR_RT_ByteArrayReader::Read1Byte( void* dst )
     NANOCLR_NOCLEANUP();
 }
 
-HRESULT CLR_RT_ByteArrayReader::Skip( unsigned int size )
+HRESULT CLR_RT_ByteArrayReader::Skip(unsigned int size)
 {
     NATIVE_PROFILE_CLR_CORE();
     NANOCLR_HEADER();
 
-    if (size > sourceSize) NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
+    if (size > sourceSize)
+        NANOCLR_SET_AND_LEAVE(CLR_E_FAIL);
 
-    source     += size;
+    source += size;
     sourceSize -= size;
 
     NANOCLR_NOCLEANUP();
 }
-
