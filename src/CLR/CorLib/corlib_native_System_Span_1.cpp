4b03921cec4231db5ea5658124f3c88db9630183
José Simões <jose.simoes@eclo.solutions>
2025-09-04 12:31:26 +0100

    Add implementation for Span<T> CopyTo

--- src/CLR/CorLib/corlib_native_System_Span_1.cpp
+++ src/CLR/CorLib/corlib_native_System_Span_1.cpp
@@ -10,7 +10,33 @@ HRESULT Library_corlib_native_System_Span_1::CopyTo___VOID__SystemSpan_1(CLR_RT_
 {
     NANOCLR_HEADER();
 
-    NANOCLR_SET_AND_LEAVE(stack.NotImplementedStub());
+    CLR_RT_HeapBlock_Array *sourceArray;
+    CLR_RT_HeapBlock_Array *destinationArray;
+    CLR_RT_HeapBlock *sourceSpan = stack.This();
+    CLR_RT_HeapBlock *destinationSpan = stack.Arg1().Dereference();
+
+    // check lengths
+    if (sourceSpan[FIELD___length].NumericByRefConst().u4 > destinationSpan[FIELD___length].NumericByRefConst().u4)
+    {
+        NANOCLR_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
+    }
+
+    // get pointers to the arrays
+    sourceArray = sourceSpan[FIELD___array].DereferenceArray();
+    destinationArray = destinationSpan[FIELD___array].DereferenceArray();
+
+    {
+        // prevent GC from moving the arrays while we copy the data
+        CLR_RT_ProtectFromGC gc1(*sourceArray);
+        CLR_RT_ProtectFromGC gc2(*destinationArray);
+
+        // copy the data
+        memcpy(
+            &destinationArray[sourceSpan[FIELD___start].NumericByRefConst().u4],
+            &sourceArray[sourceSpan[FIELD___start].NumericByRefConst().u4],
+            sourceSpan[FIELD___length].NumericByRefConst().u4 *
+                c_CLR_RT_DataTypeLookup[sourceArray->DataType()].m_sizeInBytes);
+    }
 
     NANOCLR_NOCLEANUP();
 }
