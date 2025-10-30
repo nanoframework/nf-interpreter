2447f920e9d85f9efd9b3e56e855b4de8e21fe7a
José Simões <jose.simoes@eclo.solutions>
2025-10-30 12:27:52 +0000

    Fix flash size in block storage declaration

--- targets/ChibiOS/MXCHIP_AZ3166/common/Device_BlockStorage-DEBUG.c
+++ targets/ChibiOS/MXCHIP_AZ3166/common/Device_BlockStorage-DEBUG.c
@@ -98,7 +98,7 @@ MEMORY_MAPPED_NOR_BLOCK_CONFIG Device_BlockStorageConfig = {
         0,          // UINT32 CPU_MEMORY_CONFIG::ReleaseCounts;
         16,         // UINT32 CPU_MEMORY_CONFIG::BitWidth;
         0x08000000, // UINT32 CPU_MEMORY_CONFIG::BaseAddress;
-        0x00200000, // UINT32 CPU_MEMORY_CONFIG::SizeInBytes;
+        0x00100000, // UINT32 CPU_MEMORY_CONFIG::SizeInBytes;
         0,          // UINT8  CPU_MEMORY_CONFIG::XREADYEnable
         0,          // UINT8  CPU_MEMORY_CONFIG::ByteSignalsForRead
         0,          // UINT8  CPU_MEMORY_CONFIG::ExternalBufferEnable
