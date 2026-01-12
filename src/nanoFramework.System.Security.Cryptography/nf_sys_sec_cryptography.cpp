555b9d3f451da8e766ccd2618d320f7be324dfbd
José Simões <jose.simoes@eclo.solutions>
2026-01-12 10:37:15 +0000

    Update declaration of Sys.Security.Cryptography
    ***NO_CI***

--- src/nanoFramework.System.Security.Cryptography/nf_sys_sec_cryptography.cpp
+++ src/nanoFramework.System.Security.Cryptography/nf_sys_sec_cryptography.cpp
@@ -9,38 +9,39 @@
 
 static const CLR_RT_MethodHandler method_lookup[] =
 {
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
     Library_nf_sys_sec_cryptography_System_Security_Cryptography_Aes::EncryptAesEcb___SZARRAY_U1__SZARRAY_U1,
     Library_nf_sys_sec_cryptography_System_Security_Cryptography_Aes::DecryptAesEcb___SZARRAY_U1__SZARRAY_U1,
     Library_nf_sys_sec_cryptography_System_Security_Cryptography_Aes::EncryptAesCbc___SZARRAY_U1__SZARRAY_U1,
     Library_nf_sys_sec_cryptography_System_Security_Cryptography_Aes::DecryptAesCbc___SZARRAY_U1__SZARRAY_U1,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
     Library_nf_sys_sec_cryptography_System_Security_Cryptography_HMACSHA256::HashCore___STATIC__SZARRAY_U1__SZARRAY_U1__SZARRAY_U1,
 };
 
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_nanoFramework_System_Security_Cryptography =
 {
     "nanoFramework.System.Security.Cryptography",
-    0x343142CA,
+    0x4D389100,
     method_lookup,
-    { 100, 0, 0, 3 }
+    { 100, 2, 0, 3 }
 };
 
 // clang-format on
