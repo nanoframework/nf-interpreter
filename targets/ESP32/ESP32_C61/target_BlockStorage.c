666695db9f69c0a41bb266f7d9c69fc5ba157b13
Adrian Soundy <adriansoundy@gmail.com>
2026-05-18 21:55:58 +1200

    Add ESP32_C61 target (BETA) (#3360)
    Initial test version of esp32 c61 build. Just adding to build so no need for review

---
+++ targets/ESP32/ESP32_C61/target_BlockStorage.c
@@ -0,0 +1,23 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <nanoPAL_BlockStorage.h>
+#include <targetHAL.h>
+#include <Target_BlockStorage_Esp32FlashDriver.h>
+
+extern struct BlockStorageDevice Device_BlockStorage;
+extern struct MEMORY_MAPPED_NOR_BLOCK_CONFIG Device_BlockStorageConfig;
+extern IBlockStorageDevice ESP32Flash_BlockStorageInterface;
+
+void BlockStorage_AddDevices()
+{
+    // add device AND request initialization
+    // required to setup flash partitions memory mapping
+    BlockStorageList_AddDevice(
+        (BlockStorageDevice *)&Device_BlockStorage,
+        &ESP32Flash_BlockStorageInterface,
+        &Device_BlockStorageConfig,
+        true);
+}
