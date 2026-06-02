6811f6c892a20bacfe418da8e53d5c00f02bb182
José Simões <jose.simoes@eclo.solutions>
2026-06-02 16:03:52 +0100

    Update ChibiOS repo URL

--- .devcontainer/All/scripts/git-pull-repos.sh
+++ .devcontainer/All/scripts/git-pull-repos.sh
@@ -39,7 +39,7 @@ cd /sources/ThreadX || exit 1
 git pull
 cd /
 rm -rf /sources/ChibiOs
-git clone --branch stable_21.11.x https://github.com/ChibiOS/ChibiOS.git --depth 1 /sources/ChibiOs
+git clone --branch stable_21.11.x https://github.com/chibios-upstream/chibios.git --depth 1 /sources/ChibiOs
 cd /sources/ChibiOs-Contrib || exit 1
 git pull origin chibios-21.11.x
 cd /sources/mbedtls || exit 1
