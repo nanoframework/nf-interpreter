b4c0a3b9577d8d95ac48fb22bd62394b286f5c9c
José Simões <jose.simoes@eclo.solutions>
2026-06-02 16:35:07 +0100

    Fix branch name

--- CMake/Modules/FindChibiOS.cmake
+++ CMake/Modules/FindChibiOS.cmake
@@ -154,7 +154,7 @@ foreach(SRC_FILE ${CHIBIOS_SRCS})
 
 endforeach()
 
-# chmemchecks.c only exists in ChibiOS master, not in stable-21.11.x
+# chmemchecks.c only exists in ChibiOS main, not in stable-21.11.x
 if(EXISTS ${chibios_SOURCE_DIR}/os/oslib/src/chmemchecks.c)
     list(APPEND CHIBIOS_SOURCES ${chibios_SOURCE_DIR}/os/oslib/src/chmemchecks.c)
 endif()
