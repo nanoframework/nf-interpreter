3527d66dd1ae6425e41e230402f03dddb6b4a99e
José Simões <jose.simoes@eclo.solutions>
2026-06-03 14:33:54 +0100

    Add empty ext flash to RPI Pico targets
    ***NO_CI***

---
+++ targets/ChibiOS/RP_PICO_W_RP2040/common/target_ext_flash.c
@@ -0,0 +1,7 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+// file empty on purpose - the RP2040 doesn't have external flash, so we don't need to implement the API declared in target_ext_flash.h
+
