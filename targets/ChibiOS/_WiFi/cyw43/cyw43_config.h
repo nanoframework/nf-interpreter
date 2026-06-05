daacc67093a26141e55d69e193a457ebdf179701
José Simões <jose.simoes@eclo.solutions>
2026-06-05 11:25:50 +0100

    Fix build for RP2040W

---
+++ targets/ChibiOS/_WiFi/cyw43/cyw43_config.h
@@ -0,0 +1,14 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+// cyw43-driver expects this filename as its configuration entry point.
+// Redirect to our ChibiOS/RP2040 port-specific configuration.
+
+#ifndef CYW43_CONFIG_H
+#define CYW43_CONFIG_H
+
+#include "cyw43_configport.h"
+
+#endif // CYW43_CONFIG_H
