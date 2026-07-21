f567a03e636dbf4713db795f7879dc0bebe96db5
José Simões <jose.simoes@eclo.solutions>
2026-07-21 18:25:42 +0100

    Improvements ESP32 CDC/UART detection
    - Rework detection for S2 as its the only series with USB-OTG only.
    - Moved WP_TransportType to it's own header file to allow inclusion from C and C++.
    - Adjust sdkconfig to use default console to free TinyUSB.
    - Update S2 defconfig to use only CDC for WP transport.
    - Disabled graphics and touch for S2 build.

---
+++ targets/ESP32/_include/WireProtocol_Transport.h
@@ -0,0 +1,28 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#ifndef WIREPROTOCOL_TRANSPORT_H
+#define WIREPROTOCOL_TRANSPORT_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    typedef enum
+    {
+        WP_TRANSPORT_NONE,
+        WP_TRANSPORT_UART,
+        WP_TRANSPORT_USB_JTAG,
+        WP_TRANSPORT_TINY_USB
+    } WP_TransportType;
+
+    extern WP_TransportType WP_Transport;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WIREPROTOCOL_TRANSPORT_H
