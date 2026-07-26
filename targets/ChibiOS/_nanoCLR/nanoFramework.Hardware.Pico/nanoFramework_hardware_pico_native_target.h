c2f0a071121d67226cdb4307acf61fbe510e1570
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2026-07-25 19:29:41 -0600

    Remove unused PioFromIndex helper function

--- targets/ChibiOS/_nanoCLR/nanoFramework.Hardware.Pico/nanoFramework_hardware_pico_native_target.h
+++ targets/ChibiOS/_nanoCLR/nanoFramework.Hardware.Pico/nanoFramework_hardware_pico_native_target.h
@@ -6,23 +6,6 @@
 #ifndef NANOFRAMEWORK_HARDWARE_PICO_NATIVE_TARGET_H
 #define NANOFRAMEWORK_HARDWARE_PICO_NATIVE_TARGET_H
 
-static inline PIO_TypeDef *PioFromIndex(int index)
-{
-    switch (index)
-    {
-        case 0:
-            return PIO0;
-        case 1:
-            return PIO1;
-#if defined(RP2350)
-        case 2:
-            return PIO2;
-#endif
-        default:
-            return nullptr;
-    }
-}
-
 #if defined(RP2350)
 #define PIO_MAX_BLOCK 2
 #define PIO_MAX_PIN   47
