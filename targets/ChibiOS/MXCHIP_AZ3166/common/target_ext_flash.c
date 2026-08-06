1be026b5b5a789089a851e36c90d6774b45a94f5
José Simões <jose.simoes@eclo.solutions>
2026-08-06 09:43:21 +0100

    Add blank files to non Mcuboot enabled ChibiOS targets

---
+++ targets/ChibiOS/MXCHIP_AZ3166/common/target_ext_flash.c
@@ -0,0 +1,6 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+// Empty on purpose. This target doesn't need to use a shared flash API for littlefs and Mcuboot.
