1d49d303829f30a1a12bcbe51624845531dbf5d7
José Simões <jose.simoes@eclo.solutions>
2026-09-07 16:27:26 +0100

    More case renames

--- CMake/Modules/FindnanoFramework.EspNow.cmake
+++ CMake/Modules/FindnanoFramework.EspNow.cmake
@@ -1,3 +1,4 @@
+#
 # Copyright (c) .NET Foundation and Contributors
 # See LICENSE file in the project root for full license information.
 #
@@ -7,18 +8,16 @@ set(BASE_PATH_FOR_THIS_MODULE ${BASE_PATH_FOR_CLASS_LIBRARIES_MODULES}/nanoFrame
 
 
 # set include directories
-list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/src/CLR/Core)
-list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/src/CLR/Include)
-list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/src/HAL/Include)
-list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/src/PAL/Include)
+list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/CLR/Core)
+list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/CLR/Include)
+list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/HAL/Include)
+list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/src/PAL/Include)
 list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${BASE_PATH_FOR_THIS_MODULE})
-list(APPEND nanoFramework.EspNow_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/src/nanoFramework.EspNow)
 
 # source files
 set(nanoFramework.EspNow_SRCS
     nanoFramework_espnow_native.cpp
-    nanoFramework_espnow_native_nanoFramework_Espnow_EspNowController.cpp
-    nanoFramework_espnow_native_nanoFramework_Espnow_EspNowController__EspNowEventHandler.cpp
+    nanoFramework_espnow_native_nanoFramework_EspNow_EspNowController.cpp
 )
 
 foreach(SRC_FILE ${nanoFramework.EspNow_SRCS})
@@ -27,7 +26,6 @@ foreach(SRC_FILE ${nanoFramework.EspNow_SRCS})
         PATHS
             ${BASE_PATH_FOR_THIS_MODULE}
             ${TARGET_BASE_LOCATION}
-            ${PROJECT_SOURCE_DIR}/src/nanoFramework.EspNow
         CMAKE_FIND_ROOT_PATH_BOTH
     )
 
