002f1b2c9ee14e87ea3ab94a8ba5c182d650c9ad
Дмитрий <d.kovyazin@hotmail.com>
2026-09-07 22:51:27 +0700

    Report program state in RTM builds (#3524)
    ***NO_CI***

--- src/CLR/Startup/CLRStartup.cpp
+++ src/CLR/Startup/CLRStartup.cpp
@@ -425,8 +425,8 @@ void ClrStartup(CLR_SETTINGS params)
 
         if (CLR_EE_DBG_IS_NOT(RebootPending))
         {
-#if defined(NANOCLR_ENABLE_SOURCELEVELDEBUGGING)
             CLR_EE_DBG_SET_MASK(StateProgramExited, StateMask);
+#if defined(NANOCLR_ENABLE_SOURCELEVELDEBUGGING)
             CLR_EE_DBG_EVENT_BROADCAST(CLR_DBG_Commands_c_Monitor_ProgramExit, 0, NULL, WP_Flags_c_NonCritical);
 #endif // #if defined(NANOCLR_ENABLE_SOURCELEVELDEBUGGING)
 
