105364cf9bac0dc00423d31180e3fc5e456b8665
José Simões <jose.simoes@eclo.solutions>
2026-09-22 10:35:34 +0100

    Update sys.io.filesystem declaration

--- src/System.IO.FileSystem/nf_sys_io_filesystem.cpp
+++ src/System.IO.FileSystem/nf_sys_io_filesystem.cpp
@@ -197,7 +197,7 @@ static const CLR_RT_MethodHandler method_lookup[] =
     nullptr,
     nullptr,
     nullptr,
-    Library_nf_sys_io_filesystem_System_IO_NativeFileStream::_ctor___VOID__STRING__I4,
+    Library_nf_sys_io_filesystem_System_IO_NativeFileStream::_ctor___VOID__STRING__I4__I4,
     Library_nf_sys_io_filesystem_System_IO_NativeFileStream::Read___I4__SZARRAY_U1__I4__I4__I4,
     Library_nf_sys_io_filesystem_System_IO_NativeFileStream::Write___I4__SZARRAY_U1__I4__I4__I4,
     Library_nf_sys_io_filesystem_System_IO_NativeFileStream::Seek___I8__I8__U4,
@@ -240,9 +240,9 @@ static const CLR_RT_MethodHandler method_lookup[] =
 const CLR_RT_NativeAssemblyData g_CLR_AssemblyNative_System_IO_FileSystem =
 {
     "System.IO.FileSystem",
-    0xCCF9B4CA,
+    0xD17E2F55,
     method_lookup,
-    { 100, 2, 0, 2 }
+    { 200, 1, 0, 4 }
 };
 
 // clang-format on
